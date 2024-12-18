ce3bb7e8a4eea4afd0464480d5c736b0b03442c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:04:06 +0000

    Add package xiaoqiangclub/0.1.46/xiaoqiangclub-0.1.46.tar.gz

---
+++ packages/xiaoqiangclub/xiaoqiangclub-0.1.46.tar.gz/xiaoqiangclub-0.1.46/tests/test001.py
@@ -0,0 +1,27 @@
+{
+    'title': "设置界面",
+    'logo': "https://xxxx.com",
+    "用户设置": {
+        "username": {
+            "label": "用户名",
+            "type": "text",
+            "placeholder": "请输入用户名",
+            'value': None,
+        },
+        "password": {
+            "label": "密码",
+            "type": "text",
+            "placeholder": "请输入密码",
+            'value': None,
+        }
+    },
+
+    "语言设置": {
+        "language": {
+            "label": "语言选择",
+            "type": "select",
+            "options": ["简体中文", "English", "日本語"],
+            'value': "简体中文",
+        }
+    }
+}
