d71b3cc448a31512780738f22bca72423275b11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:44 +0000

    Add package types-aiobotocore-ssm-quicksetup/2.16.0/types_aiobotocore_ssm_quicksetup-2.16.0.tar.gz

---
+++ packages/types-aiobotocore-ssm-quicksetup/types_aiobotocore_ssm_quicksetup-2.16.0.tar.gz/types_aiobotocore_ssm_quicksetup-2.16.0/types_aiobotocore_ssm_quicksetup/client.py
@@ -0,0 +1,289 @@
+"""
+Type annotations for ssm-quicksetup service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ssm_quicksetup.client import SystemsManagerQuickSetupClient
+
+    session = get_session()
+    async with session.create_client("ssm-quicksetup") as client:
+        client: SystemsManagerQuickSetupClient
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .paginator import ListConfigurationManagersPaginator, ListConfigurationsPaginator
+from .type_defs import (
+    CreateConfigurationManagerInputRequestTypeDef,
+    CreateConfigurationManagerOutputTypeDef,
+    DeleteConfigurationManagerInputRequestTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetConfigurationInputRequestTypeDef,
+    GetConfigurationManagerInputRequestTypeDef,
+    GetConfigurationManagerOutputTypeDef,
+    GetConfigurationOutputTypeDef,
+    GetServiceSettingsOutputTypeDef,
+    ListConfigurationManagersInputRequestTypeDef,
+    ListConfigurationManagersOutputTypeDef,
+    ListConfigurationsInputRequestTypeDef,
+    ListConfigurationsOutputTypeDef,
+    ListQuickSetupTypesOutputTypeDef,
+    ListTagsForResourceRequestRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceInputRequestTypeDef,
+    UntagResourceInputRequestTypeDef,
+    UpdateConfigurationDefinitionInputRequestTypeDef,
+    UpdateConfigurationManagerInputRequestTypeDef,
+    UpdateServiceSettingsInputRequestTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+
+__all__ = ("SystemsManagerQuickSetupClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class SystemsManagerQuickSetupClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup.html#SystemsManagerQuickSetup.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        SystemsManagerQuickSetupClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup.html#SystemsManagerQuickSetup.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#generate_presigned_url)
+        """
+
+    async def close(self) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/close.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#close)
+        """
+
+    async def create_configuration_manager(
+        self, **kwargs: Unpack[CreateConfigurationManagerInputRequestTypeDef]
+    ) -> CreateConfigurationManagerOutputTypeDef:
+        """
+        Creates a Quick Setup configuration manager resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/create_configuration_manager.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#create_configuration_manager)
+        """
+
+    async def delete_configuration_manager(
+        self, **kwargs: Unpack[DeleteConfigurationManagerInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Deletes a configuration manager.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/delete_configuration_manager.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#delete_configuration_manager)
+        """
+
+    async def get_configuration(
+        self, **kwargs: Unpack[GetConfigurationInputRequestTypeDef]
+    ) -> GetConfigurationOutputTypeDef:
+        """
+        Returns details about the specified configuration.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/get_configuration.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#get_configuration)
+        """
+
+    async def get_configuration_manager(
+        self, **kwargs: Unpack[GetConfigurationManagerInputRequestTypeDef]
+    ) -> GetConfigurationManagerOutputTypeDef:
+        """
+        Returns a configuration manager.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/get_configuration_manager.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#get_configuration_manager)
+        """
+
+    async def get_service_settings(self) -> GetServiceSettingsOutputTypeDef:
+        """
+        Returns settings configured for Quick Setup in the requesting Amazon Web
+        Services account and Amazon Web Services Region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/get_service_settings.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#get_service_settings)
+        """
+
+    async def list_configuration_managers(
+        self, **kwargs: Unpack[ListConfigurationManagersInputRequestTypeDef]
+    ) -> ListConfigurationManagersOutputTypeDef:
+        """
+        Returns Quick Setup configuration managers.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/list_configuration_managers.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#list_configuration_managers)
+        """
+
+    async def list_configurations(
+        self, **kwargs: Unpack[ListConfigurationsInputRequestTypeDef]
+    ) -> ListConfigurationsOutputTypeDef:
+        """
+        Returns configurations deployed by Quick Setup in the requesting Amazon Web
+        Services account and Amazon Web Services Region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/list_configurations.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#list_configurations)
+        """
+
+    async def list_quick_setup_types(self) -> ListQuickSetupTypesOutputTypeDef:
+        """
+        Returns the available Quick Setup types.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/list_quick_setup_types.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#list_quick_setup_types)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Returns tags assigned to the resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Assigns key-value pairs of metadata to Amazon Web Services resources.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#tag_resource)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Removes tags from the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#untag_resource)
+        """
+
+    async def update_configuration_definition(
+        self, **kwargs: Unpack[UpdateConfigurationDefinitionInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Updates a Quick Setup configuration definition.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/update_configuration_definition.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#update_configuration_definition)
+        """
+
+    async def update_configuration_manager(
+        self, **kwargs: Unpack[UpdateConfigurationManagerInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Updates a Quick Setup configuration manager.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/update_configuration_manager.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#update_configuration_manager)
+        """
+
+    async def update_service_settings(
+        self, **kwargs: Unpack[UpdateServiceSettingsInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Updates settings configured for Quick Setup.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/update_service_settings.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#update_service_settings)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_configuration_managers"]
+    ) -> ListConfigurationManagersPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_configurations"]
+    ) -> ListConfigurationsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> "SystemsManagerQuickSetupClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup.html#SystemsManagerQuickSetup.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup.html#SystemsManagerQuickSetup.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/client/)
+        """
---
+++ packages/types-aiobotocore-ssm-quicksetup/types_aiobotocore_ssm_quicksetup-2.16.0.tar.gz/types_aiobotocore_ssm_quicksetup-2.16.0/types_aiobotocore_ssm_quicksetup/paginator.pyi
@@ -0,0 +1,81 @@
+"""
+Type annotations for ssm-quicksetup service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_ssm_quicksetup.client import SystemsManagerQuickSetupClient
+    from types_aiobotocore_ssm_quicksetup.paginator import (
+        ListConfigurationManagersPaginator,
+        ListConfigurationsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("ssm-quicksetup") as client:
+        client: SystemsManagerQuickSetupClient
+
+        list_configuration_managers_paginator: ListConfigurationManagersPaginator = client.get_paginator("list_configuration_managers")
+        list_configurations_paginator: ListConfigurationsPaginator = client.get_paginator("list_configurations")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListConfigurationManagersInputListConfigurationManagersPaginateTypeDef,
+    ListConfigurationManagersOutputTypeDef,
+    ListConfigurationsInputListConfigurationsPaginateTypeDef,
+    ListConfigurationsOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListConfigurationManagersPaginator", "ListConfigurationsPaginator")
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListConfigurationManagersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/paginator/ListConfigurationManagers.html#SystemsManagerQuickSetup.Paginator.ListConfigurationManagers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/paginators/#listconfigurationmanagerspaginator)
+    """
+    def paginate(
+        self,
+        **kwargs: Unpack[ListConfigurationManagersInputListConfigurationManagersPaginateTypeDef],
+    ) -> AsyncIterator[ListConfigurationManagersOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/paginator/ListConfigurationManagers.html#SystemsManagerQuickSetup.Paginator.ListConfigurationManagers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/paginators/#listconfigurationmanagerspaginator)
+        """
+
+class ListConfigurationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/paginator/ListConfigurations.html#SystemsManagerQuickSetup.Paginator.ListConfigurations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/paginators/#listconfigurationspaginator)
+    """
+    def paginate(
+        self, **kwargs: Unpack[ListConfigurationsInputListConfigurationsPaginateTypeDef]
+    ) -> AsyncIterator[ListConfigurationsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm-quicksetup/paginator/ListConfigurations.html#SystemsManagerQuickSetup.Paginator.ListConfigurations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ssm_quicksetup/paginators/#listconfigurationspaginator)
+        """
