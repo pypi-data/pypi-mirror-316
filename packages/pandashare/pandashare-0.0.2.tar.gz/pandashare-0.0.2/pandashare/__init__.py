14bb48730cbf045ebac83e734b29b662fb41a331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:36 +0000

    Add package pandashare/0.0.2/pandashare-0.0.2.tar.gz

---
+++ packages/pandashare/pandashare-0.0.2.tar.gz/pandashare-0.0.2/pandashare/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*- 
+# pandashare/__init__.py
+import codecs
+import os
+
+__version__ = '0.0.2'
+__author__ = 'Quantdrchow'
+
+def greet(name):
+    return f"Hello, {name}!"
+
+
+
+
+
