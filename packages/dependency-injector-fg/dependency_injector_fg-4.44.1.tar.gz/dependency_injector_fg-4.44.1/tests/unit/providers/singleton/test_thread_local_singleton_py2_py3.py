4492cc2d0b7c511ef590abf0af161248478b974b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 17:03:56 +0000

    Add package dependency-injector-fg/4.44.1/dependency_injector_fg-4.44.1.tar.gz

---
+++ packages/dependency-injector-fg/dependency_injector_fg-4.44.1.tar.gz/dependency_injector_fg-4.44.1/tests/unit/providers/singleton/test_thread_local_singleton_py2_py3.py
@@ -0,0 +1,16 @@
+import pytest
+from dependency_injector.providers import ThreadLocalSingleton
+from dependency_injector.containers import Container
+
+class FailingClass:
+    def __init__(self):
+        raise ValueError("FAILING CLASS")
+
+class TestContainer(Container):
+    failing_class = ThreadLocalSingleton(FailingClass)
+
+def test_on_failure_value_error_is_raised():
+    container = TestContainer()
+
+    with pytest.raises(ValueError, match="FAILING CLASS"):
+        container.failing_class()
