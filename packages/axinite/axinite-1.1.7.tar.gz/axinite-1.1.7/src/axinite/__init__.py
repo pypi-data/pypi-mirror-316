d586a0bed66f04cf5f376b44fd34fb5a4bc22f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:03:58 +0000

    Add package axinite/1.1.7/axinite-1.1.7.tar.gz

---
+++ packages/axinite/axinite-1.1.7.tar.gz/axinite-1.1.7/src/axinite/__init__.py
@@ -0,0 +1,4 @@
+from axinite.body import Body
+from axinite.functions import apply_to_vector, vector_to, vector_magnitude, to_vector
+import axinite.functions as functions
+from axinite.load import load
