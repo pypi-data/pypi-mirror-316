07d1075166abf3c101b1c69d6e48a40c63bd7ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:04:16 +0000

    Add package fivitech-mt5-connector/0.1.3/fivitech_mt5_connector-0.1.3.tar.gz

---
+++ packages/fivitech-mt5-connector/fivitech_mt5_connector-0.1.3.tar.gz/fivitech_mt5_connector-0.1.3/src/fivitech_mt5_connector/exceptions.py
@@ -0,0 +1,7 @@
+class MT5ConnectionError(Exception):
+  """Exception raised for MT5 connection errors"""
+  pass
+
+class MT5ArchiveError(Exception):
+  """Exception raised for MT5 archive errors"""
+  pass
