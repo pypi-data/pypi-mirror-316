8daf9c1254a28f7cf2db9321bae644b4e928788a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:45 +0000

    Add package reflex-intersection-observer/0.0.5/reflex_intersection_observer-0.0.5.tar.gz

---
+++ packages/reflex-intersection-observer/reflex_intersection_observer-0.0.5.tar.gz/reflex_intersection_observer-0.0.5/custom_components/reflex_intersection_observer/__init__.py
@@ -0,0 +1,11 @@
+from .intersection_observer import (
+    intersection_observer,
+    IntersectionObserver,
+    IntersectionObserverEntry,
+)
+
+__all__ = [
+    "intersection_observer",
+    "IntersectionObserver",
+    "IntersectionObserverEntry",
+]
