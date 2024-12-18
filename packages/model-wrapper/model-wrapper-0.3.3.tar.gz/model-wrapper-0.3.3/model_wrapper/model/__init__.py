2604d1ef38c074718bfd2a7bfc132072c2a15cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:03:39 +0000

    Add package model-wrapper/0.3.3/model-wrapper-0.3.3.tar.gz

---
+++ packages/model-wrapper/model-wrapper-0.3.3.tar.gz/model-wrapper-0.3.3/model_wrapper/model/__init__.py
@@ -0,0 +1,14 @@
+from ._attention import RotaryAttention, attention, ClassSelfAttention, MultiHeadClassSelfAttention, \
+	RotaryClassSelfAttention, RNNAttention, CNNRNNAttention, RNNCNNAttention, ResRNNCNNAttention
+
+
+__all__ = [
+	"RotaryAttention",
+	"attention",
+	"ClassSelfAttention",
+	"MultiHeadClassSelfAttention",
+	"RotaryClassSelfAttention",
+	"RNNAttention",
+	"CNNRNNAttention",
+	"RNNCNNAttention"
+]
