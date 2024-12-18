e52fb2d4c846cb4c655ad8bc1ac119467e7700ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:04:21 +0000

    Add package dataeval/0.74.1/dataeval-0.74.1.tar.gz

---
+++ packages/dataeval/dataeval-0.74.1.tar.gz/dataeval-0.74.1/src/dataeval/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.74.1"
+
+from importlib.util import find_spec
+
+_IS_TORCH_AVAILABLE = find_spec("torch") is not None
+_IS_TORCHVISION_AVAILABLE = find_spec("torchvision") is not None
+
+del find_spec
+
+from dataeval import detectors, metrics  # noqa: E402
+
+__all__ = ["detectors", "metrics"]
+
+if _IS_TORCH_AVAILABLE:
+    from dataeval import utils, workflows
+
+    __all__ += ["utils", "workflows"]
