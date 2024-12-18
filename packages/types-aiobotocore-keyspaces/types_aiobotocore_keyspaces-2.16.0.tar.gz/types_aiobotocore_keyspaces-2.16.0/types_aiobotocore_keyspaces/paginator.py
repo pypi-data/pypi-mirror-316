5d3ea06f6d9eae8cf60a42a99b93cd097028f233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:50 +0000

    Add package types-aiobotocore-keyspaces/2.16.0/types_aiobotocore_keyspaces-2.16.0.tar.gz

---
+++ packages/types-aiobotocore-keyspaces/types_aiobotocore_keyspaces-2.16.0.tar.gz/types_aiobotocore_keyspaces-2.16.0/types_aiobotocore_keyspaces/paginator.py
@@ -0,0 +1,130 @@
+"""
+Type annotations for keyspaces service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_keyspaces.client import KeyspacesClient
+    from types_aiobotocore_keyspaces.paginator import (
+        ListKeyspacesPaginator,
+        ListTablesPaginator,
+        ListTagsForResourcePaginator,
+        ListTypesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("keyspaces") as client:
+        client: KeyspacesClient
+
+        list_keyspaces_paginator: ListKeyspacesPaginator = client.get_paginator("list_keyspaces")
+        list_tables_paginator: ListTablesPaginator = client.get_paginator("list_tables")
+        list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+        list_types_paginator: ListTypesPaginator = client.get_paginator("list_types")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListKeyspacesRequestListKeyspacesPaginateTypeDef,
+    ListKeyspacesResponseTypeDef,
+    ListTablesRequestListTablesPaginateTypeDef,
+    ListTablesResponseTypeDef,
+    ListTagsForResourceRequestListTagsForResourcePaginateTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    ListTypesRequestListTypesPaginateTypeDef,
+    ListTypesResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "ListKeyspacesPaginator",
+    "ListTablesPaginator",
+    "ListTagsForResourcePaginator",
+    "ListTypesPaginator",
+)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListKeyspacesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListKeyspaces.html#Keyspaces.Paginator.ListKeyspaces)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listkeyspacespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListKeyspacesRequestListKeyspacesPaginateTypeDef]
+    ) -> AsyncIterator[ListKeyspacesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListKeyspaces.html#Keyspaces.Paginator.ListKeyspaces.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listkeyspacespaginator)
+        """
+
+
+class ListTablesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListTables.html#Keyspaces.Paginator.ListTables)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listtablespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListTablesRequestListTablesPaginateTypeDef]
+    ) -> AsyncIterator[ListTablesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListTables.html#Keyspaces.Paginator.ListTables.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listtablespaginator)
+        """
+
+
+class ListTagsForResourcePaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListTagsForResource.html#Keyspaces.Paginator.ListTagsForResource)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listtagsforresourcepaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListTagsForResourceRequestListTagsForResourcePaginateTypeDef]
+    ) -> AsyncIterator[ListTagsForResourceResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListTagsForResource.html#Keyspaces.Paginator.ListTagsForResource.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listtagsforresourcepaginator)
+        """
+
+
+class ListTypesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListTypes.html#Keyspaces.Paginator.ListTypes)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listtypespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListTypesRequestListTypesPaginateTypeDef]
+    ) -> AsyncIterator[ListTypesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/keyspaces/paginator/ListTypes.html#Keyspaces.Paginator.ListTypes.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_keyspaces/paginators/#listtypespaginator)
+        """
