c0d5b4055bd3f505c6a81022132b9461eed323dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:54 +0000

    Add package types-aiobotocore-geo-maps/2.16.0/types_aiobotocore_geo_maps-2.16.0.tar.gz

---
+++ packages/types-aiobotocore-geo-maps/types_aiobotocore_geo_maps-2.16.0.tar.gz/types_aiobotocore_geo_maps-2.16.0/types_aiobotocore_geo_maps/client.py
@@ -0,0 +1,165 @@
+"""
+Type annotations for geo-maps service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_geo_maps.client import LocationServiceMapsV2Client
+
+    session = get_session()
+    async with session.create_client("geo-maps") as client:
+        client: LocationServiceMapsV2Client
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .type_defs import (
+    GetGlyphsRequestRequestTypeDef,
+    GetGlyphsResponseTypeDef,
+    GetSpritesRequestRequestTypeDef,
+    GetSpritesResponseTypeDef,
+    GetStaticMapRequestRequestTypeDef,
+    GetStaticMapResponseTypeDef,
+    GetStyleDescriptorRequestRequestTypeDef,
+    GetStyleDescriptorResponseTypeDef,
+    GetTileRequestRequestTypeDef,
+    GetTileResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("LocationServiceMapsV2Client",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class LocationServiceMapsV2Client(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps.html#LocationServiceMapsV2.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        LocationServiceMapsV2Client exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps.html#LocationServiceMapsV2.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#generate_presigned_url)
+        """
+
+    async def close(self) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/close.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#close)
+        """
+
+    async def get_glyphs(
+        self, **kwargs: Unpack[GetGlyphsRequestRequestTypeDef]
+    ) -> GetGlyphsResponseTypeDef:
+        """
+        Returns the map's glyphs.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/get_glyphs.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#get_glyphs)
+        """
+
+    async def get_sprites(
+        self, **kwargs: Unpack[GetSpritesRequestRequestTypeDef]
+    ) -> GetSpritesResponseTypeDef:
+        """
+        Returns the map's sprites.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/get_sprites.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#get_sprites)
+        """
+
+    async def get_static_map(
+        self, **kwargs: Unpack[GetStaticMapRequestRequestTypeDef]
+    ) -> GetStaticMapResponseTypeDef:
+        """
+        Provides high-quality static map images with customizable options.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/get_static_map.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#get_static_map)
+        """
+
+    async def get_style_descriptor(
+        self, **kwargs: Unpack[GetStyleDescriptorRequestRequestTypeDef]
+    ) -> GetStyleDescriptorResponseTypeDef:
+        """
+        Returns information about the style.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/get_style_descriptor.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#get_style_descriptor)
+        """
+
+    async def get_tile(
+        self, **kwargs: Unpack[GetTileRequestRequestTypeDef]
+    ) -> GetTileResponseTypeDef:
+        """
+        Returns a tile.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps/client/get_tile.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/#get_tile)
+        """
+
+    async def __aenter__(self) -> "LocationServiceMapsV2Client":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps.html#LocationServiceMapsV2.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-maps.html#LocationServiceMapsV2.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_maps/client/)
+        """
