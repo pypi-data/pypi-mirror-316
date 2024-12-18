834f22e34eae06f18207262a7fb36c516881bcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:12:05 +0000

    Add package conductor-py/0.1.0a48/conductor_py-0.1.0a48.tar.gz

---
+++ packages/conductor-py/conductor_py-0.1.0a48.tar.gz/conductor_py-0.1.0a48/src/conductor/types/end_user_passthrough_params.py
@@ -0,0 +1,18 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["EndUserPassthroughParams"]
+
+
+class EndUserPassthroughParams(TypedDict, total=False):
+    id: Required[str]
+    """The ID of the EndUser who owns the integration connection."""
+
+    qbd_payload: Required[Annotated[Dict[str, object], PropertyInfo(alias="qbdPayload")]]
+    """The request body to send to the integration connection."""
