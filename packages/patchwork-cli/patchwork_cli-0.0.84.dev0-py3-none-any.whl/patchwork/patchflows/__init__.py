0c8cfc4e4c4d52671f52b0148c8a4a14819392d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:14 +0000

    Add package patchwork-cli/0.0.84.dev0/patchwork_cli-0.0.84.dev0-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.84.dev0-py3-none-any.whl/patchwork/patchflows/__init__.py
@@ -0,0 +1,19 @@
+from .AutoFix.AutoFix import AutoFix
+from .DependencyUpgrade.DependencyUpgrade import DependencyUpgrade
+from .GenerateDiagram.GenerateDiagram import GenerateDiagram
+from .GenerateDocstring.GenerateDocstring import GenerateDocstring
+from .GenerateREADME.GenerateREADME import GenerateREADME
+from .GenerateUnitTests.GenerateUnitTests import GenerateUnitTests
+from .PRReview.PRReview import PRReview
+from .ResolveIssue.ResolveIssue import ResolveIssue
+
+__all__ = [
+    "AutoFix",
+    "DependencyUpgrade",
+    "GenerateREADME",
+    "PRReview",
+    "ResolveIssue",
+    "GenerateDocstring",
+    "GenerateUnitTests",
+    "GenerateDiagram",
+]
