0ab2a5bc7dee1ca8409abf672934a00441005026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:34 +0000

    Add package the-flx/0.1.1/the_flx-0.1.1.tar.gz

---
+++ packages/the-flx/the_flx-0.1.1.tar.gz/the_flx-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="the-flx",
+    description="Rewrite emacs-flx in Python",
+    version="0.1.1",
+    url="https://github.com/the-flx/flx.py",
+    project_urls={
+        "Source Code": "https://github.com/the-flx/flx.py",
+    },
+    author="Jen-Chieh Shen",
+    author_email="jcs090218@gmail.com",
+    license="MIT",
+    python_requires=">=3.9",
+    packages=["flx"],
+    install_requires= [],
+    )
