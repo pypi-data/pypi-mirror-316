a95bc7744fe5d53a6fa4716b900e09d07d28af0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:04 +0000

    Add package brainannex/5.0rc0/brainannex-5.0rc0.tar.gz

---
+++ packages/brainannex/brainannex-5.0rc0.tar.gz/brainannex-5.0rc0/brainannex/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "5.0-rc.0"
+
+
+from brainannex.neo_schema.neo_schema import NeoSchema
+from brainannex.data_manager import DataManager
+from brainannex.user_manager import UserManager
+from brainannex.categories import Categories
+from brainannex.collections import Collections
+from brainannex.full_text_indexing import FullTextIndexing
+import brainannex.PLUGINS.plugin_support as plugin_support
+
+
+__all__ = [
+    'NeoSchema',
+    'DataManager',
+    'UserManager',
+    'Categories',
+    'Collections',
+    'FullTextIndexing',
+    'plugin_support',
+    'version'
+]
+
+
+def version():
+    return __version__
