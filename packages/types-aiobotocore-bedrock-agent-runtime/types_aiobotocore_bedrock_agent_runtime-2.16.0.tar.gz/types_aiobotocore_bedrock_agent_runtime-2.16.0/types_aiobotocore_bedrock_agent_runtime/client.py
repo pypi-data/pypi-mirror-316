52e5bc83874ec89b5c338fccfa9042ec083fba2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:04:00 +0000

    Add package types-aiobotocore-bedrock-agent-runtime/2.16.0/types_aiobotocore_bedrock_agent_runtime-2.16.0.tar.gz

---
+++ packages/types-aiobotocore-bedrock-agent-runtime/types_aiobotocore_bedrock_agent_runtime-2.16.0.tar.gz/types_aiobotocore_bedrock_agent_runtime-2.16.0/types_aiobotocore_bedrock_agent_runtime/client.py
@@ -0,0 +1,274 @@
+"""
+Type annotations for bedrock-agent-runtime service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_bedrock_agent_runtime.client import AgentsforBedrockRuntimeClient
+
+    session = get_session()
+    async with session.create_client("bedrock-agent-runtime") as client:
+        client: AgentsforBedrockRuntimeClient
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .paginator import GetAgentMemoryPaginator, RerankPaginator, RetrievePaginator
+from .type_defs import (
+    DeleteAgentMemoryRequestRequestTypeDef,
+    GenerateQueryRequestRequestTypeDef,
+    GenerateQueryResponseTypeDef,
+    GetAgentMemoryRequestRequestTypeDef,
+    GetAgentMemoryResponseTypeDef,
+    InvokeAgentRequestRequestTypeDef,
+    InvokeAgentResponseTypeDef,
+    InvokeFlowRequestRequestTypeDef,
+    InvokeFlowResponseTypeDef,
+    InvokeInlineAgentRequestRequestTypeDef,
+    InvokeInlineAgentResponseTypeDef,
+    OptimizePromptRequestRequestTypeDef,
+    OptimizePromptResponseTypeDef,
+    RerankRequestRequestTypeDef,
+    RerankResponseTypeDef,
+    RetrieveAndGenerateRequestRequestTypeDef,
+    RetrieveAndGenerateResponseTypeDef,
+    RetrieveAndGenerateStreamRequestRequestTypeDef,
+    RetrieveAndGenerateStreamResponseTypeDef,
+    RetrieveRequestRequestTypeDef,
+    RetrieveResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+
+__all__ = ("AgentsforBedrockRuntimeClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    BadGatewayException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    DependencyFailedException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class AgentsforBedrockRuntimeClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime.html#AgentsforBedrockRuntime.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        AgentsforBedrockRuntimeClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime.html#AgentsforBedrockRuntime.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#generate_presigned_url)
+        """
+
+    async def close(self) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/close.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#close)
+        """
+
+    async def delete_agent_memory(
+        self, **kwargs: Unpack[DeleteAgentMemoryRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Deletes memory from the specified memory identifier.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/delete_agent_memory.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#delete_agent_memory)
+        """
+
+    async def generate_query(
+        self, **kwargs: Unpack[GenerateQueryRequestRequestTypeDef]
+    ) -> GenerateQueryResponseTypeDef:
+        """
+        Generates an SQL query from a natural language query.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/generate_query.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#generate_query)
+        """
+
+    async def get_agent_memory(
+        self, **kwargs: Unpack[GetAgentMemoryRequestRequestTypeDef]
+    ) -> GetAgentMemoryResponseTypeDef:
+        """
+        Gets the sessions stored in the memory of the agent.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/get_agent_memory.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#get_agent_memory)
+        """
+
+    async def invoke_agent(
+        self, **kwargs: Unpack[InvokeAgentRequestRequestTypeDef]
+    ) -> InvokeAgentResponseTypeDef:
+        """
+        The CLI doesn't support streaming operations in Amazon Bedrock, including
+        <code>InvokeAgent</code>.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_agent.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#invoke_agent)
+        """
+
+    async def invoke_flow(
+        self, **kwargs: Unpack[InvokeFlowRequestRequestTypeDef]
+    ) -> InvokeFlowResponseTypeDef:
+        """
+        Invokes an alias of a flow to run the inputs that you specify and return the
+        output of each node as a stream.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_flow.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#invoke_flow)
+        """
+
+    async def invoke_inline_agent(
+        self, **kwargs: Unpack[InvokeInlineAgentRequestRequestTypeDef]
+    ) -> InvokeInlineAgentResponseTypeDef:
+        """
+        Invokes an inline Amazon Bedrock agent using the configurations you provide
+        with the request.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_inline_agent.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#invoke_inline_agent)
+        """
+
+    async def optimize_prompt(
+        self, **kwargs: Unpack[OptimizePromptRequestRequestTypeDef]
+    ) -> OptimizePromptResponseTypeDef:
+        """
+        Optimizes a prompt for the task that you specify.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/optimize_prompt.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#optimize_prompt)
+        """
+
+    async def rerank(self, **kwargs: Unpack[RerankRequestRequestTypeDef]) -> RerankResponseTypeDef:
+        """
+        Reranks the relevance of sources based on queries.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/rerank.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#rerank)
+        """
+
+    async def retrieve(
+        self, **kwargs: Unpack[RetrieveRequestRequestTypeDef]
+    ) -> RetrieveResponseTypeDef:
+        """
+        Queries a knowledge base and retrieves information from it.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/retrieve.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#retrieve)
+        """
+
+    async def retrieve_and_generate(
+        self, **kwargs: Unpack[RetrieveAndGenerateRequestRequestTypeDef]
+    ) -> RetrieveAndGenerateResponseTypeDef:
+        """
+        Queries a knowledge base and generates responses based on the retrieved results
+        and using the specified foundation model or <a
+        href="https://docs.aws.amazon.com/bedrock/latest/userguide/cross-region-inference.html">inference
+        profile</a>.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/retrieve_and_generate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#retrieve_and_generate)
+        """
+
+    async def retrieve_and_generate_stream(
+        self, **kwargs: Unpack[RetrieveAndGenerateStreamRequestRequestTypeDef]
+    ) -> RetrieveAndGenerateStreamResponseTypeDef:
+        """
+        Queries a knowledge base and generates responses based on the retrieved
+        results, with output in streaming format.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/retrieve_and_generate_stream.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#retrieve_and_generate_stream)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["get_agent_memory"]) -> GetAgentMemoryPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["rerank"]) -> RerankPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["retrieve"]) -> RetrievePaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> "AgentsforBedrockRuntimeClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime.html#AgentsforBedrockRuntime.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime.html#AgentsforBedrockRuntime.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent_runtime/client/)
+        """
