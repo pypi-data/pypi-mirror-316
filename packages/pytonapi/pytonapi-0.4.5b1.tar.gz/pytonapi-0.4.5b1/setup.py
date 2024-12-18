e75e56d4298bac84d10dbca6f7e048200c7f288b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 17:03:47 +0000

    Add package pytonapi/0.4.5b1/pytonapi-0.4.5b1.tar.gz

---
+++ packages/pytonapi/pytonapi-0.4.5b1.tar.gz/pytonapi-0.4.5b1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytonapi",
+    version="0.4.5b1",
+    author="nessshon",
+    description="Provide access to indexed TON blockchain.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tonkeeper/pytonapi/",
+    packages=setuptools.find_packages(include=["pytonapi", "pytonapi.*"]),
+    install_requires=[
+        "aiohttp~=3.10.10",
+        "pydantic==2.*",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "": ["*py.typed"],
+    },
+)
