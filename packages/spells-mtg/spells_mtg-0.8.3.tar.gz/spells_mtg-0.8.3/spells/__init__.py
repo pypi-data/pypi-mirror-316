9172e816be517897ae8c6ea59239b7d930946588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:00 +0000

    Add package spells-mtg/0.8.3/spells_mtg-0.8.3.tar.gz

---
+++ packages/spells-mtg/spells_mtg-0.8.3.tar.gz/spells_mtg-0.8.3/spells/__init__.py
@@ -0,0 +1,5 @@
+from spells.columns import ColSpec
+from spells.enums import ColType, ColName
+from spells.draft_data import summon, view_select, get_names
+
+__all__ = ["summon", "view_select", "get_names", "ColSpec", "ColType", "ColName"]
