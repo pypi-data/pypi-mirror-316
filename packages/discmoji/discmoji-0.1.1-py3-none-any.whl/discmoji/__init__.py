2d030f46e82dfa135ac9fdd61ae2e308f9510f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:02 +0000

    Add package discmoji/0.1.1/discmoji-0.1.1-py3-none-any.whl

---
+++ packages/discmoji/discmoji-0.1.1-py3-none-any.whl/discmoji/__init__.py
@@ -0,0 +1,14 @@
+"""# DISCMOJI 0.1.0
+A discord api wrapper, made for fun.
+Pre-alpha, so bugs may be present."""
+__package__ = "discmoji"
+
+from .guild_domain import *
+from ._gateway import DiscordWebsocket
+from .intents import BotIntents
+from .user import User
+from ._types import *
+from .exceptions import *
+from .decoration import AvatarDecoration
+from .snowflake import Snowflake
+from .bot import Bot
