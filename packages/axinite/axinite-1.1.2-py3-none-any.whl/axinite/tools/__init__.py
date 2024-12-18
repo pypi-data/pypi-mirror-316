748caa6d645bf04de27c9f136ec2c72ee0b6e811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:07 +0000

    Add package axinite/1.1.2/axinite-1.1.2-py3-none-any.whl

---
+++ packages/axinite/axinite-1.1.2-py3-none-any.whl/axinite/tools/__init__.py
@@ -0,0 +1,7 @@
+from axtools.args import AxiniteArgs
+from axtools.body import Body
+from axtools.functions import *
+from axtools.load import load
+from axtools.read import read
+from axtools.show import show
+from axtools.live import live
---
+++ packages/axinite/axinite-1.1.2-py3-none-any.whl/axinite/tools/show.py
@@ -0,0 +1,50 @@
+import axinite as ax
+from vpython import *
+from tools import to_vec, to_float, string_to_color
+from itertools import cycle
+import vpython as vp
+
+colors = cycle([color.red, color.blue, color.green, color.orange, color.purple, color.yellow])
+
+def show(limit, delta, *bodies: axtools.Body, radius_multiplier=1, rate=100, retain=200, name=None):
+    if rate is None:
+        rate = 100
+    if radius_multiplier is None:
+        radius_multiplier = 1
+    if retain is None:
+        retain = 200
+
+    global pause
+    pause = False
+    
+    scene = canvas()
+    scene.select()
+    if name: scene.title = name
+
+    def pause_fn():
+        global pause
+        pause = not pause
+
+    pause_btn = button(bind=pause_fn, text='Pause', pos=scene.caption_anchor)
+
+    spheres = {}
+    labels = {}
+    
+    for body in bodies:
+        body_color = string_to_color(body.color) if body.color != "" else next(colors)
+        body_retain = body.retain if body.retain != None else retain
+        spheres[body.name] = sphere(pos=to_vec(body.r[0]), radius=body.radius.value * radius_multiplier, color=body_color, make_trail=True, retain=body_retain, interval=10)
+        labels[body.name] = label(pos=spheres[body.name].pos, text=body.name, xoffset=15, yoffset=15, space=30, height=10, border=4, font='sans')
+    
+    t = to_float(0)
+    while t < limit:
+        vp.rate(rate)
+        for body in bodies:
+            spheres[body.name].pos = to_vec(body.r[t])
+            labels[body.name].pos = spheres[body.name].pos
+        t += delta
+        print(f"t = {t}", end='\r')
+        while pause: vp.rate(10)
+
+
+
