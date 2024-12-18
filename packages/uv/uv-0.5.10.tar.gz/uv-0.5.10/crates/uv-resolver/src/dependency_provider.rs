dbd41e3720fcf10bf1fe3c604c56b07d18466fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:12:24 +0000

    Add package uv/0.5.10/uv-0.5.10.tar.gz

---
+++ packages/uv/uv-0.5.10.tar.gz/uv-0.5.10/crates/uv-resolver/src/dependency_provider.rs
@@ -0,0 +1,47 @@
+use std::convert::Infallible;
+
+use pubgrub::{Dependencies, DependencyProvider, PackageResolutionStatistics, Range};
+
+use uv_pep440::Version;
+
+use crate::pubgrub::{PubGrubPackage, PubGrubPriority};
+use crate::resolver::UnavailableReason;
+
+/// We don't use a dependency provider, we interact with state directly, but we still need this one
+/// for type
+#[derive(Clone)]
+pub(crate) struct UvDependencyProvider;
+
+impl DependencyProvider for UvDependencyProvider {
+    type P = PubGrubPackage;
+    type V = Version;
+    type VS = Range<Version>;
+    type M = UnavailableReason;
+    type Priority = Option<PubGrubPriority>;
+    type Err = Infallible;
+
+    fn prioritize(
+        &self,
+        _package: &Self::P,
+        _range: &Self::VS,
+        _stats: &PackageResolutionStatistics,
+    ) -> Self::Priority {
+        unimplemented!()
+    }
+
+    fn choose_version(
+        &self,
+        _package: &Self::P,
+        _range: &Self::VS,
+    ) -> Result<Option<Self::V>, Self::Err> {
+        unimplemented!()
+    }
+
+    fn get_dependencies(
+        &self,
+        _package: &Self::P,
+        _version: &Self::V,
+    ) -> Result<Dependencies<Self::P, Self::VS, Self::M>, Self::Err> {
+        unimplemented!()
+    }
+}
