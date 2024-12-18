1b6672b037a732981159ec6d05159311f929a83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:37 +0000

    Add package fastsim/2.1.3/fastsim-2.1.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fastsim/fastsim-2.1.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/fastsim/__init__.py
@@ -0,0 +1,57 @@
+"""Package containing modules for running FASTSim.
+For example usage, see """
+
+from pathlib import Path
+import sys
+import logging
+import traceback
+from typing import Dict
+
+from fastsim import parameters as params
+from fastsim import utils
+from fastsim import simdrive, vehicle, cycle, calibration, tests
+from fastsim import calibration as cal
+from fastsim.resample import resample
+from fastsim import auxiliaries
+from fastsim import fastsimrust
+from fastsim import fastsimrust as fsr
+
+
+def package_root() -> Path:
+    """Returns the package root directory."""
+    return Path(__file__).parent
+
+def resources_root() -> Path:
+    """Returns the resources root directory."""
+    return Path(__file__).parent / "resources"
+
+
+DEFAULT_LOGGING_CONFIG = dict(
+    format = "%(asctime)s.%(msecs)03d | %(filename)s:%(lineno)s | %(levelname)s: %(message)s",
+    datefmt = "%Y-%m-%d %H:%M:%S",
+) 
+
+
+
+# Set up logging
+logging.basicConfig(**DEFAULT_LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
+from pkg_resources import get_distribution
+__version__ = get_distribution("fastsim").version
+
+__doc__ += "\nhttps://pypi.org/project/fastsim/"
+__doc__ += "\nhttps://www.nrel.gov/transportation/fastsim.html"
+
+# Enable np.array() on array structs
+import numpy as np
+
+
+def _as_numpy_array(self, *args, **kwargs):
+    return np.array(list(self), *args, **kwargs)
+
+
+setattr(fsr.Pyo3ArrayF64, "__array__", _as_numpy_array)
+setattr(fsr.Pyo3ArrayU32, "__array__", _as_numpy_array)
+setattr(fsr.Pyo3ArrayBool, "__array__", _as_numpy_array)
+setattr(fsr.Pyo3VecF64, "__array__", _as_numpy_array)
