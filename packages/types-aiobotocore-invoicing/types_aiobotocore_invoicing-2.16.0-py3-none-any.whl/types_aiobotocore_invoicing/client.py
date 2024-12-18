a3170e152ab34d28cfd5d1e76858e59baf24d264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:49 +0000

    Add package types-aiobotocore-invoicing/2.16.0/types_aiobotocore_invoicing-2.16.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-invoicing/types_aiobotocore_invoicing-2.16.0-py3-none-any.whl/types_aiobotocore_invoicing/client.py
@@ -0,0 +1,226 @@
+"""
+Type annotations for invoicing service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_invoicing.client import InvoicingClient
+
+    session = get_session()
+    async with session.create_client("invoicing") as client:
+        client: InvoicingClient
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .paginator import ListInvoiceUnitsPaginator
+from .type_defs import (
+    BatchGetInvoiceProfileRequestRequestTypeDef,
+    BatchGetInvoiceProfileResponseTypeDef,
+    CreateInvoiceUnitRequestRequestTypeDef,
+    CreateInvoiceUnitResponseTypeDef,
+    DeleteInvoiceUnitRequestRequestTypeDef,
+    DeleteInvoiceUnitResponseTypeDef,
+    GetInvoiceUnitRequestRequestTypeDef,
+    GetInvoiceUnitResponseTypeDef,
+    ListInvoiceUnitsRequestRequestTypeDef,
+    ListInvoiceUnitsResponseTypeDef,
+    ListTagsForResourceRequestRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceRequestRequestTypeDef,
+    UntagResourceRequestRequestTypeDef,
+    UpdateInvoiceUnitRequestRequestTypeDef,
+    UpdateInvoiceUnitResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+
+__all__ = ("InvoicingClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class InvoicingClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing.html#Invoicing.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        InvoicingClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing.html#Invoicing.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#generate_presigned_url)
+        """
+
+    async def close(self) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/close.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#close)
+        """
+
+    async def batch_get_invoice_profile(
+        self, **kwargs: Unpack[BatchGetInvoiceProfileRequestRequestTypeDef]
+    ) -> BatchGetInvoiceProfileResponseTypeDef:
+        """
+        This gets the invoice profile associated with a set of accounts.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/batch_get_invoice_profile.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#batch_get_invoice_profile)
+        """
+
+    async def create_invoice_unit(
+        self, **kwargs: Unpack[CreateInvoiceUnitRequestRequestTypeDef]
+    ) -> CreateInvoiceUnitResponseTypeDef:
+        """
+        This creates a new invoice unit with the provided definition.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/create_invoice_unit.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#create_invoice_unit)
+        """
+
+    async def delete_invoice_unit(
+        self, **kwargs: Unpack[DeleteInvoiceUnitRequestRequestTypeDef]
+    ) -> DeleteInvoiceUnitResponseTypeDef:
+        """
+        This deletes an invoice unit with the provided invoice unit ARN.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/delete_invoice_unit.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#delete_invoice_unit)
+        """
+
+    async def get_invoice_unit(
+        self, **kwargs: Unpack[GetInvoiceUnitRequestRequestTypeDef]
+    ) -> GetInvoiceUnitResponseTypeDef:
+        """
+        This retrieves the invoice unit definition.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/get_invoice_unit.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#get_invoice_unit)
+        """
+
+    async def list_invoice_units(
+        self, **kwargs: Unpack[ListInvoiceUnitsRequestRequestTypeDef]
+    ) -> ListInvoiceUnitsResponseTypeDef:
+        """
+        This fetches a list of all invoice unit definitions for a given account, as of
+        the provided <code>AsOf</code> date.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/list_invoice_units.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#list_invoice_units)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Lists the tags for a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Adds a tag to a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#tag_resource)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Removes a tag from a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#untag_resource)
+        """
+
+    async def update_invoice_unit(
+        self, **kwargs: Unpack[UpdateInvoiceUnitRequestRequestTypeDef]
+    ) -> UpdateInvoiceUnitResponseTypeDef:
+        """
+        You can update the invoice unit configuration at any time, and Amazon Web
+        Services will use the latest configuration at the end of the month.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/update_invoice_unit.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#update_invoice_unit)
+        """
+
+    def get_paginator(
+        self, operation_name: Literal["list_invoice_units"]
+    ) -> ListInvoiceUnitsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> "InvoicingClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing.html#Invoicing.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/invoicing.html#Invoicing.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_invoicing/client/)
+        """
