7ea37d64d65817d716d214c540de7b0ebc473c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:03:57 +0000

    Add package unstructured-ingest/0.3.10/unstructured_ingest-0.3.10.tar.gz

---
+++ packages/unstructured-ingest/unstructured_ingest-0.3.10.tar.gz/unstructured_ingest-0.3.10/unstructured_ingest/embed/openai.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from pydantic import Field, SecretStr
+
+from unstructured_ingest.embed.interfaces import BaseEmbeddingEncoder, EmbeddingConfig
+from unstructured_ingest.utils.dep_check import requires_dependencies
+
+if TYPE_CHECKING:
+    from openai import OpenAI
+
+
+class OpenAIEmbeddingConfig(EmbeddingConfig):
+    api_key: SecretStr
+    embedder_model_name: str = Field(default="text-embedding-ada-002", alias="model_name")
+
+    @requires_dependencies(["openai"], extras="openai")
+    def get_client(self) -> "OpenAI":
+        from openai import OpenAI
+
+        return OpenAI(api_key=self.api_key.get_secret_value())
+
+
+@dataclass
+class OpenAIEmbeddingEncoder(BaseEmbeddingEncoder):
+    config: OpenAIEmbeddingConfig
+
+    def embed_query(self, query: str) -> list[float]:
+        client = self.config.get_client()
+        response = client.embeddings.create(input=query, model=self.config.embedder_model_name)
+        return response.data[0].embedding
+
+    def embed_documents(self, elements: list[dict]) -> list[dict]:
+        embeddings = self._embed_documents([e.get("text", "") for e in elements])
+        elements_with_embeddings = self._add_embeddings_to_elements(elements, embeddings)
+        return elements_with_embeddings
