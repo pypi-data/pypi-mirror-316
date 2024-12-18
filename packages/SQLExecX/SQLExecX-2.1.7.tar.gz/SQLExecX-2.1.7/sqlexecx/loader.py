8d2be1223be90b2ac0e1bfa9c1c08e0345ee0e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:36 +0000

    Add package SQLExecX/2.1.7/SQLExecX-2.1.7.tar.gz

---
+++ packages/SQLExecX/SQLExecX-2.1.7.tar.gz/SQLExecX-2.1.7/sqlexecx/loader.py
@@ -0,0 +1,98 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from datetime import datetime, date
+
+
+class Loader:
+
+    def __init__(self, data, description):
+        self.data = data
+        self.description = description
+
+    def to_df(self, df_type: str = "auto"):
+        """
+        transform to DataFrame of pandas or polars.
+
+        :param df_type: auto, pandas or polars
+        :return DataFrame of pandas or polars. if df_type is 'auto', use polars by default. if polars not installed, use pandas.
+        """
+        assert df_type in ["auto", "pandas", "polars"], "df_type must be 'auto', 'pandas' or 'polars'"
+        if df_type == "auto":
+            use_polars = True
+            try:
+                import polars as pd
+            except:
+                try:
+                    import pandas as pd
+                    use_polars = False
+                except:
+                    raise ModuleNotFoundError("No module named 'pandas', please install it use 'pip install pandas'")
+
+            if self.description:
+                description = [x[0] for x in self.description]
+                if use_polars:
+                    return pd.DataFrame(data=self.data, schema=description, orient="row")
+                else:
+                    return pd.DataFrame(data=self.data, columns=description)
+                
+            if use_polars:
+                return pd.DataFrame(data=self.data, orient="row")
+            else:
+                return pd.DataFrame(data=self.data)
+        elif df_type == "pandas":
+            try:
+                import pandas as pd
+            except:
+                raise ModuleNotFoundError("No module named 'pandas', please install it use 'pip install pandas'")
+            if self.description:
+                description = [x[0] for x in self.description]
+                return pd.DataFrame(data=self.data, columns=description)
+            else:
+                return pd.DataFrame(data=self.data)
+        elif df_type == "polars":
+            try:
+                import polars as pl
+            except:
+                raise ModuleNotFoundError("No module named 'polars', please install it use 'pip install polars'")
+            if self.description:
+                description = [x[0] for x in self.description]
+                return pl.DataFrame(data=self.data, schema=description, orient="row")
+            else:
+                return pl.DataFrame(data=self.data, orient="row")
+
+    def to_csv(self, file_name: str, delimiter=',', header=True, encoding='utf-8'):
+        """
+        Save as csv
+        :param file_name
+        :param delimiter
+        :param header bool, include header or not
+        :param encoding default utf-8
+        """
+        import csv
+        with open(file_name, 'w', newline='', encoding=encoding) as f:
+            writer = csv.writer(f, delimiter=delimiter)
+            if header and self.description:
+                description = [x[0] for x in self.description]
+                writer.writerow(description)
+            writer.writerows(self.data)
+
+    def to_json(self, file_name: str, encoding='utf-8'):
+        """
+        Save as json
+        :param file_name
+        :param encoding default utf-8
+        """
+        import json
+        from sqlexecutorx import Dict
+
+        if self.data and self.description:
+            names = [x[0] for x in self.description]
+            data = list(map(lambda x: Dict(names, x), self.data))
+            with open(file_name, 'w', encoding=encoding) as f:
+                json.dump(data, f, default=_datetime_handler)
+
+
+def _datetime_handler(obj):
+    if isinstance(obj, datetime) or isinstance(obj, date):
+        return obj.__str__()
