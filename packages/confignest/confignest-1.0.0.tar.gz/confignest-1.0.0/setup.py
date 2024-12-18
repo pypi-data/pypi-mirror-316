08a3898dd08e5fb46b5e9ffb72573df2e0f681e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 17:03:58 +0000

    Add package confignest/1.0.0/confignest-1.0.0.tar.gz

---
+++ packages/confignest/confignest-1.0.0.tar.gz/confignest-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+# python setup.py sdist bdist_wheel 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="confignest",
+    version="1.0.0",
+    keywords=("pip", "confignest"),
+    author="github.com/0-1CxH",
+    author_email="h0.1c@foxmail.com",
+    description="Managing Complex Configs in Nested Way.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/0-1CxH/ConfigNest",
+    packages=setuptools.find_packages(),
+    license = "MIT Licence",
+    platforms = "any",
+
+    # entry_points={},
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+    # python_requires='>=3.6',
+)
