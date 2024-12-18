d1505afc2bd6b57574466e36cba20f0fb695f0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:45 +0000

    Add package types-aiobotocore-transfer/2.16.0/types_aiobotocore_transfer-2.16.0.tar.gz

---
+++ packages/types-aiobotocore-transfer/types_aiobotocore_transfer-2.16.0.tar.gz/types_aiobotocore_transfer-2.16.0/types_aiobotocore_transfer/paginator.py
@@ -0,0 +1,310 @@
+"""
+Type annotations for transfer service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_transfer.client import TransferClient
+    from types_aiobotocore_transfer.paginator import (
+        ListAccessesPaginator,
+        ListAgreementsPaginator,
+        ListCertificatesPaginator,
+        ListConnectorsPaginator,
+        ListExecutionsPaginator,
+        ListFileTransferResultsPaginator,
+        ListProfilesPaginator,
+        ListSecurityPoliciesPaginator,
+        ListServersPaginator,
+        ListTagsForResourcePaginator,
+        ListUsersPaginator,
+        ListWebAppsPaginator,
+        ListWorkflowsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("transfer") as client:
+        client: TransferClient
+
+        list_accesses_paginator: ListAccessesPaginator = client.get_paginator("list_accesses")
+        list_agreements_paginator: ListAgreementsPaginator = client.get_paginator("list_agreements")
+        list_certificates_paginator: ListCertificatesPaginator = client.get_paginator("list_certificates")
+        list_connectors_paginator: ListConnectorsPaginator = client.get_paginator("list_connectors")
+        list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+        list_file_transfer_results_paginator: ListFileTransferResultsPaginator = client.get_paginator("list_file_transfer_results")
+        list_profiles_paginator: ListProfilesPaginator = client.get_paginator("list_profiles")
+        list_security_policies_paginator: ListSecurityPoliciesPaginator = client.get_paginator("list_security_policies")
+        list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+        list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+        list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+        list_web_apps_paginator: ListWebAppsPaginator = client.get_paginator("list_web_apps")
+        list_workflows_paginator: ListWorkflowsPaginator = client.get_paginator("list_workflows")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListAccessesRequestListAccessesPaginateTypeDef,
+    ListAccessesResponseTypeDef,
+    ListAgreementsRequestListAgreementsPaginateTypeDef,
+    ListAgreementsResponseTypeDef,
+    ListCertificatesRequestListCertificatesPaginateTypeDef,
+    ListCertificatesResponseTypeDef,
+    ListConnectorsRequestListConnectorsPaginateTypeDef,
+    ListConnectorsResponseTypeDef,
+    ListExecutionsRequestListExecutionsPaginateTypeDef,
+    ListExecutionsResponseTypeDef,
+    ListFileTransferResultsRequestListFileTransferResultsPaginateTypeDef,
+    ListFileTransferResultsResponseTypeDef,
+    ListProfilesRequestListProfilesPaginateTypeDef,
+    ListProfilesResponseTypeDef,
+    ListSecurityPoliciesRequestListSecurityPoliciesPaginateTypeDef,
+    ListSecurityPoliciesResponseTypeDef,
+    ListServersRequestListServersPaginateTypeDef,
+    ListServersResponseTypeDef,
+    ListTagsForResourceRequestListTagsForResourcePaginateTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    ListUsersRequestListUsersPaginateTypeDef,
+    ListUsersResponseTypeDef,
+    ListWebAppsRequestListWebAppsPaginateTypeDef,
+    ListWebAppsResponseTypeDef,
+    ListWorkflowsRequestListWorkflowsPaginateTypeDef,
+    ListWorkflowsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "ListAccessesPaginator",
+    "ListAgreementsPaginator",
+    "ListCertificatesPaginator",
+    "ListConnectorsPaginator",
+    "ListExecutionsPaginator",
+    "ListFileTransferResultsPaginator",
+    "ListProfilesPaginator",
+    "ListSecurityPoliciesPaginator",
+    "ListServersPaginator",
+    "ListTagsForResourcePaginator",
+    "ListUsersPaginator",
+    "ListWebAppsPaginator",
+    "ListWorkflowsPaginator",
+)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListAccessesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListAccesses.html#Transfer.Paginator.ListAccesses)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listaccessespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAccessesRequestListAccessesPaginateTypeDef]
+    ) -> AsyncIterator[ListAccessesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListAccesses.html#Transfer.Paginator.ListAccesses.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listaccessespaginator)
+        """
+
+
+class ListAgreementsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListAgreements.html#Transfer.Paginator.ListAgreements)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listagreementspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgreementsRequestListAgreementsPaginateTypeDef]
+    ) -> AsyncIterator[ListAgreementsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListAgreements.html#Transfer.Paginator.ListAgreements.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listagreementspaginator)
+        """
+
+
+class ListCertificatesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListCertificates.html#Transfer.Paginator.ListCertificates)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listcertificatespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListCertificatesRequestListCertificatesPaginateTypeDef]
+    ) -> AsyncIterator[ListCertificatesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListCertificates.html#Transfer.Paginator.ListCertificates.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listcertificatespaginator)
+        """
+
+
+class ListConnectorsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListConnectors.html#Transfer.Paginator.ListConnectors)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listconnectorspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListConnectorsRequestListConnectorsPaginateTypeDef]
+    ) -> AsyncIterator[ListConnectorsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListConnectors.html#Transfer.Paginator.ListConnectors.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listconnectorspaginator)
+        """
+
+
+class ListExecutionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListExecutions.html#Transfer.Paginator.ListExecutions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listexecutionspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListExecutionsRequestListExecutionsPaginateTypeDef]
+    ) -> AsyncIterator[ListExecutionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListExecutions.html#Transfer.Paginator.ListExecutions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listexecutionspaginator)
+        """
+
+
+class ListFileTransferResultsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListFileTransferResults.html#Transfer.Paginator.ListFileTransferResults)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listfiletransferresultspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListFileTransferResultsRequestListFileTransferResultsPaginateTypeDef]
+    ) -> AsyncIterator[ListFileTransferResultsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListFileTransferResults.html#Transfer.Paginator.ListFileTransferResults.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listfiletransferresultspaginator)
+        """
+
+
+class ListProfilesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListProfiles.html#Transfer.Paginator.ListProfiles)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listprofilespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListProfilesRequestListProfilesPaginateTypeDef]
+    ) -> AsyncIterator[ListProfilesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListProfiles.html#Transfer.Paginator.ListProfiles.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listprofilespaginator)
+        """
+
+
+class ListSecurityPoliciesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListSecurityPolicies.html#Transfer.Paginator.ListSecurityPolicies)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listsecuritypoliciespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListSecurityPoliciesRequestListSecurityPoliciesPaginateTypeDef]
+    ) -> AsyncIterator[ListSecurityPoliciesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListSecurityPolicies.html#Transfer.Paginator.ListSecurityPolicies.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listsecuritypoliciespaginator)
+        """
+
+
+class ListServersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListServers.html#Transfer.Paginator.ListServers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listserverspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListServersRequestListServersPaginateTypeDef]
+    ) -> AsyncIterator[ListServersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListServers.html#Transfer.Paginator.ListServers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listserverspaginator)
+        """
+
+
+class ListTagsForResourcePaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListTagsForResource.html#Transfer.Paginator.ListTagsForResource)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listtagsforresourcepaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListTagsForResourceRequestListTagsForResourcePaginateTypeDef]
+    ) -> AsyncIterator[ListTagsForResourceResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListTagsForResource.html#Transfer.Paginator.ListTagsForResource.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listtagsforresourcepaginator)
+        """
+
+
+class ListUsersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListUsers.html#Transfer.Paginator.ListUsers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listuserspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListUsersRequestListUsersPaginateTypeDef]
+    ) -> AsyncIterator[ListUsersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListUsers.html#Transfer.Paginator.ListUsers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listuserspaginator)
+        """
+
+
+class ListWebAppsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListWebApps.html#Transfer.Paginator.ListWebApps)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listwebappspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListWebAppsRequestListWebAppsPaginateTypeDef]
+    ) -> AsyncIterator[ListWebAppsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListWebApps.html#Transfer.Paginator.ListWebApps.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listwebappspaginator)
+        """
+
+
+class ListWorkflowsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListWorkflows.html#Transfer.Paginator.ListWorkflows)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listworkflowspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListWorkflowsRequestListWorkflowsPaginateTypeDef]
+    ) -> AsyncIterator[ListWorkflowsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/transfer/paginator/ListWorkflows.html#Transfer.Paginator.ListWorkflows.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_transfer/paginators/#listworkflowspaginator)
+        """
