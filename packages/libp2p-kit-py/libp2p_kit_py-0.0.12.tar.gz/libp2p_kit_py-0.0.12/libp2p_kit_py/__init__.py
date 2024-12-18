25583886f4fa86644f9bf17a9dc2b6b152c410ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 17:03:51 +0000

    Add package libp2p-kit-py/0.0.12/libp2p_kit_py-0.0.12.tar.gz

---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.12.tar.gz/libp2p_kit_py-0.0.12/libp2p_kit_py/__init__.py
@@ -0,0 +1,6 @@
+from .libp2p_kit import libp2p_kit 
+# from .websocket_kit import websocket_kit_lib
+# from .websocket_kit import websocket_kit
+# from .s3_kit import s3_kit
+# from .aria2_kit import aria2_kit
+
---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.12.tar.gz/libp2p_kit_py-0.0.12/libp2p_kit_py/libp2p_kit.py
@@ -0,0 +1,19 @@
+# from .libp2p_kit import libp2p
+from .aria2_kit import aria2_kit
+from .s3_kit import s3_kit
+from .websocket_kit import websocket_kit
+from .websocket_kit import websocket_kit_lib
+
+class libp2p_kit:
+    def __init__(self, resources, metadata):
+        self.libp2p_kit = libp2p_kit(resources, metadata)
+        self.aria2_kit = aria2_kit(resources, metadata)
+        self.s3_kit = s3_kit(resources, metadata)
+        self.websocket_kit = websocket_kit(resources, metadata)
+        self.websocket_kit_lib = websocket_kit_lib(resources, metadata)
+
+    def run(self):
+
+        return True
+    
+    
