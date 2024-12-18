ada965eb778b638ad86cc257e0add63ebd68704c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:06 +0000

    Add package czo/0.1.20/czo-0.1.20-py3-none-any.whl

---
+++ packages/czo/czo-0.1.20-py3-none-any.whl/czo/__init__.py
@@ -0,0 +1,18 @@
+"""
+辅助测试的一些常用功能
+"""
+
+from ._date import DateLib, Timer
+from ._net import NetLib
+from ._path import DirLib, PathLib
+from ._person import Person
+from ._rand import Random
+
+__all__: list[str] = [
+    "DateLib",
+    "PathLib",
+    "DirLib",
+    "Random",
+    "NetLib",
+    "Person",
+]
