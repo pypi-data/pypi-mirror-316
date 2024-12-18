24a9d01f7d696a5e753443596c6dd3ff6657e5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:53 +0000

    Add package MyLabLibraryTAM1/0.1.1/mylablibrarytam1-0.1.1.tar.gz

---
+++ packages/MyLabLibraryTAM1/mylablibrarytam1-0.1.1.tar.gz/mylablibrarytam1-0.1.1/MyLabLibraryTAM1/__init__.py
@@ -0,0 +1,5 @@
+from .Compiler import *
+from .Parser import *
+from .Reader import *
+from .Struck import *
+from .Value import *
---
+++ packages/MyLabLibraryTAM1/mylablibrarytam1-0.1.1.tar.gz/mylablibrarytam1-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MyLabLibraryTAM1",
+    version="0.1.1",
+    author="Timur Mitrofanov",
+    author_email="mitrofanov-t@bk.ru",
+    description="Каастомная библиотека по второй лабораторной работе лог. програмирования",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MitrofanovTimurAlexsandrovich/my_library ",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
