7f54d6464eb4361286fba3653a37abc136b67d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:03:54 +0000

    Add package pandashare/0.0.4/pandashare-0.0.4.tar.gz

---
+++ packages/pandashare/pandashare-0.0.4.tar.gz/pandashare-0.0.4/pandashare/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*- 
+# pandashare/__init__.py
+import codecs
+import os
+from .pro.data_pro import pro_api,pro_bar
+
+__version__ = '0.0.4'
+__author__ = 'quantdrchow'
+
+def greet(name):
+    return f"Hello, {name}!"
+
+
+
+
+
---
+++ packages/pandashare/pandashare-0.0.4.tar.gz/pandashare-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pandashare',                          # 包名
+    version='0.0.4',                             # 版本号
+    packages=find_packages(),                   # 自动查找包
+    install_requires=[],                        # 依赖（如果有的话）
+    long_description=open('README.md').read(),  # 长描述
+    long_description_content_type='text/markdown',  # 内容类型
+    author='quantdrchow',                         # 作者信息
+    author_email='quantdrchow@gmail.com',       # 作者邮箱
+    description='Sharing data focused on financial AI',    # 简短描述
+    license='MIT',                              # 许可协议
+    url='https://github.com/yourusername/simple_pkg',  # 项目链接
+    classifiers=[                               # 分类
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
