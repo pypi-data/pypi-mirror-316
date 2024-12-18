323e2be450be2246d950a625a30bd11be9a413ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:09 +0000

    Add package pyEGAF/1.0.12/pyEGAF-1.0.12-py3-none-any.whl

---
+++ packages/pyEGAF/pyEGAF-1.0.12-py3-none-any.whl/pyEGAF/__init__.py
@@ -0,0 +1,17 @@
+from .pyEGAF import *
+from .base_egaf import *
+from .separation import Separation
+from .cross_section import CrossSection
+from .decay import Levels, Gammas
+from .analysis import Analysis
+from .cap_gam import CapGam
+from .file_handler import RIPL, JSONFile, ENSDF
+
+__version__='1.0.1'
+__author__='Aaron M. Hurst'
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+def get_data(path):
+    """Function to return absolute path of the data files inside the root of 
+    the Python package."""
+    return os.path.join(_ROOT, path)
