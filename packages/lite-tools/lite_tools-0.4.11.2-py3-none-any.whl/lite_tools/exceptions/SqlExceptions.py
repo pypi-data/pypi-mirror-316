8fc889aefc7aae4943107d7d8a1e3070cc5c85fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:04:31 +0000

    Add package lite-tools/0.4.11.2/lite_tools-0.4.11.2-py3-none-any.whl

---
+++ packages/lite-tools/lite_tools-0.4.11.2-py3-none-any.whl/lite_tools/exceptions/SqlExceptions.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Author  : Lodge
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　    ┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+"""
+
+
+class DuplicateEntryException(Exception):
+    def __init__(self):
+        pass
+
+
+class IterNotNeedRun(Exception):
+    def __init__(self):
+        pass
+
+
+class NotSupportType(Exception):
+    def __init__(self):
+        self.error = "不支持的类型"
+
+    def __str__(self):
+        return self.error
+
+
+class LengthError(Exception):
+    pass
