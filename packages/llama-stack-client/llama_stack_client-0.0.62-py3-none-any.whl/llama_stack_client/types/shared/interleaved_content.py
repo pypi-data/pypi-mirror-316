63b838ca61d3722f18680ffcb1e99be3c0bbfb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:49 +0000

    Add package llama-stack-client/0.0.62/llama_stack_client-0.0.62-py3-none-any.whl

---
+++ packages/llama-stack-client/llama_stack_client-0.0.62-py3-none-any.whl/llama_stack_client/types/shared/interleaved_content.py
@@ -0,0 +1,26 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Union, Optional
+from typing_extensions import Literal, TypeAlias
+
+from ..._models import BaseModel
+from .interleaved_content_item import InterleavedContentItem
+
+__all__ = ["InterleavedContent", "ImageContentItem", "TextContentItem"]
+
+
+class ImageContentItem(BaseModel):
+    type: Literal["image"]
+
+    data: Optional[str] = None
+
+    url: Optional[str] = None
+
+
+class TextContentItem(BaseModel):
+    text: str
+
+    type: Literal["text"]
+
+
+InterleavedContent: TypeAlias = Union[str, ImageContentItem, TextContentItem, List[InterleavedContentItem]]
