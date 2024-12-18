2226f0794e14b22c752e85f1ce4af35cefacaf81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:04:00 +0000

    Add package types-aiobotocore-bedrock-agent/2.16.0/types_aiobotocore_bedrock_agent-2.16.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-bedrock-agent/types_aiobotocore_bedrock_agent-2.16.0-py3-none-any.whl/types_aiobotocore_bedrock_agent/paginator.py
@@ -0,0 +1,333 @@
+"""
+Type annotations for bedrock-agent service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_bedrock_agent.client import AgentsforBedrockClient
+    from types_aiobotocore_bedrock_agent.paginator import (
+        ListAgentActionGroupsPaginator,
+        ListAgentAliasesPaginator,
+        ListAgentCollaboratorsPaginator,
+        ListAgentKnowledgeBasesPaginator,
+        ListAgentVersionsPaginator,
+        ListAgentsPaginator,
+        ListDataSourcesPaginator,
+        ListFlowAliasesPaginator,
+        ListFlowVersionsPaginator,
+        ListFlowsPaginator,
+        ListIngestionJobsPaginator,
+        ListKnowledgeBaseDocumentsPaginator,
+        ListKnowledgeBasesPaginator,
+        ListPromptsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("bedrock-agent") as client:
+        client: AgentsforBedrockClient
+
+        list_agent_action_groups_paginator: ListAgentActionGroupsPaginator = client.get_paginator("list_agent_action_groups")
+        list_agent_aliases_paginator: ListAgentAliasesPaginator = client.get_paginator("list_agent_aliases")
+        list_agent_collaborators_paginator: ListAgentCollaboratorsPaginator = client.get_paginator("list_agent_collaborators")
+        list_agent_knowledge_bases_paginator: ListAgentKnowledgeBasesPaginator = client.get_paginator("list_agent_knowledge_bases")
+        list_agent_versions_paginator: ListAgentVersionsPaginator = client.get_paginator("list_agent_versions")
+        list_agents_paginator: ListAgentsPaginator = client.get_paginator("list_agents")
+        list_data_sources_paginator: ListDataSourcesPaginator = client.get_paginator("list_data_sources")
+        list_flow_aliases_paginator: ListFlowAliasesPaginator = client.get_paginator("list_flow_aliases")
+        list_flow_versions_paginator: ListFlowVersionsPaginator = client.get_paginator("list_flow_versions")
+        list_flows_paginator: ListFlowsPaginator = client.get_paginator("list_flows")
+        list_ingestion_jobs_paginator: ListIngestionJobsPaginator = client.get_paginator("list_ingestion_jobs")
+        list_knowledge_base_documents_paginator: ListKnowledgeBaseDocumentsPaginator = client.get_paginator("list_knowledge_base_documents")
+        list_knowledge_bases_paginator: ListKnowledgeBasesPaginator = client.get_paginator("list_knowledge_bases")
+        list_prompts_paginator: ListPromptsPaginator = client.get_paginator("list_prompts")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListAgentActionGroupsRequestListAgentActionGroupsPaginateTypeDef,
+    ListAgentActionGroupsResponseTypeDef,
+    ListAgentAliasesRequestListAgentAliasesPaginateTypeDef,
+    ListAgentAliasesResponseTypeDef,
+    ListAgentCollaboratorsRequestListAgentCollaboratorsPaginateTypeDef,
+    ListAgentCollaboratorsResponseTypeDef,
+    ListAgentKnowledgeBasesRequestListAgentKnowledgeBasesPaginateTypeDef,
+    ListAgentKnowledgeBasesResponseTypeDef,
+    ListAgentsRequestListAgentsPaginateTypeDef,
+    ListAgentsResponseTypeDef,
+    ListAgentVersionsRequestListAgentVersionsPaginateTypeDef,
+    ListAgentVersionsResponseTypeDef,
+    ListDataSourcesRequestListDataSourcesPaginateTypeDef,
+    ListDataSourcesResponseTypeDef,
+    ListFlowAliasesRequestListFlowAliasesPaginateTypeDef,
+    ListFlowAliasesResponseTypeDef,
+    ListFlowsRequestListFlowsPaginateTypeDef,
+    ListFlowsResponseTypeDef,
+    ListFlowVersionsRequestListFlowVersionsPaginateTypeDef,
+    ListFlowVersionsResponseTypeDef,
+    ListIngestionJobsRequestListIngestionJobsPaginateTypeDef,
+    ListIngestionJobsResponseTypeDef,
+    ListKnowledgeBaseDocumentsRequestListKnowledgeBaseDocumentsPaginateTypeDef,
+    ListKnowledgeBaseDocumentsResponseTypeDef,
+    ListKnowledgeBasesRequestListKnowledgeBasesPaginateTypeDef,
+    ListKnowledgeBasesResponseTypeDef,
+    ListPromptsRequestListPromptsPaginateTypeDef,
+    ListPromptsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "ListAgentActionGroupsPaginator",
+    "ListAgentAliasesPaginator",
+    "ListAgentCollaboratorsPaginator",
+    "ListAgentKnowledgeBasesPaginator",
+    "ListAgentVersionsPaginator",
+    "ListAgentsPaginator",
+    "ListDataSourcesPaginator",
+    "ListFlowAliasesPaginator",
+    "ListFlowVersionsPaginator",
+    "ListFlowsPaginator",
+    "ListIngestionJobsPaginator",
+    "ListKnowledgeBaseDocumentsPaginator",
+    "ListKnowledgeBasesPaginator",
+    "ListPromptsPaginator",
+)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListAgentActionGroupsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentActionGroups.html#AgentsforBedrock.Paginator.ListAgentActionGroups)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentactiongroupspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgentActionGroupsRequestListAgentActionGroupsPaginateTypeDef]
+    ) -> AsyncIterator[ListAgentActionGroupsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentActionGroups.html#AgentsforBedrock.Paginator.ListAgentActionGroups.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentactiongroupspaginator)
+        """
+
+
+class ListAgentAliasesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentAliases.html#AgentsforBedrock.Paginator.ListAgentAliases)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentaliasespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgentAliasesRequestListAgentAliasesPaginateTypeDef]
+    ) -> AsyncIterator[ListAgentAliasesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentAliases.html#AgentsforBedrock.Paginator.ListAgentAliases.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentaliasespaginator)
+        """
+
+
+class ListAgentCollaboratorsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentCollaborators.html#AgentsforBedrock.Paginator.ListAgentCollaborators)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentcollaboratorspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgentCollaboratorsRequestListAgentCollaboratorsPaginateTypeDef]
+    ) -> AsyncIterator[ListAgentCollaboratorsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentCollaborators.html#AgentsforBedrock.Paginator.ListAgentCollaborators.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentcollaboratorspaginator)
+        """
+
+
+class ListAgentKnowledgeBasesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentKnowledgeBases.html#AgentsforBedrock.Paginator.ListAgentKnowledgeBases)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentknowledgebasespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgentKnowledgeBasesRequestListAgentKnowledgeBasesPaginateTypeDef]
+    ) -> AsyncIterator[ListAgentKnowledgeBasesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentKnowledgeBases.html#AgentsforBedrock.Paginator.ListAgentKnowledgeBases.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentknowledgebasespaginator)
+        """
+
+
+class ListAgentVersionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentVersions.html#AgentsforBedrock.Paginator.ListAgentVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentversionspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgentVersionsRequestListAgentVersionsPaginateTypeDef]
+    ) -> AsyncIterator[ListAgentVersionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgentVersions.html#AgentsforBedrock.Paginator.ListAgentVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentversionspaginator)
+        """
+
+
+class ListAgentsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgents.html#AgentsforBedrock.Paginator.ListAgents)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListAgentsRequestListAgentsPaginateTypeDef]
+    ) -> AsyncIterator[ListAgentsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListAgents.html#AgentsforBedrock.Paginator.ListAgents.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listagentspaginator)
+        """
+
+
+class ListDataSourcesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListDataSources.html#AgentsforBedrock.Paginator.ListDataSources)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listdatasourcespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListDataSourcesRequestListDataSourcesPaginateTypeDef]
+    ) -> AsyncIterator[ListDataSourcesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListDataSources.html#AgentsforBedrock.Paginator.ListDataSources.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listdatasourcespaginator)
+        """
+
+
+class ListFlowAliasesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListFlowAliases.html#AgentsforBedrock.Paginator.ListFlowAliases)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listflowaliasespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListFlowAliasesRequestListFlowAliasesPaginateTypeDef]
+    ) -> AsyncIterator[ListFlowAliasesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListFlowAliases.html#AgentsforBedrock.Paginator.ListFlowAliases.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listflowaliasespaginator)
+        """
+
+
+class ListFlowVersionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListFlowVersions.html#AgentsforBedrock.Paginator.ListFlowVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listflowversionspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListFlowVersionsRequestListFlowVersionsPaginateTypeDef]
+    ) -> AsyncIterator[ListFlowVersionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListFlowVersions.html#AgentsforBedrock.Paginator.ListFlowVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listflowversionspaginator)
+        """
+
+
+class ListFlowsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListFlows.html#AgentsforBedrock.Paginator.ListFlows)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listflowspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListFlowsRequestListFlowsPaginateTypeDef]
+    ) -> AsyncIterator[ListFlowsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListFlows.html#AgentsforBedrock.Paginator.ListFlows.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listflowspaginator)
+        """
+
+
+class ListIngestionJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListIngestionJobs.html#AgentsforBedrock.Paginator.ListIngestionJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listingestionjobspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListIngestionJobsRequestListIngestionJobsPaginateTypeDef]
+    ) -> AsyncIterator[ListIngestionJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListIngestionJobs.html#AgentsforBedrock.Paginator.ListIngestionJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listingestionjobspaginator)
+        """
+
+
+class ListKnowledgeBaseDocumentsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListKnowledgeBaseDocuments.html#AgentsforBedrock.Paginator.ListKnowledgeBaseDocuments)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listknowledgebasedocumentspaginator)
+    """
+
+    def paginate(
+        self,
+        **kwargs: Unpack[
+            ListKnowledgeBaseDocumentsRequestListKnowledgeBaseDocumentsPaginateTypeDef
+        ],
+    ) -> AsyncIterator[ListKnowledgeBaseDocumentsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListKnowledgeBaseDocuments.html#AgentsforBedrock.Paginator.ListKnowledgeBaseDocuments.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listknowledgebasedocumentspaginator)
+        """
+
+
+class ListKnowledgeBasesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListKnowledgeBases.html#AgentsforBedrock.Paginator.ListKnowledgeBases)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listknowledgebasespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListKnowledgeBasesRequestListKnowledgeBasesPaginateTypeDef]
+    ) -> AsyncIterator[ListKnowledgeBasesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListKnowledgeBases.html#AgentsforBedrock.Paginator.ListKnowledgeBases.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listknowledgebasespaginator)
+        """
+
+
+class ListPromptsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListPrompts.html#AgentsforBedrock.Paginator.ListPrompts)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listpromptspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListPromptsRequestListPromptsPaginateTypeDef]
+    ) -> AsyncIterator[ListPromptsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent/paginator/ListPrompts.html#AgentsforBedrock.Paginator.ListPrompts.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock_agent/paginators/#listpromptspaginator)
+        """
