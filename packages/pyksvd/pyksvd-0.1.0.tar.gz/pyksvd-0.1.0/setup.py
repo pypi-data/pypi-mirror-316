007cabe2f2b014058c8f5e65844ee48f53f7335f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:12:02 +0000

    Add package pyksvd/0.1.0/pyksvd-0.1.0.tar.gz

---
+++ packages/pyksvd/pyksvd-0.1.0.tar.gz/pyksvd-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyksvd',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Mathias Grau and Antoine Martinez',
+    author_email='mathias4grau@gmail.com',
+    description='A Python implementation of the K-SVD algorithm',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mathias-grau/PyKSVD',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
