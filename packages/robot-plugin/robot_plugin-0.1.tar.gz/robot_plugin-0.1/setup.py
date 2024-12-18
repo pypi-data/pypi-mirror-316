498a469e5d9f844c26edafa8d9fe5052766a72d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:00 +0000

    Add package robot-plugin/0.1/robot_plugin-0.1.tar.gz

---
+++ packages/robot-plugin/robot_plugin-0.1.tar.gz/robot_plugin-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robot_plugin',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author='NightMing',
+    author_email='961880295@qq.com',
+    description='机器人插件依赖包'
+)
