04d4a74d97d59887a00637dbf16e406098fd0135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 16:03:58 +0000

    Add package chunking-pandas/0.1.5/chunking_pandas-0.1.5-py3-none-any.whl

---
+++ packages/chunking-pandas/chunking_pandas-0.1.5-py3-none-any.whl/chunking_pandas/__init__.py
@@ -0,0 +1,5 @@
+from .core import ChunkingExperiment, ChunkingStrategy, FileFormat
+from .utils.logging import setup_logging
+
+__version__ = "0.1.4"
+__all__ = ["ChunkingExperiment", "ChunkingStrategy", "FileFormat", "setup_logging"] 
