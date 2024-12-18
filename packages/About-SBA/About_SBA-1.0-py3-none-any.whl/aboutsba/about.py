001720177b3a7dab9557869f5e7ed505e727ae28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:04:09 +0000

    Add package About-SBA/1.0/About_SBA-1.0-py3-none-any.whl

---
+++ packages/About-SBA/About_SBA-1.0-py3-none-any.whl/aboutsba/about.py
@@ -0,0 +1,6 @@
+def aboutus():
+
+    print(" SBA Info Solutions Private Limited is a Chennai-based automation company with over 30 years of experience in delivering enterprise solutions. We strive to empower professionals by advancing innovative solutions that seamlessly integrate Data & AI, IT & Business Automation, Security & Sustainability, and Open hybrid cloud technologies.")
+
+def founded():
+    print("1996")
