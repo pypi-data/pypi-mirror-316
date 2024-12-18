9d8537ed96b5b4dc3f5284bf03edee98dc2ace31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:04:05 +0000

    Add package types-aiobotocore-billing/2.16.0/types_aiobotocore_billing-2.16.0.tar.gz

---
+++ packages/types-aiobotocore-billing/types_aiobotocore_billing-2.16.0.tar.gz/types_aiobotocore_billing-2.16.0/types_aiobotocore_billing/client.py
@@ -0,0 +1,125 @@
+"""
+Type annotations for billing service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_billing.client import BillingClient
+
+    session = get_session()
+    async with session.create_client("billing") as client:
+        client: BillingClient
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .paginator import ListBillingViewsPaginator
+from .type_defs import ListBillingViewsRequestRequestTypeDef, ListBillingViewsResponseTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+
+__all__ = ("BillingClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class BillingClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing.html#Billing.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        BillingClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing.html#Billing.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/#generate_presigned_url)
+        """
+
+    async def close(self) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/close.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/#close)
+        """
+
+    async def list_billing_views(
+        self, **kwargs: Unpack[ListBillingViewsRequestRequestTypeDef]
+    ) -> ListBillingViewsResponseTypeDef:
+        """
+        Lists the billing views available for a given time period.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/list_billing_views.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/#list_billing_views)
+        """
+
+    def get_paginator(
+        self, operation_name: Literal["list_billing_views"]
+    ) -> ListBillingViewsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> "BillingClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing.html#Billing.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing.html#Billing.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_billing/client/)
+        """
