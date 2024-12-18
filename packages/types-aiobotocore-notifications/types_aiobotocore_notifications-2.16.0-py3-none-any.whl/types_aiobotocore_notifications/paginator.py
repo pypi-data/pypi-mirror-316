a0afac9a313cb1d171c569fe10869c29538f0641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 15:03:43 +0000

    Add package types-aiobotocore-notifications/2.16.0/types_aiobotocore_notifications-2.16.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-notifications/types_aiobotocore_notifications-2.16.0-py3-none-any.whl/types_aiobotocore_notifications/paginator.py
@@ -0,0 +1,153 @@
+"""
+Type annotations for notifications service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_notifications.client import UserNotificationsClient
+    from types_aiobotocore_notifications.paginator import (
+        ListChannelsPaginator,
+        ListEventRulesPaginator,
+        ListNotificationConfigurationsPaginator,
+        ListNotificationEventsPaginator,
+        ListNotificationHubsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("notifications") as client:
+        client: UserNotificationsClient
+
+        list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+        list_event_rules_paginator: ListEventRulesPaginator = client.get_paginator("list_event_rules")
+        list_notification_configurations_paginator: ListNotificationConfigurationsPaginator = client.get_paginator("list_notification_configurations")
+        list_notification_events_paginator: ListNotificationEventsPaginator = client.get_paginator("list_notification_events")
+        list_notification_hubs_paginator: ListNotificationHubsPaginator = client.get_paginator("list_notification_hubs")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListChannelsRequestListChannelsPaginateTypeDef,
+    ListChannelsResponseTypeDef,
+    ListEventRulesRequestListEventRulesPaginateTypeDef,
+    ListEventRulesResponseTypeDef,
+    ListNotificationConfigurationsRequestListNotificationConfigurationsPaginateTypeDef,
+    ListNotificationConfigurationsResponseTypeDef,
+    ListNotificationEventsRequestListNotificationEventsPaginateTypeDef,
+    ListNotificationEventsResponseTypeDef,
+    ListNotificationHubsRequestListNotificationHubsPaginateTypeDef,
+    ListNotificationHubsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "ListChannelsPaginator",
+    "ListEventRulesPaginator",
+    "ListNotificationConfigurationsPaginator",
+    "ListNotificationEventsPaginator",
+    "ListNotificationHubsPaginator",
+)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListChannelsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListChannels.html#UserNotifications.Paginator.ListChannels)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listchannelspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListChannelsRequestListChannelsPaginateTypeDef]
+    ) -> AsyncIterator[ListChannelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListChannels.html#UserNotifications.Paginator.ListChannels.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listchannelspaginator)
+        """
+
+
+class ListEventRulesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListEventRules.html#UserNotifications.Paginator.ListEventRules)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listeventrulespaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListEventRulesRequestListEventRulesPaginateTypeDef]
+    ) -> AsyncIterator[ListEventRulesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListEventRules.html#UserNotifications.Paginator.ListEventRules.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listeventrulespaginator)
+        """
+
+
+class ListNotificationConfigurationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListNotificationConfigurations.html#UserNotifications.Paginator.ListNotificationConfigurations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listnotificationconfigurationspaginator)
+    """
+
+    def paginate(
+        self,
+        **kwargs: Unpack[
+            ListNotificationConfigurationsRequestListNotificationConfigurationsPaginateTypeDef
+        ],
+    ) -> AsyncIterator[ListNotificationConfigurationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListNotificationConfigurations.html#UserNotifications.Paginator.ListNotificationConfigurations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listnotificationconfigurationspaginator)
+        """
+
+
+class ListNotificationEventsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListNotificationEvents.html#UserNotifications.Paginator.ListNotificationEvents)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listnotificationeventspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListNotificationEventsRequestListNotificationEventsPaginateTypeDef]
+    ) -> AsyncIterator[ListNotificationEventsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListNotificationEvents.html#UserNotifications.Paginator.ListNotificationEvents.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listnotificationeventspaginator)
+        """
+
+
+class ListNotificationHubsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListNotificationHubs.html#UserNotifications.Paginator.ListNotificationHubs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listnotificationhubspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListNotificationHubsRequestListNotificationHubsPaginateTypeDef]
+    ) -> AsyncIterator[ListNotificationHubsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/notifications/paginator/ListNotificationHubs.html#UserNotifications.Paginator.ListNotificationHubs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_notifications/paginators/#listnotificationhubspaginator)
+        """
