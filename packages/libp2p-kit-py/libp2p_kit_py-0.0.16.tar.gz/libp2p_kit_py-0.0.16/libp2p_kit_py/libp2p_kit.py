3b63c1d9e8d740d92ca45e0a8c76aca42727baef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 17:03:41 +0000

    Add package libp2p-kit-py/0.0.16/libp2p_kit_py-0.0.16.tar.gz

---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.16.tar.gz/libp2p_kit_py-0.0.16/libp2p_kit_py/libp2p_kit.py
@@ -0,0 +1,28 @@
+
+# try:
+#     from aria2_kit import aria2_kit
+# except:    
+#     from .aria2_kit import aria2_kit
+# try:
+#     from s3_kit import s3_kit
+# except:
+#     from .s3_kit import s3_kit    
+# try:
+#     from websocket_kit import websocket_kit
+# except:
+#     from .websocket_kit import websocket_kit
+
+class libp2p_kit:
+    def __init__(self, resources=None, metadata=None):
+        # self.libp2p_kit = libp2p_kit(resources, metadata)
+        # self.aria2_kit = aria2_kit(resources, metadata)
+        # self.s3_kit = s3_kit(resources, metadata)
+        # self.websocket_kit = websocket_kit(resources, metadata)
+        # self.websocket_kit_lib = websocket_kit_lib(resources, metadata)
+        pass
+    
+    def run(self):
+
+        return True
+    
+    
