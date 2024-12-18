fad7915eec6885e97f876801daff30861a4fb7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 14:04:04 +0000

    Add package types-aiobotocore-dsql/2.16.0/types_aiobotocore_dsql-2.16.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-dsql/types_aiobotocore_dsql-2.16.0-py3-none-any.whl/types_aiobotocore_dsql/client.pyi
@@ -0,0 +1,250 @@
+"""
+Type annotations for dsql service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_dsql.client import AuroraDSQLClient
+
+    session = get_session()
+    async with session.create_client("dsql") as client:
+        client: AuroraDSQLClient
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .paginator import ListClustersPaginator
+from .type_defs import (
+    CreateClusterInputRequestTypeDef,
+    CreateClusterOutputTypeDef,
+    CreateMultiRegionClustersInputRequestTypeDef,
+    CreateMultiRegionClustersOutputTypeDef,
+    DeleteClusterInputRequestTypeDef,
+    DeleteClusterOutputTypeDef,
+    DeleteMultiRegionClustersInputRequestTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetClusterInputRequestTypeDef,
+    GetClusterOutputTypeDef,
+    ListClustersInputRequestTypeDef,
+    ListClustersOutputTypeDef,
+    ListTagsForResourceInputRequestTypeDef,
+    ListTagsForResourceOutputTypeDef,
+    TagResourceInputRequestTypeDef,
+    UntagResourceInputRequestTypeDef,
+    UpdateClusterInputRequestTypeDef,
+    UpdateClusterOutputTypeDef,
+)
+from .waiter import ClusterActiveWaiter, ClusterNotExistsWaiter
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+__all__ = ("AuroraDSQLClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+class AuroraDSQLClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql.html#AuroraDSQL.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        AuroraDSQLClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql.html#AuroraDSQL.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#generate_presigned_url)
+        """
+
+    async def close(self) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/close.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#close)
+        """
+
+    async def create_cluster(
+        self, **kwargs: Unpack[CreateClusterInputRequestTypeDef]
+    ) -> CreateClusterOutputTypeDef:
+        """
+        Creates a cluster in Amazon Aurora DSQL.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/create_cluster.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#create_cluster)
+        """
+
+    async def create_multi_region_clusters(
+        self, **kwargs: Unpack[CreateMultiRegionClustersInputRequestTypeDef]
+    ) -> CreateMultiRegionClustersOutputTypeDef:
+        """
+        Creates multi-Region clusters in Amazon Aurora DSQL.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/create_multi_region_clusters.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#create_multi_region_clusters)
+        """
+
+    async def delete_cluster(
+        self, **kwargs: Unpack[DeleteClusterInputRequestTypeDef]
+    ) -> DeleteClusterOutputTypeDef:
+        """
+        Deletes a cluster in Amazon Aurora DSQL.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/delete_cluster.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#delete_cluster)
+        """
+
+    async def delete_multi_region_clusters(
+        self, **kwargs: Unpack[DeleteMultiRegionClustersInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Deletes a multi-Region cluster in Amazon Aurora DSQL.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/delete_multi_region_clusters.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#delete_multi_region_clusters)
+        """
+
+    async def get_cluster(
+        self, **kwargs: Unpack[GetClusterInputRequestTypeDef]
+    ) -> GetClusterOutputTypeDef:
+        """
+        Retrieves information about a cluster.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/get_cluster.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#get_cluster)
+        """
+
+    async def list_clusters(
+        self, **kwargs: Unpack[ListClustersInputRequestTypeDef]
+    ) -> ListClustersOutputTypeDef:
+        """
+        Retrieves information about a list of clusters.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/list_clusters.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#list_clusters)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceInputRequestTypeDef]
+    ) -> ListTagsForResourceOutputTypeDef:
+        """
+        Lists all of the tags for a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Tags a resource with a map of key and value pairs.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#tag_resource)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceInputRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Removes a tag from a resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#untag_resource)
+        """
+
+    async def update_cluster(
+        self, **kwargs: Unpack[UpdateClusterInputRequestTypeDef]
+    ) -> UpdateClusterOutputTypeDef:
+        """
+        Updates a cluster.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/update_cluster.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#update_cluster)
+        """
+
+    def get_paginator(self, operation_name: Literal["list_clusters"]) -> ListClustersPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#get_paginator)
+        """
+
+    @overload
+    def get_waiter(self, waiter_name: Literal["cluster_active"]) -> ClusterActiveWaiter:
+        """
+        Returns an object that can wait for some condition.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/get_waiter.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#get_waiter)
+        """
+
+    @overload
+    def get_waiter(self, waiter_name: Literal["cluster_not_exists"]) -> ClusterNotExistsWaiter:
+        """
+        Returns an object that can wait for some condition.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/client/get_waiter.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/#get_waiter)
+        """
+
+    async def __aenter__(self) -> "AuroraDSQLClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql.html#AuroraDSQL.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql.html#AuroraDSQL.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/client/)
+        """
---
+++ packages/types-aiobotocore-dsql/types_aiobotocore_dsql-2.16.0-py3-none-any.whl/types_aiobotocore_dsql/paginator.py
@@ -0,0 +1,65 @@
+"""
+Type annotations for dsql service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_dsql.client import AuroraDSQLClient
+    from types_aiobotocore_dsql.paginator import (
+        ListClustersPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("dsql") as client:
+        client: AuroraDSQLClient
+
+        list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import ListClustersInputListClustersPaginateTypeDef, ListClustersOutputTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListClustersPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListClustersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/paginator/ListClusters.html#AuroraDSQL.Paginator.ListClusters)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/paginators/#listclusterspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListClustersInputListClustersPaginateTypeDef]
+    ) -> AsyncIterator[ListClustersOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dsql/paginator/ListClusters.html#AuroraDSQL.Paginator.ListClusters.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dsql/paginators/#listclusterspaginator)
+        """
