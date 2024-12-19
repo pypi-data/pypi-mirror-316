6f8560e3567586aa30698d16a14d2ab4b23ddb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:19 +0000

    Add package gba/0.0.0/gba-0.0.0.tar.gz

---
+++ packages/gba/gba-0.0.0.tar.gz/gba-0.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+"""GBApy (Growth Balance Analysis for Python) Python Package.
+
+See:
+https://github.com/charlesrocabert/gbapy
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+	name        = "gba",
+	version     = "0.0.0",
+	author      = "Charles Rocabert",
+    description = "Coming soon",
+)
+
