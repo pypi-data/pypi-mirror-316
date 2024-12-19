58eab15e4b4a16a592c45b9f09e0d008db34bda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:05:34 +0000

    Add package math-spec-mapping/0.3.19/math_spec_mapping-0.3.19.tar.gz

---
+++ packages/math-spec-mapping/math_spec_mapping-0.3.19.tar.gz/math_spec_mapping-0.3.19/src/math_spec_mapping/Classes/Type.py
@@ -0,0 +1,9 @@
+class Type:
+
+    def __init__(self, data):
+        self.name = data["name"]
+        self.type = data["type"]
+        self.type_name = data["type_name"]
+        self.notes = data["notes"]
+        self.metadata = data["metadata"]
+        self.original_type_name = data["original_type_name"]
