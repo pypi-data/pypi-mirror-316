93e333394c8328e6f8d9bdb42858b4d9167b5dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:25 +0000

    Add package mag-tools/0.1.49/mag_tools-0.1.49.tar.gz

---
+++ packages/mag-tools/mag_tools-0.1.49.tar.gz/mag_tools-0.1.49/mag_tools/bean/common/base_test.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from mag_tools.model.common.log_type import LogType
+
+from mag_tools.log.logger import Logger
+from mag_tools.model.test.test_component_type import TestComponentType
+
+
+class BaseTest:
+    def __init__(self, name:str, index:Optional[int]=None, test_component_type:Optional[TestComponentType] = None, description:Optional[str]=None):
+        self._name = name
+        self._index = index
+        self._test_component_type = test_component_type
+        self._description = description
+
+    def start(self, driver):
+        self._report()
+        return driver
+
+    def _report(self):
+        Logger.info(LogType.FRAME, f"开始执行{self._test_component_type.desc}({self._name})")
