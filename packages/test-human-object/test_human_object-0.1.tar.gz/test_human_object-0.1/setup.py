ce17ccaf406fa3d8542bc78467c127dff7f23b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:10 +0000

    Add package test-human-object/0.1/test_human_object-0.1.tar.gz

---
+++ packages/test-human-object/test_human_object-0.1.tar.gz/test_human_object-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_human_object",  # Replace with your package name
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Any dependencies your package needs (e.g., ['numpy'])
+    long_description_content_type="text/markdown",  # Use markdown for your README
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    url="https://github.com/yourusername/my_package",  # Your project URL
+    classifiers=[  # These help people discover your package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Specify the license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Minimum Python version requirement
+)
