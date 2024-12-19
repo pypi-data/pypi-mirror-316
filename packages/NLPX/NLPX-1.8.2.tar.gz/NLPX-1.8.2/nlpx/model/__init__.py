fb44d69b8c0494d712fadaec53af4cb02ae841aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:35 +0000

    Add package NLPX/1.8.2/NLPX-1.8.2.tar.gz

---
+++ packages/NLPX/NLPX-1.8.2.tar.gz/NLPX-1.8.2/nlpx/model/__init__.py
@@ -0,0 +1,27 @@
+from torch_model_hub.model import TextCNN, RotaryAttention, attention, ClassSelfAttention, MultiHeadClassSelfAttention, \
+	RotaryClassSelfAttention, RNNAttention, CNNRNNAttention, RNNCNNAttention, ResRNNCNNAttention, \
+	TransformerEncoder, TransformerDecoder, Transformer2D, TransformerEncoder2D, TransformerDecoder2D, \
+    TextCNN2D, RNNAttention2D, CNNRNNAttention2D, RNNCNNAttention2D, ResRNNCNNAttention2D
+
+
+__all__ = [
+	"TextCNN",
+	"RotaryAttention",
+	"attention",
+	"ClassSelfAttention",
+	"MultiHeadClassSelfAttention",
+	"RotaryClassSelfAttention",
+	"RNNAttention",
+	"CNNRNNAttention",
+	"RNNCNNAttention",
+    "TransformerEncoder",
+    "TransformerDecoder",
+    "Transformer2D",
+    "TransformerEncoder2D",
+    "TransformerDecoder2D",
+    "RNNAttention2D",
+    "CNNRNNAttention2D",
+    "RNNCNNAttention2D",
+    "ResRNNCNNAttention2D"
+    "TextCNN2D"
+]
