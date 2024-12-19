fb0ba47ccd842ee19acdfe11439e9e42dd686781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:39 +0000

    Add package testhwsdk/0.1/testhwsdk-0.1.tar.gz

---
+++ packages/testhwsdk/testhwsdk-0.1.tar.gz/testhwsdk-0.1/README.md
@@ -0,0 +1,8 @@
+# Hello World SDK
+
+A simple SDK that prints "Hello, World!".
+
+## Installation
+
+```bash
+pip install .
---
+++ packages/testhwsdk/testhwsdk-0.1.tar.gz/testhwsdk-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testhwsdk',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple Hello World SDK',
+    author='miguel yannis',
+    author_email='ymanyama@quantum-soft.ai',
+    url='https://github.com/yourusername/hello_world_sdk',
+)
