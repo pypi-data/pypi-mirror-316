08204f644efb726a5760f1003b897a5155bcf6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:40 +0000

    Add package cloudoll/2.2.38/cloudoll-2.2.38-py2.py3-none-any.whl

---
+++ packages/cloudoll/cloudoll-2.2.38-py2.py3-none-any.whl/cloudoll/utils/common.py
@@ -0,0 +1,29 @@
+class Object(dict):
+    # __setattr__ = dict.__setitem__
+    # __getattr__ = dict.__getitem__
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, key):
+        return self.get(key, None)
+    
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError(f"'DotDict' object has no attribute '{key}'")
+    
+    def __getstate__(self):
+        '''to fix pickle.dumps '''
+        return dict(self)
+
+    def __setstate__(self, state):
+        self.update(state)
+
+def chainMap(*dicts):
+    merged_dict = Object()
+    for d in dicts:
+        for key, value in d.items():
+            if key not in merged_dict or value is not None:
+                merged_dict[key] = value
+    return merged_dict
