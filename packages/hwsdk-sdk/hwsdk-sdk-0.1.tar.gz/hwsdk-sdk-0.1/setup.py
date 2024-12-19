06df44f862bb062a50edd096c13babdcd8af6ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:15 +0000

    Add package hwsdk-sdk/0.1/hwsdk-sdk-0.1.tar.gz

---
+++ packages/hwsdk-sdk/hwsdk-sdk-0.1.tar.gz/hwsdk-sdk-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hwsdk-sdk',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple Hello World SDK',
+    author='miguel yannis',
+    author_email='ymanyama@quantum-soft.ai',
+    url='https://github.com/yourusername/hello_world_sdk',
+)
