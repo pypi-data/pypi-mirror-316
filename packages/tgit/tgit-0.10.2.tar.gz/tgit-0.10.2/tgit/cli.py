fea14c8b4fd9b47ef4383ffdbe6476c7a47f1d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:03:40 +0000

    Add package tgit/0.10.2/tgit-0.10.2.tar.gz

---
+++ packages/tgit/tgit-0.10.2.tar.gz/tgit-0.10.2/tgit/cli.py
@@ -0,0 +1,45 @@
+import argparse
+import importlib.metadata
+
+import rich
+import rich.traceback
+
+from tgit.add import define_add_parser
+from tgit.changelog import define_changelog_parser
+from tgit.commit import define_commit_parser
+from tgit.utils import console
+from tgit.version import define_version_parser
+
+rich.traceback.install()
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="TGIT cli",
+        prog="tgit",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("--version", action="store_true", help="show version")
+    subparsers = parser.add_subparsers(title="Subcommands")
+
+    define_commit_parser(subparsers)
+    define_version_parser(subparsers)
+    define_changelog_parser(subparsers)
+    define_add_parser(subparsers)
+
+    args = parser.parse_args()
+    handle(parser, args)
+
+
+def handle(parser: argparse.ArgumentParser, args: argparse.Namespace) -> None:
+    if hasattr(args, "func"):
+        args.func(args)
+    elif args.version:
+        version = importlib.metadata.version("tgit")
+        console.print(f"TGIT - ver.{version}", highlight=False)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
