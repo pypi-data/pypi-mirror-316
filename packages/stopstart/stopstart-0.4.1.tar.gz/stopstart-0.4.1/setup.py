c9fbd16a49aed28206ec8ac2c638fd336c381a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:04:43 +0000

    Add package stopstart/0.4.1/stopstart-0.4.1.tar.gz

---
+++ packages/stopstart/stopstart-0.4.1.tar.gz/stopstart-0.4.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stopstart",
+    version="0.4.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Dan",
+    author_email="break.a.wish@gmail.com",
+    description="A simple and intuitive Python class for tracking elapsed time across multiple sessions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ReverieTides/stopstart",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: BSD 2-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
