593ae4f78595650c332c661b30ada59dec2b8dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:23 +0000

    Add package cxcsvtopandas/0.1.0/cxcsvtopandas-0.1.0.tar.gz

---
+++ packages/cxcsvtopandas/cxcsvtopandas-0.1.0.tar.gz/cxcsvtopandas-0.1.0/README.md
@@ -0,0 +1,17 @@
+# cxpandascsvloader
+A simple example package for loading and processing CSV data into Pandas DataFrames.
+
+## Installation
+You can install the `cxpandascsvloader` package using pip:
+
+```sh
+pip install cxpandascsvloader
+```
+
+## Usage
+```sh
+import cxpandascsvloader.dataframeloader as dfl
+dfl.printinfo()
+## output: dataframeloader module loaded
+
+
---
+++ packages/cxcsvtopandas/cxcsvtopandas-0.1.0.tar.gz/cxcsvtopandas-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cxcsvtopandas",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Amit Gupta",
+    author_email="amitgupta@example.com",
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/amitgupta7/my_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          "pandas==2.2.2"
+          ,"plotly==5.22.0"
+    ],
+    python_requires='>=3.6',
+)
