b6c0bb12d588f994e5f1ba96983e4ff3d8698957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:03:55 +0000

    Add package Noema/1.1.0/noema-1.1.0.tar.gz

---
+++ packages/Noema/noema-1.1.0.tar.gz/noema-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Lire le contenu du README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Noema',
+    version='1.1.0',
+    description='Description of Noema',
+    long_description=long_description,  # Inclure la description longue
+    long_description_content_type='text/markdown',  # Spécifiez le format de la description (markdown ou rst)
+    author='Alban Perli',
+    author_email='alban.perli@gmail.com',
+    url='https://github.com/AlbanPerli/Noema-Declarative-AI',
+    packages=find_packages(),
+    install_requires=[
+        'guidance',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
