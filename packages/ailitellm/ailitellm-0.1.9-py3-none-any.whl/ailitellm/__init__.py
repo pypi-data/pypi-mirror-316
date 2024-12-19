314cd737dd7c7badb34657bad0766434e10b1749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:42 +0000

    Add package ailitellm/0.1.9/ailitellm-0.1.9-py3-none-any.whl

---
+++ packages/ailitellm/ailitellm-0.1.9-py3-none-any.whl/ailitellm/__init__.py
@@ -0,0 +1,7 @@
+from ._serve_models import OpenAI,ollama_serve
+from ._main import ai,ailite_model,AILite,HFModelType
+from ._unified_ai_api_server import start_chat_server
+from ._ai_operations import streamai,yieldai
+
+#tools
+from .tools import GitRepoIngestor
