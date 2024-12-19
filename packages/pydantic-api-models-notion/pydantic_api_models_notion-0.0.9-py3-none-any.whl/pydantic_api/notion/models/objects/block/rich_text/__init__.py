502446c7af66c3e352204b1fd9ea8cb9ecf3e03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:13 +0000

    Add package pydantic-api-models-notion/0.0.9/pydantic_api_models_notion-0.0.9-py3-none-any.whl

---
+++ packages/pydantic-api-models-notion/pydantic_api_models_notion-0.0.9-py3-none-any.whl/pydantic_api/notion/models/objects/block/rich_text/__init__.py
@@ -0,0 +1,64 @@
+"""
+Reference: https://developers.notion.com/reference/rich-text
+"""
+
+from .base import TextAnnotationColorLiteral, TextAnnotations, RichTextTypeLiteral
+from .text import TextObject, TextRichTextObject
+from .equation import EquationObject, EquationRichTextObject
+from .mention import (
+    MentionTypeLiteral,
+    DatabaseMentionBody,
+    DatabaseMentionObject,
+    DateMentionBody,
+    DateMentionObject,
+    LinkPreviewMentionBody,
+    LinkPreviewMentionObject,
+    PageMentionBody,
+    PageMentionObject,
+    TemplateMentionBody,
+    TemplateMentionObject,
+    TemplateMentionTypeLiteral,
+    TemplateMentionDateValueLiteral,
+    TemplateMentionUserValueLiteral,
+    UserMentionBody,
+    UserMentionObject,
+    MentionObject,
+    MentionRichTextObject,
+)
+from .rich_text import RichTextObject, RichTextObjectFactory
+
+
+__all__ = [
+    # Base
+    "TextAnnotationColorLiteral",
+    "TextAnnotations",
+    "RichTextTypeLiteral",
+    # Text
+    "TextObject",
+    "TextRichTextObject",
+    # Equation
+    "EquationObject",
+    "EquationRichTextObject",
+    # Mention
+    "MentionTypeLiteral",
+    "DatabaseMentionBody",
+    "DatabaseMentionObject",
+    "DateMentionBody",
+    "DateMentionObject",
+    "LinkPreviewMentionBody",
+    "LinkPreviewMentionObject",
+    "PageMentionBody",
+    "PageMentionObject",
+    "TemplateMentionBody",
+    "TemplateMentionObject",
+    "TemplateMentionTypeLiteral",
+    "TemplateMentionDateValueLiteral",
+    "TemplateMentionUserValueLiteral",
+    "UserMentionBody",
+    "UserMentionObject",
+    "MentionObject",
+    "MentionRichTextObject",
+    # Rich Text
+    "RichTextObject",
+    "RichTextObjectFactory",
+]
