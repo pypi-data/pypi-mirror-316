65697c5fc26b326963f9499310b900e1862237fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:43 +0000

    Add package servitorconnectqr/1.0.0/servitorconnectqr-1.0.0.tar.gz

---
+++ packages/servitorconnectqr/servitorconnectqr-1.0.0.tar.gz/servitorconnectqr-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="servitorconnectqr",
+    version="1.0.0",
+    author="AnthroHeart (Thomas Sweet)",
+    author_email="healing@intentionrepeater.com",
+    description="A tool to use Universe Sourcecode with an intention to generate a QR Code.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tsweet77/servitorconnectqr",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPLv3",
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "servitorconnectqr=servitorconnectqr.cli:main",
+        ],
+    },
+)
