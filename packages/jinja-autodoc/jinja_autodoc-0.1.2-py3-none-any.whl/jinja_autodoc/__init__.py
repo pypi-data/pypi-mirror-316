e0a2593a10e23cf304467d01f188ea5f5d49625d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:15 +0000

    Add package jinja-autodoc/0.1.2/jinja_autodoc-0.1.2-py3-none-any.whl

---
+++ packages/jinja-autodoc/jinja_autodoc-0.1.2-py3-none-any.whl/jinja_autodoc/__init__.py
@@ -0,0 +1,15 @@
+from sphinx.application import Sphinx
+from sphinx.util.typing import ExtensionMetadata
+
+from .autotemplate import AutojinjaDirective
+from .domain import JinjaDomain
+
+
+def setup(app: Sphinx) -> ExtensionMetadata:
+    app.add_domain(JinjaDomain)
+
+    app.add_directive("autotemplate", AutojinjaDirective)
+    app.add_config_value("jinja_template_path", "", "")
+    app.add_config_value("jinja_template_pattern", "", "")
+
+    return {}
