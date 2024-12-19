74c1bf40f97eb481cfd5f595a78142ce027a7ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:04:48 +0000

    Add package TELUS_NFV_Taranis/0.0.4/telus_nfv_taranis-0.0.4.tar.gz

---
+++ packages/TELUS_NFV_Taranis/telus_nfv_taranis-0.0.4.tar.gz/telus_nfv_taranis-0.0.4/src/TELUS_NFV_Taranis/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module provides types used for NATS messages.
+
+It allows for python NATS clients to validate the messages they send and receive.
+"""
+
+from .message_schemas import Profile, VlanType, L2VNLogical, L2VNCreateRequest
+
+__version__ = "0.0.4"
