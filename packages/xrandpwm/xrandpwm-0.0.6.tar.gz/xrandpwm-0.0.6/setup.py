d687686cb00ca09d69cc79628a5ebb096919ad0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:04:37 +0000

    Add package xrandpwm/0.0.6/xrandpwm-0.0.6.tar.gz

---
+++ packages/xrandpwm/xrandpwm-0.0.6.tar.gz/xrandpwm-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xrandpwm",
+    version="0.0.6",
+    packages=find_packages(),
+    description="xrandr screen/monitors info collector classes (intended for bspwm)",
+    author="kokaito",
+    author_email="kokaito.git@gmail.com",
+    url="https://github.com/kokaito-git/xrandpwm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        "pydantic",
+        "kcolors",
+    ],
+)
