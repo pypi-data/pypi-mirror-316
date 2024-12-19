e80e8783a925e321c98f3b1ae6728bef7cb2129f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:14:08 +0000

    Add package pypsbuilder/2.5.3/pypsbuilder-2.5.3-py3-none-any.whl

---
+++ packages/pypsbuilder/pypsbuilder-2.5.3-py3-none-any.whl/pypsbuilder/ui/compile_ui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pyuic5 ptbuilder.ui > ../ui_ptbuilder.py
+pyuic5 txbuilder.ui > ../ui_txbuilder.py
+pyuic5 pxbuilder.ui > ../ui_pxbuilder.py
+pyuic5 adduni.ui > ../ui_adduni.py
+pyuic5 addinv.ui > ../ui_addinv.py
+pyuic5 uniguess.ui > ../ui_uniguess.py
+
