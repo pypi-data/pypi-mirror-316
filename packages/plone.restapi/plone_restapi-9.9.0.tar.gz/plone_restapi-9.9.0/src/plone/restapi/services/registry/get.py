6bae7786b6ea267ee3d42361408771b3a0d61ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 07:05:09 +0000

    Add package plone.restapi/9.9.0/plone_restapi-9.9.0.tar.gz

---
+++ packages/plone.restapi/plone_restapi-9.9.0.tar.gz/plone_restapi-9.9.0/src/plone/restapi/services/registry/get.py
@@ -0,0 +1,39 @@
+from plone.registry.interfaces import IRegistry
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.services import Service
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+
+
+@implementer(IPublishTraverse)
+class RegistryGet(Service):
+    def __init__(self, context, request):
+        super().__init__(context, request)
+        self.params = []
+
+    def publishTraverse(self, request, name):
+        # Consume any path segments after /@registry as parameters
+        self.params.append(name)
+        return self
+
+    @property
+    def _get_record_name(self):
+        if len(self.params) != 1:
+            raise Exception(
+                "Must supply exactly one parameter (dotted name of"
+                "the record to be retrieved)"
+            )
+
+        return self.params[0]
+
+    def reply(self):
+        registry = getUtility(IRegistry)
+        if self.params:
+            value = registry[self._get_record_name]
+            return json_compatible(value)
+        else:  # batched listing
+            serializer = getMultiAdapter((registry, self.request), ISerializeToJson)
+            return serializer()
