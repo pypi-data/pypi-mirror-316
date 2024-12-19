89a71ba83b32038f429bd74fe53a8d68e60869c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:15 +0000

    Add package lumigator-sdk/0.0.2/lumigator_sdk-0.0.2.tar.gz

---
+++ packages/lumigator-sdk/lumigator_sdk-0.0.2.tar.gz/lumigator_sdk-0.0.2/tests/integration/test_scenarios.py
@@ -0,0 +1,90 @@
+"""Integration tests for the SDK. The lumigator backend needs to
+be started prior to running these tests using
+`make start-lumigator-build`.
+"""
+
+from pathlib import Path
+from time import sleep
+
+import requests
+from loguru import logger
+from lumigator_schemas.datasets import DatasetFormat
+from lumigator_schemas.jobs import JobType
+from lumigator_sdk.strict_schemas import JobEvalCreate
+
+
+def test_sdk_healthcheck_ok(lumi_client_int):
+    """Test the healthcheck endpoint."""
+    healthy = False
+    for i in range(10):
+        try:
+            lumi_client_int.health.healthcheck()
+            healthy = True
+            break
+        except Exception as e:
+            print(f"failed health check, retry {i} - due to {e}")
+            sleep(1)
+    assert healthy
+
+
+def test_get_datasets_remote_ok(lumi_client_int):
+    """Test the `get_datasets` endpoint."""
+    datasets = lumi_client_int.datasets.get_datasets()
+    assert datasets is not None
+
+
+def test_dataset_lifecycle_remote_ok(lumi_client_int, dialog_data):
+    """Test a complete dataset lifecycle test: add a new dataset,
+    list datasets, remove the dataset
+    """
+    datasets = lumi_client_int.datasets.get_datasets()
+    n_initial_datasets = datasets.total
+    lumi_client_int.datasets.create_dataset(dataset=dialog_data, format=DatasetFormat.JOB)
+    datasets = lumi_client_int.datasets.get_datasets()
+    n_current_datasets = datasets.total
+    created_dataset = lumi_client_int.datasets.get_dataset(datasets.items[0].id)
+    assert created_dataset is not None
+    lumi_client_int.datasets.delete_dataset(datasets.items[0].id)
+    datasets = lumi_client_int.datasets.get_datasets()
+    assert n_current_datasets - n_initial_datasets == 1
+
+
+def test_job_lifecycle_remote_ok(lumi_client_int, dialog_data, simple_eval_template):
+    """Test a complete job lifecycle test: add a new dataset,
+    create a new job, run the job, get the results
+    """
+    logger.info("Starting jobs lifecycle")
+    datasets = lumi_client_int.datasets.get_datasets()
+    if datasets.total > 0:
+        for removed_dataset in datasets.items:
+            lumi_client_int.datasets.delete_dataset(removed_dataset.id)
+    datasets = lumi_client_int.datasets.get_datasets()
+    n_initial_datasets = datasets.total
+    dataset = lumi_client_int.datasets.create_dataset(dataset=dialog_data, format=DatasetFormat.JOB)
+    datasets = lumi_client_int.datasets.get_datasets()
+    n_current_datasets = datasets.total
+    assert n_current_datasets - n_initial_datasets == 1
+
+    jobs = lumi_client_int.jobs.get_jobs()
+    assert jobs is not None
+    logger.info(lumi_client_int.datasets.get_dataset(dataset.id))
+
+    job = JobEvalCreate(
+        name="test-job-int-001",
+        model="hf://trl-internal-testing/tiny-random-LlamaForCausalLM",
+        dataset=dataset.id,
+        config_template=simple_eval_template,
+    )
+    logger.info(job)
+    job.description = "This is a test job"
+    job.max_samples = 2
+    job_creation_result = lumi_client_int.jobs.create_job(JobType.EVALUATION, job)
+    assert job_creation_result is not None
+    assert lumi_client_int.jobs.get_jobs() is not None
+
+    job_status = lumi_client_int.jobs.wait_for_job(job_creation_result.id)
+    logger.info(job_status)
+
+    download_info = lumi_client_int.jobs.get_job_download(job_creation_result.id)
+    logger.info(f"getting result from {download_info.download_url}")
+    requests.get(download_info.download_url, allow_redirects=True)
---
+++ packages/lumigator-sdk/lumigator_sdk-0.0.2.tar.gz/lumigator_sdk-0.0.2/tests/unit/test_jobs.py
@@ -0,0 +1,124 @@
+import json
+from http import HTTPStatus
+from pathlib import Path
+
+from lumigator_schemas.jobs import JobType
+from lumigator_sdk.health import Health
+from lumigator_sdk.jobs import Jobs
+from lumigator_sdk.strict_schemas import JobEvalCreate
+from pydantic import ValidationError
+from pytest import raises
+from requests import Response
+from requests.exceptions import HTTPError
+from tests.helpers import load_json
+
+
+def test_create_job_ok_all(
+    lumi_client, json_data_job_response, json_data_job_all, request_mock
+):
+    request_mock.post(
+        url=lumi_client.client._api_url + f"/{Jobs.JOBS_ROUTE}/{JobType.EVALUATION.value}/",
+        status_code=HTTPStatus.OK,
+        json=load_json(json_data_job_response),
+    )
+
+    job_json = load_json(json_data_job_all)
+    job_ret = lumi_client.jobs.create_job(
+        JobType.EVALUATION, JobEvalCreate.model_validate(job_json)
+    )
+    assert job_ret is not None
+    assert str(job_ret.id) == "daab39ac-be9f-4de9-87c0-c4c94b297a97"
+    assert job_ret.name == "test-job-001"
+    assert job_ret.status == "created"
+
+
+def test_create_job_ok_minimal(
+    lumi_client,
+    json_data_job_response,
+    json_data_job_minimal,
+    request_mock
+):
+    request_mock.post(
+        url=lumi_client.client._api_url + f"/{Jobs.JOBS_ROUTE}/{JobType.INFERENCE.value}/",
+        status_code=HTTPStatus.OK,
+        json=load_json(json_data_job_response),
+    )
+
+    job_json = load_json(json_data_job_minimal)
+    job_ret = lumi_client.jobs.create_job(JobType.INFERENCE, JobEvalCreate.model_validate(job_json))
+    assert job_ret is not None
+    assert str(job_ret.id) == "daab39ac-be9f-4de9-87c0-c4c94b297a97"
+    assert job_ret.name == "test-job-001"
+    assert job_ret.status == "created"
+
+
+def test_create_job_ok_extra(
+    lumi_client,
+    json_data_job_response,
+    json_data_job_extra,
+    request_mock
+):
+    request_mock.post(
+        url=lumi_client.client._api_url + f"/{Jobs.JOBS_ROUTE}/{JobType.INFERENCE.value}/",
+        status_code=HTTPStatus.OK,
+        json=load_json(json_data_job_response),
+    )
+
+    job_json = load_json(json_data_job_extra)
+    with raises(ValidationError):
+        lumi_client.jobs.create_job(JobType.INFERENCE, JobEvalCreate.model_validate(job_json))
+
+
+def test_get_jobs_ok(lumi_client, json_data_jobs, request_mock):
+    request_mock.get(
+        url=lumi_client.client._api_url + f"/{Jobs.JOBS_ROUTE}",
+        status_code=HTTPStatus.OK,
+        json=load_json(json_data_jobs),
+    )
+
+    jobs = lumi_client.jobs.get_jobs()
+    assert jobs is not None
+    assert len(jobs.items) == jobs.total
+
+
+def test_get_jobs_none(lumi_client, request_mock):
+    request_mock.get(
+        url=lumi_client.client._api_url + f"/{Jobs.JOBS_ROUTE}",
+        status_code=HTTPStatus.OK,
+        json={"total":0,"items":[]},
+    )
+
+    jobs = lumi_client.jobs.get_jobs()
+    assert jobs is not None
+    assert jobs.items == []
+
+
+def test_get_job_ok(lumi_client, json_data_job, request_mock):
+    job_id = "6f6487ac-7170-4a11-af7a-0f6db1ec9a74"
+    request_mock.get(
+        url=lumi_client.client._api_url + f"/{Health.HEALTH_ROUTE}/{Jobs.JOBS_ROUTE}/{job_id}",
+        status_code=HTTPStatus.OK,
+        json=load_json(json_data_job),
+    )
+
+    job = lumi_client.health.get_job(job_id)
+
+    assert job is not None
+    assert job.type == "SUBMISSION"
+    assert job.status == "PENDING"
+    assert job.submission_id == job_id
+
+
+def test_get_job_id_does_not_exist(lumi_client, request_mock):
+    job_id = "00000000-0000-0000-0000-000000000000"
+    response = Response()
+    response.status_code = HTTPStatus.NOT_FOUND
+    error = HTTPError(response=response)
+    request_mock.get(
+        url=lumi_client.client._api_url + f"/{Health.HEALTH_ROUTE}/{Jobs.JOBS_ROUTE}/{job_id}",
+        exc = error
+    )
+
+    # We expect the SDK to handle the 404 and return None.
+    job = lumi_client.health.get_job(job_id)
+    assert job is None
