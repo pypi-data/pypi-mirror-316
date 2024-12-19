6c3639ce343ec7d1b4d77a53123f7f089016d56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:07 +0000

    Add package snmp-diode/0.0.6/snmp_diode-0.0.6-py3-none-any.whl

---
+++ packages/snmp-diode/snmp_diode-0.0.6-py3-none-any.whl/snmp_diode/discover.py
@@ -0,0 +1,156 @@
+from easysnmp import Session
+import netaddr
+from snmp_diode import models
+from snmp_diode.sysobjectid import manufacturers
+
+
+def gater_device_data(address, snmp_data, role=None, site=None):
+    session_data = {
+        "hostname": address,
+        "use_sprint_value": True,
+        "version": snmp_data["version"],
+        "use_long_names": True,
+        "use_numeric": True,
+    }
+    if snmp_data["version"] == 2:
+        session_data["community"] = snmp_data["version_data"]["community"]
+    elif snmp_data["version"] == 3:
+        session_data["security_level"] = snmp_data["version_data"]["level"]
+        session_data["security_username"] = snmp_data["version_data"]["username"]
+        if snmp_data["version_data"]["level"] == "authNoPriv":
+            session_data["auth_protocol"] = snmp_data["version_data"]["auth_protocol"]
+            session_data["auth_password"] = snmp_data["version_data"]["auth"]
+        elif snmp_data["version_data"]["level"] == "authPriv":
+            session_data["auth_protocol"] = snmp_data["version_data"]["auth_protocol"]
+            session_data["auth_password"] = snmp_data["version_data"]["auth"]
+            session_data["privacy_protocol"] = snmp_data["version_data"]["privacy_protocol"]
+            session_data["privacy_password"] = snmp_data["version_data"]["privacy"]
+        
+    
+    session = Session(**session_data)    
+
+    device_name_oid = session.get(".1.3.6.1.2.1.1.5.0")
+    sysid_oid = session.get(".1.3.6.1.2.1.1.2.0")
+    location_oid = session.get(".1.3.6.1.2.1.1.6.0")
+
+    device_data = {"hostname": device_name_oid.value}
+    manufacturer, device_type = get_device_model(sysid_oid.value)
+
+    interfaces = process_interfaces(session)
+
+    device_data = {
+        "name": device_name_oid.value.replace('"', ""),
+        "manufacturer": manufacturer,
+        "device_type": device_type,
+        "site": location_oid.value.replace('"', ""),
+        "interfaces": interfaces,
+    }
+    if site is not None:
+        device_data["site"] = site
+
+    if role is not None:
+        device_data["role"] = role
+        
+    return models.Device(**device_data)
+
+
+def process_interfaces(session):
+    if_name = session.walk(".1.3.6.1.2.1.2.2.1.2")
+
+    interfaces = {}
+    for item in if_name:
+        iface_name = item.value
+        interface_id = item.oid_index
+        interfaces[interface_id] = {"index": interface_id, "name": iface_name}
+
+    if_mac = session.walk("1.3.6.1.2.1.2.2.1.6")
+
+    for item in if_mac:
+        iface_name = item.value
+        interface_id = item.oid_index
+        interfaces[interface_id]["mac"] = item.value.replace('"', "")[:-1].replace(
+            " ", ":"
+        )
+
+    if_admin = session.walk("1.3.6.1.2.1.2.2.1.7")
+
+
+    for item in if_admin:
+        iface_name = item.value
+        interface_id = item.oid_index
+        enabled = False
+        if item.value == "1":
+            enabled = True
+        interfaces[interface_id]["enabled"] = enabled
+
+    address_items = session.walk(".1.3.6.1.2.1.4.20.1.1")
+
+    addresses = {}
+    for item in address_items:
+        addresses[item.value] = {"address": item.value}
+
+    address_if_items = session.walk(".1.3.6.1.2.1.4.20.1.2")
+
+    for item in address_if_items:
+        full_oid = f"{item.oid}.{item.oid_index}"
+        address = full_oid.replace(".1.3.6.1.2.1.4.20.1.2.", "")
+        if address in addresses:
+            addresses[address]["if_oid"] = item.value
+
+    address_mask_items = session.walk(".1.3.6.1.2.1.4.20.1.3")
+
+    for item in address_mask_items:
+        full_oid = f"{item.oid}.{item.oid_index}"
+        address = full_oid.replace(".1.3.6.1.2.1.4.20.1.3.", "")
+        if address in addresses:
+            addresses[address]["netmask"] = item.value
+
+    for address in addresses:
+        if addresses[address]["if_oid"] in interfaces:
+            ipnet = netaddr.IPNetwork(
+                    f'{addresses[address]["address"]}/{addresses[address]["netmask"]}'
+                )
+            interfaces[addresses[address]["if_oid"]]["address"] = f"{ipnet.ip}/{ipnet.prefixlen}"
+
+    description_items = session.walk(".1.3.6.1.2.1.31.1.1.1.18")
+    
+
+    for item in description_items:
+        interface_id = item.oid_index
+        if interface_id in interfaces:
+            interfaces[interface_id]["description"] = item.value.replace('"', "")
+
+    output = []
+    for interface in interfaces:
+        iface_data = {
+            "name": interfaces[interface]["name"].replace('"', ""),
+            "mac_address": interfaces[interface]["mac"],
+            "enabled": interfaces[interface]["enabled"],
+        }
+        if "description" in interfaces[interface]:
+            iface_data["description"] = interfaces[interface]["description"]
+        else:
+            iface_data["description"] = ""
+
+        if "address" in interfaces[interface]:
+            iface_data["address"] = interfaces[interface]["address"]
+
+        output.append(models.Interface(**iface_data))
+    return output
+
+
+def get_device_model(sysoid):
+    print(sysoid)
+    manufacturer = "unknow"
+    device_type = "unknow"
+    sysoid_list = sysoid.split(".")
+    if sysoid_list[0] == "":
+        sysoid_list = sysoid_list[1:]
+    if len(sysoid_list) > 6:
+        man_id = int(sysoid_list[6])
+        if man_id in manufacturers:
+            manufacturer = manufacturers[man_id]["name"]
+            model_id = int(sysoid_list[-1])
+            if model_id in manufacturers[man_id]["products"]:
+                device_type = manufacturers[man_id]["products"][model_id]
+    return (manufacturer, device_type)
---
+++ packages/snmp-diode/snmp_diode-0.0.6-py3-none-any.whl/snmp_diode/entrypoint.py
@@ -0,0 +1,150 @@
+import traceback
+import argparse
+import netaddr
+import os
+from netboxlabs.diode.sdk import DiodeClient
+from snmp_diode import discover
+
+
+parser = argparse.ArgumentParser(description="SNMP Discovery Tool for NetBoxLabs Diode")
+
+snmpv3_levels = ["noAuthNoPriv", "authNoPriv", "authPriv"]
+snmpv3_auth_protocols = ["MD5", "SHA"] 
+
+parser.add_argument("-t", "--host", type=str, help="Target Host Address", required=False)
+parser.add_argument("-n", "--network", type=str, help="Target Network Address", required=False)
+parser.add_argument("-v", "--version", type=str, help="SNMP Version", required=True)
+parser.add_argument("-c", "--community", type=str, help="SNMP Community String", required=False)
+parser.add_argument("-u", "--username", type=str, help="SNMPv3 Username", required=False)
+parser.add_argument("-a", "--auth", type=str, help="SNMPv3 Auth Password", required=False)
+parser.add_argument("-A", "--auth_protocol", type=str, help="SNMPv3 Auth Protocol", required=False, choices=snmpv3_auth_protocols)
+parser.add_argument("-x", "--privacy", type=str, help="SNMPv3 Privacy Password", required=False)
+parser.add_argument("-X", "--privacy_protocol", type=str, help="SNMPv3 Privacy Protocol", required=False)
+parser.add_argument("-l", "--level", type=str, help="SNMPv3 Security Level", required=False, choices=snmpv3_levels)
+parser.add_argument("-d", "--diode", type=str, help="Diode Server", required=False)
+parser.add_argument("-k", "--api_key", type=str, help="Diode API Key", required=False)
+parser.add_argument( "--apply", action="store_true", default=False, help="Apply the changes to NetBox", required=False,)
+parser.add_argument("-r", "--role" , type=str, help="Role of the device", required=False)
+parser.add_argument("-s", "--site", type=str, help="Site of the device", required=False)
+ 
+
+def main():
+    args = parser.parse_args()
+    versions = ["2", "2c", "3"]
+    if args.host is None and args.network is None:
+        print("Please provide either a target IP address or a target network address")
+        exit(1)
+    if args.host is not None and args.network is not None:
+        print(
+            "Please provide either a target IP address or a target network address, not both"
+        )
+        exit(1)
+    if args.version not in versions:
+        print("Please provide a valid SNMP version, options are: 2, 2c, 3")
+        exit(1)
+    elif args.version == "2" or args.version == "2c":
+        version = 2
+        if args.community is None:
+            print("Please provide an SNMP community string")
+            exit(1)
+    elif args.version == "3":
+        version = 3
+
+    snmp_data = {
+        "version": version,
+    }
+    if version == 2:
+        snmp_data["version_data"] = {"community": args.community}
+    elif version == 3:
+        if args.level is None:
+            print("Please provide an SNMPv3 security level")
+            exit(1)
+        if args.level == "authNoPriv":
+            if args.username is None or args.auth is None or args.auth_protocol is None:
+                print("Please provide a username, auth password, and auth protocol")
+                exit(1)
+            snmp_data["version_data"] = {
+                "level": "authNoPriv",  # "authNoPriv", "authPriv", "noAuthNoPriv
+                "username": args.username,
+                "auth": args.auth,
+                "auth_protocol": args.auth_protocol,
+            }
+        elif args.level == "authPriv":
+            if (
+                args.username is None
+                or args.auth is None
+                or args.auth_protocol is None
+                or args.privacy is None
+                or args.privacy_protocol is None
+            ):
+                print(
+                    "Please provide a username, auth password, auth protocol, privacy password, and privacy protocol"
+                )
+                exit(1)
+            snmp_data["version_data"] = {
+                "level": "authPriv",  # "authNoPriv", "authPriv", "noAuthNoPriv
+                "username": args.username,
+                "auth": args.auth,
+                "auth_protocol": args.auth_protocol,
+                "privacy": args.privacy,
+                "privacy_protocol": args.privacy_protocol,
+            }
+        elif args.level == "noAuthNoPriv":
+            if args.username is None:
+                print("Please provide a username")
+                exit(1)
+            snmp_data["version_data"] = {
+                "level": "noAuthNoPriv",  # "authNoPriv", "authPriv", "noAuthNoPriv
+                "username": args.username
+                }
+    
+    api_key = args.api_key
+    if args.apply:
+        if args.diode is None:
+            print("Please provide a Diode server, with the --diode or -d flag")
+            exit(1)
+        if args.api_key is None:
+            api_key = os.getenv("DIODE_API_KEY")
+        if api_key is None:
+            print("Please provide a Diode API key, with the --api_key or -k flag, or set the DIODE_API_KEY environment variable")
+            exit(1)
+
+    entities = []
+    discover_errors = {}
+    if args.host:
+        try:
+            device_data = discover.gater_device_data(args.host, snmp_data, args.role, args.site)
+            entities = entities + device_data.model_dump()
+        except Exception as e:
+            error_message = f"{str(e)}\n{traceback.format_exc()}"
+            discover_errors[args.host] = error_message
+
+    if args.network:
+        network = netaddr.IPNetwork(args.network)
+        for address in network:
+            try:
+                device_data = discover.gater_device_data(str(address), snmp_data, args.role, args.site)
+                entities = entities + device_data.model_dump()
+            except Exception as e:
+                error_message = f"{str(e)}\n{traceback.format_exc()}"
+                discover_errors[address] = error_message
+
+    if discover_errors:
+        print("ERROR: The following errors were encountered during discovery:")
+        for address, error in discover_errors.items():
+            print(f"ERROR: {address} - {error}")
+
+    if not args.apply:
+        print(entities)
+    else:
+        with DiodeClient(
+            target=args.diode,
+            app_name="snmp-diode",
+            app_version="0.0.1",
+            api_key=api_key,
+        ) as client:
+            response = client.ingest(entities=entities)
+            if response.errors:
+                print(f"FAIL: response errors: {response.errors}")
+            else:
+                print("INFO: data ingested successfully")
