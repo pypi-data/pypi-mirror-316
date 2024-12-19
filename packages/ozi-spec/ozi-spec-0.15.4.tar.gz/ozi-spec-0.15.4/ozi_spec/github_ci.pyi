83160c2248b9363a50c3de6cd70c627f58ba3e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:44 +0000

    Add package ozi-spec/0.15.4/ozi-spec-0.15.4.tar.gz

---
+++ packages/ozi-spec/ozi-spec-0.15.4.tar.gz/ozi-spec-0.15.4/ozi_spec/github_ci.pyi
@@ -0,0 +1,63 @@
+# ozi/spec/ci.py
+# Part of the OZI Project.
+# See LICENSE.txt for license information.
+# SPDX-License-Identifier: Unlicense
+"""Continuous integration specification."""
+from __future__ import annotations
+
+from collections.abc import Mapping  # noqa: TCH003,TC003,RUF100
+from dataclasses import dataclass
+from dataclasses import field
+
+from ozi_spec.base import Default
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Publish(Default):
+    """Publishing patterns for packaged project."""
+
+    include: tuple[str, ...] = ('*.tar.gz', '*.whl', 'sig/*')
+    version: str = '@github_publish_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Draft(Default):
+    """Draft release patterns for packaged project."""
+
+    version: str = '@github_draft_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Release(Default):
+    """Release patterns for packaged project."""
+
+    version: str = '@github_release_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class GenerateProvenance(Default):
+    """SLSA provenance generator metadata.
+    
+    .. versionadded:: 0.11.7
+    """
+
+    version: str = '@github_slsa_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class Checkpoint(Default):
+    """Checkpoint suites to run."""
+
+    suites: tuple[str, ...] = ('dist', 'lint', 'test')
+    version: str = '@github_checkpoint_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class HardenRunner(Default):
+    """Github Step-Security harden runner."""
+    version: str = '@github_harden_runner_version@'
+
+
+@dataclass(slots=True, frozen=True, eq=True)
+class GithubActionPyPI(Default):
+    """pypa/gh-action-pypi-publish"""
+    version: str = '@github_action_pypi_publish_version@'
