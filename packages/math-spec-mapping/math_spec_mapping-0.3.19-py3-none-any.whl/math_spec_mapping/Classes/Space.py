d1f63f3b16c8e60a55c9c9785dc8558ec340d145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:05:34 +0000

    Add package math-spec-mapping/0.3.19/math_spec_mapping-0.3.19-py3-none-any.whl

---
+++ packages/math-spec-mapping/math_spec_mapping-0.3.19-py3-none-any.whl/math_spec_mapping/Classes/Space.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+
+class Space:
+    def __init__(self, data: Dict):
+        self.name = data["name"]
+        self.schema = data["schema"]
+        self.metadata = data["metadata"]
+        self.name_variable = self.name.replace(" ", "").replace("-", "_")
+        self.domain_blocks = []
+        self.codomain_blocks = []
+
+    def __repr__(self):
+        return self.name
+
+
+TerminatingSpace = Space({"name": "Terminating Space", "schema": {}, "metadata": {}})
+EmptySpace = Space({"name": "Empty Space", "schema": {}, "metadata": {}})
