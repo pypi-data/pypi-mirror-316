5b6ef9a909161ee9996566dfd78d30a40c64d53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:13:40 +0000

    Add package nnthvn/0.3.0/nnthvn-0.3.0.tar.gz

---
+++ packages/nnthvn/nnthvn-0.3.0.tar.gz/nnthvn-0.3.0/nnthvn/module1.py
@@ -0,0 +1,19 @@
+def add(a, b):
+   return a + b
+
+def multiply(a, b):
+    return a * b
+def sum_of_squares(a, b, c):
+    return a**2 + b**2 + c**2
+def diff_max_min(a, b, c):
+    return max(a, b, c) - min(a, b, c)
+
+
+
+
+
+
+
+
+
+
