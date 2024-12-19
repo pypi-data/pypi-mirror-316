5d7a13b57e981164f78ba9eca181c1d2c88d6dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:22 +0000

    Add package django-sage-timezone/0.1.3/django_sage_timezone-0.1.3.tar.gz

---
+++ packages/django-sage-timezone/django_sage_timezone-0.1.3.tar.gz/django_sage_timezone-0.1.3/sage_timezone/templatetags/timezone_tags.py
@@ -0,0 +1,18 @@
+import pytz
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_session_value(request, key):
+    """Retrieves the value from the session for the given key."""
+    name = getattr(settings, "TIME_ZONE_SESSION_NAME", "user_timezone")
+    return request.session.get(name, "Default")
+
+
+@register.simple_tag
+def get_timezones():
+    """Returns a list of all available timezones."""
+    return pytz.all_timezones
