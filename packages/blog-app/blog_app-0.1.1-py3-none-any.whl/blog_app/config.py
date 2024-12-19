0c9f84ede4871b77fd952a180683a504cceed398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:04:36 +0000

    Add package blog-app/0.1.1/blog_app-0.1.1-py3-none-any.whl

---
+++ packages/blog-app/blog_app-0.1.1-py3-none-any.whl/blog_app/config.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///blog.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'segretissima')
+    PORT = os.getenv('PORT', '5000')
+    DOMAIN = os.getenv('DOMAIN', 'http://localhost:{port}').format(port=PORT)
