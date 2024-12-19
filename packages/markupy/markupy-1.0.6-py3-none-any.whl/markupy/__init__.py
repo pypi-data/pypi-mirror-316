16aa3a1442dc893ea1f76979ae39ece0429375c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:03:49 +0000

    Add package markupy/1.0.6/markupy-1.0.6-py3-none-any.whl

---
+++ packages/markupy/markupy-1.0.6-py3-none-any.whl/markupy/__init__.py
@@ -0,0 +1,13 @@
+from ._private.component import Component
+from ._private.html import to_markupy as html2markupy
+from ._private.node import Node, render_node
+from ._private.node import iter_unsafe_node as iter_node
+
+__all__ = [
+    "Node",
+    "Component",
+    "html2markupy",
+    "iter_node",
+    "render_node",
+]
+__version__ = "1.0.6"
