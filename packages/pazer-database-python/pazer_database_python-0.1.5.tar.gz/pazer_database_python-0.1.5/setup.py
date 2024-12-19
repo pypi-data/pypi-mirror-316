ebdff8933d46ea3894379b672bc40bb4e047788b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:25 +0000

    Add package pazer-database-python/0.1.5/pazer_database_python-0.1.5.tar.gz

---
+++ packages/pazer-database-python/pazer_database_python-0.1.5.tar.gz/pazer_database_python-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pazer_database_python",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[],
+    description="Pazer Database - Python",
+    author="David Kim",
+    author_email="woody@woody.co.kr",
+)
