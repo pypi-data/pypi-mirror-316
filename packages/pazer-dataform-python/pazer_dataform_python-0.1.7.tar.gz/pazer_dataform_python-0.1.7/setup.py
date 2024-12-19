905d71de49e05b0af0b7eee244e13e4b3fb3f441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:32 +0000

    Add package pazer-dataform-python/0.1.7/pazer_dataform_python-0.1.7.tar.gz

---
+++ packages/pazer-dataform-python/pazer_dataform_python-0.1.7.tar.gz/pazer_dataform_python-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pazer_dataform_python",
+    version="0.1.7",
+    packages=find_packages(),
+    install_requires=[],
+    description="Pazer DataForm - Python",
+    author="David Kim",
+    author_email="woody@woody.co.kr",
+)
