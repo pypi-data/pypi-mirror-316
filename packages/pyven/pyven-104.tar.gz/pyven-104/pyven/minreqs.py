a61c4d8cde044b15e8cf2537e7626811f44634d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 07:04:56 +0000

    Add package pyven/104/pyven-104.tar.gz

---
+++ packages/pyven/pyven-104.tar.gz/pyven-104/pyven/minreqs.py
@@ -0,0 +1,14 @@
+'Pin requires to their minimum allowed versions.'
+from .projectinfo import ProjectInfo
+from venvpool import initlogging
+
+def main():
+    initlogging()
+    info = ProjectInfo.seek('.')
+    minreqs = [r.minstr() for r in info.parsedrequires()]
+    with (info.projectdir / 'project.arid').open('a') as f:
+        print(f"requires = $list({' '.join(minreqs)})", file = f)
+    (info.projectdir / 'requirements.txt').write_text(''.join(f"{r}\n" for r in minreqs) if minreqs else '# This file blank.\n')
+
+if '__main__' == __name__:
+    main()
