32ecde6c5382822aa01c25dcbf1eb6e74e2b5523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:40 +0000

    Add package kedro-datasets/6.0.0/kedro_datasets-6.0.0.tar.gz

---
+++ packages/kedro-datasets/kedro_datasets-6.0.0.tar.gz/kedro_datasets-6.0.0/kedro_datasets/matplotlib/__init__.py
@@ -0,0 +1,11 @@
+"""``AbstractDataset`` implementation to save matplotlib objects as image files."""
+
+from typing import Any
+
+import lazy_loader as lazy
+
+MatplotlibWriter: Any
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__, submod_attrs={"matplotlib_writer": ["MatplotlibWriter"]}
+)
