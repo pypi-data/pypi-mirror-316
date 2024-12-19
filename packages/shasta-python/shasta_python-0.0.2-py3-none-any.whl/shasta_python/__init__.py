32dcf386950cedd13f897e3b6044de004a573296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:05:08 +0000

    Add package shasta-python/0.0.2/shasta_python-0.0.2-py3-none-any.whl

---
+++ packages/shasta-python/shasta_python-0.0.2-py3-none-any.whl/shasta_python/__init__.py
@@ -0,0 +1,53 @@
+from shasta_python._client import Client, AsyncClient
+from shasta_python._types import (
+  Options,
+  Message,
+  Image,
+  Tool,
+  GenerateResponse,
+  ChatResponse,
+  EmbedResponse,
+  EmbeddingsResponse,
+  StatusResponse,
+  ProgressResponse,
+  ListResponse,
+  ShowResponse,
+  ProcessResponse,
+  RequestError,
+  ResponseError,
+)
+
+__all__ = [
+  'Client',
+  'AsyncClient',
+  'Options',
+  'Message',
+  'Image',
+  'Tool',
+  'GenerateResponse',
+  'ChatResponse',
+  'EmbedResponse',
+  'EmbeddingsResponse',
+  'StatusResponse',
+  'ProgressResponse',
+  'ListResponse',
+  'ShowResponse',
+  'ProcessResponse',
+  'RequestError',
+  'ResponseError',
+]
+
+_client = Client()
+
+generate = _client.generate
+chat = _client.chat
+embed = _client.embed
+embeddings = _client.embeddings
+pull = _client.pull
+push = _client.push
+create = _client.create
+delete = _client.delete
+list = _client.list
+copy = _client.copy
+show = _client.show
+ps = _client.ps
