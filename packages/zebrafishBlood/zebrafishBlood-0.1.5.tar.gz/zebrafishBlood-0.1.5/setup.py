414f268e1ce5f6bafcff211b754e921763c076d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:39 +0000

    Add package zebrafishBlood/0.1.5/zebrafishBlood-0.1.5.tar.gz

---
+++ packages/zebrafishBlood/zebrafishBlood-0.1.5.tar.gz/zebrafishBlood-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+requirements = ['pytorch','torchvision','cudatoolkit','numpy','ultralytics', 'opencv']
+
+setup(
+    name='zebrafishBlood',
+    version='0.1.5',
+    description='Zebrafish blood smear cell counter',
+    author=['Eunhye Yang'],
+    author_email='eunhye@connect.hku.hk',
+    packages=find_packages(),
+    install_requires=requirements
+)
