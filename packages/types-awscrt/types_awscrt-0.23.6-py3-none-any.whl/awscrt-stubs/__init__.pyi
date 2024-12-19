6cb82bb8ac5d62eaabbdd41ada271706e56b9b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:32 +0000

    Add package types-awscrt/0.23.6/types_awscrt-0.23.6-py3-none-any.whl

---
+++ packages/types-awscrt/types_awscrt-0.23.6-py3-none-any.whl/awscrt-stubs/__init__.pyi
@@ -0,0 +1,16 @@
+"""
+Type annotations for awscrt module.
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from awscrt import auth as auth
+from awscrt import crypto as crypto
+from awscrt import http as http
+from awscrt import io as io
+from awscrt import mqtt as mqtt
+from awscrt import s3 as s3
+from awscrt import websocket as websocket
+
+class NativeResource:
+    def __init__(self) -> None: ...
