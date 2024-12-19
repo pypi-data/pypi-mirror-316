8f954433f32d5a7f4562afe843a44eedacf1082b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:16 +0000

    Add package tan-lib/0.1/tan_lib-0.1-py3-none-any.whl

---
+++ packages/tan-lib/tan_lib-0.1-py3-none-any.whl/tan_lib/core.py
@@ -0,0 +1,9 @@
+# tan_lib/core.py
+
+def add(a, b):
+    """Return the sum of a and b."""
+    return a + b
+
+def subtract(a, b):
+    """Return the result of subtracting b from a."""
+    return a - b
