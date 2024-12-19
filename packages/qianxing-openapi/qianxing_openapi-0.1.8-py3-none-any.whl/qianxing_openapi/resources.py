fc9ca8d42f00adb1b9fba2bd07029776cac1ac2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:21 +0000

    Add package qianxing-openapi/0.1.8/qianxing_openapi-0.1.8-py3-none-any.whl

---
+++ packages/qianxing-openapi/qianxing_openapi-0.1.8-py3-none-any.whl/qianxing_openapi/resources.py
@@ -0,0 +1,19 @@
+from qianxing_openapi.http_client import HttpClient
+from qianxing_openapi.qxmap_model import Qxmap
+
+
+class Resources:
+    client: HttpClient
+
+    def __init__(self, client: HttpClient):
+        self.client = client
+
+    def get_map(
+        self,
+        scen_id: str = None,
+        scen_ver: str = None,
+    ) -> Qxmap:
+        req_data = {"scen_id": scen_id, "scen_ver": scen_ver}
+        resp = self.client.post("/openapi/resource/v2/scenario/map/get", req_data)
+
+        return Qxmap(resp["data"])
