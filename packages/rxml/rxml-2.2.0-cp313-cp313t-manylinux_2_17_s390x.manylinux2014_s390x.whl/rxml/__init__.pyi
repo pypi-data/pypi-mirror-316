64c1fb961813c7f4c7980b3ca36edecc270f5a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:05:02 +0000

    Add package rxml/2.2.0/rxml-2.2.0-cp313-cp313t-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/rxml/rxml-2.2.0-cp313-cp313t-manylinux_2_17_s390x.manylinux2014_s390x.whl/rxml/__init__.pyi
@@ -0,0 +1,33 @@
+from enum import Enum
+
+class SearchType(Enum):
+    Tag: int
+    Attr: int
+    Text: int
+
+class Node:
+    name: str
+    attrs: dict[str, str]
+    children: list[Node]
+    text: str
+
+    def __new__(
+        cls,
+        name: str,
+        attrs: dict[str, str] = dict(),
+        children: list[Node] = list(),
+        text: str | None = None,
+    ) -> Node: ...
+    def __to_string(self, spacing: int | None) -> str: ...
+    def __str__(self) -> str: ...
+    def __repr__(self) -> str: ...
+    def search(
+        self, by: SearchType, value: str, depth: int | None = None
+    ) -> list[Node]: ...
+
+def read_file(file_path: str, root_tag: str) -> Node: ...
+def read_string(xml_string: str, root_tag: str) -> Node: ...
+def write_file(
+    node: Node, file_path: str, indent: int = 4, default_xml_def: bool = True
+): ...
+def write_string(node: Node, indent: int = 4, default_xml_def: bool = True) -> str: ...
