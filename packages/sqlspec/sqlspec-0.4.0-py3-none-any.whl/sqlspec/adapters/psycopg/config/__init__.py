0666417ec79f49117eb2e3af12b938772f9b5cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:03:52 +0000

    Add package sqlspec/0.4.0/sqlspec-0.4.0-py3-none-any.whl

---
+++ packages/sqlspec/sqlspec-0.4.0-py3-none-any.whl/sqlspec/adapters/psycopg/config/__init__.py
@@ -0,0 +1,9 @@
+from ._async import PsycoPgAsyncDatabaseConfig, PsycoPgAsyncPoolConfig
+from ._sync import PsycoPgSyncDatabaseConfig, PsycoPgSyncPoolConfig
+
+__all__ = (
+    "PsycoPgAsyncDatabaseConfig",
+    "PsycoPgAsyncPoolConfig",
+    "PsycoPgSyncDatabaseConfig",
+    "PsycoPgSyncPoolConfig",
+)
