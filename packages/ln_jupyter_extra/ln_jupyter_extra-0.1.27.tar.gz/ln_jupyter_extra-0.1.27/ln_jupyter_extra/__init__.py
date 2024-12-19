7e193fa941f196b961a747c448aa40d69f73614b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:14:17 +0000

    Add package ln_jupyter_extra/0.1.27/ln_jupyter_extra-0.1.27.tar.gz

---
+++ packages/ln_jupyter_extra/ln_jupyter_extra-0.1.27.tar.gz/ln_jupyter_extra-0.1.27/ln_jupyter_extra/__init__.py
@@ -0,0 +1,10 @@
+"""
+ln_jupyter_extra
+
+A Jupyter Lab extension providing extra functionality for enhanced user experience.
+
+This package extends JupyterLab with additional features and improvements to 
+streamline scientific computing and data analysis workflows.
+"""
+
+__version__ = "0.1.27"  # 确保有版本号
