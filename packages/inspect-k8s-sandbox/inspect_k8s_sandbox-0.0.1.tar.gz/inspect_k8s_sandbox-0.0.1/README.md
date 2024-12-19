259258774475bb5cfbf1fdca58133be0c76b613e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:02 +0000

    Add package inspect-k8s-sandbox/0.0.1/inspect_k8s_sandbox-0.0.1.tar.gz

---
+++ packages/inspect-k8s-sandbox/inspect_k8s_sandbox-0.0.1.tar.gz/inspect_k8s_sandbox-0.0.1/README.md
@@ -0,0 +1,5 @@
+This is an empty package to reserve the name `inspect-k8s-sandbox` on PyPI.
+Please install the package from source.
+https://github.com/UKGovernmentBEIS/inspect_k8s_sandbox
+
+A Kubernetes Sandbox Environment for Inspect.
---
+++ packages/inspect-k8s-sandbox/inspect_k8s_sandbox-0.0.1.tar.gz/inspect_k8s_sandbox-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="inspect-k8s-sandbox",
+    version="0.0.1",
+    description="Placeholder package only. Please install from source "
+    "https://github.com/UKGovernmentBEIS/inspect_k8s_sandbox. A Kubernetes Sandbox "
+    "Environment for Inspect.",
+    author="UK AI Safety Institute",
+    packages=find_packages(),
+    url="https://github.com/UKGovernmentBEIS/inspect_k8s_sandbox",
+)
