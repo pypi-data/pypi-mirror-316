6ae4cd3266d12a0f90af91921f9a55d7efb6a361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:14:08 +0000

    Add package shancx/1.9.32.22/shancx-1.9.32.22-py3-none-any.whl

---
+++ packages/shancx/shancx-1.9.32.22-py3-none-any.whl/shancx/_info.py
@@ -0,0 +1,54 @@
+dict_pro = {"安徽省":"34"
+           ,"北京市":"11"
+           ,"福建省":"35"
+           ,"甘肃省":"62"
+           ,"广东省":"44"
+           ,"广西壮族自治区":"45"
+           ,"贵州省":"52"
+           ,"海南省":"46"
+           ,"河北省":"13"
+           ,"河南省":"41"
+           ,"黑龙江省":"23"
+           ,"湖北省":"42"
+           ,"湖南省":"43"
+           ,"吉林省":"22"
+           ,"江苏省":"32"
+           ,"江西省":"36"
+           ,"辽宁省":"21"
+           ,"内蒙古自治区":"15"
+           ,"宁夏回族自治区":"64"
+           ,"青海省":"63"
+           ,"山东省":"37"
+           ,"山西省":"14"
+           ,"陕西省":"61"
+           ,"上海市":"31"
+           ,"四川省":"51"
+           ,"天津市":"12"
+           ,"西藏自治区":"54"
+           ,"新疆维吾尔自治区":"65"
+           ,"云南省":"53"
+           ,"浙江省":"33"
+           ,"重庆市":"50"
+           ,"合肥":"3401"
+           ,"全国":"100000"
+           ,"广州市":"4401"
+           ,"深圳市":"4403"
+           }
+
+users = {
+    "key": "fUcKxqq-b5S27EmVGy3PqCmCky3lxbxzaANzSPHlPmo=",
+    "users": {
+        "001": {
+            "s": "gAAAAABnYicFWX2_dX859GX77716zeIgPaFnOl6ePEsCbBm4kwILZtYwBTpusHXL6xsHHC4PeYtWdQHkBSeQ_6_9HILK36o7EjWZS89huVUkFJ5cvkelOf7-9_WE5vKcSyBrBpzkmIGYACjZ01wtMK9Lpm5RQK7TSTr5Ph6lCSBifG64IRn2aeeLICbsgFN_OIfLiWdL-XPbLtI_AKSc2WNnDuJLRgxuZQ=="
+        },
+        "002": {
+            "s": "gAAAAABnYicFJAVYnZIuM8VNb8p-JOTk6Buxs1TRhY8Kg2oN7kwcV-I0xQtpxPDSC5kOIN3ftwML4-cMX8MdhEc0o_INEyUyEVYI8n_jwk3t3GrsjePWPZA3Dm6nQZiZ0dYLl7mfv9300FQYxmcApuhhjlPOUVr5k-UlnLtJ99BgOO2dbzNT3ND-R7e_8p7A8iLW8Cl4JdHb3hgro26ev1M8Y7qBMgAbiw=="
+        },
+        "003": {
+            "s": "gAAAAABnYicFiFw-xDnq25-foEUmfdIYon78Rc41mxVvwOW47hjMpPEuQuLhlBP9syMgfNt1oRWJDX2MW0UsSP79__5w6Vk0a9iZlacx5OpbW3rRhE1H_6CIQ6FLafSfb_Lz6mpT_UZlYNFElAclVe0M5MdNsFAloAgHxbuDrUCEqUZ6gY7WuOHnyRpszqoihSGsdW2EWkwtXf9OAH1M5dSt_f3YdPNdRQ=="
+        },
+        "004": {
+            "s": "gAAAAABnYrgivKhvqu7bv2VBwl1scHtwFLhw95Z2GahuSjDanmbwnFebI1yls2cLwpBfYgBBu5WRBlPLiFo4CgPhDmSrrFznb_EBjsi80RhtFeUbirq6HcueMNwFrsRnreK8bB0tZu2uytMYI0KvoO3ogjWibSPTPO2gxWx3R-0HfGH2n7fFEixO2kQKCDauIz0teiVTQR1_CaFY9PM0sEoFuLCO4l6YWQ=="
+        }
+    }
+}
