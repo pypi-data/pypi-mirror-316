ff965b1d9e598ca5db2d46812ef05a6b759eeb8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:07 +0000

    Add package metabolights-utils/1.3.10/metabolights_utils-1.3.10-py3-none-any.whl

---
+++ packages/metabolights-utils/metabolights_utils-1.3.10-py3-none-any.whl/metabolights_utils/utils/search_utils.py
@@ -0,0 +1,23 @@
+import glob
+import logging
+import os
+from typing import List
+
+from metabolights_utils.utils.filename_utils import join_path
+
+logger = logging.getLogger(__name__)
+
+
+class MetabolightsSearchUtils(object):
+    @staticmethod
+    def get_isa_metadata_files(folder_path: str, recursive: bool = False) -> List[str]:
+        metadata_files = []
+        patterns = ["[asi]_*.txt", "m_*.tsv"]
+        if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+            logger.warning("Not valid directory %s", folder_path)
+            return metadata_files
+
+        for pattern in patterns:
+            search_pattern = join_path(folder_path, pattern)
+            metadata_files.extend(glob.glob(search_pattern, recursive=recursive))
+        return metadata_files
