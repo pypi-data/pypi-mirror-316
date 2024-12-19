9bf34f25e0b5a2d6c71785dd8d9b74370f67fd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:22 +0000

    Add package PySDM-examples/2.91/PySDM_examples-2.91-py3-none-any.whl

---
+++ packages/PySDM-examples/PySDM_examples-2.91-py3-none-any.whl/PySDM_examples/Bulenok_2023_MasterThesis/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=invalid-name
+"""
+Box-model coalescence-breakup performance benchmark from
+[Bulenok 2023 MSc thesis](https://www.ap.uj.edu.pl/diplomas/166879)
+
+performance_comparison_Srivastava_Setup.ipynb:
+.. include:: ./performance_comparison_Srivastava_Setup.ipynb.badges.md
+"""
