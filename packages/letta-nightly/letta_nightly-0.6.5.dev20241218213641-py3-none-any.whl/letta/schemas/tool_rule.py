de192de2f89db26cd96f3b6f6fc05c97172fa5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 07:04:42 +0000

    Add package letta-nightly/0.6.5.dev20241218213641/letta_nightly-0.6.5.dev20241218213641-py3-none-any.whl

---
+++ packages/letta-nightly/letta_nightly-0.6.5.dev20241218213641-py3-none-any.whl/letta/schemas/tool_rule.py
@@ -0,0 +1,40 @@
+from typing import List, Union
+
+from pydantic import Field
+
+from letta.schemas.enums import ToolRuleType
+from letta.schemas.letta_base import LettaBase
+
+
+class BaseToolRule(LettaBase):
+    __id_prefix__ = "tool_rule"
+    tool_name: str = Field(..., description="The name of the tool. Must exist in the database for the user's organization.")
+    type: ToolRuleType
+
+
+class ChildToolRule(BaseToolRule):
+    """
+    A ToolRule represents a tool that can be invoked by the agent.
+    """
+
+    type: ToolRuleType = ToolRuleType.constrain_child_tools
+    children: List[str] = Field(..., description="The children tools that can be invoked.")
+
+
+class InitToolRule(BaseToolRule):
+    """
+    Represents the initial tool rule configuration.
+    """
+
+    type: ToolRuleType = ToolRuleType.run_first
+
+
+class TerminalToolRule(BaseToolRule):
+    """
+    Represents a terminal tool rule configuration where if this tool gets called, it must end the agent loop.
+    """
+
+    type: ToolRuleType = ToolRuleType.exit_loop
+
+
+ToolRule = Union[ChildToolRule, InitToolRule, TerminalToolRule]
