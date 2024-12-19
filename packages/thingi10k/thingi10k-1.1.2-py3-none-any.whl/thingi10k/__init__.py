b9a652ec3ddb3323c085bca2eebdbc500f35a5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:19 +0000

    Add package thingi10k/1.1.2/thingi10k-1.1.2-py3-none-any.whl

---
+++ packages/thingi10k/thingi10k-1.1.2-py3-none-any.whl/thingi10k/__init__.py
@@ -0,0 +1,25 @@
+""" Thingi10k dataset.
+
+
+## Usage:
+
+    ```python
+    import thingi10k
+
+    thingi10k.init()
+
+    # Iterate over all data
+    for entry in thingi10k.dataset():
+        file_id = entry['file_id']
+        vertices, facets = thingi10k.load_file(entry['file_path'])
+
+    # Iterate over closed mesh with at most 1000 vertices
+    for entry in thingi10k.dataset(num_vertices=(None, 1000), closed=True):
+        file_id = entry['file_id']
+        vertices, facets = thingi10k.load_file(entry['file_path'])
+    ```
+"""
+
+__version__ = '1.1.2'
+
+from ._utils import load_file, init, dataset
