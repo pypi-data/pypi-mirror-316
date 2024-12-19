92eab638131b8f46664bc1d94f15cea7f7d9ed9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:43 +0000

    Add package skys-llc-auth/0.1.20/skys_llc_auth-0.1.20-py3-none-any.whl

---
+++ packages/skys-llc-auth/skys_llc_auth-0.1.20-py3-none-any.whl/skys_llc_auth/schemas.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Credentails(BaseModel):
+    access_token: str
+    refresh_token: str
+    login: str
+    password: str
+    access_until: datetime
+    service_name: str
