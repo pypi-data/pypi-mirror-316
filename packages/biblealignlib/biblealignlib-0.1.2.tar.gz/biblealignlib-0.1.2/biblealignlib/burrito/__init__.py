a940272a63c332da3eb7b95793ca8799aa4cf40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:05:52 +0000

    Add package biblealignlib/0.1.2/biblealignlib-0.1.2.tar.gz

---
+++ packages/biblealignlib/biblealignlib-0.1.2.tar.gz/biblealignlib-0.1.2/biblealignlib/burrito/__init__.py
@@ -0,0 +1,61 @@
+"""Code for working with alignment data in Scripture Burrito format."""
+
+# where Clear-Bible repos are rooted
+from biblealignlib import CLEARROOT, SOURCES
+
+from .AlignmentGroup import (
+    Document,
+    Metadata,
+    AlignmentReference,
+    AlignmentRecord,
+    AlignmentGroup,
+    TopLevelGroups,
+)
+
+from .AlignmentSet import AlignmentSet
+
+# probably don't need the other types
+from .AlignmentType import TranslationType
+from .alignments import AlignmentsReader, write_alignment_group
+from .manager import Manager, VerseData
+from .BaseToken import BaseToken, asbool, bare_id
+from .source import macula_prefixer, macula_unprefixer, Source, SourceReader
+from .target import Target, TargetReader
+
+
+__all__ = [
+    "CLEARROOT",
+    "SOURCES",
+    # AlignmentGroup
+    "Document",
+    "Metadata",
+    "AlignmentReference",
+    "AlignmentRecord",
+    "AlignmentGroup",
+    "TopLevelGroups",
+    # AlignmentSet
+    "AlignmentSet",
+    # AlignmentType
+    "TranslationType",
+    # BaseToken
+    "BaseToken",
+    "asbool",
+    "bare_id",
+    # alignments
+    "AlignmentsReader",
+    "write_alignment_group",
+    # manager
+    "Manager",
+    "VerseData",
+    # source
+    "macula_prefixer",
+    "macula_unprefixer",
+    "Source",
+    "SourceReader",
+    # target
+    "Target",
+    "TargetReader",
+    "TargetWriter",
+    # util
+    "groupby_bcv",
+]
