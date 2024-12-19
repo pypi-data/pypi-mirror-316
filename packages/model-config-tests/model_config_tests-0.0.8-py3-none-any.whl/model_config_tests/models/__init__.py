a6c4c78324d53a80ac1daa31cee8bd1a083c72b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:29 +0000

    Add package model-config-tests/0.0.8/model_config_tests-0.0.8-py3-none-any.whl

---
+++ packages/model-config-tests/model_config_tests-0.0.8-py3-none-any.whl/model_config_tests/models/__init__.py
@@ -0,0 +1,6 @@
+from model_config_tests.models.accessesm1p5 import AccessEsm1p5
+from model_config_tests.models.accessom2 import AccessOm2
+from model_config_tests.models.accessom3 import AccessOm3
+
+# Mapping payu configuration model name to class
+index = {"access-om2": AccessOm2, "access-om3": AccessOm3, "access": AccessEsm1p5}
---
+++ packages/model-config-tests/model_config_tests-0.0.8-py3-none-any.whl/model_config_tests/qa/test_access_om3_config.py
@@ -0,0 +1,14 @@
+# Copyright 2024 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+
+"""ACCESS-OM3 specific configuration tests"""
+
+import pytest
+
+
+@pytest.mark.access_om3
+class TestAccessOM3:
+    """ACCESS-OM3 Specific configuration and metadata tests"""
+
+    def test_pass(self):
+        pass
