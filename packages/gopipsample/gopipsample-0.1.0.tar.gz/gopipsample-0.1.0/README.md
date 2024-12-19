86f728792d19db608551f9ffe5976a67b6e897dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:09 +0000

    Add package gopipsample/0.1.0/gopipsample-0.1.0.tar.gz

---
+++ packages/gopipsample/gopipsample-0.1.0.tar.gz/gopipsample-0.1.0/README.md
@@ -0,0 +1,3 @@
+# Go Pip Sample Package
+
+This is a sample Python package to demonstrate private PyPI deployment.
---
+++ packages/gopipsample/gopipsample-0.1.0.tar.gz/gopipsample-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gopipsample',
+    version='0.1.0',
+    description='A sample package to demonstrate private PyPI deployment',
+    author='Devanand Bhowan',
+    packages=find_packages(),
+    install_requires=[],
+)
