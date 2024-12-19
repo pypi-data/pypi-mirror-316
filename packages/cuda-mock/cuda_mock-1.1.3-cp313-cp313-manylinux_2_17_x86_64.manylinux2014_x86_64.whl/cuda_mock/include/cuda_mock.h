9f3c0c6d525506e1c141fc524917ca062c96a3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:05 +0000

    Add package cuda-mock/1.1.3/cuda_mock-1.1.3-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cuda-mock/cuda_mock-1.1.3-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cuda_mock/include/cuda_mock.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <functional>
+
+#include "macro.h"
+
+#ifdef __cplusplus
+
+extern "C" {
+
+typedef void* HookContext_t;
+typedef const char* HookString_t;
+
+void dh_initialize();
+
+void dh_uninitialize();
+
+void dh_internal_install_hook(HookString_t srcLib, HookString_t targetLib,
+                              HookString_t symbolName,
+                              HookString_t hookerLibPath = nullptr,
+                              HookString_t hookerSymbolName = nullptr);
+
+void dh_internal_install_hook_regex(HookString_t srcLib, HookString_t targetLib,
+                                    HookString_t symbolName,
+                                    HookString_t hookerLibPath,
+                                    HookString_t hookerSymbolName);
+
+HookContext_t dh_open_hook_context();
+
+HookContext_t dh_close_hook_context();
+
+void dh_set_symbol_define_library(HookContext_t ctx, HookString_t str,
+                                  bool regex);
+
+void dh_set_symbol_replace_library(HookContext_t ctx, HookString_t str,
+                                   bool regex);
+
+void dh_set_new_symbol_define_library(HookContext_t ctx, HookString_t str,
+                                      bool regex);
+
+void dh_set_new_symbol_name(HookContext_t ctx, HookString_t str, bool regex);
+
+void dh_create_py_hook_installer(
+    const std::function<bool(HookString_t name)>& isTarget,
+    const std::function<bool(HookString_t name)>& isSymbol, HookString_t lib,
+    const std::function<HookString_t(HookString_t name)>& newSymbol);
+
+// for runtime check python version build vs runtime
+const char* get_build_python_string_version();
+int get_build_python_int_version();
+}
+
+void install_triton_hook();
+void dh_any_hook_install();
+
+#endif
