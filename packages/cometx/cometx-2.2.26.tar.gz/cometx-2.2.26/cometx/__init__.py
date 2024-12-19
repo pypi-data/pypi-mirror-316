25aa529f66d0bd6ec8602c28de8616d6cc660b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:05:15 +0000

    Add package cometx/2.2.26/cometx-2.2.26.tar.gz

---
+++ packages/cometx/cometx-2.2.26.tar.gz/cometx-2.2.26/cometx/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# ****************************************
+#                              __
+#   _________  ____ ___  ___  / /__  __
+#  / ___/ __ \/ __ `__ \/ _ \/ __/ |/_/
+# / /__/ /_/ / / / / / /  __/ /__>  <
+# \___/\____/_/ /_/ /_/\___/\__/_/|_|
+#
+#
+#  Copyright (c) 2022 Cometx Development
+#      Team. All rights reserved.
+# ****************************************
+
+import os
+
+from comet_ml import login  # noqa
+
+# To keep comet_ml logger silent:
+os.environ["COMET_LOGGING_CONSOLE"] = "CRITICAL"
+
+from ._version import __version__  # noqa
+from .api import API  # noqa
