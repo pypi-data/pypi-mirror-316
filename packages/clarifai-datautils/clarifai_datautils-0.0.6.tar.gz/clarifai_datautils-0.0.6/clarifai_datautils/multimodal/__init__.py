d7c5c42cb32090837ffb1e67d7e5f934f6f85ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:14:11 +0000

    Add package clarifai-datautils/0.0.6/clarifai_datautils-0.0.6.tar.gz

---
+++ packages/clarifai-datautils/clarifai_datautils-0.0.6.tar.gz/clarifai_datautils-0.0.6/clarifai_datautils/multimodal/__init__.py
@@ -0,0 +1,5 @@
+from clarifai_datautils.multimodal.pipeline.base import Pipeline
+from clarifai_datautils.multimodal.pipeline.PDF import PDFPartition
+from clarifai_datautils.multimodal.pipeline.Text import TextPartition
+
+__all__ = ['Pipeline', 'PDFPartition', 'TextPartition', 'PDFPartitionMultimodal']
