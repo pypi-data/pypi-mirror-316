2e2d40de1d4a1610aae709ef2968181da5eb11cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:04:55 +0000

    Add package mtproto/0.1.0b6/mtproto-0.1.0b6.tar.gz

---
+++ packages/mtproto/mtproto-0.1.0b6.tar.gz/mtproto-0.1.0b6/mtproto/transports/__init__.py
@@ -0,0 +1,5 @@
+from .abridged import AbridgedTransport
+from .intermediate import IntermediateTransport
+from .padded_intermediate import PaddedIntermediateTransport
+from .full import FullTransport
+from .obfuscated import ObfuscatedTransport
