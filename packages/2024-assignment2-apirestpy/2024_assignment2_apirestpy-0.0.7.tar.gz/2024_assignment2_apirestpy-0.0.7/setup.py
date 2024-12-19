86ddbc2ed1e623d0f496be1dc9443fb74968fa97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 07:04:25 +0000

    Add package 2024-assignment2-apirestpy/0.0.7/2024_assignment2_apirestpy-0.0.7.tar.gz

---
+++ packages/2024-assignment2-apirestpy/2024_assignment2_apirestpy-0.0.7.tar.gz/2024_assignment2_apirestpy-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="2024_assignment2_apirestpy",
+    version='0.0.7',
+    author="MVP",
+    description="a basic restful api",
+    packages=find_packages(),
+    install_requires=[
+        "flask"
+    ]
+)
+
