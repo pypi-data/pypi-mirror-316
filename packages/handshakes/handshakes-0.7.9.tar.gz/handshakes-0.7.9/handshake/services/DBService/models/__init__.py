ad12b7ec634774bfdfd53995b1d51fe00016e302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:04:14 +0000

    Add package handshakes/0.7.9/handshakes-0.7.9.tar.gz

---
+++ packages/handshakes/handshakes-0.7.9.tar.gz/handshakes-0.7.9/handshake/services/DBService/models/__init__.py
@@ -0,0 +1,29 @@
+from .result_base import (
+    SessionBase,
+    SuiteBase,
+    RunBase,
+    RollupBase,
+    RetriedBase,
+    TestLogBase,
+)
+from .config_base import ConfigBase, TestConfigBase, MigrationBase
+from .static_base import AttachmentBase, StaticBase
+from .dynamic_base import TaskBase
+from .attachmentBase import AssertBase, EntityLogBase
+
+__all__ = [
+    SessionBase,
+    SuiteBase,
+    MigrationBase,
+    RunBase,
+    TestConfigBase,
+    AttachmentBase,
+    TaskBase,
+    ConfigBase,
+    RollupBase,
+    StaticBase,
+    RetriedBase,
+    AssertBase,
+    TestLogBase,
+    EntityLogBase,
+]
