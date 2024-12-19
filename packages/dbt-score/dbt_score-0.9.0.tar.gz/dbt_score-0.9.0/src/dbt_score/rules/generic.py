1c1cb942bfef233464dc7e547266e9ded23da0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:23 +0000

    Add package dbt-score/0.9.0/dbt_score-0.9.0.tar.gz

---
+++ packages/dbt-score/dbt_score-0.9.0.tar.gz/dbt_score-0.9.0/src/dbt_score/rules/generic.py
@@ -0,0 +1,53 @@
+"""All generic rules."""
+
+from dbt_score import Model, RuleViolation, Severity, rule
+
+
+@rule
+def has_description(model: Model) -> RuleViolation | None:
+    """A model should have a description."""
+    if not model.description:
+        return RuleViolation(message="Model lacks a description.")
+
+
+@rule
+def columns_have_description(model: Model) -> RuleViolation | None:
+    """All columns of a model should have a description."""
+    invalid_column_names = [
+        column.name for column in model.columns if not column.description
+    ]
+    if invalid_column_names:
+        max_length = 60
+        message = f"Columns lack a description: {', '.join(invalid_column_names)}."
+        if len(message) > max_length:
+            message = f"{message[:60]}…"
+        return RuleViolation(message=message)
+
+
+@rule
+def has_owner(model: Model) -> RuleViolation | None:
+    """A model should have an owner."""
+    if not model.meta.get("owner"):
+        return RuleViolation(message="Model lacks an owner.")
+
+
+@rule
+def sql_has_reasonable_number_of_lines(
+    model: Model, max_lines: int = 200
+) -> RuleViolation | None:
+    """The SQL query of a model should not be too long."""
+    count_lines = len(model.raw_code.split("\n"))
+    if count_lines > max_lines:
+        return RuleViolation(
+            message=f"SQL query too long: {count_lines} lines (> {max_lines})."
+        )
+
+
+@rule(severity=Severity.LOW)
+def has_example_sql(model: Model) -> RuleViolation | None:
+    """The documentation of a model should have an example query."""
+    if model.language == "sql":
+        if "```sql" not in model.description:
+            return RuleViolation(
+                "The model description does not include an example SQL query."
+            )
