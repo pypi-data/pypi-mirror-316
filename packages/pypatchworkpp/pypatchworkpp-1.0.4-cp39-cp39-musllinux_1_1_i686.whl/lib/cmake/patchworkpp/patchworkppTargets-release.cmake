beccc77eabe4ce7faa39740ee0c9846117705952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:03:35 +0000

    Add package pypatchworkpp/1.0.4/pypatchworkpp-1.0.4-cp39-cp39-musllinux_1_1_i686.whl

---
+++ packages/pypatchworkpp/pypatchworkpp-1.0.4-cp39-cp39-musllinux_1_1_i686.whl/lib/cmake/patchworkpp/patchworkppTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "patchworkpp::ground_seg_cores" for configuration "Release"
+set_property(TARGET patchworkpp::ground_seg_cores APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(patchworkpp::ground_seg_cores PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libground_seg_cores.a"
+  )
+
+list(APPEND _cmake_import_check_targets patchworkpp::ground_seg_cores )
+list(APPEND _cmake_import_check_files_for_patchworkpp::ground_seg_cores "${_IMPORT_PREFIX}/lib/libground_seg_cores.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
