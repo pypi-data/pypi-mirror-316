46246744731d02bdf3f77a6e6e1918ac29fb1a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:05:21 +0000

    Add package vedro-pw/0.2.1/vedro_pw-0.2.1-py3-none-any.whl

---
+++ packages/vedro-pw/vedro_pw-0.2.1-py3-none-any.whl/vedro_pw/__init__.py
@@ -0,0 +1,15 @@
+from ._capture_mode import CaptureMode
+from ._pw_browser import PlaywrightBrowser
+from ._pw_plugin import Playwright, PlaywrightPlugin
+from .async_api import (
+    created_browser_context,
+    launched_browser,
+    launched_local_browser,
+    launched_remote_browser,
+    opened_browser_page,
+)
+
+__version__ = "0.2.1"
+__all__ = ("opened_browser_page", "created_browser_context", "launched_browser",
+           "launched_local_browser", "launched_remote_browser",
+           "Playwright", "PlaywrightPlugin", "PlaywrightBrowser", "CaptureMode",)
