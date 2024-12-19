bd6ea70fcdfa78e4b43008e89e95d0e716dfe07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:36 +0000

    Add package AdsbAnomalyDetector/0.8.2/AdsbAnomalyDetector-0.8.2-py3-none-any.whl

---
+++ packages/AdsbAnomalyDetector/AdsbAnomalyDetector-0.8.2-py3-none-any.whl/AdsbAnomalyDetector/B_Model_InterpolationDetector.py
@@ -0,0 +1,126 @@
+
+import tensorflow as tf
+from keras.layers import *
+
+from .B_Model_AbstractModel import Model as AbstactModel
+from .B_Model_Utils_TF_Modules import *
+
+from numpy_typing import np, ax
+
+from ._Utils_os_wrapper import os
+
+from . import _Utils_Color as C
+from ._Utils_Color import prntC
+
+class Model(AbstactModel):
+
+    name = "LSTM"
+
+    def __init__(self, CTX:dict):
+        """
+        Generate model architecture
+        Define loss function
+        Define optimizer
+        """
+
+
+        # load context
+        self.CTX = CTX
+        self.dropout = CTX["DROPOUT"]
+
+        # save the number of training steps
+        self.nb_train = 0
+
+        x_input_shape = (self.CTX["INPUT_LEN"], self.CTX["FEATURES_IN"])
+        x = Input(shape=x_input_shape, name='input')
+        
+        z = x
+
+        z = Conv1DModule(128, 1)(z)
+        z = MaxPool1D(2)(z)
+        z = Conv1DModule(64, 1)(z)
+        z = MaxPool1D(2)(z)
+        z = Conv1DModule(32, 1)(z)
+        z = Flatten()(z)
+        
+        # n = self.CTX["LAYERS"]
+        # for _ in range(n-1):
+            # res = z * self.CTX["RESUDUAL"]
+            # z = LSTM(128, return_sequences=True, dropout=self.dropout)(z)
+
+            # z = Add()([z, res])
+        # z = LSTM(128, return_sequences=False)(z)
+
+        z = DenseModule(128, self.dropout)(z)
+        # z = DenseModule(64, self.dropout)(z)
+        # z = DenseModule(32, self.dropout)(z)
+        z = Dense(1, activation="sigmoid")(z)
+        y = z
+
+        self.model = tf.keras.Model(x, y)
+
+
+        # define loss function
+        self.loss = tf.keras.losses.MeanSquaredError()
+
+        # define optimizer
+        self.opt = tf.keras.optimizers.Adam(learning_rate=CTX["LEARNING_RATE"])
+        # self.opt = tf.keras.optimizers.SGD(learning_rate=CTX["LEARNING_RATE"])
+
+
+    def predict(self, x):
+        """
+        Make prediction for x
+        """
+        return self.model(x)
+
+    def compute_loss(self, x, y):
+        """
+        Make a prediction and compute the loss
+        that will be used for training
+        """
+        y_ = self.model(x)
+        return self.loss(y_, y), y_
+
+    def training_step(self, x, y):
+        """
+        Do one forward pass and gradient descent
+        for the given batch
+        """
+        with tf.GradientTape(watch_accessed_variables=True) as tape:
+            loss, output = self.compute_loss(x, y)
+
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+            self.opt.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        self.nb_train += 1
+        return loss, output
+
+
+
+    def visualize(self, save_path="./_Artifacts/"):
+        """
+        Generate a visualization of the model's architecture
+        """
+
+        params = 0
+        for i in range(len(self.model.trainable_variables)):
+            params += np.prod(self.model.trainable_variables[i].shape)
+
+        filename = os.path.join(save_path, self.name+".png")
+        tf.keras.utils.plot_model(self.model, to_file=filename, show_shapes=True)
+
+
+
+    def get_variables(self):
+        """
+        Return the variables of the model
+        """
+        return self.model.trainable_variables
+
+    def set_variables(self, variables):
+        """
+        Set the variables of the model
+        """
+        for i in range(len(variables)):
+            self.model.trainable_variables[i].assign(variables[i])
---
+++ packages/AdsbAnomalyDetector/AdsbAnomalyDetector-0.8.2-py3-none-any.whl/AdsbAnomalyDetector/C_Constants_FloodingSolver.py
@@ -0,0 +1,46 @@
+
+LEARNING_RATE = 0.0003
+EPOCHS = 0
+BATCH_SIZE = 64
+NB_BATCH = 32
+
+# LOAD_EP = 80
+
+HISTORY = 32
+DILATION_RATE = 1
+INPUT_LEN = HISTORY // DILATION_RATE
+
+RELATIVE_POSITION = True
+RELATIVE_TRACK = True
+RANDOM_TRACK = False
+
+HORIZON = 3
+
+THRESHOLD = 25.0
+
+LAYERS = 3
+DROPOUT = 0.3
+
+
+USED_FEATURES = [
+    "latitude", "longitude",
+    "groundspeed", "track",
+    "vertical_rate",
+    # "alert", "spi", "squawk",
+    "altitude", "geoaltitude",
+    "timestamp", "pred_distance"
+]
+FEATURES_IN = len(USED_FEATURES)
+FEATURE_MAP = dict([[USED_FEATURES[i], i] for i in range(FEATURES_IN)])
+
+PRED_FEATURES = [
+    "latitude", "longitude"
+]
+FEATURES_OUT = len(PRED_FEATURES)
+PRED_FEATURE_MAP = dict([[PRED_FEATURES[i], i] for i in range(FEATURES_OUT)])
+
+RESUDUAL = 1.0
+
+# possibilities "valid", "last", "nan"
+INPUT_PADDING = "nan"
+
