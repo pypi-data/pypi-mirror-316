af215df8e471e779120321f8ad42603d1f01bd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:42 +0000

    Add package fastapix-py/0.10.5/fastapix_py-0.10.5-py3-none-any.whl

---
+++ packages/fastapix-py/fastapix_py-0.10.5-py3-none-any.whl/fastapix/innerclient.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env Python3
+# -*- coding: utf-8 -*-
+# @Author   : zhangzhanqi
+# @FILE     : innerclient.py
+# @Time     : 2024/1/5 下午4:00
+# @Desc     : $
+from fastapi.testclient import TestClient as InnerClient  # noqa: F401
