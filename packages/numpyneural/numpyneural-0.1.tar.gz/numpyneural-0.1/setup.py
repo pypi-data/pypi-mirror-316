1ecd254fd344aa11f1997e3c4712fbf228c33c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:36 +0000

    Add package numpyneural/0.1/numpyneural-0.1.tar.gz

---
+++ packages/numpyneural/numpyneural-0.1.tar.gz/numpyneural-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text() #Gets the long description from Readme file
+
+setup(
+    name='numpyneural',
+    version='0.1',
+    packages=find_packages(),
+
+    author='nndl',
+    author_email='nndl@xyz.com',
+    description='This is the short description',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+)
