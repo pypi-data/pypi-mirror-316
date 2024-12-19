380885dfff1105b4587043281d60408026ce131b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 04:05:24 +0000

    Add package falconry/0.1.2/falconry-0.1.2-py3-none-any.whl

---
+++ packages/falconry/falconry-0.1.2-py3-none-any.whl/falconry/translate.py
@@ -0,0 +1,17 @@
+
+# Translation of the Job status
+statusMessage = {
+    0: "UNKNOWN",
+    1: "IDLE",
+    2: "RUNNING",
+    3: "REMOVED",
+    4: "COMPLETE",
+    5: "HELD",
+    6: "TRANSPORTING",
+    7: "SUSPENDED",
+    8: "SKIPPED",
+    9: "NOT SUBMITTED",
+    10: "LOG FILE MISSING",
+    11: "ABNORMAL TERMINATION",
+    12: "ABORTED BY USER"
+}
