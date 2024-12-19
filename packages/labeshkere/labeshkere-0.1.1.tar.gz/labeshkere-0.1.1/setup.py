467ede9a919535317c88a49e22f5528a4eb0707e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:13:39 +0000

    Add package labeshkere/0.1.1/labeshkere-0.1.1.tar.gz

---
+++ packages/labeshkere/labeshkere-0.1.1.tar.gz/labeshkere-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='labeshkere',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='',
+    author='BOSS431',
+    author_email='ony.resset@gmail.com',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
