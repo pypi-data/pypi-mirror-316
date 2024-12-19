05d8b762b8419938ee8ce0f09090dbe25bc541d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 07:04:51 +0000

    Add package hiddifypanel/10.80.11/hiddifypanel-10.80.11-py3-none-any.whl

---
+++ packages/hiddifypanel/hiddifypanel-10.80.11-py3-none-any.whl/hiddifypanel/database.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import UUIDType
+import re
+import os
+from sqlalchemy import Row, text, Sequence
+
+
+
+
+
+db: SQLAlchemy = SQLAlchemy()
+db.UUID = UUIDType  # type: ignore
+
+
+def init_app(app):
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db.init_app(app)
+    with app.app_context():
+        from hiddifypanel.panel.init_db import init_db
+        init_db()
+        
+
+
+def db_execute(query: str, return_val: bool = False, commit: bool = False, **params: dict):
+    q = db.session.execute(text(query), params)
+    if commit:
+        db.session.commit()
+    if return_val:
+        return q.fetchall()
+
+    # with db.engine.connect() as connection:
+    #     res = connection.execute(text(query), params)
+    #     connection.commit()s
+    # return res
