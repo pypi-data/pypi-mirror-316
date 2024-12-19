630010372feb95e60765d876410927c5f6e79af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 05:04:35 +0000

    Add package resume-module/0.2/resume_module-0.2.tar.gz

---
+++ packages/resume-module/resume_module-0.2.tar.gz/resume_module-0.2/Resume/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+from .resume import input_fields, generate_resume, save_to_pdf, generate_and_save_resume
+
+__all__ = ['input_fields', 'generate_resume', 'save_to_pdf', 'generate_and_save_resume']
