67766fbd04681a6c71dd620f05956a0287107c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 15:04:36 +0000

    Add package funutil/1.0.45/funutil-1.0.45-py3-none-any.whl

---
+++ packages/funutil/funutil-1.0.45-py3-none-any.whl/funutil/__init__.py
@@ -0,0 +1,11 @@
+from .time import RunTimer, run_timer
+from .util import deep_get, get_logger, get_package_version, getLogger
+
+__all__ = [
+    "RunTimer",
+    "deep_get",
+    "get_logger",
+    "getLogger",
+    "run_timer",
+    "get_package_version",
+]
