feefc8f40c2bff891605bf2c4d7eac44f8cb4888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 14:04:27 +0000

    Add package atlaslb/1.0.0/atlaslb-1.0.0-py3-none-any.whl

---
+++ packages/atlaslb/atlaslb-1.0.0-py3-none-any.whl/leastconnectionlb.py
@@ -0,0 +1,52 @@
+from .originserver import OriginServer
+from .roundrobinlb import RoundRobinLoadBalancer
+
+class LeastConnectionLoadBalancer(RoundRobinLoadBalancer):
+    """
+    A load balancer that selects the origin server with the least number of active connections.
+    """
+    def __init__(self, servers: dict[str, OriginServer] = None):
+        """
+        Initialize the least connection load balancer with the given servers.
+        
+        Args:
+            servers (dict[str, OriginServer]): A dictionary of servers with hostnames as keys and OriginServer instances as values.
+        """
+        super().__init__(servers)
+        
+    async def get_next_server(self):
+        """
+        Get the next server with the least number of active connections.
+        
+        Returns:
+            OriginServer: The next server to handle the request.
+        """
+        min_connections = float('inf')
+        candiate_hosts = []
+        host_indices = []
+        async with self.lock:
+            for index, host in enumerate(self.hosts):
+                if self.servers[host].local_rif < min_connections:
+                    min_connections = self.servers[host].local_rif
+                    candiate_hosts = [host]
+                    host_indices = [index]
+                elif self.servers[host].local_rif == min_connections:
+                    candiate_hosts.append(host)
+                    host_indices.append(index)
+        print(candiate_hosts, host_indices)
+        if len(candiate_hosts) == 1:
+            self.current_index = host_indices[0]
+            return self.servers[candiate_hosts[0]]
+        else:
+            distance = float('inf')
+            selected_index = None
+            async with self.lock:
+                for index, host in zip(host_indices, candiate_hosts):
+                    curr_dist = (index - self.current_index) % len(self.hosts)
+                    print(index, self.current_index, curr_dist, distance)
+                    if curr_dist < distance and index != self.current_index:
+                        distance = curr_dist
+                        selected_index = index
+            self.current_index = selected_index
+            print(self.current_index)
+            return self.servers[self.hosts[self.current_index]]
---
+++ packages/atlaslb/atlaslb-1.0.0-py3-none-any.whl/roundrobinlb.py
@@ -0,0 +1,28 @@
+from .baselb import BaseLoadBalancer
+from .originserver import OriginServer
+
+class RoundRobinLoadBalancer(BaseLoadBalancer):
+    """
+    A load balancer that selects the origin server in a round-robin fashion.
+    """
+    def __init__(self, servers: dict[str, OriginServer] = None):
+        """
+        Initialize the round-robin load balancer with the given servers.
+        
+        Args:
+            servers (dict[str, OriginServer]): A dictionary of servers with hostnames as keys and OriginServer instances as values.
+        """
+        super().__init__(servers)
+        self.current_index = 0
+        
+    async def get_next_server(self):
+        """
+        Get the next origin server based on the round-robin selection algorithm.
+        
+        Returns:
+            OriginServer: The next server to handle the request.
+        """
+        host = self.hosts[self.current_index]
+        async with self.lock:
+            self.current_index = (self.current_index + 1) % len(self.hosts)
+        return self.servers[host]
