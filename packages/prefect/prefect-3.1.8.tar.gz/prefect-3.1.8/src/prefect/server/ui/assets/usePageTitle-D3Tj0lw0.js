0d694b1ef5640464dbf2504ca7abf5d19315bf33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:06:09 +0000

    Add package prefect/3.1.8/prefect-3.1.8.tar.gz

---
+++ packages/prefect/prefect-3.1.8.tar.gz/prefect-3.1.8/src/prefect/server/ui/assets/usePageTitle-D3Tj0lw0.js
@@ -0,0 +1,2 @@
+import{e as a,bw as c,k as n}from"./index-BfXinIUU.js";function i(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>n(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-D3Tj0lw0.js.map
---
+++ packages/prefect/prefect-3.1.8.tar.gz/prefect-3.1.8/src/prefect/server/ui/assets/usePrefectApi-j20OJaO_.js
@@ -0,0 +1,2 @@
+import{cD as r}from"./index-BfXinIUU.js";import{p as e}from"./api-CPUqIYs_.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-j20OJaO_.js.map
