ec097f219dd6831f732cf1b3f7dfe3ade73bf394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:13:05 +0000

    Add package s3p-sdk/0.2.9/s3p_sdk-0.2.9-py3-none-any.whl

---
+++ packages/s3p-sdk/s3p_sdk-0.2.9-py3-none-any.whl/s3p_sdk/module/__init__.py
@@ -0,0 +1,29 @@
+"""
+S3P modules package.
+It consists different modules: its unique name.
+"""
+
+__all__ = [
+    'names',
+    'WebDriver',
+    'TimezoneSafeControl',
+    'CutJunkCharactersFromDocumentText',
+    'FilterOnlyNewDocumentWithDB',
+    'SaveDocument',
+]
+
+WebDriver: str = 'WebDriver'
+TimezoneSafeControl: str = 'TimezoneSafeControl'
+CutJunkCharactersFromDocumentText: str = 'CutJunkCharactersFromDocumentText'
+FilterOnlyNewDocumentWithDB: str = 'FilterOnlyNewDocumentWithDB'
+SaveOnlyNewDocuments: str = 'SaveOnlyNewDocuments'
+SaveDocument: str = 'SaveDocumentToDB'
+
+names = (
+    WebDriver,
+    TimezoneSafeControl,
+    CutJunkCharactersFromDocumentText,
+    FilterOnlyNewDocumentWithDB,
+    SaveOnlyNewDocuments,
+    SaveDocument,
+)
