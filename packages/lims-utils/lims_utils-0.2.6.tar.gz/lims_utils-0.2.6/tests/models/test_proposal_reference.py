437eb621de85b8afe74bdc55537249d895d24d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:13:41 +0000

    Add package lims-utils/0.2.6/lims_utils-0.2.6.tar.gz

---
+++ packages/lims-utils/lims_utils-0.2.6.tar.gz/lims_utils-0.2.6/tests/models/test_proposal_reference.py
@@ -0,0 +1,39 @@
+import pytest
+from pydantic import ValidationError
+
+from lims_utils.models import parse_proposal
+
+
+def test_proposal_reference():
+    """Should create proposal reference"""
+    proposal_reference = parse_proposal("cm123")
+
+    assert proposal_reference.code == "cm"
+    assert proposal_reference.number == 123
+
+
+def test_proposal_reference_visit_number():
+    """Should create proposal reference with visit number"""
+    proposal_reference = parse_proposal("cm123", 1)
+
+    assert proposal_reference.code == "cm"
+    assert proposal_reference.number == 123
+    assert proposal_reference.visit_number == 1
+
+
+def test_proposal_reference_code_not_alpha():
+    """Should raise error if code contains numeric characters"""
+    with pytest.raises(ValidationError):
+        parse_proposal("c0123")
+
+
+def test_proposal_reference_number_not_digit():
+    """Should raise error if number contains non-numeric characters"""
+    with pytest.raises(ValidationError):
+        parse_proposal("c01a3", 1)
+
+
+def test_proposal_reference_too_short():
+    """Should raise error if proposal reference is too short"""
+    with pytest.raises(ValueError):
+        parse_proposal("c", 1)
