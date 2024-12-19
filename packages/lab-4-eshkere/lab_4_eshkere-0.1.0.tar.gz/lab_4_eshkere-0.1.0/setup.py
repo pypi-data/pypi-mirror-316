501d53a0f5147adbd1d1986c49c3607e65cffbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 03:13:34 +0000

    Add package lab-4-eshkere/0.1.0/lab_4_eshkere-0.1.0.tar.gz

---
+++ packages/lab-4-eshkere/lab_4_eshkere-0.1.0.tar.gz/lab_4_eshkere-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lab_4_eshkere',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='',
+    author='BOSS431',
+    author_email='ony.resset@gmail.com',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
