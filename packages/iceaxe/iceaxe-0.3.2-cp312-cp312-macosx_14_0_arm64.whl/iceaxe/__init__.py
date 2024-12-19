592bea8c5b261ac91f37e1de8e867b4fddd80d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 06:05:35 +0000

    Add package iceaxe/0.3.2/iceaxe-0.3.2-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/iceaxe/iceaxe-0.3.2-cp312-cp312-macosx_14_0_arm64.whl/iceaxe/__init__.py
@@ -0,0 +1,18 @@
+from .base import (
+    IndexConstraint as IndexConstraint,
+    TableBase as TableBase,
+    UniqueConstraint as UniqueConstraint,
+)
+from .field import Field as Field
+from .functions import func as func
+from .postgres import PostgresDateTime as PostgresDateTime, PostgresTime as PostgresTime
+from .queries import (
+    QueryBuilder as QueryBuilder,
+    and_ as and_,
+    delete as delete,
+    or_ as or_,
+    select as select,
+    update as update,
+)
+from .session import DBConnection as DBConnection
+from .typing import column as column
