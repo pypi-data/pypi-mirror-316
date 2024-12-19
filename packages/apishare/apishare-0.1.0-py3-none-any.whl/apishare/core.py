10d574b06613bf841d1d494c180d58b2554a8a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-19 16:04:16 +0000

    Add package apishare/0.1.0/apishare-0.1.0-py3-none-any.whl

---
+++ packages/apishare/apishare-0.1.0-py3-none-any.whl/apishare/core.py
@@ -0,0 +1,10 @@
+class APIShare:
+    """Main class for API sharing functionality"""
+    
+    def __init__(self):
+        """Initialize APIShare instance"""
+        pass
+    
+    def example_method(self):
+        """Example method"""
+        return "Hello from APIShare!"
