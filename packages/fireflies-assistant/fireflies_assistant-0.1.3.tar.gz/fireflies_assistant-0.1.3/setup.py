ce25d88bb72aaf86f9643c8c988cc3f36a3bc76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:04:49 +0000

    Add package fireflies-assistant/0.1.3/fireflies_assistant-0.1.3.tar.gz

---
+++ packages/fireflies-assistant/fireflies_assistant-0.1.3.tar.gz/fireflies_assistant-0.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fireflies-assistant",
+    version="0.1.3",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Click",
+        "requests",
+        "python-dotenv",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ff=meet_joiner:cli",
+        ],
+    },
+    author="Dan Schwartz",
+    author_email="dan.schwartz@trilogy.com",
+    description="A CLI tool for managing Fireflies.ai meetings and transcripts",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/trilogy-group/pocs/tree/main/fireflies-assistant",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
