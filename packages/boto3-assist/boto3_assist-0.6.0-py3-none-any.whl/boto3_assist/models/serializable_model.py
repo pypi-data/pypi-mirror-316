ead7ca2405617ac7e684a495189a6e47fc90caa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:05:16 +0000

    Add package boto3-assist/0.6.0/boto3_assist-0.6.0-py3-none-any.whl

---
+++ packages/boto3-assist/boto3_assist-0.6.0-py3-none-any.whl/boto3_assist/models/serializable_model.py
@@ -0,0 +1,9 @@
+"""
+Geek Cafe, LLC
+Maintainers: Eric Wilson
+MIT License.  See Project Root for the license information.
+"""
+
+from __future__ import annotations
+from typing import TypeVar, Dict, Any
+from boto3_assist.utilities.serialization_utility import SerializableModel
