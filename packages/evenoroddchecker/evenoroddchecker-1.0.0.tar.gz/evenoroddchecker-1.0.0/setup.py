758caa07046857d208d6611b38069cce3efe509c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:06:19 +0000

    Add package evenoroddchecker/1.0.0/evenoroddchecker-1.0.0.tar.gz

---
+++ packages/evenoroddchecker/evenoroddchecker-1.0.0.tar.gz/evenoroddchecker-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="evenoroddchecker",
+    version="1.0.0",
+    description="Checks if the number provided is even or odd.",
+    author="superhenryman",
+    author_ermail="kareem.a.alhaj@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
