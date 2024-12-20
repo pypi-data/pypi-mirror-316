4edb549249a2ae01ed279de55bba062e9d11ae5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:05:03 +0000

    Add package libp2p-kit-py/0.0.18/libp2p_kit_py-0.0.18.tar.gz

---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.18.tar.gz/libp2p_kit_py-0.0.18/libp2p_kit_py/libp2p_kit/libp2p.py
@@ -0,0 +1,8 @@
+class  libp2p:
+    def __init__(self, resources, metadata):
+        
+        return None
+    
+    def run(self):
+
+        return True
---
+++ packages/libp2p-kit-py/libp2p_kit_py-0.0.18.tar.gz/libp2p_kit_py-0.0.18/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='lihp2p_kit_py',
+	version='0.0.18',
+	packages=find_packages(),
+	install_requires=[
+	]
+)
