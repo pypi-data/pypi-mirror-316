49e7533ad050bff610cf17fc4b3628774faee194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:05:08 +0000

    Add package codescope/0.2.5/codescope-0.2.5.tar.gz

---
+++ packages/codescope/codescope-0.2.5.tar.gz/codescope-0.2.5/scope/enums/__init__.py
@@ -0,0 +1,6 @@
+from scope.enums.LSPSymbolType import LSPSymbolType
+from scope.enums.ReferenceType import ReferenceType
+from scope.enums.CallTreeType import CallTreeType
+from scope.enums.AllowedSymbols import AllowedSymbols
+
+__all__ = ["LSPSymbolType", "ReferenceType", "CallTreeType", "AllowedSymbols"]
