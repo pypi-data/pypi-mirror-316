9e9632ee68f51ede86b8a3f72aca698c8eb958d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:05:30 +0000

    Add package kalib/0.21.1/kalib-0.21.1.tar.gz

---
+++ packages/kalib/kalib-0.21.1.tar.gz/kalib-0.21.1/src/kalib/__init__.py
@@ -0,0 +1,125 @@
+from kalib import (
+    datastructures,
+    descriptors,
+    importer,
+    internals,
+    loggers,
+    misc,
+    monkey,
+    signals,
+    text,
+    versions,
+)
+from kalib.dataclass import (
+    autoclass,
+    dataclass,
+)
+from kalib.datastructures import (
+    dumps,
+    json,
+    loads,
+    pack,
+    serializer,
+    unpack,
+)
+from kalib.descriptors import (
+    Class,
+    Property,
+    cache,
+    pin,
+)
+from kalib.exceptions import (
+    exception,
+)
+from kalib.hypertext import (
+    HTTP,
+)
+from kalib.importer import (
+    add_path,
+    optional,
+    required,
+    sort,
+)
+from kalib.internals import (
+    Nothing,
+    Who,
+    class_of,
+    is_class,
+    issubstance,
+    stacktrace,
+    unique,
+)
+from kalib.loggers import (
+    Logging,
+    logger,
+)
+from kalib.misc import (
+    Now,
+    Timer,
+    lazy_proxy_to,
+    stamp,
+    tty,
+)
+from kalib.monkey import (
+    Monkey,
+)
+from kalib.signals import (
+    quit_at,
+)
+from kalib.text import (
+    Str,
+)
+from kalib.versions import (
+    Git,
+)
+
+__all__ = (
+    'HTTP',
+    'Class',
+    'Git',
+    'Logging',
+    'Monkey',
+    'Nothing',
+    'Now',
+    'Property',
+    'Str',
+    'Time',
+    'Who',
+    'add_path',
+    'autoclass',
+    'cache',
+    'class_of',
+    'dataclass',
+    'datastructures',
+    'descriptors',
+    'dumps',
+    'exception',
+    'importer',
+    'internals',
+    'is_class',
+    'issubstance',
+    'json',
+    'lazy_proxy_to',
+    'loads',
+    'logger',
+    'loggers',
+    'misc',
+    'monkey',
+    'optional',
+    'pack',
+    'pin',
+    'quit_at',
+    'required',
+    'serializer',
+    'signals',
+    'sort',
+    'stacktrace',
+    'stamp',
+    'text',
+    'tty',
+    'unique',
+    'unpack',
+    'versions',
+)
+
+Time = Timer()
