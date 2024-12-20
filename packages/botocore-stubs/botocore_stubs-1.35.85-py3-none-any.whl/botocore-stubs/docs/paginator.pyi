fd7d2192bc826e5bf77cab313937f7d9b42575bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 07:05:10 +0000

    Add package botocore-stubs/1.35.85/botocore_stubs-1.35.85-py3-none-any.whl

---
+++ packages/botocore-stubs/botocore_stubs-1.35.85-py3-none-any.whl/botocore-stubs/docs/paginator.pyi
@@ -0,0 +1,26 @@
+"""
+Type annotations for botocore.docs.paginator module.
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from typing import Any
+
+from botocore.client import BaseClient
+from botocore.hooks import BaseEventHooks
+from botocore.model import ServiceModel
+
+class PaginatorDocumenter:
+    def __init__(
+        self, client: BaseClient, service_paginator_model: ServiceModel, root_docs_path: str
+    ) -> None: ...
+    def document_paginators(self, section: Any) -> None: ...
+
+def document_paginate_method(
+    section: Any,
+    paginator_name: str,
+    event_emitter: BaseEventHooks,
+    service_model: ServiceModel,
+    paginator_config: Any,
+    include_signature: bool = ...,
+) -> None: ...
