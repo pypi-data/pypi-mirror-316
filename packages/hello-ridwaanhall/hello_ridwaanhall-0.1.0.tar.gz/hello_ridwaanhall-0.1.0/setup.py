576435fd14ad7669f3d8eec104ff8173a7a30ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:05:12 +0000

    Add package hello-ridwaanhall/0.1.0/hello_ridwaanhall-0.1.0.tar.gz

---
+++ packages/hello-ridwaanhall/hello_ridwaanhall-0.1.0.tar.gz/hello_ridwaanhall-0.1.0/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_ridwaanhall',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],  # Add your dependencies here
+    author='Ridwan Halim',
+    author_email='ridwaanhall.dev@gmail.com',
+    description='A package that says hello to Ridwaan',
+    url='https://github.com/ridwaanhall/hello_ridwaanhall',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
