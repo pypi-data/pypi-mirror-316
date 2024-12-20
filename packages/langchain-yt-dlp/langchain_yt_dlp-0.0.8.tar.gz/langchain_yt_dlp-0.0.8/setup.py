79977fa2d5415514048dd80c9c130539c3d2583c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:05:03 +0000

    Add package langchain-yt-dlp/0.0.8/langchain_yt_dlp-0.0.8.tar.gz

---
+++ packages/langchain-yt-dlp/langchain_yt_dlp-0.0.8.tar.gz/langchain_yt_dlp-0.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain-yt-dlp',
+    version='0.0.8',
+    description='YouTube loader for LangChain using yt-dlp',
+    long_description=open('README.md').read(),
+    url="https://github.com/aqib0770/langchain-yt-dlp",
+    author='Aqib Ansari',
+    author_email='aqibansari72a@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    long_description_content_type='text/markdown',
+    keywords='langchain yt-dlp loader',
+    install_requires=[
+        'yt-dlp',
+        'langchain',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.8',
+)
