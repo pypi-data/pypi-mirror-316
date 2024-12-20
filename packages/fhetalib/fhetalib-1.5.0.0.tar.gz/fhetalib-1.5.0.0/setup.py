cb9c6e62927bb6d42ba5a12fa08e7bcb6ecaacd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:21 +0000

    Add package fhetalib/1.5.0.0/fhetalib-1.5.0.0.tar.gz

---
+++ packages/fhetalib/fhetalib-1.5.0.0.tar.gz/fhetalib-1.5.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fhetalib",
+    version="1.5.0.0",
+    description="",
+    author="FHeta",
+    packages=find_packages(),
+    install_requires=[
+        "telethon",
+        "requests",
+        "flask",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
