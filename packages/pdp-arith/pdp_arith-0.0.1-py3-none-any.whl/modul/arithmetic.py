8b715a7281deb9400592be3eac74eee30ce9be52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:09:06 +0000

    Add package pdp-arith/0.0.1/pdp_arith-0.0.1-py3-none-any.whl

---
+++ packages/pdp-arith/pdp_arith-0.0.1-py3-none-any.whl/modul/arithmetic.py
@@ -0,0 +1,14 @@
+def summa_2(a, b):
+    return a + b
+
+
+def summa_3(a, b, c):
+    return a + b + c
+
+
+def summa_4(a, b, c, d):
+    return a + b + c + d
+
+
+def summa_n(*args):
+    return sum(args)
