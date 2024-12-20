737b0fa132eb57061d32b1be5f1cd59eefbec4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:04:59 +0000

    Add package toolmate/0.5.87/toolmate-0.5.87-py3-none-any.whl

---
+++ packages/toolmate/toolmate-0.5.87-py3-none-any.whl/toolmate/setup.py
@@ -0,0 +1,147 @@
+from toolmate import config, print2
+
+import os, shutil, argparse, pyperclip, subprocess
+from pathlib import Path
+
+from toolmate import updateApp, configFile, getOllamaServerClient, exportOllamaModels
+from toolmate.utils.assistant import ToolMate
+from prompt_toolkit.shortcuts import set_title, clear_title
+
+
+def main():
+    print(f"Setting up {config.toolMateAIName} ...")
+
+    # Create the parser
+    parser = argparse.ArgumentParser(description="ToolMate AI setup options")
+    # Add arguments
+    parser.add_argument('-ag', '--autogen', action='store_true', dest='autogen', help="configure AutoGen parameters; applicable to plugin 'create_agents'.")
+    parser.add_argument('-b', '--backend', action='store_true', dest='backend', help="configure AI backend and models")
+    parser.add_argument('-cs', '--chatsystem', action='store_true', dest='chatsystem', help="configure chat system message")
+    parser.add_argument('-d', '--developer', action='store', dest='developer', help="configure developer mode; true / false")
+    parser.add_argument('-ed', '--editor', action='store_true', dest='editor', help="configure custom editor")
+    parser.add_argument('-ec', '--editconfigs', action='store_true', dest='editconfigs', help="configure config.py")
+    parser.add_argument('-em', '--exportmodels', action='store', dest='exportmodels', help="""export models, downloaded with ollama, to ~/toolmate/LLMs/gguf/; pass a list of models for the export, e.g. "['llama3.2:1b','llama3.2:3b']"; pass an empty list "[]" to export all downloaded models""")
+    parser.add_argument('-k', '--apikeys', action='store_true', dest='apikeys', help="configure API keys")
+    parser.add_argument('-mo', '--maximumoutput', action='store_true', dest='maximumoutput', help="configure maximum output tokens")
+    parser.add_argument('-p', '--plugins', action='store_true', dest='plugins', help="configure plugins")
+    parser.add_argument('-rt', '--riskthreshold', action='store_true', dest='riskthreshold', help="configure the risk threshold for user confirmation before code execution")
+    parser.add_argument('-sg', '--speechgeneration', action='store_true', dest='speechgeneration', help="configure speech generation")
+    parser.add_argument('-so', '--searchoptions', action='store_true', dest='searchoptions', help="configure search options")
+    parser.add_argument('-sr', '--speechrecognition', action='store_true', dest='speechrecognition', help="configure speech recognition")
+    parser.add_argument('-t', '--temperature', action='store_true', dest='temperature', help="configure inference temperature")
+    parser.add_argument('-tms', '--tmsystems', action='store_true', dest='tmsystems', help="configure chat system messages for running with commands `tms1`, `tms2`, `tms3`, ... `tms20`")
+    parser.add_argument('-tmt', '--tmtools', action='store_true', dest='tmtools', help="configure tools for running with commands `tmt1`, `tmt2`, `tmt3`, ... `tmt20`")
+    parser.add_argument('-ta', '--toolagent', action='store_true', dest='toolagent', help="configure tool selection agent")
+    parser.add_argument('-ws', '--windowsize', action='store_true', dest='windowsize', help="configure context window size")
+    parser.add_argument('-ww', '--wordwrap', action='store', dest='wordwrap', help="configure word wrap; true / false")
+    # Parse arguments
+    args = parser.parse_args()
+    # Check what kind of arguments were provided and perform actions accordingly
+
+    # update to the latest version
+    config.tempInterface = ""
+    config.custom_config = ""
+    config.initialCompletionCheck = False
+    config.includeIpInDeviceInfoTemp = False
+    config.defaultEntry = ""
+    config.accept_default = False
+
+    # set window title
+    set_title(config.toolMateAIName)
+
+    config.toolmate = ToolMate(plugins=True if args.tmsystems or args.tmtools else False)
+
+    if args.exportmodels:
+        exportmodels = eval(args.exportmodels)
+        if isinstance(exportmodels, list):
+            exportOllamaModels(exportmodels)
+        elif isinstance(exportmodels, str):
+            exportOllamaModels(exportmodels.split())
+        else:
+            print2("""To export models, downloaded with ollama, either:
+* pass a list of models for the export, e.g. "['llama3.2:1b','llama3.2:3b']"
+* pass an empty list "[]" to export all downloaded models""")
+
+    if args.backend:
+        config.toolmate.setLlmModel()
+    if args.chatsystem:
+        config.toolmate.setCustomSystemMessage()
+    if args.developer:
+        if args.developer.lower() == "true":
+            config.developer = True
+            print2("Developer mode enabled!")
+        elif args.developer.lower() == "false":
+            config.developer = False
+            print2("Developer mode disabled!")
+        else:
+            print2("Developer mode unchanged! Accept 'True' or 'False' only!")
+    if args.editconfigs:
+        config.toolmate.editConfigs()
+    if args.apikeys:
+        config.toolmate.changeAPIkeys()
+    if args.maximumoutput:
+        config.toolmate.setMaxTokens()
+    if args.plugins:
+        config.toolmate.selectPlugins()
+    if args.tmsystems:
+        config.toolmate.setTmsMessages()
+    if args.tmtools:
+        config.toolmate.setTmtTools()
+    if args.speechgeneration:
+        config.toolmate.setTextToSpeechConfig()
+    if args.speechrecognition:
+        config.toolmate.setSpeechToTextConfig()
+    if args.temperature:
+        config.toolmate.setTemperature()
+    if args.toolagent:
+        config.toolmate.setToolSelectionConfigs()
+    if args.riskthreshold:
+        config.toolmate.manageCodeExecutionRisk()
+    if args.windowsize:
+        config.toolmate.setContextWindowSize()
+    if args.wordwrap:
+        if args.wordwrap.lower() == "true":
+            config.wrapWords = True
+            print2("Word wrap enabled!")
+        elif args.wordwrap.lower() == "false":
+            config.wrapWords = False
+            print2("Word wrap disabled!")
+        else:
+            print2("Word wrap unchanged! Accept 'True' or 'False' only!")
+    if args.searchoptions:
+        config.toolmate.changeSearchSettings()
+    if args.editor:
+        config.toolmate.setCustomTextEditor()
+    if args.autogen:
+        config.toolmate.setAutoGenBuilderConfig()
+
+    # unload llama.cpp model to free VRAM
+    try:
+        config.llamacppToolModel.close()
+        print("Llama.cpp model unloaded!")
+    except:
+        pass
+    if config.llmInterface == "ollama":
+        getOllamaServerClient().generate(model=config.ollamaToolModel, keep_alive=0, stream=False,)
+        print(f"Ollama model '{config.ollamaToolModel}' unloaded!")
+    if hasattr(config, "llamacppToolModel"):
+        del config.llamacppToolModel
+
+    # delete temporary content
+    try:
+        tempFolder = os.path.join(config.toolMateAIFolder, "temp")
+        shutil.rmtree(tempFolder, ignore_errors=True)
+        Path(tempFolder).mkdir(parents=True, exist_ok=True)
+    except:
+        pass
+
+    # backup configurations
+    config.saveConfig()
+    if os.path.isdir(config.localStorage):
+        shutil.copy(configFile, os.path.join(config.localStorage, "config_lite_backup.py" if config.isLite else "config_backup.py"))
+
+    # clear title
+    clear_title()
+
+if __name__ == "__main__":
+    main()
