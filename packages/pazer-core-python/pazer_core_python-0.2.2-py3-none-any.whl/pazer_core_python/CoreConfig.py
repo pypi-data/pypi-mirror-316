b2e1a8811684a1afd023c545ec8f32109b20213b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:08:35 +0000

    Add package pazer-core-python/0.2.2/pazer_core_python-0.2.2-py3-none-any.whl

---
+++ packages/pazer-core-python/pazer_core_python-0.2.2-py3-none-any.whl/pazer_core_python/CoreConfig.py
@@ -0,0 +1,29 @@
+import os
+from pydantic import BaseModel, Field
+
+
+class CoreConfig(BaseModel):
+    TITLE: str = "Pazer Core - Python"
+    VERSION: str = "0.1.0"
+    CROSS_ALLOW_CROSS: bool = False
+    CROSS_ALLOW_ORIGINS: list | None = None
+    CROSS_ALLOW_CREDENTIALS: bool = False
+    CROSS_ALLOW_METHODS: list | None = None
+    CROSS_ALLOW_HEADERS: list | None = None
+    DATABASE_HOST: dict | None = None
+    SESSION_HOST: dict | None = None
+    @staticmethod
+    def MODE()->str:
+        return os.getenv("MODE","NoneMode")
+
+    @staticmethod
+    def DEBUG()->bool:
+        return os.getenv("DEBUG","False") in ["True","1","true", True]
+
+    @staticmethod
+    def LOG_ENABLE()->bool:
+        return os.getenv("LOG_ENABLE","False") in ["True","1","true", True]
+
+    @staticmethod
+    def LOG_PATH()->str:
+        return os.getenv("LOG_PATH","./Logs/")
