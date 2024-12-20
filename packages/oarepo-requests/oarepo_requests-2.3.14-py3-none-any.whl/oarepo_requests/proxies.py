3d03db6c30301c5b8167a702abe3937f9155bcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:04:44 +0000

    Add package oarepo-requests/2.3.14/oarepo_requests-2.3.14-py3-none-any.whl

---
+++ packages/oarepo-requests/oarepo_requests-2.3.14-py3-none-any.whl/oarepo_requests/proxies.py
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Proxy objects for accessing the current application's requests service and resource."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from flask import current_app
+from werkzeug.local import LocalProxy
+
+if TYPE_CHECKING:
+    from oarepo_requests.ext import OARepoRequests
+    from oarepo_requests.resources.oarepo.resource import OARepoRequestsResource
+    from oarepo_requests.services.oarepo.service import OARepoRequestsService
+
+current_oarepo_requests: OARepoRequests = LocalProxy(  # type: ignore
+    lambda: current_app.extensions["oarepo-requests"]
+)
+current_oarepo_requests_service: OARepoRequestsService = LocalProxy(  # type: ignore
+    lambda: current_app.extensions["oarepo-requests"].requests_service
+)
+current_oarepo_requests_resource: OARepoRequestsResource = LocalProxy(  # type: ignore
+    lambda: current_app.extensions["oarepo-requests"].requests_resource
+)
