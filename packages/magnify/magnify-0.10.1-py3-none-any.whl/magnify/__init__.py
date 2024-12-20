aacbe0d4b7caeaa3432a8d2bbb6e21e204c4d251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:29 +0000

    Add package magnify/0.10.1/magnify-0.10.1-py3-none-any.whl

---
+++ packages/magnify/magnify-0.10.1-py3-none-any.whl/magnify/__init__.py
@@ -0,0 +1,46 @@
+__version__ = "0.10.1"
+
+__all__ = [
+    "mini_chip",
+    "mini_chip_pipe",
+    "pc_chip",
+    "pc_chip_pipe",
+    "ps_chip",
+    "ps_chip_pipe",
+    "mrbles",
+    "mrbles_pipe",
+    "beads",
+    "beads_pipe",
+    "image",
+    "image_pipe",
+    "save",
+    "load",
+    "accessor",
+    "filter",
+    "find",
+    "identify",
+    "postprocess",
+    "preprocess",
+    "reader",
+    "stitch",
+]
+
+from . import accessor, filter, find, identify, postprocess, preprocess, reader, stitch
+from .file import (
+    load,
+    save,
+)
+from .registry import (
+    beads,
+    beads_pipe,
+    image,
+    image_pipe,
+    mini_chip,
+    mini_chip_pipe,
+    mrbles,
+    mrbles_pipe,
+    pc_chip,
+    pc_chip_pipe,
+    ps_chip,
+    ps_chip_pipe,
+)
