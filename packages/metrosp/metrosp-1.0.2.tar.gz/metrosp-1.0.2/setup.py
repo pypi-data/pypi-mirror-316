c1890be8112f41e631a7c8abcfff64747b52c43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:04:38 +0000

    Add package metrosp/1.0.2/metrosp-1.0.2.tar.gz

---
+++ packages/metrosp/metrosp-1.0.2.tar.gz/metrosp-1.0.2/setup.py
@@ -0,0 +1,20 @@
+# coding: UTF-8
+from setuptools import setup
+
+setup(
+    name = 'metrosp',
+    version = '1.0.2',
+    packages = ['metrosp'],
+    author = 'Renato Lipi',
+    author_email = '',
+    description = 'Ferramenta de linha de comando para recuperar situação atual do Metro de SP',
+    license = 'LGPLv3',
+    keywords = 'metro metrosp SP',
+    url = 'https://github.com/renatolipi/status_metro_sp',
+    long_description_content_type="text/plain",
+    long_description = 'Ferramenta de linha de comando para recuperar situação atual das linhas do Metro de SP. Para mais infos: https://github.com/renatolipi/status_metro_sp/blob/master/README.md',
+    install_requires = ['beautifulsoup4==4.12.3', 'requests==2.32.3',],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+)
