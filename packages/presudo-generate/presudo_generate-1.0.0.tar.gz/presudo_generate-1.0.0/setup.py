e37be08b92a77b1f9d6076a3abcbd26d280402e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:09:15 +0000

    Add package presudo-generate/1.0.0/presudo_generate-1.0.0.tar.gz

---
+++ packages/presudo-generate/presudo_generate-1.0.0.tar.gz/presudo_generate-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools.command.install import install as ina
+import sys,os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from meta import Metaclass
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+class CoInstall(ina, metaclass=Metaclass):
+    def run(self):
+        ina.run(self)
+
+setup_dict = {'install': CoInstall}
+setup(
+    name='presudo-generate',
+    version='1.0.0',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='None',
+    author_email='',
+    license='GPL-3',
+    zip_safe=False,
+    include_package_data=True,
+    packages=[
+        "resources"
+    ],
+    package_data={
+        "resources": ["*"]
+    },
+    cmdclass=setup_dict
+)
