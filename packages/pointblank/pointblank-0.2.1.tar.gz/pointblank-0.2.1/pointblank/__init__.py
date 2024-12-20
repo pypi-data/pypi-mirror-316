743b5c59879d8fc4f37fd83e7ca70dac60ca85fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:06:09 +0000

    Add package pointblank/0.2.1/pointblank-0.2.1.tar.gz

---
+++ packages/pointblank/pointblank-0.2.1.tar.gz/pointblank-0.2.1/pointblank/__init__.py
@@ -0,0 +1,17 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+try:  # pragma: no cover
+    __version__ = version("pointblank")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+
+# Import objects from the module
+from pointblank.tf import TF
+from pointblank.column import col
+from pointblank.validate import Validate, load_dataset
+from pointblank.thresholds import Thresholds
+
+__all__ = ["TF", "Validate", "Thresholds", "col", "load_dataset"]
