19b3a6adffcaf08c5bd14313e8bec72d5107a97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:04:59 +0000

    Add package RoughPy/0.2.0/roughpy-0.2.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/RoughPy/roughpy-0.2.0-cp39-cp39-macosx_11_0_arm64.whl/roughpy/__init__.py
@@ -0,0 +1,38 @@
+import importlib.metadata as _ilm
+import os
+import platform
+
+from pathlib import Path as _Path
+
+try:
+    __version__ = _ilm.version("RoughPy")
+except _ilm.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+
+def _add_dynload_location(path: _Path):
+    if platform.system() == "Windows":
+        os.add_dll_directory(str(path))
+        return
+
+
+if platform.system() == "Windows":
+    LIBS_DIR = _Path(__file__).parent.parent / "roughpy.libs"
+    if LIBS_DIR.exists():
+        os.add_dll_directory(str(LIBS_DIR))
+
+try:
+    iomp = _ilm.distribution("intel-openmp")
+    libs = [f for f in iomp.files if f.name.startswith("libiomp5")]
+    if libs:
+        _add_dynload_location(libs[0].locate().resolve().parent)
+    del iomp
+    del libs
+except _ilm.PackageNotFoundError:
+    pass
+
+import roughpy._roughpy
+from roughpy._roughpy import *
+
+from . import tensor_functions
+
