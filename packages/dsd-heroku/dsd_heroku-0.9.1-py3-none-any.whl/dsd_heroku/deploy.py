0bbb994aeeadbc37618c6f329544b3555efe0c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:04:47 +0000

    Add package dsd-heroku/0.9.1/dsd_heroku-0.9.1-py3-none-any.whl

---
+++ packages/dsd-heroku/dsd_heroku-0.9.1-py3-none-any.whl/dsd_heroku/deploy.py
@@ -0,0 +1,20 @@
+"""Manages all Heroku-specific aspects of the deployment process."""
+
+import simple_deploy
+
+from dsd_heroku.platform_deployer import PlatformDeployer
+from .plugin_config import PluginConfig
+
+
+@simple_deploy.hookimpl
+def simple_deploy_get_plugin_config():
+    """Get platform-specific attributes needed by core."""
+    plugin_config = PluginConfig()
+    return plugin_config
+
+
+@simple_deploy.hookimpl
+def simple_deploy_deploy():
+    """Carry out platform-specific deployment steps."""
+    platform_deployer = PlatformDeployer()
+    platform_deployer.deploy()
