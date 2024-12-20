22bddfbc65cb9d7ab33c74335c58329b48169284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:04:54 +0000

    Add package mtmaisdk/0.4.186/mtmaisdk-0.4.186.tar.gz

---
+++ packages/mtmaisdk/mtmaisdk-0.4.186.tar.gz/mtmaisdk-0.4.186/mtmaisdk/workflow.py
@@ -0,0 +1,200 @@
+import functools
+from typing import Any, Callable, List, Tuple
+
+from mtmaisdk import ConcurrencyLimitStrategy
+from mtmaisdk.contracts.workflows_pb2 import (
+    CreateWorkflowJobOpts,
+    CreateWorkflowStepOpts,
+    CreateWorkflowVersionOpts,
+    WorkflowConcurrencyOpts,
+    WorkflowKind,
+)
+from mtmaisdk.logger import logger
+
+stepsType = List[Tuple[str, Callable[..., Any]]]
+
+
+class ConcurrencyExpression:
+    """
+    Defines concurrency limits for a workflow using a CEL expression.
+
+    Args:
+        expression (str): CEL expression to determine concurrency grouping. (i.e. "input.user_id")
+        max_runs (int): Maximum number of concurrent workflow runs.
+        limit_strategy (ConcurrencyLimitStrategy): Strategy for handling limit violations.
+
+    Example:
+        ConcurrencyExpression("input.user_id", 5, ConcurrencyLimitStrategy.CANCEL_IN_PROGRESS)
+    """
+
+    def __init__(
+        self, expression: str, max_runs: int, limit_strategy: ConcurrencyLimitStrategy
+    ):
+        self.expression = expression
+        self.max_runs = max_runs
+        self.limit_strategy = limit_strategy
+
+
+class WorkflowMeta(type):
+    def __new__(cls, name, bases, attrs):
+        concurrencyActions: stepsType = [
+            (getattr(func, "_concurrency_fn_name"), attrs.pop(func_name))
+            for func_name, func in list(attrs.items())
+            if hasattr(func, "_concurrency_fn_name")
+        ]
+        steps: stepsType = [
+            (getattr(func, "_step_name"), attrs.pop(func_name))
+            for func_name, func in list(attrs.items())
+            if hasattr(func, "_step_name")
+        ]
+        onFailureSteps: stepsType = [
+            (getattr(func, "_on_failure_step_name"), attrs.pop(func_name))
+            for func_name, func in list(attrs.items())
+            if hasattr(func, "_on_failure_step_name")
+        ]
+
+        # Define __init__ and get_step_order methods
+        original_init = attrs.get("__init__")  # Get the original __init__ if it exists
+
+        def __init__(self, *args, **kwargs):
+            if original_init:
+                original_init(self, *args, **kwargs)  # Call original __init__
+
+        def get_service_name(namespace: str) -> str:
+            return f"{namespace}{name.lower()}"
+
+        @functools.cache
+        def get_actions(self, namespace: str) -> stepsType:
+            serviceName = get_service_name(namespace)
+            func_actions = [
+                (serviceName + ":" + func_name, func) for func_name, func in steps
+            ]
+            concurrency_actions = [
+                (serviceName + ":" + func_name, func)
+                for func_name, func in concurrencyActions
+            ]
+            onFailure_actions = [
+                (serviceName + ":" + func_name, func)
+                for func_name, func in onFailureSteps
+            ]
+
+            return func_actions + concurrency_actions + onFailure_actions
+
+        # Add these methods and steps to class attributes
+        attrs["__init__"] = __init__
+        attrs["get_actions"] = get_actions
+
+        for step_name, step_func in steps:
+            attrs[step_name] = step_func
+
+        def get_name(self, namespace: str):
+            return namespace + attrs["name"]
+
+        attrs["get_name"] = get_name
+
+        def get_description(self):
+            return self.description
+
+        attrs["get_description"] = get_description
+
+        cron_triggers = attrs["on_crons"]
+        version = attrs["version"]
+        schedule_timeout = attrs["schedule_timeout"]
+        sticky = attrs["sticky"]
+        default_priority = attrs["default_priority"]
+
+        @functools.cache
+        def get_create_opts(self, namespace: str):
+            serviceName = get_service_name(namespace)
+            name = self.get_name(namespace)
+            # description = self.get_description()
+            description = self.get_description()
+            event_triggers = [namespace + event for event in attrs["on_events"]]
+            createStepOpts: List[CreateWorkflowStepOpts] = [
+                CreateWorkflowStepOpts(
+                    readable_id=step_name,
+                    action=serviceName + ":" + step_name,
+                    timeout=func._step_timeout or "60s",
+                    inputs="{}",
+                    parents=[x for x in func._step_parents],
+                    retries=func._step_retries,
+                    rate_limits=func._step_rate_limits,
+                    worker_labels=func._step_desired_worker_labels,
+                )
+                for step_name, func in steps
+            ]
+
+            concurrency: WorkflowConcurrencyOpts | None = None
+
+            if len(concurrencyActions) > 0:
+                action = concurrencyActions[0]
+
+                concurrency = WorkflowConcurrencyOpts(
+                    action=serviceName + ":" + action[0],
+                    max_runs=action[1]._concurrency_max_runs,
+                    limit_strategy=action[1]._concurrency_limit_strategy,
+                )
+
+            if self.concurrency_expression:
+                concurrency = WorkflowConcurrencyOpts(
+                    expression=self.concurrency_expression.expression,
+                    max_runs=self.concurrency_expression.max_runs,
+                    limit_strategy=self.concurrency_expression.limit_strategy,
+                )
+
+            if len(concurrencyActions) > 0 and self.concurrency_expression:
+                raise ValueError(
+                    "Error: Both concurrencyActions and concurrency_expression are defined. Please use only one concurrency configuration method."
+                )
+
+            on_failure_job: List[CreateWorkflowJobOpts] | None = None
+
+            if len(onFailureSteps) > 0:
+                func_name, func = onFailureSteps[0]
+                on_failure_job = CreateWorkflowJobOpts(
+                    name=name + "-on-failure",
+                    description=description,  # 增加
+                    steps=[
+                        CreateWorkflowStepOpts(
+                            readable_id=func_name,
+                            action=serviceName + ":" + func_name,
+                            timeout=func._on_failure_step_timeout or "60s",
+                            inputs="{}",
+                            parents=[],
+                            retries=func._on_failure_step_retries,
+                            rate_limits=func._on_failure_step_rate_limits,
+                        )
+                    ],
+                )
+
+            validated_priority = (
+                max(1, min(3, default_priority)) if default_priority else None
+            )
+            if validated_priority != default_priority:
+                logger.warning(
+                    "Warning: Default Priority Must be between 1 and 3 -- inclusively. Adjusted to be within the range."
+                )
+
+            return CreateWorkflowVersionOpts(
+                name=name,
+                kind=WorkflowKind.DAG,
+                version=version,
+                description=description,  # 增加
+                event_triggers=event_triggers,
+                cron_triggers=cron_triggers,
+                schedule_timeout=schedule_timeout,
+                sticky=sticky,
+                jobs=[
+                    CreateWorkflowJobOpts(
+                        name=name,
+                        steps=createStepOpts,
+                    )
+                ],
+                on_failure_job=on_failure_job,
+                concurrency=concurrency,
+                default_priority=validated_priority,
+            )
+
+        attrs["get_create_opts"] = get_create_opts
+
+        return super(WorkflowMeta, cls).__new__(cls, name, bases, attrs)
