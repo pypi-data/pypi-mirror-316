a16d3d23fb3b62299641387e6779b13744deb347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:29 +0000

    Add package msconvert/1.3.post3/msconvert-1.3.post3.tar.gz

---
+++ packages/msconvert/msconvert-1.3.post3.tar.gz/msconvert-1.3.post3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "setuptools",
+    "setuptools_scm",
+    "docker",
+    "psutil",
+]
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='msconvert',
+    author="Falk Boudewijn Schimweg",
+    author_email='git@falk.schimweg.de',
+    license="GNU Affero General Public License v3 or later (AGPLv3+)",
+    description="A wrapper for the pwiz/msconvert docker container with memory limits and automatic retry.",
+    long_description=readme,
+    python_requires='>=3.10',
+    use_scm_version=True,
+    requirements=requirements,
+    packages=find_packages(include=['msconvert', 'msconvert.*']),
+    setup_requires=['setuptools_scm'],
+    entry_points={
+        'console_scripts': [
+            'msconvert=msconvert.__cli:main',
+        ],
+    },
+)
