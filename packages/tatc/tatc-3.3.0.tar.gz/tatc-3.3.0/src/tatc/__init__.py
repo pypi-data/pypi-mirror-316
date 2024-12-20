db61cf8db4d68cbeed75d13557548d77bacca407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:50 +0000

    Add package tatc/3.3.0/tatc-3.3.0.tar.gz

---
+++ packages/tatc/tatc-3.3.0.tar.gz/tatc-3.3.0/src/tatc/__init__.py
@@ -0,0 +1,12 @@
+"""
+Tradespace Analysis Toolkit for Constellations (TAT-C)
+"""
+
+from . import analysis
+from . import generation
+from . import schemas
+from . import constants
+from . import utils
+from . import resources
+
+__version__ = "3.3.0"
