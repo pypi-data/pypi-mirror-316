25bef8f1c1b8e0e0c738c20d15af6e589379af99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:05:07 +0000

    Add package digitz/0.5.0/digitz-0.5.0-py3-none-any.whl

---
+++ packages/digitz/digitz-0.5.0-py3-none-any.whl/digitz/enums.py
@@ -0,0 +1,67 @@
+from enum import IntEnum
+import phonenumbers as pn
+
+
+__all__ = [
+    "CountryCodeSource",
+    "MatchType",
+    "NumberParseErrorType",
+    "PhoneNumberFormat",
+    "PhoneNumberType",
+]
+
+
+class CountryCodeSource(IntEnum):
+    """Enum for phone number country code sources."""
+
+    UNSPECIFIED = pn.CountryCodeSource.UNSPECIFIED
+    FROM_NUMBER_WITH_PLUS_SIGN = pn.CountryCodeSource.FROM_NUMBER_WITH_PLUS_SIGN
+    FROM_NUMBER_WITH_IDD = pn.CountryCodeSource.FROM_NUMBER_WITH_IDD
+    FROM_NUMBER_WITHOUT_PLUS_SIGN = pn.CountryCodeSource.FROM_NUMBER_WITHOUT_PLUS_SIGN
+    FROM_DEFAULT_COUNTRY = pn.CountryCodeSource.FROM_DEFAULT_COUNTRY
+
+
+class MatchType(IntEnum):
+    """Enum for phone number match types."""
+
+    EXACT_MATCH = pn.MatchType.EXACT_MATCH
+    NOT_A_NUMBER = pn.MatchType.NOT_A_NUMBER
+    NO_MATCH = pn.MatchType.NO_MATCH
+    NSN_MATCH = pn.MatchType.NSN_MATCH
+    SHORT_NSN_MATCH = pn.MatchType.SHORT_NSN_MATCH
+
+
+class NumberParseErrorType(IntEnum):
+    """Enum for phone number parsing error types."""
+
+    INVALID_COUNTRY_CODE = pn.NumberParseException.INVALID_COUNTRY_CODE
+    NOT_A_NUMBER = pn.NumberParseException.NOT_A_NUMBER
+    TOO_SHORT_AFTER_IDD = pn.NumberParseException.TOO_SHORT_AFTER_IDD
+    TOO_SHORT_NSN = pn.NumberParseException.TOO_SHORT_NSN
+    TOO_LONG = pn.NumberParseException.TOO_LONG
+
+
+class PhoneNumberFormat(IntEnum):
+    """Enum for phone number formats."""
+
+    E164 = pn.PhoneNumberFormat.E164
+    INTERNATIONAL = pn.PhoneNumberFormat.INTERNATIONAL
+    NATIONAL = pn.PhoneNumberFormat.NATIONAL
+    RFC3966 = pn.PhoneNumberFormat.RFC3966
+
+
+class PhoneNumberType(IntEnum):
+    """Enum for phone number types."""
+
+    FIXED_LINE = pn.PhoneNumberType.FIXED_LINE
+    MOBILE = pn.PhoneNumberType.MOBILE
+    FIXED_LINE_OR_MOBILE = pn.PhoneNumberType.FIXED_LINE_OR_MOBILE
+    TOLL_FREE = pn.PhoneNumberType.TOLL_FREE
+    PREMIUM_RATE = pn.PhoneNumberType.PREMIUM_RATE
+    SHARED_COST = pn.PhoneNumberType.SHARED_COST
+    VOIP = pn.PhoneNumberType.VOIP
+    PERSONAL_NUMBER = pn.PhoneNumberType.PERSONAL_NUMBER
+    PAGER = pn.PhoneNumberType.PAGER
+    UAN = pn.PhoneNumberType.UAN
+    VOICEMAIL = pn.PhoneNumberType.VOICEMAIL
+    UNKNOWN = pn.PhoneNumberType.UNKNOWN
