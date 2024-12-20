c396ff551683197cf1fb383c2e87d8324ac14c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:08:53 +0000

    Add package ics-utils/0.0.3/ics_utils-0.0.3.tar.gz

---
+++ packages/ics-utils/ics_utils-0.0.3.tar.gz/ics_utils-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@author: hanyanling
+@date: 2024/5/21 下午5:38
+@email:
+---------
+@summary:
+"""
+
+
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+
+setup(
+    name='ics_utils',
+    version='0.0.3',
+    author='hanyanling',
+    url='https://github.com/StealDivinePower/ics_utils',
+    packages=packages,
+    python_requires='>=3.6',
+    requires=[
+        'requests',
+    ]
+)
