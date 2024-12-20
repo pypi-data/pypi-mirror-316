0c71559d4ee74a0384597066a298fb9010967362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:05:15 +0000

    Add package a9x-webstatistics/1.2.8/a9x_webstatistics-1.2.8.tar.gz

---
+++ packages/a9x-webstatistics/a9x_webstatistics-1.2.8.tar.gz/a9x_webstatistics-1.2.8/tests/test_z090_gencockpit010.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+from pathlib import Path
+from subprocess import run
+
+from a9x_webstatistics.gencockpitV0001 import *
+
+class TestGencockpit010(unittest.TestCase):
+
+    def test_gencockpit010(self):
+        cmddata = run('ls -altr', capture_output=True, shell=True, text=True)
+        print(cmddata.stdout) 
+        
+        # calling runws expecting return 0
+        assert runGenCockpitV0001(infile="webstat.json",outfile="webstat.html",domain="https://logikfabrik.com", omit='') == 0
+        with open('webstat.html') as f:  
+            file_data = f.read()  
+        print(str(file_data))
+        
+
+if __name__ == '__main__':
+    unittest.main()
