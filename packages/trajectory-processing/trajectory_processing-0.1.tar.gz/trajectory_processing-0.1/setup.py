b531b73eedf5f05fdaa655494c87aa43ff739352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:05:02 +0000

    Add package trajectory-processing/0.1/trajectory_processing-0.1.tar.gz

---
+++ packages/trajectory-processing/trajectory_processing-0.1.tar.gz/trajectory_processing-0.1/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="trajectory_processing",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'geopandas',
+        'ptrail',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'joblib',
+        'sklearn',
+        'torch',
+    ],
+    author="Name",
+    author_email="email@example.com",
+    description="A package for processing trajectory data",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/trajectory-processing/trajectory_processing-0.1.tar.gz/trajectory_processing-0.1/trajectory_processing/__init__.py
@@ -0,0 +1,3 @@
+# trajectory_processing/__init__.py
+
+from .preprocess import all, import_and_preprocess_excel, interpolate_cubic_spline_by_index, Median_filtering, Span
