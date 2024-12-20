9ecca7493b9f5730a7e796894b6685a14152bed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:05:08 +0000

    Add package lock-run/0.6.4/lock_run-0.6.4-py3-none-any.whl

---
+++ packages/lock-run/lock_run-0.6.4-py3-none-any.whl/lock_run/main.py
@@ -0,0 +1,58 @@
+import reader
+import handler_code
+import utilitiles
+import argparse
+
+def generate_key(key_len: int = 32):
+    key = reader.CipherReader.generate_key(key_len=key_len)
+    print(key)
+    return key
+
+def cipher_code(root_dir: str, key: str):
+    fix_root = utilitiles.refactor_path(path=root_dir)
+
+    worker = handler_code.Worker(key=key, root_dir=fix_root)
+    text, key_code, path = worker.cipher_file()
+
+    print(f"Project was ciphered in {path} with key: {key_code}")
+
+    return text, key_code, path
+
+def run_file(root_dir: str, key: str, file_path: str):
+    fix_root = utilitiles.refactor_path(path=root_dir)
+
+    worker = handler_code.Worker(key=key, root_dir=fix_root)
+    print(f"run {file_path} from {root_dir}")
+
+    worker.run_cpython(file_path=file_path)
+
+def cipher_code_cui():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--root_dir", type=str, help="root of project", required=True)
+    parser.add_argument("--key", type=str, help="cipher key", required=True)
+
+    args = parser.parse_args()
+
+    cipher_code(root_dir=args.root_dir, key=args.key)
+
+def run_code_cui():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--root_dir", type=str, help="root of project", required=True)
+    parser.add_argument("--key", type=str, help="cipher key", required=True)
+    parser.add_argument("--file_path", type=str, help="file path", required=True)
+
+
+    args = parser.parse_args()
+
+    run_file(root_dir=args.root_dir, key=args.key, file_path=args.file_path)
+
+def gen_key_cui():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--key_len", type=int, help="key len", required=False)
+    args = parser.parse_args()
+    if args.key_len is not None:
+        key_len = args.key_len
+    else:
+        key_len = 32
+    generate_key(key_len=key_len)
+
