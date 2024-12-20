a0860177b349519b34d0bf173e20fd3d809e00d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:05:06 +0000

    Add package jupyterlab-cell-diff/0.2.0/jupyterlab_cell_diff-0.2.0.tar.gz

---
+++ packages/jupyterlab-cell-diff/jupyterlab_cell_diff-0.2.0.tar.gz/jupyterlab_cell_diff-0.2.0/src/plugin.ts
@@ -0,0 +1,36 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import {
+  showCellDiffCommand
+} from './command';
+
+import { 
+  ICellFooterTracker
+} from 'jupyterlab-cell-input-footer';
+
+/**
+ * A JupyterLab plugin providing a command for displaying a diff below a cell.
+ */
+const showDiff: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-cell-diff',
+  requires: [ICellFooterTracker],
+  autoStart: true,
+  activate: async (
+    app: JupyterFrontEnd, 
+    cellFooterTracker: ICellFooterTracker,
+  ) => {
+    await app.serviceManager.ready;
+
+    app.commands.addCommand(
+      'show-diff', 
+      {
+        execute: showCellDiffCommand(cellFooterTracker)
+      }
+    )
+  }
+};
+
+export default [showDiff];
