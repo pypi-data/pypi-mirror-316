190a7c006e8d4fb7aa8e66e20751ef3fae425c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:26 +0000

    Add package graylog-telemidia/1.0.3/graylog_telemidia-1.0.3.tar.gz

---
+++ packages/graylog-telemidia/graylog_telemidia-1.0.3.tar.gz/graylog_telemidia-1.0.3/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='graylog_telemidia',
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'graypy',
+    ],
+    description='Uma abstração da biblioteca graypy, projetada para adaptar as requisições do Graylog ao padrão utilizado pela Telemidia.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    readme="README.md",
+    author='Thales Casa Grande de Lima',
+    author_email='thales.lima@telemidia.net.br',
+    url='http://git.telemidia.net.br/Telemidia/graylog-telemidia-py',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
