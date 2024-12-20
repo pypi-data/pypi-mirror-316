ecd214b479e9b3914720003ac365c3adf94c37d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:05:02 +0000

    Add package pytrilogy/0.0.2.55/pytrilogy-0.0.2.55-py3-none-any.whl

---
+++ packages/pytrilogy/pytrilogy-0.0.2.55-py3-none-any.whl/trilogy/core/exceptions.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class UndefinedConceptException(Exception):
+    def __init__(self, message, suggestions: List[str]):
+        super().__init__(self, message)
+        self.message = message
+        self.suggestions = suggestions
+
+
+class InvalidSyntaxException(Exception):
+    pass
+
+
+class NoDatasourceException(Exception):
+    pass
+
+
+class FrozenEnvironmentException(Exception):
+    pass
+
+
+class AmbiguousRelationshipResolutionException(Exception):
+    def __init__(self, message, parents: List[set[str]]):
+        super().__init__(self, message)
+        self.message = message
+        self.parents = parents
