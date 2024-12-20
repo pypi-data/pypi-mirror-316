eb28747f4e44f9d8afbabbcfa63b802ea6af099a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 07:04:30 +0000

    Add package pywatershed/2.0.1/pywatershed-2.0.1.tar.gz

---
+++ packages/pywatershed/pywatershed-2.0.1.tar.gz/pywatershed-2.0.1/pywatershed/version.py
@@ -0,0 +1,13 @@
+# pywatershed version file automatically created using update_version.py on December 19, 2024 21:53:33  #noqa: E501
+
+__version__ = "2.0.1"
+__pakname__ = "pywatershed"
+
+author_dict = {
+    "James McCreight": "jmccreight@usgs.gov",
+    "Christian D. Langevin": "langevin@usgs.gov",
+    "Joseph D. Hughes": "jdhughes@usgs.gov",
+    "Wesley P. Bonelli": "wbonelli@contractor.usgs.gov",
+}
+__author__ = ", ".join(author_dict.keys())
+__author_email__ = ", ".join(s for _, s in author_dict.items())
