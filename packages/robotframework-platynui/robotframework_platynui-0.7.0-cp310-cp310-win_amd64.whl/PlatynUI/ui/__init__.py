a7a6ef1bb0a4abb81905b9ef87c63a3426baa99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:08:54 +0000

    Add package robotframework-platynui/0.7.0/robotframework_platynui-0.7.0-cp310-cp310-win_amd64.whl

---
+++ packages/robotframework-platynui/robotframework_platynui-0.7.0-cp310-cp310-win_amd64.whl/PlatynUI/ui/__init__.py
@@ -0,0 +1,79 @@
+# SPDX-FileCopyrightText: 2024 Daniel Biehl <daniel.biehl@imbus.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .application import Application
+from .buttons import AbstractButton, Button, CheckBox, Link, RadioButton
+from .combobox import ComboBox
+from .container import Container
+from .control import Control, CustomControl
+from .data_item import DataItem
+from .desktop import Desktop
+from .desktopbase import DesktopBase
+from .dialog import Dialog
+from .edit import Edit
+from .element import Element
+from .graphic import Graphic, GraphicItem
+from .header import Header, HeaderItem
+from .item import Item
+from .lists import List, ListItem
+from .locator import Locator, locator
+from .menus import Menu, MenuBar, MenuItem
+from .orientation import Orientation
+from .pane import Group, Pane
+from .proxies import *
+from .scrollbar import ScrollBar
+from .table import Cell, Row, Table
+from .tabs import TabItem, TabList
+from .text import Text
+from .togglestate import ToggleState
+from .tree import Tree, TreeItem
+from .unknown import Unknown
+from .window import Window
+
+__all__ = [
+    "Application",
+    "Control",
+    "CustomControl",
+    "AbstractButton",
+    "Button",
+    "Link",
+    "Locator",
+    "locator",
+    "ToggleState",
+    "CheckBox",
+    "RadioButton",
+    "Element",
+    "Container",
+    "DataItem",
+    "DesktopBase",
+    "Desktop",
+    "Dialog",
+    "Text",
+    "Edit",
+    "Item",
+    "Header",
+    "HeaderItem",
+    "Orientation",
+    "List",
+    "ListItem",
+    "ComboBox",
+    "Menu",
+    "MenuBar",
+    "MenuItem",
+    "Table",
+    "Cell",
+    "Row",
+    "TabList",
+    "TabItem",
+    "ToggleState",
+    "Tree",
+    "TreeItem",
+    "Unknown",
+    "Window",
+    "ScrollBar",
+    "Graphic",
+    "GraphicItem",
+    "Pane",
+    "Group",
+]
