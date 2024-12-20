6629e17790081ecf7eaebb4dc44eb82ef55d627e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:04:47 +0000

    Add package sentry-feishu-gsmini/1.8/sentry_feishu_gsmini-1.8.tar.gz

---
+++ packages/sentry-feishu-gsmini/sentry_feishu_gsmini-1.8.tar.gz/sentry_feishu_gsmini-1.8/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sentry_feishu_gsmini",
+    version='1.8',
+    author='gsmini',
+    author_email='gsmini@sina.cn',
+    url='https://github.com/gsmini/sentry-feishu',
+    description='A Sentry extension which send errors stats to FeiShu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry feishu notify',
+    include_package_data=True,
+    zip_safe=False,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    entry_points={
+        'sentry.plugins': [
+            'sentry_feishu_gsmini = sentry_feishu_gsmini.plugin:FeiShuPlugin'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+    ]
+)
---
+++ packages/sentry-feishu-gsmini/sentry_feishu_gsmini-1.8.tar.gz/sentry_feishu_gsmini-1.8/src/sentry_feishu_gsmini/forms.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from django import forms
+
+
+class FeiShuOptionsForm(forms.Form):
+    webhook_url = forms.CharField(
+        max_length=255,
+        help_text='robot Webhook url(飞书机器人webhook地址url)'
+    )
