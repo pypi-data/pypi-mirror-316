eafc8edc1beed42c068b06dcb33b5f8079825a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:04:54 +0000

    Add package ry/0.0.23/ry-0.0.23-cp311-cp311-win32.whl

---
+++ packages/ry/ry-0.0.23-cp311-cp311-win32.whl/ry/_types/jiff.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from typing import Literal, TypedDict
+
+__all__ = (
+    "JIFF_ROUND_MODE_STRING",
+    "JIFF_UNIT_STRING",
+    "DateTimeTypedDict",
+    "DateTypedDict",
+    "TimeSpanTypedDict",
+    "TimeTypedDict",
+)
+
+JIFF_UNIT_STRING = Literal[
+    "year",
+    "month",
+    "week",
+    "day",
+    "hour",
+    "minute",
+    "second",
+    "millisecond",
+    "microsecond",
+    "nanosecond",
+]
+
+JIFF_ROUND_MODE_STRING = Literal[
+    "ceil",
+    "floor",
+    "expand",
+    "trunc",
+    "half_ceil",
+    "half_floor",
+    "half_expand",
+    "half_trunc",
+    "half_even",
+]
+
+
+class DateTypedDict(TypedDict):
+    year: int
+    month: int
+    day: int
+
+
+class TimeTypedDict(TypedDict):
+    hour: int
+    minute: int
+    second: int
+    millisecond: int
+    microsecond: int
+    nanosecond: int
+    subsec_nanosecond: int
+
+
+class DateTimeTypedDict(TypedDict):
+    year: int
+    month: int
+    day: int
+    hour: int
+    minute: int
+    second: int
+    millisecond: int
+    microsecond: int
+    nanosecond: int
+    subsec_nanosecond: int
+
+
+class TimeSpanTypedDict(TypedDict):
+    years: int
+    months: int
+    weeks: int
+    days: int
+    hours: int
+    minutes: int
+    seconds: int
+    milliseconds: int
+    microseconds: int
+    nanoseconds: int
