8b18aae61efb71dd38a7833f1bce8351bf247050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:49 +0000

    Add package jewcal/0.7.4/jewcal-0.7.4-py3-none-any.whl

---
+++ packages/jewcal/jewcal-0.7.4-py3-none-any.whl/jewcal/__init__.py
@@ -0,0 +1,41 @@
+"""Package jewcal."""
+
+from sys import modules
+from types import ModuleType
+from typing import cast
+from warnings import warn
+
+from .core import JewCal
+from .models.events import Events
+from .models.jewish_date import JewishDate, Month
+from .models.zmanim import Location, Zmanim
+
+Jewcal = JewCal
+
+
+class _Wrapper:  # pylint: disable=too-few-public-methods
+    """Rename class `Jewcal` to `JewCal`."""
+
+    def __init__(self, wrapped: ModuleType) -> None:
+        self.wrapped: ModuleType = wrapped
+
+    def __getattr__(self, name: str) -> ModuleType:
+        if name == 'Jewcal':
+            # DeprecationWarning does not alert the user if not in
+            # development mode
+            warn('Jewcal is deprecated, use JewCal', stacklevel=2)
+
+        return cast(ModuleType, getattr(self.wrapped, name))
+
+
+modules[__name__] = cast(ModuleType, _Wrapper(modules[__name__]))
+
+
+__all__ = [
+    'Events',
+    'JewCal',
+    'JewishDate',
+    'Location',
+    'Month',
+    'Zmanim',
+]
