5d45d5f7e5fb0bb0fd35b596d73919eaa3d92f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:45 +0000

    Add package fortify-getorcreateapp/0.2.14/fortify_getorcreateapp-0.2.14.tar.gz

---
+++ packages/fortify-getorcreateapp/fortify_getorcreateapp-0.2.14.tar.gz/fortify_getorcreateapp-0.2.14/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fortify_getorcreateapp",
+    version="0.2.14",
+    author="Fabio Arciniegas",
+    author_email="fabio_arciniegas@trendmicro.com",
+    description="Get a fortify application and version ids, create if they don't exist",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://.trendmicro.com/cloudone-common/fortify-getorcreateapp",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts':
+        ['fortify-getorcreateapp=fortify_getorcreateapp.fortify_getorcreateapp:cli'],
+    },
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    python_requires='>=3.6',
+    install_requires=[
+        'fortifyapi',
+        'requests'
+    ]
+)
