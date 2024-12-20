a75028eee98bbb41e2f7a02ab1e4bcab204c6658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:04:47 +0000

    Add package Named-Entity-Recognition-BERT-Multilingual-Library-LUX/0.1.10/Named_Entity_Recognition_BERT_Multilingual_Library_LUX-0.1.10-py3-none-any.whl

---
+++ packages/Named-Entity-Recognition-BERT-Multilingual-Library-LUX/Named_Entity_Recognition_BERT_Multilingual_Library_LUX-0.1.10-py3-none-any.whl/Named_Entity_Recognition_BERT_Multilingual_Library_LUX/__init__.py
@@ -0,0 +1,11 @@
+from .ner_pipeline import NERPipeline
+
+__all__ = ["NERPipeline"]
+
+def welcome_message():
+    print(
+        "Welcome to Named_Entity_Recognition_BERT_Multilingual_Library_LU!\n"
+        "This library provides an easy-to-use NERPipeline for Named Entity Recognition (NER) tasks."
+    )
+
+welcome_message()
