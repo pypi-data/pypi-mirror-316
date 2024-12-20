3520729356230892758cb6724bcd25e3029782de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:08:47 +0000

    Add package switchai/0.2.0/switchai-0.2.0.tar.gz

---
+++ packages/switchai/switchai-0.2.0.tar.gz/switchai-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='switchai',
+    version='0.2.0',
+    description='A unified library for interacting with various AI APIs through a standardized interface.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yassine El Boudouri',
+    author_email='boudouriyassine@gmail.com',
+    url='https://github.com/yelboudouri/SwitchAI',
+    license="Apache 2.0 License",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=[
+        "pydantic",
+        "openai",
+        "mistralai",
+        "anthropic",
+        "google-generativeai",
+        "deepgram",
+    ],
+    python_requires='>=3.6',
+)
