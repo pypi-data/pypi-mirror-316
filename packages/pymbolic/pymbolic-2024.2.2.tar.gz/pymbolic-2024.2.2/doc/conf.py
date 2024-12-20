24e03b772d82d416e050cdf1712671c69c6f0277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:05:03 +0000

    Add package pymbolic/2024.2.2/pymbolic-2024.2.2.tar.gz

---
+++ packages/pymbolic/pymbolic-2024.2.2.tar.gz/pymbolic-2024.2.2/doc/conf.py
@@ -0,0 +1,50 @@
+from importlib import metadata
+from urllib.request import urlopen
+
+
+_conf_url = \
+        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
+
+copyright = "2013-24, Andreas Kloeckner"
+release = metadata.version("pymbolic")
+version = ".".join(release.split(".")[:2])
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ["_build"]
+
+intersphinx_mapping = {
+    "galgebra": ("https://galgebra.readthedocs.io/en/latest/", None),
+    "mako": ("https://docs.makotemplates.org/en/latest/", None),
+    "matchpy": ("https://matchpy.readthedocs.io/en/latest/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "python": ("https://docs.python.org/3", None),
+    "sympy": ("https://docs.sympy.org/dev/", None),
+    "typing_extensions":
+        ("https://typing-extensions.readthedocs.io/en/latest/", None),
+    "immutabledict":
+        ("https://immutabledict.corenting.fr/", None)
+}
+autodoc_type_aliases = {
+    "Expression": "Expression",
+    "ArithmeticExpression": "ArithmeticExpression",
+}
+
+
+import sys
+
+
+nitpick_ignore_regex = [
+    # Avoids this error in pymbolic.typing.
+    # <unknown>:1: WARNING: py:class reference target not found: ExpressionNode [ref.class]  # noqa: E501
+    # Understandable, because typing can't import primitives, which would be needed
+    # to resolve the reference.
+    ["py:class", r"ExpressionNode"],
+    ["py:class", r"_Expression"],
+    ["py:class", r"p\.AlgebraicLeaf"],
+    ]
+
+
+sys._BUILDING_SPHINX_DOCS = True
---
+++ packages/pymbolic/pymbolic-2024.2.2.tar.gz/pymbolic-2024.2.2/pymbolic/mapper/substitutor.py
@@ -0,0 +1,142 @@
+"""
+.. autoclass:: SubstitutionMapper
+.. autoclass:: CachedSubstitutionMapper
+.. autofunction:: make_subst_func
+.. autofunction:: substitute
+
+.. autoclass:: Callable[[AlgebraicLeaf], Expression | None]
+
+References
+----------
+
+.. class:: SupportsGetItem
+
+    A protocol with a ``__getitem__`` method.
+"""
+
+from __future__ import annotations
+
+
+__copyright__ = "Copyright (C) 2009-2013 Andreas Kloeckner"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import sys
+from typing import TYPE_CHECKING, Any
+
+from pymbolic.mapper import CachedIdentityMapper, IdentityMapper
+
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from useful_types import SupportsGetItem, SupportsItems
+
+    from pymbolic.primitives import AlgebraicLeaf
+    from pymbolic.typing import Expression
+
+
+if getattr(sys, "_BUILDING_SPHINX_DOCS", None):
+    from collections.abc import Callable  # noqa: TC003
+
+
+class SubstitutionMapper(IdentityMapper[[]]):
+    def __init__(
+        self, subst_func: Callable[[AlgebraicLeaf], Expression | None]
+    ) -> None:
+        self.subst_func = subst_func
+
+    def map_variable(self, expr):
+        result = self.subst_func(expr)
+        if result is not None:
+            return result
+        else:
+            return expr
+
+    def map_subscript(self, expr):
+        result = self.subst_func(expr)
+        if result is not None:
+            return result
+        else:
+            return IdentityMapper.map_subscript(self, expr)
+
+    def map_lookup(self, expr):
+        result = self.subst_func(expr)
+        if result is not None:
+            return result
+        else:
+            return IdentityMapper.map_lookup(self, expr)
+
+
+class CachedSubstitutionMapper(CachedIdentityMapper[[]], SubstitutionMapper):
+    def __init__(
+        self, subst_func: Callable[[AlgebraicLeaf], Expression | None]
+    ) -> None:
+        # FIXME Mypy says:
+        # error: Argument 1 to "__init__" of "CachedMapper" has incompatible type
+        # "CachedSubstitutionMapper"; expected "CachedMapper[ResultT, P]"  [arg-type]
+        # This seems spurious?
+        CachedIdentityMapper.__init__(self)  # type: ignore[arg-type]
+        SubstitutionMapper.__init__(self, subst_func)
+
+
+def make_subst_func(
+    # "Any" here avoids the whole Mapping variance disaster
+    # e.g. https://github.com/python/typing/issues/445
+    variable_assignments: SupportsGetItem[Any, Expression],
+) -> Callable[[AlgebraicLeaf], Expression | None]:
+    import pymbolic.primitives as primitives
+
+    def subst_func(var: AlgebraicLeaf) -> Expression | None:
+        try:
+            return variable_assignments[var]
+        except KeyError:
+            if isinstance(var, primitives.Variable):
+                try:
+                    return variable_assignments[var.name]
+                except KeyError:
+                    return None
+            else:
+                return None
+
+    return subst_func
+
+
+def substitute(
+    expression: Expression,
+    variable_assignments: SupportsItems[AlgebraicLeaf | str, Expression] | None
+    = None,
+    mapper_cls=CachedSubstitutionMapper,
+    **kwargs: Expression,
+):
+    """
+    :arg mapper_cls: A :class:`type` of the substitution mapper
+        whose instance applies the substitution.
+    """
+    if variable_assignments is None:
+        # "Any" here avoids pointless grief about variance
+        # e.g. https://github.com/python/typing/issues/445
+        v_ass_copied: dict[Any, Expression] = {}
+    else:
+        v_ass_copied = dict(variable_assignments.items())
+
+    v_ass_copied.update(kwargs)
+
+    return mapper_cls(make_subst_func(v_ass_copied))(expression)
