b4174e8e00d9c915492cd838cc43e43fabec4d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:05:02 +0000

    Add package optrabot/0.10.0a15/optrabot-0.10.0a15.tar.gz

---
+++ packages/optrabot/optrabot-0.10.0a15.tar.gz/optrabot-0.10.0a15/optrabot/tradetemplate/templatedata.py
@@ -0,0 +1,14 @@
+
+class ShortStrikeData:
+	"""
+	Data class which holds configuration of the short strike of a template
+	"""
+	def __init__(self):
+		self.offset :float = None
+
+class LongStrikeData:
+	"""
+	Data class which holds configuration of the long strike of a template
+	"""
+	def __init__(self):
+		self.width :float = None
