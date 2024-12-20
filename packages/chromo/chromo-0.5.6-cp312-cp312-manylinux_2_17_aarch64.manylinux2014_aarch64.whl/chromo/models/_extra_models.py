8a9353d52a25fa276659af36dd25e4c2ddd7a1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 16:04:45 +0000

    Add package chromo/0.5.6/chromo-0.5.6-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/chromo/chromo-0.5.6-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/chromo/models/_extra_models.py
@@ -0,0 +1,25 @@
+from chromo.models.sibyll import (  # noqa
+    Sibyll23c00,
+    Sibyll23c01,
+    Sibyll23c02,
+    Sibyll23c03,
+    Sibyll23StarNoEnh,
+    Sibyll23StarRho,
+    Sibyll23StarBar,
+    Sibyll23StarStrange,
+    Sibyll23StarMixed,
+)
+
+from chromo.models.dpmjetIII import DpmjetIII193_DEV  # noqa
+
+__all__ = (
+    "Sibyll23c00",
+    "Sibyll23c01",
+    "Sibyll23c02",
+    "Sibyll23c03",
+    "DpmjetIII193_DEV",
+    "Sibyll23StarNoEnh",
+    "Sibyll23StarRho",
+    "Sibyll23StarBar",
+    "Sibyll23StarStrange",
+)
