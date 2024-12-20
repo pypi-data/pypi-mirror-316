6968d4f3b3bd961e02cf3b7968676ed527fe37eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:06:11 +0000

    Add package re-matrix/0.1.0/re_matrix-0.1.0.tar.gz

---
+++ packages/re-matrix/re_matrix-0.1.0.tar.gz/re_matrix-0.1.0/re_matrix/__init__.py
@@ -0,0 +1,5 @@
+from .row_reducer import (get_row_reduced_matrix, pretty_print_matrix,
+                          get_matrix_solutions, print_solution_set,
+                          analyze_and_row_reduce_matrix, check_solution_set)
+from .matrix_math_helpers import (dot_product, add_row_a_to_b, subtract_row_a_from_b, multiply_row,
+                         divide_row, create_test_matrix)
