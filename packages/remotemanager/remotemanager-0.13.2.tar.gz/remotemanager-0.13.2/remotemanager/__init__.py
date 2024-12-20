7791b08e40b54f27e7f4984a3d0bbeb006b44667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:05:59 +0000

    Add package remotemanager/0.13.2/remotemanager-0.13.2.tar.gz

---
+++ packages/remotemanager/remotemanager-0.13.2.tar.gz/remotemanager-0.13.2/remotemanager/__init__.py
@@ -0,0 +1,29 @@
+from remotemanager.connection.computers.base import BaseComputer
+from remotemanager.connection.computers.computer import Computer
+from remotemanager.connection.url import URL
+from remotemanager.dataset.dataset import Dataset
+from remotemanager.decorators.remotefunction import RemoteFunction
+from remotemanager.decorators.sanzufunction import SanzuFunction
+from remotemanager.logging_utils.log import Handler
+from remotemanager.script.script import Script
+
+__all__ = [
+    "Dataset",
+    "URL",
+    "RemoteFunction",
+    "Computer",
+    "Script",
+    "BaseComputer",
+    "SanzuFunction",
+]  # noqa: F405
+__version__ = "0.13.2"
+
+# attach a global Logger to the manager
+Logger = Handler()  # noqa: F405
+
+
+# ipython magic
+def load_ipython_extension(ipython):
+    from remotemanager.decorators.magic import RCell
+
+    ipython.register_magics(RCell)
