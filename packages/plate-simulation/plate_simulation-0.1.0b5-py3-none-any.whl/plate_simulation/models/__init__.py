0a4afb169ab26d019474f77c56421fc920ac21a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:23 +0000

    Add package plate-simulation/0.1.0b5/plate_simulation-0.1.0b5-py3-none-any.whl

---
+++ packages/plate-simulation/plate_simulation-0.1.0b5-py3-none-any.whl/plate_simulation/models/__init__.py
@@ -0,0 +1,8 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2024 Mira Geoscience Ltd.                                             '
+#                                                                                      '
+#  This file is part of plate-simulation package.                                      '
+#                                                                                      '
+#  plate-simulation is distributed under the terms and conditions of the MIT License   '
+#  (see LICENSE file at the root of this source code package).                         '
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
