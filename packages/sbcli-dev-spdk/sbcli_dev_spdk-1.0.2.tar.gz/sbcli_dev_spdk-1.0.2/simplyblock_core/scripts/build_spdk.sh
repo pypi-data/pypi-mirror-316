9f800030e97713aba6aeb6d3a20f7820e71c51a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:06:12 +0000

    Add package sbcli-dev-spdk/1.0.2/sbcli_dev_spdk-1.0.2.tar.gz

---
+++ packages/sbcli-dev-spdk/sbcli_dev_spdk-1.0.2.tar.gz/sbcli_dev_spdk-1.0.2/simplyblock_core/scripts/build_spdk.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+if [[ "1" == "$(gh auth status > /dev/null ; echo $?)" ]]
+then
+  gh auth login
+fi
+
+gh repo clone simplyblock-io/ultra
+cd ultra/testing/distrib_integration/
+sudo bash -x spdk_deploy.sh --force -c 0xE -m 4000 # -c the cpu mask for spdk, -m the hugepages memory
+
+systemctl status spdk
