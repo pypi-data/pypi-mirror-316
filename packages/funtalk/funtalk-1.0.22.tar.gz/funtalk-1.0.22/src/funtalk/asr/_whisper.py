6aac2317639dc43c73507a0b8331b7bf32d46a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 16:04:43 +0000

    Add package funtalk/1.0.22/funtalk-1.0.22.tar.gz

---
+++ packages/funtalk/funtalk-1.0.22.tar.gz/funtalk-1.0.22/src/funtalk/asr/_whisper.py
@@ -0,0 +1,25 @@
+import sys
+
+import tqdm
+
+from .base import BaseASR
+
+
+class _CustomProgressBar(tqdm.tqdm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+transcribe_module = sys.modules["whisper.transcribe"]
+transcribe_module.tqdm.tqdm = _CustomProgressBar
+
+
+class WhisperASR(BaseASR):
+    def __init__(self, name="turbo", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        import whisper
+
+        self.model = whisper.load_model(name, *args, **kwargs)
+
+    def transcribe(self, audio, *args, **kwargs):
+        self.model.transcribe(audio, *args, **kwargs)
