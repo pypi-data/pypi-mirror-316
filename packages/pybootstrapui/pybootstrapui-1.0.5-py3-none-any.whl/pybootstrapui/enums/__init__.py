57d721997d60c701208948b58550b960c23cd276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 05:04:33 +0000

    Add package pybootstrapui/1.0.5/pybootstrapui-1.0.5-py3-none-any.whl

---
+++ packages/pybootstrapui/pybootstrapui-1.0.5-py3-none-any.whl/pybootstrapui/enums/__init__.py
@@ -0,0 +1,22 @@
+from .buttons import ButtonStyle
+from .spacing import Spacing
+from .colors import ColorUtilities
+from .utilities import (
+    FloatUtilities, Display, Flex, TextTransform,
+    FontWeight, Overflow, Position, Shadow, Visibility
+)
+
+__all__ = [
+    "ButtonStyle",
+    "Spacing",
+    "ColorUtilities",
+    "FloatUtilities",
+    "Display",
+    "Flex",
+    "TextTransform",
+    "FontWeight",
+    "Overflow",
+    "Position",
+    "Shadow",
+    "Visibility",
+]
