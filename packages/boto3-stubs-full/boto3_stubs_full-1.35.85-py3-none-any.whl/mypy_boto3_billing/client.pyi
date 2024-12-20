01c74570abb9fcd69128e5e2bb1139ffa6f1c9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:05:05 +0000

    Add package boto3-stubs-full/1.35.85/boto3_stubs_full-1.35.85-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.85-py3-none-any.whl/mypy_boto3_billing/client.pyi
@@ -0,0 +1,98 @@
+"""
+Type annotations for billing service Client.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_billing.client import BillingClient
+
+    session = Session()
+    client: BillingClient = session.client("billing")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import Any, Mapping
+
+from botocore.client import BaseClient, ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListBillingViewsPaginator
+from .type_defs import ListBillingViewsRequestRequestTypeDef, ListBillingViewsResponseTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+__all__ = ("BillingClient",)
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    ThrottlingException: type[BotocoreClientError]
+    ValidationException: type[BotocoreClientError]
+
+class BillingClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing.html#Billing.Client)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        BillingClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing.html#Billing.Client)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/can_paginate.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/#can_paginate)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/generate_presigned_url.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/#generate_presigned_url)
+        """
+
+    def list_billing_views(
+        self, **kwargs: Unpack[ListBillingViewsRequestRequestTypeDef]
+    ) -> ListBillingViewsResponseTypeDef:
+        """
+        Lists the billing views available for a given time period.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/list_billing_views.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/#list_billing_views)
+        """
+
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_billing_views"]
+    ) -> ListBillingViewsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/billing/client/get_paginator.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/client/#get_paginator)
+        """
---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.85-py3-none-any.whl/mypy_boto3_billing/type_defs.py
@@ -0,0 +1,85 @@
+"""
+Type annotations for billing service type definitions.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_billing/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_billing.type_defs import TimestampTypeDef
+
+    data: TimestampTypeDef = ...
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from datetime import datetime
+from typing import Union
+
+from .literals import BillingViewTypeType
+
+if sys.version_info >= (3, 12):
+    from typing import NotRequired, TypedDict
+else:
+    from typing_extensions import NotRequired, TypedDict
+
+
+__all__ = (
+    "ActiveTimeRangeTypeDef",
+    "BillingViewListElementTypeDef",
+    "ListBillingViewsRequestPaginateTypeDef",
+    "ListBillingViewsRequestRequestTypeDef",
+    "ListBillingViewsResponseTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResponseMetadataTypeDef",
+    "TimestampTypeDef",
+)
+
+TimestampTypeDef = Union[datetime, str]
+
+
+class BillingViewListElementTypeDef(TypedDict):
+    arn: NotRequired[str]
+    name: NotRequired[str]
+    ownerAccountId: NotRequired[str]
+    billingViewType: NotRequired[BillingViewTypeType]
+
+
+class PaginatorConfigTypeDef(TypedDict):
+    MaxItems: NotRequired[int]
+    PageSize: NotRequired[int]
+    StartingToken: NotRequired[str]
+
+
+class ResponseMetadataTypeDef(TypedDict):
+    RequestId: str
+    HTTPStatusCode: int
+    HTTPHeaders: dict[str, str]
+    RetryAttempts: int
+    HostId: NotRequired[str]
+
+
+class ActiveTimeRangeTypeDef(TypedDict):
+    activeAfterInclusive: TimestampTypeDef
+    activeBeforeInclusive: TimestampTypeDef
+
+
+class ListBillingViewsResponseTypeDef(TypedDict):
+    billingViews: list[BillingViewListElementTypeDef]
+    ResponseMetadata: ResponseMetadataTypeDef
+    nextToken: NotRequired[str]
+
+
+class ListBillingViewsRequestPaginateTypeDef(TypedDict):
+    activeTimeRange: ActiveTimeRangeTypeDef
+    PaginationConfig: NotRequired[PaginatorConfigTypeDef]
+
+
+class ListBillingViewsRequestRequestTypeDef(TypedDict):
+    activeTimeRange: ActiveTimeRangeTypeDef
+    maxResults: NotRequired[int]
+    nextToken: NotRequired[str]
