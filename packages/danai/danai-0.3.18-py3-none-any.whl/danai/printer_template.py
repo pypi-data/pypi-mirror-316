cf33576d6bbb419c730333570b8309dacaf4e6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 16:04:24 +0000

    Add package danai/0.3.18/danai-0.3.18-py3-none-any.whl

---
+++ packages/danai/danai-0.3.18-py3-none-any.whl/danai/printer_template.py
@@ -0,0 +1,18 @@
+import os
+from danai.print import print_directory_contents, print_directory_tree, join_summaries
+
+output_directory = "summaries"
+
+ignore_files = ["printer.py", "transcripts.txt", "package-lock.json"]
+ignore_dirs = ["venv", "node_modules", ".git", "summaries", ".idea", "transcripts", "flask_session"]
+ignore_extensions = [".pyc"]
+full_ignore = ["__pycache__"]
+
+exempt_files = ['Dockerfile']
+exempt_extensions = ['.json', '.config', '.config.js', ".env"]
+
+print_directory_contents(".", output_directory, ignore_dirs, full_ignore, ignore_files, ignore_extensions, exempt_files, exempt_extensions)
+
+print_directory_tree(".", output_directory, ignore_dirs, full_ignore)
+
+join_summaries(output_directory)
