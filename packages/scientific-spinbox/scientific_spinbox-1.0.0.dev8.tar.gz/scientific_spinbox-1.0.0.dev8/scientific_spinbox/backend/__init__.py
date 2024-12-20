d49a76ca1ad513ea9df3a270f9f195b8b0b30280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:28 +0000

    Add package scientific-spinbox/1.0.0.dev8/scientific_spinbox-1.0.0.dev8.tar.gz

---
+++ packages/scientific-spinbox/scientific_spinbox-1.0.0.dev8.tar.gz/scientific_spinbox-1.0.0.dev8/scientific_spinbox/backend/__init__.py
@@ -0,0 +1,12 @@
+"""
+ScientificSpinbox's backend module.
+
+Provides the interfaces between unit manager and ScientificWidget
+and other resources that shouldn't be coupled with the GUI.
+
+Since:
+  2024/02/06
+
+Authors:
+  - Breno H. Pelegrin S. <breno.pelegrin@usp.br>
+"""
---
+++ packages/scientific-spinbox/scientific_spinbox-1.0.0.dev8.tar.gz/scientific_spinbox-1.0.0.dev8/scientific_spinbox/errors/__init__.py
@@ -0,0 +1,11 @@
+"""
+Errors module of scientific_spinbox.
+
+Provides meaningful error messages for exceptions.
+
+Since:
+  2024/02/06
+
+Authors:
+  - Breno H. Pelegrin S. <breno.pelegrin@usp.br>
+"""
