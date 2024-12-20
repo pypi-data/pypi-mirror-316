dcdb2f07ea894e384a79b176ac22564529face13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:46 +0000

    Add package python-nicepay/1.0.2/python-nicepay-1.0.2.tar.gz

---
+++ packages/python-nicepay/python-nicepay-1.0.2.tar.gz/python-nicepay-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='python-nicepay',
+    version='1.0.2',
+    packages=find_packages(),
+    description='This is the Official Python API client / library for NICEPAY Payment API',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='Harfa Thandila',
+    author_email='harfa.thandila@nicepay.co.id',
+    url='https://github.com/nicepay-dev/python-nicepay',
+    license='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'pycryptodome',
+        'requests',
+    ],
+    keywords=["pypi", "config", "nicepay"],
+    project_urls={
+        "Documentation": "https://github.com/nicepay-dev/python-nicepay/blob/main/README.md",
+        "Source": "https://github.com/nicepay-dev/python-nicepay",
+    },
+
+
+)
+
+
