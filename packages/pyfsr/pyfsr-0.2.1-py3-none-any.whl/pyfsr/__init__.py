30f000b992e1194335986b89c7cefa7f19bd07e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 14:04:46 +0000

    Add package pyfsr/0.2.1/pyfsr-0.2.1-py3-none-any.whl

---
+++ packages/pyfsr/pyfsr-0.2.1-py3-none-any.whl/pyfsr/__init__.py
@@ -0,0 +1,5 @@
+from .client import FortiSOAR
+from .exceptions import FortiSOARException, AuthenticationError
+
+__all__ = ['FortiSOAR', 'FortiSOARException', 'AuthenticationError']
+__version__ = "0.1.0"
