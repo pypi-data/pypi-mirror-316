6965d63fcd5da8a90797a3492aab1e49c5e084d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:05:14 +0000

    Add package re-matrix/0.0.2/re_matrix-0.0.2.tar.gz

---
+++ packages/re-matrix/re_matrix-0.0.2.tar.gz/re_matrix-0.0.2/re_matrix/__init__.py
@@ -0,0 +1,4 @@
+from base_re_matrix.re_matrix.row_reducer import (get_row_reduced_matrix, pretty_print_matrix,
+                                                  get_matrix_solutions, print_solution_set,
+                                                  analyze_and_row_reduce_matrix, check_solution_set)
+
---
+++ packages/re-matrix/re_matrix-0.0.2.tar.gz/re_matrix-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Simple python package for row reducing matrices'
+LONG_DESCRIPTION = 'Simple python package for row reducing matrices'
+
+# Setting up
+setup(
+        # the name must match the folder name 'verysimplemodule'
+        name="re_matrix",
+        version=VERSION,
+        author="Charlie",
+        author_email="<charlie@thatcharlie.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],  # add any additional packages that
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'matrices', 'row reduction', 'linear algebra'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
