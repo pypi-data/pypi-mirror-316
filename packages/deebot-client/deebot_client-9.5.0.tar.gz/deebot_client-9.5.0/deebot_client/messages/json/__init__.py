183e1273214042d2669ad9a938a9297400e503fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 16:04:42 +0000

    Add package deebot-client/9.5.0/deebot_client-9.5.0.tar.gz

---
+++ packages/deebot-client/deebot_client-9.5.0.tar.gz/deebot_client-9.5.0/deebot_client/messages/json/__init__.py
@@ -0,0 +1,30 @@
+"""Json messages."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .battery import OnBattery
+from .map import OnMapSetV2
+from .stats import ReportStats
+
+if TYPE_CHECKING:
+    from deebot_client.message import Message
+
+__all__ = [
+    "OnBattery",
+    "OnMapSetV2",
+    "ReportStats",
+]
+
+# fmt: off
+# ordered by file asc
+_MESSAGES: list[type[Message]] = [
+    OnBattery,
+    OnMapSetV2,
+
+    ReportStats
+]
+# fmt: on
+
+MESSAGES: dict[str, type[Message]] = {message.NAME: message for message in _MESSAGES}
