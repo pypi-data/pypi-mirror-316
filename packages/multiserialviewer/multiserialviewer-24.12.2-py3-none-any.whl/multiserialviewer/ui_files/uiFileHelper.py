5c11b5aeefcf0a6ce08db365cd0b8e0621bfadf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 07:04:31 +0000

    Add package multiserialviewer/24.12.2/multiserialviewer-24.12.2-py3-none-any.whl

---
+++ packages/multiserialviewer/multiserialviewer-24.12.2-py3-none-any.whl/multiserialviewer/ui_files/uiFileHelper.py
@@ -0,0 +1,17 @@
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+import pathlib
+
+
+def createWidgetFromUiFile(ui_file_name):
+    ui_file_path_file_name = pathlib.Path(__file__).parent.joinpath(ui_file_name).resolve()
+
+    ui_file = QFile(ui_file_path_file_name)
+    if not ui_file.open(QFile.ReadOnly):
+        raise Exception(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+    loader = QUiLoader()
+    widget = loader.load(ui_file)
+    ui_file.close()
+    if not widget:
+        raise Exception(loader.errorString())
+    return widget
