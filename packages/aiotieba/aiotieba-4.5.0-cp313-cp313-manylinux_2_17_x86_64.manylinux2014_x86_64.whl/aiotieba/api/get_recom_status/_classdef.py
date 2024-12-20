deb85229ad5751d58d484eae58693e46c6b03052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 03:09:14 +0000

    Add package aiotieba/4.5.0/aiotieba-4.5.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-4.5.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/api/get_recom_status/_classdef.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import dataclasses as dcs
+from collections.abc import Mapping
+
+from ...exception import TbErrorExt
+
+
+@dcs.dataclass
+class RecomStatus(TbErrorExt):
+    """
+    大吧主推荐功能的月度配额状态
+
+    Attributes:
+        err (Exception | None): 捕获的异常
+
+        total_recom_num (int): 本月总推荐配额
+        used_recom_num (int): 本月已使用的推荐配额
+    """
+
+    total_recom_num: int = 0
+    used_recom_num: int = 0
+
+    @staticmethod
+    def from_tbdata(data_map: Mapping) -> RecomStatus:
+        total_recom_num = int(data_map['total_recommend_num'])
+        used_recom_num = int(data_map['used_recommend_num'])
+        return RecomStatus(total_recom_num, used_recom_num)
