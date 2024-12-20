fea6a4d748c98d44a4a21749b7cb6e8bd96e436b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 15:06:19 +0000

    Add package smuls/0.2.0/smuls-0.2.0.tar.gz

---
+++ packages/smuls/smuls-0.2.0.tar.gz/smuls-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md" ,"r") as f:
+    description_long = f.read()
+
+setup(
+    name="smuls",
+    version="0.2.0",
+    author="Sam_x",
+    description="A brief description of your package",
+    long_description=description_long,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],  # Add dependencies here
+        entry_points={
+        "console_scripts":[
+            "smul = smul:hello",
+        ],
+    },
+)
