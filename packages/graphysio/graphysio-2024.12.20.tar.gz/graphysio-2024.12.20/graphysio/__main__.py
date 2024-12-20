3b09237b2100a3f459bf070b8a65ab1d3435af20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 16:04:42 +0000

    Add package graphysio/2024.12.20/graphysio-2024.12.20.tar.gz

---
+++ packages/graphysio/graphysio-2024.12.20.tar.gz/graphysio-2024.12.20/graphysio/__main__.py
@@ -0,0 +1,26 @@
+import argparse
+
+from pyshortcuts import make_shortcut
+
+from graphysio.main import main as graphysio_main
+
+
+def mk_shortscuts() -> None:
+    pycmd = "_ -m graphysio"
+    make_shortcut(pycmd, name="GraPhysio", terminal=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("GraPhysio")
+    parser.add_argument(
+        "-S",
+        "--shortcuts",
+        action="store_true",
+        help="Install Desktop shortcuts.",
+    )
+    args = parser.parse_args()
+
+    if args.shortcuts:
+        mk_shortscuts()
+    else:
+        graphysio_main()
