aa1bc1986ce4802d0812c30ddc4e62557fbb2666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:05:24 +0000

    Add package fspacker/0.5.6/fspacker-0.5.6-py3-none-any.whl

---
+++ packages/fspacker/fspacker-0.5.6-py3-none-any.whl/fspacker/packer/libspec/sci.py
@@ -0,0 +1,40 @@
+from fspacker.packer.libspec.base import ChildLibSpecPacker
+
+
+class MatplotlibSpecPacker(ChildLibSpecPacker):
+    PATTERNS = dict(
+        matplotlib={
+            "matplotlib/",
+            "matplotlib.libs/",
+            "mpl_toolkits/",
+            "pylab.py",
+        },
+    )
+    EXCLUDES = dict(
+        matplotlib={"matplotlib-.*.pth"},
+    )
+
+
+class NumbaSpecPacker(ChildLibSpecPacker):
+    PATTERNS = dict(
+        numba={"numba/", "data/"},
+        cffi=set(),
+        pycparser=set(),
+    )
+
+
+class TorchSpecPacker(ChildLibSpecPacker):
+    PATTERNS = dict(
+        torch={"functorch/", "torch/", "torchgen/"},
+    )
+    EXCLUDES = dict(
+        torch={
+            # for debug
+            "torch/utils/bottleneck/",
+            "torch/utils/checkpoint/",
+            "torch/utils/tensorboard/",
+            # for test
+            "torch/utils/data/dataset/",
+            "torch/utils/data/dataloader/",
+        }
+    )
