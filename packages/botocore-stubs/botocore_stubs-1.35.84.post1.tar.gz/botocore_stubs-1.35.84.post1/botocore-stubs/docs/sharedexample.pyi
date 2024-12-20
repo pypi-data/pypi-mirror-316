0b84bb4fc0efe70ff1d4f01def3d9d7a64f038e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 04:05:02 +0000

    Add package botocore-stubs/1.35.84.post1/botocore_stubs-1.35.84.post1.tar.gz

---
+++ packages/botocore-stubs/botocore_stubs-1.35.84.post1.tar.gz/botocore_stubs-1.35.84.post1/botocore-stubs/docs/sharedexample.pyi
@@ -0,0 +1,20 @@
+"""
+Type annotations for botocore.docs.sharedexample module.
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from typing import Any
+
+from botocore.model import OperationModel, Shape
+
+class SharedExampleDocumenter:
+    def document_shared_example(
+        self, example: Any, prefix: str, section: Any, operation_model: OperationModel
+    ) -> None: ...
+    def document_input(self, section: Any, example: Any, prefix: str, shape: Shape) -> None: ...
+    def document_output(self, section: Any, example: Any, shape: Shape) -> None: ...
+
+def document_shared_examples(
+    section: Any, operation_model: OperationModel, example_prefix: str, shared_examples: Any
+) -> None: ...
