6ccb0a19aff4e94b41e4483a5b6d8281c8be2322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 16:04:50 +0000

    Add package texttoapi/0.1.1/texttoapi-0.1.1.tar.gz

---
+++ packages/texttoapi/texttoapi-0.1.1.tar.gz/texttoapi-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="texttoapi",
+    version="0.1.1",
+    description="A package for the TextToAPI tool",
+    author="Gopesh Sharma",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "setuptools",
+        "python-dotenv",
+        "llama-index",
+        "llama-index-embeddings-azure-openai",
+        "llama-index-llms-azure-openai",
+        "openinference-instrumentation-llama-index",
+        "opentelemetry-sdk",
+        "opentelemetry-exporter-otlp",
+        "opentelemetry-proto",
+        "rich",
+        "Flask",
+    ],
+    python_requires=">=3.9",
+)
