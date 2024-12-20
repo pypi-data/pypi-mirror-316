c3752b9d911d63dbc20cb477535c9eb2948cb56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-20 06:05:53 +0000

    Add package nave-visualization/1.9/nave_visualization-1.9.tar.gz

---
+++ packages/nave-visualization/nave_visualization-1.9.tar.gz/nave_visualization-1.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = """A Python package for visualizing environmental data, specifically evapotranspiration, leaching risk,
+and precipitation/irrigation data. The package includes functions to generate time-series date lists, 
+load datasets into dictionaries, and create various types of maps for visualizing environmental phenomena, 
+such as evapotranspiration and leaching risk, using geospatial data. 
+Outputs are typically saved as PDF files with detailed visualizations."""
+
+NAVE_GRANULE_V = 1.9
+
+with open("README.md", "r") as f:
+          description2 = f.read()
+
+setup(
+    name="nave_visualization",  
+    version=NAVE_GRANULE_V,  
+    description=DESCRIPTION,  
+    author_email="<nic@naveanalytics.com>",  
+    packages=find_packages(),  
+    install_requires=[ 
+        'geopandas',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'shapely',
+        'xarray',
+        'rasterio',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    long_description = description2,
+    long_description_content_type= "text/markdown",
+)
