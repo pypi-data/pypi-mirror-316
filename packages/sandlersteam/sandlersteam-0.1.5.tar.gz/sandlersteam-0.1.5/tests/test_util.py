ed6ea611fc626eda0fccba15da27a7317335770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:04:46 +0000

    Add package sandlersteam/0.1.5/sandlersteam-0.1.5.tar.gz

---
+++ packages/sandlersteam/sandlersteam-0.1.5.tar.gz/sandlersteam-0.1.5/tests/test_util.py
@@ -0,0 +1,9 @@
+import os
+from importlib import resources
+from sandlersteam.util import data_path
+
+def test_data_location():
+    with resources.as_file(resources.files('sandlersteam')) as src_root:
+        inst_root=src_root.parent
+        dp=data_path()
+        assert dp==os.path.join(inst_root,'data')
