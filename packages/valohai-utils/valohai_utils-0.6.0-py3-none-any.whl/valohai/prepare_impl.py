0933e5f4c7ff6809bd6edb732b4c42a8e31ce8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:07:40 +0000

    Add package valohai-utils/0.6.0/valohai_utils-0.6.0-py3-none-any.whl

---
+++ packages/valohai-utils/valohai_utils-0.6.0-py3-none-any.whl/valohai/prepare_impl.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from valohai.internals import global_state
+from valohai.internals.global_state_loader import load_global_state
+from valohai.types import InputDict, ParameterDict
+
+
+def prepare(
+    *,
+    step: str,
+    default_parameters: Optional[ParameterDict] = None,
+    default_inputs: Optional[InputDict] = None,
+    image: Optional[str] = None,
+    environment: Optional[str] = None,
+    multifile: bool = False,
+) -> None:
+    """Define the name of the step and its required inputs, parameters and Docker image
+
+    Has dual purpose:
+    - Provide default values for inputs, parameters and Docker image so the user code can be executed
+    - Provide entry-point for the parser that generates/updates valohai.yaml integration file
+
+    :param step: Step name for valohai.yaml
+    :param default_parameters: Dict of parameters and default values
+    :param default_inputs: Dict of inputs with (list of) default URIs
+    :param image: Default docker image
+    :param environment: Default environment ID or slug
+    :param multifile: allow step to be prepared from multiple different source files
+
+    """
+
+    global_state.step_name = step
+    global_state.image_name = image
+    global_state.environment = environment
+    global_state.multifile = multifile
+
+    load_global_state(
+        default_inputs_from_prepare=default_inputs,
+        default_parameters_from_prepare=default_parameters,
+    )
