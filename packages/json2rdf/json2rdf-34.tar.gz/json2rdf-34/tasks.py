dd1fcb58f97da8117d4b4665329cb22de57e3cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:36 +0000

    Add package json2rdf/34/json2rdf-34.tar.gz

---
+++ packages/json2rdf/json2rdf-34.tar.gz/json2rdf-34/tasks.py
@@ -0,0 +1,21 @@
+
+
+def run(*p, **k):
+    from subprocess import run
+    from pathlib import Path
+    _ = run(*p, cwd=Path(__file__).parent, **k,)
+    if _.stdout: print(_.stdout)
+    if _.stderr: print(_.stderr)
+    return _
+
+def build(commit=False):
+    if commit:
+        run('uv lock --upgrade-package json2rdf')
+        run('uvx hatchling version major')
+        run('git add -u') # https://github.com/pre-commit/pre-commit/issues/747#issuecomment-386782080
+    run('uv build')
+
+
+if __name__ == '__main__':
+    from fire import Fire
+    Fire({'build': build})
