04a9532ee18086fd3743b3768c0aad39b2acbba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:25 +0000

    Add package tmseegpy/0.1.5/tmseegpy-0.1.5-py3-none-any.whl

---
+++ packages/tmseegpy/tmseegpy-0.1.5-py3-none-any.whl/tmseegpy/__init__.py
@@ -0,0 +1,12 @@
+# tmseegpy/__init__.py
+from .gui.gui_app import TMSEEG_GUI
+from .analyze import *
+from .clean import *
+from .pcist import *
+from .preproc import *
+from .preproc_vis import *
+from .run import *
+from .validate_tep import *
+from .dataloader import *
+
+__version__ = "0.1.0"
