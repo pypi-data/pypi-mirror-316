d97d3878dc995fc873b62114a76fb424c4db9f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 06:04:57 +0000

    Add package pyesoify/0.1.1/pyesoify-0.1.1.tar.gz

---
+++ packages/pyesoify/pyesoify-0.1.1.tar.gz/pyesoify-0.1.1/src/pyesoify/pyesoify.py
@@ -0,0 +1,16 @@
+import ast
+from obfuscate import obfuscate
+
+def get_code(fpath="examples/code.py") -> str:
+    with open(fpath, "r") as f:
+        code = f.read()
+
+    return code
+
+def main(code: str) -> None:
+    print('__builtins__: ', end='(')
+    for elem in ast.parse(code).body:
+        print(obfuscate(elem))
+
+    print(')=__import__("builtins").__dict__')
+
