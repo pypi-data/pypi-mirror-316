9594c9fb6d8ce007df1532c7bc8a9fe3c0d6532a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:24 +0000

    Add package GenomeViewer/0.0.0/genomeviewer-0.0.0.tar.gz

---
+++ packages/GenomeViewer/genomeviewer-0.0.0.tar.gz/genomeviewer-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="GenomeViewer",
+    python_requires=">=3.9",
+    author="Li Yao",
+    description="Python Genome Viewer",
+    long_description_content_type="text/markdown"
+)
