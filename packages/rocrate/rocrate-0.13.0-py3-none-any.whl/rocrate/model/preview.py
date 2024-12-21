4bad73323fe88bd3fd0fb7adc91a82977cf86167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:05:25 +0000

    Add package rocrate/0.13.0/rocrate-0.13.0-py3-none-any.whl

---
+++ packages/rocrate/rocrate-0.13.0-py3-none-any.whl/rocrate/model/preview.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+# Copyright 2019-2024 The University of Manchester, UK
+# Copyright 2020-2024 Vlaams Instituut voor Biotechnologie (VIB), BE
+# Copyright 2020-2024 Barcelona Supercomputing Center (BSC), ES
+# Copyright 2020-2024 Center for Advanced Studies, Research and Development in Sardinia (CRS4), IT
+# Copyright 2022-2024 École Polytechnique Fédérale de Lausanne, CH
+# Copyright 2024 Data Centre, SciLifeLab, SE
+# Copyright 2024 National Institute of Informatics (NII), JP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from pathlib import Path
+
+from jinja2 import Template
+from .file import File
+
+
+class Preview(File):
+    """
+    RO-Crate preview file
+
+    This object holds a preview of an RO Crate in HTML format_
+    """
+    BASENAME = "ro-crate-preview.html"
+
+    def __init__(self, crate, source=None, properties=None):
+        super().__init__(crate, source, self.BASENAME, properties=properties)
+
+    def _empty(self):
+        # default properties of the metadata entry
+        val = {
+            "@id": self.BASENAME,
+            "@type": "CreativeWork",
+            "about": {"@id": "./"}
+        }
+        return val
+
+    def generate_html(self):
+        base_path = os.path.abspath(os.path.dirname(__file__))
+        template = open(
+            os.path.join(base_path, '..', 'templates', 'preview_template.html.j2'),
+            'r', encoding='utf-8'
+        )
+        src = Template(template.read())
+
+        def template_function(func):
+            src.globals[func.__name__] = func
+            return func
+
+        @template_function
+        def stringify(a):
+            if type(a) is list:
+                return ', '.join(a)
+            elif type(a) is str:
+                return a
+            else:
+                if a._jsonld and a._jsonld['name']:
+                    return a._jsonld['name']
+                else:
+                    return a
+
+        @template_function
+        def is_object_list(a):
+            if type(a) is list:
+                for obj in a:
+                    if obj is not str:
+                        return True
+            else:
+                return False
+
+        template.close()
+        context_entities = []
+        data_entities = []
+        for entity in self.crate.contextual_entities:
+            context_entities.append(entity._jsonld)
+        for entity in self.crate.data_entities:
+            data_entities.append(entity._jsonld)
+        out_html = src.render(crate=self.crate, context=context_entities, data=data_entities)
+        return out_html
+
+    def write(self, dest_base):
+        if self.source:
+            super().write(dest_base)
+        else:
+            write_path = Path(dest_base) / self.id
+            out_html = self.generate_html()
+            with open(write_path, 'w', encoding='utf-8') as outfile:
+                outfile.write(out_html)
---
+++ packages/rocrate/rocrate-0.13.0-py3-none-any.whl/rocrate/vocabs.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# Copyright 2019-2024 The University of Manchester, UK
+# Copyright 2020-2024 Vlaams Instituut voor Biotechnologie (VIB), BE
+# Copyright 2020-2024 Barcelona Supercomputing Center (BSC), ES
+# Copyright 2020-2024 Center for Advanced Studies, Research and Development in Sardinia (CRS4), IT
+# Copyright 2022-2024 École Polytechnique Fédérale de Lausanne, CH
+# Copyright 2024 Data Centre, SciLifeLab, SE
+# Copyright 2024 National Institute of Informatics (NII), JP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import json
+if sys.version_info.minor < 9:
+    import pkg_resources
+else:
+    import importlib.resources
+
+# FIXME: Avoid eager loading?
+if sys.version_info.minor < 9:
+    RO_CRATE = json.loads(pkg_resources.resource_string(
+        __name__, "data/ro-crate.jsonld"
+    ))
+    SCHEMA = json.loads(pkg_resources.resource_string(
+        __name__, "data/schema.jsonld"
+    ))
+else:
+    RO_CRATE = json.loads(
+        importlib.resources.files(__package__).joinpath("data/ro-crate.jsonld").read_text("utf8")
+    )
+    SCHEMA = json.loads(
+        importlib.resources.files(__package__).joinpath("data/schema.jsonld").read_text("utf8")
+    )
+SCHEMA_MAP = dict((e["@id"], e) for e in SCHEMA["@graph"])
+
+
+def term_to_uri(name):
+    # NOTE: Assumes RO-Crate's flat-style context
+    return RO_CRATE["@context"][name]
+
+
+def schema_doc(uri):
+    # NOTE: Ensure rdfs:comment still appears in newer schema.org downloads
+    # TODO: Support terms outside schema.org?
+    return SCHEMA_MAP[uri].get("rdfs:comment", "")
