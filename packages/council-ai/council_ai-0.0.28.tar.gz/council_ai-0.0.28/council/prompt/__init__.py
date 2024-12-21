e0af980754701b25a8265bcb21d8d29345ce3e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 06:05:12 +0000

    Add package council-ai/0.0.28/council_ai-0.0.28.tar.gz

---
+++ packages/council-ai/council_ai-0.0.28.tar.gz/council_ai-0.0.28/council/prompt/__init__.py
@@ -0,0 +1,8 @@
+from .llm_dataset import (
+    LLMDatasetConversation,
+    LLMDatasetObject,
+    LLMDatasetSpec,
+    LLMDatasetValidator,
+)
+from .llm_prompt_config_object import LLMPromptConfigObject, LLMPromptConfigSpec
+from .prompt_builder import PromptBuilder
