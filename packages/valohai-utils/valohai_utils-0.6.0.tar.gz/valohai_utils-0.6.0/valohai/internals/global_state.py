f59bf7b1b3c1375436a1e2a7b8a1b02ce0c2be90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:07:40 +0000

    Add package valohai-utils/0.6.0/valohai_utils-0.6.0.tar.gz

---
+++ packages/valohai-utils/valohai_utils-0.6.0.tar.gz/valohai_utils-0.6.0/valohai/internals/global_state.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict, Optional
+
+from valohai.distributed import Distributed
+from valohai.internals.input_info import InputInfo
+
+loaded: bool = False
+inputs: Dict[str, InputInfo] = {}
+parameters: Dict[str, Any] = {}
+step_name: Optional[str] = None
+image_name: Optional[str] = None
+environment: Optional[str] = None
+distributed = Distributed()
+multifile: bool = False
+
+
+def flush_global_state() -> None:
+    # fmt: off
+    global loaded, inputs, parameters, step_name, image_name, distributed, environment, multifile
+    # fmt: off
+    loaded = False
+    inputs = {}
+    parameters = {}
+    step_name = None
+    image_name = None
+    environment = None
+    multifile = False
+    distributed.flush_state()
