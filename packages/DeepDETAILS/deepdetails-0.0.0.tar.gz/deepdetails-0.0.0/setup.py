d8b559a95168afd0d4dd99d584e88529edb17d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:46 +0000

    Add package DeepDETAILS/0.0.0/deepdetails-0.0.0.tar.gz

---
+++ packages/DeepDETAILS/deepdetails-0.0.0.tar.gz/deepdetails-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="DeepDETAILS",
+    python_requires=">=3.9",
+    author="Li Yao",
+    description="DeepDETAILS",
+    long_description_content_type="text/markdown"
+)
