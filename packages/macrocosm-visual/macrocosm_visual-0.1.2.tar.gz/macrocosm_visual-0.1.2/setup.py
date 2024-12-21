14db0ab951be618d9077360e5f53db700b9692ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:04:48 +0000

    Add package macrocosm-visual/0.1.2/macrocosm_visual-0.1.2.tar.gz

---
+++ packages/macrocosm-visual/macrocosm_visual-0.1.2.tar.gz/macrocosm_visual-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from os.path import abspath, dirname, join
+
+from setuptools import find_packages, setup
+
+this_dir = abspath(dirname(__file__))
+
+with open(join(this_dir, "README.md"), encoding="utf-8") as file:
+    long_description = file.read()
+
+with open(join(this_dir, "requirements.txt")) as f:
+    requirements = f.read().split("\n")
+
+setup(
+    name="macrocosm-visual",
+    version="0.1.2",
+    description="Macrocosm Visual Engine",
+    url="https://github.com/macro-cosm/macrocosm-visual",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="Macrocosm",
+    author_email="luca.mungo@macrocosm.group",
+    license="MIT",
+    install_requires=requirements,
+    packages=find_packages(exclude=["docs"]),
+    include_package_data=True,
+)
