5b2d749fa418d1198e51f9e32b1f6e6ddea4e07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:05:24 +0000

    Add package PSNAWP-HA/2.3.0/psnawp_ha-2.3.0-py3-none-any.whl

---
+++ packages/PSNAWP-HA/psnawp_ha-2.3.0-py3-none-any.whl/psnawp_api/psn.py
@@ -0,0 +1,88 @@
+"""PlayStation Network Simplified Access"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+import aiohttp
+
+from psnawp_api import PSNAWP
+from psnawp_api.models.user import User
+
+
+@dataclass
+class PlaystationNetworkData:
+    """Dataclass representing data retrieved from the Playstation Network api."""
+
+    presence: dict[str, Any]
+    username: str
+    account_id: str
+    available: bool
+    title_metadata: dict[str, Any]
+    platform: dict[str, Any]
+    registered_platforms: list[str]
+
+class PlaystationNetwork:
+    """Helper Class to return playstation network data in an easy to use structure"""
+    def __init__(self, npsso:str):
+        self.psn = PSNAWP(npsso)
+        self.client = self.psn.me()
+        self.user: User | None = None
+        self.data: PlaystationNetworkData | None = None
+
+    def validate_connection(self):
+        self.psn.me()
+        
+    def get_user(self):
+        self.user = self.psn.user(online_id='me')
+        return self.user
+
+    def get_data(self):
+        data: PlaystationNetworkData = PlaystationNetworkData(
+            {}, "", "", False, {}, {}, []
+        )
+        
+        if not self.user:
+            self.user = self.get_user()
+        
+        devices = self.client.get_account_devices()
+        for device in devices:
+            if device.get("deviceType") in ["PS5", "PS4"] and device.get("deviceType") not in data.registered_platforms:
+                data.registered_platforms.append(device.get("deviceType",""))
+            
+        data.username = self.user.online_id
+        data.account_id = self.user.account_id
+        data.presence = self.user.get_presence()
+
+        data.available = (
+            data.presence.get("basicPresence", {}).get("availability")
+            == "availableToPlay"
+        )
+        data.platform = data.presence.get("basicPresence", {}).get(
+            "primaryPlatformInfo"
+        )
+        game_title_info_list = data.presence.get("basicPresence", {}).get(
+            "gameTitleInfoList"
+        )
+
+        if game_title_info_list:
+            data.title_metadata = game_title_info_list[0]
+
+        self.data = data
+        return self.data
+    
+    async def retrieve_npsso_token(self) -> str | None:
+        """Returns NPSSO token if user is already authenticated to the playstation network"""
+        client = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=2))
+        try:
+            async with (
+                    client as session,
+                    session.get("https://ca.account.sony.com/api/v1/ssocookie") as response,
+                ):
+                if response.ok:
+                    return await response.json()
+                return None
+        except Exception:
+            return None
+            
