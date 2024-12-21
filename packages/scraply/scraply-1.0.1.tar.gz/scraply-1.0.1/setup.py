a4018eeeb939313e1a57e57bc7aeabb5795da676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:05:20 +0000

    Add package scraply/1.0.1/scraply-1.0.1.tar.gz

---
+++ packages/scraply/scraply-1.0.1.tar.gz/scraply-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scraply',
+    version='1.0.1',
+    packages=find_packages(),
+    description='A Python package to scrape and clone websites.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Fidal',
+    author_email='mrfidal@proton.me',
+    url='https://github.com/ByteBreach/scraply',
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    entry_points={
+        'console_scripts': [
+            'scraply = scraply.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
