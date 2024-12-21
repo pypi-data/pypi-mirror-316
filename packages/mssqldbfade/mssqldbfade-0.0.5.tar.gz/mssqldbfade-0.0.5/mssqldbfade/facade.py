2770f86b880ea50cc46b7a85eb435e286bcfe7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:05:57 +0000

    Add package mssqldbfade/0.0.5/mssqldbfade-0.0.5.tar.gz

---
+++ packages/mssqldbfade/mssqldbfade-0.0.5.tar.gz/mssqldbfade-0.0.5/mssqldbfade/facade.py
@@ -0,0 +1,10 @@
+class DatabaseFacade:
+    """
+        Fachada de base de datos, crea conexion con un singleton a mssql, y define las operaciones dentro de esta como funciones independientes
+    """
+    def __init__(self, connection_string: str):
+        self.connection_string. str = connection_string
+        # self.db = Singleton(connection_string)
+        
+    def connection_string(self) -> str:
+        return self.connection_string
