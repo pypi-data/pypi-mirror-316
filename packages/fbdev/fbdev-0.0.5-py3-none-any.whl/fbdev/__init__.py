3810e326787da646c798df59b28eff058d9a5730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:05:50 +0000

    Add package fbdev/0.0.5/fbdev-0.0.5-py3-none-any.whl

---
+++ packages/fbdev/fbdev-0.0.5-py3-none-any.whl/fbdev/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.5"
+
+from . import exceptions
+from . import _utils
+from . import comp
+from . import graph
+from . import complib
+from . import runtime
+from . import dev_utils
+from . import concurrent
