de15404d40cb7b604fc83d9bd98b12fd620b5c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:05:56 +0000

    Add package regex-enumerator/0.8.0/regex_enumerator-0.8.0.tar.gz

---
+++ packages/regex-enumerator/regex_enumerator-0.8.0.tar.gz/regex_enumerator-0.8.0/tests/test_backreference.py
@@ -0,0 +1,50 @@
+from regex_enumerator import RegexEnumerator
+from .test_function import f_finite, f_infinite
+
+
+def test_backreference():
+    regexEnumerator = RegexEnumerator(r'(a)\1')
+    possibilities = ['aa']
+
+    f_finite(regexEnumerator, possibilities)
+
+
+def test_backreference_with_group_quantifier():
+    regexEnumerator = RegexEnumerator(r'(a)+\1')
+    possibilities = ['aa' * i for i in range(1, 6)]
+
+    f_infinite(regexEnumerator, possibilities)
+
+
+def test_backreference_with_quantifier():
+    regexEnumerator = RegexEnumerator(r'(a)\1+')
+    possibilities = ['a' * i + 'a' for i in range(1, 6)]
+
+    f_infinite(regexEnumerator, possibilities)
+
+
+def test_backreference_with_named_group():
+    regexEnumerator = RegexEnumerator(r'(?<name>[a-b])\k<name>')
+    possibilities = ['aa', 'bb']
+
+    f_finite(regexEnumerator, possibilities)
+
+
+def test_backreference_with_named_group_and_quantifier():
+    regexEnumerator = RegexEnumerator(r'(?<name>[a-b])\k<name>{1, 2}')
+    possibilities = ['aa', 'bb', 'aaa', 'bbb']
+
+    f_finite(regexEnumerator, possibilities)
+
+
+def test_zero_width_backreference():
+    regexEnumerator = RegexEnumerator(r'(a)?\1{0}')
+    possibilities = ['a', '']
+
+    f_finite(regexEnumerator, possibilities)
+
+def test_10_backreference():
+    regexEnumerator = RegexEnumerator(r'(a)(b)(c)(d)(e)(f)(g)(h)(i)(j)\10')
+    possibilities = ['abcdefghijj']
+
+    f_finite(regexEnumerator, possibilities)
