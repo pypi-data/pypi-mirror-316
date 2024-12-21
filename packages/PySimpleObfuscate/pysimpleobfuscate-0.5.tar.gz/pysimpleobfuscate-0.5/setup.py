a8b8aaebc7dc4371aae670ceb0401a9dfe0cfca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:08:50 +0000

    Add package PySimpleObfuscate/0.5/pysimpleobfuscate-0.5.tar.gz

---
+++ packages/PySimpleObfuscate/pysimpleobfuscate-0.5.tar.gz/pysimpleobfuscate-0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# pip install ../../Azure/PyObfuscate --upgrade -t ./.venv/lib/python3.10/site-packages/PyObfuscate
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setuptools.setup(
+    name='PySimpleObfuscate',
+    version='0.5',
+    description='A Simple Python Obfuscator',
+    packages=setuptools.find_packages('src'),
+    install_requires=[],
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'pyobfuscate=PySimpleObfuscate.lib.cli:cli',
+        ],
+    },
+    include_package_data=True,
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
