67ac3edf8de82b7d02100315261d4698ad57db20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:05:48 +0000

    Add package nonebot-plugin-tetris-stats/1.7.1/nonebot_plugin_tetris_stats-1.7.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-tetris-stats/nonebot_plugin_tetris_stats-1.7.1-py3-none-any.whl/nonebot_plugin_tetris_stats/games/tetrio/api/schemas/leaderboards/by.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+from ...typing import Rank, ValidRank
+from ..base import ArCounts, FailedModel, P, SuccessModel
+
+
+class BaseLeague(BaseModel):
+    gamesplayed: int
+    gameswon: int
+    tr: float
+    gxe: float
+    rank: Rank
+    bestrank: ValidRank
+    glicko: float
+    rd: float
+    pps: float
+    decaying: bool
+
+
+class InvalidLeague(BaseLeague):
+    apm: None
+    vs: None
+
+
+class League(BaseLeague):
+    apm: float
+    vs: float
+
+
+class BaseEntry(BaseModel):
+    id: str = Field(..., alias='_id')
+    username: str
+    role: Literal['anon', 'user', 'bot', 'halfmod', 'mod', 'admin', 'sysop']
+    ts: datetime | None = None
+    xp: float
+    country: str | None = None
+    supporter: bool | None = None
+    gamesplayed: int
+    gameswon: int
+    gametime: float
+    ar: int
+    ar_counts: ArCounts
+    p: P
+
+
+class InvalidEntry(BaseEntry):
+    league: InvalidLeague
+
+
+class Entry(BaseEntry):
+    league: League
+
+
+class Data(BaseModel):
+    entries: list[Entry | InvalidEntry]
+
+
+class BySuccessModel(SuccessModel):
+    data: Data
+
+
+By = BySuccessModel | FailedModel
