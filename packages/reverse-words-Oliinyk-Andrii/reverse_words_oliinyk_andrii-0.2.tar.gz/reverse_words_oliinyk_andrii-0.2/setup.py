de4e4480064caa6c48b78ceaca402c3d58c5773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 06:04:59 +0000

    Add package reverse-words-Oliinyk-Andrii/0.2/reverse_words_oliinyk_andrii-0.2.tar.gz

---
+++ packages/reverse-words-Oliinyk-Andrii/reverse_words_oliinyk_andrii-0.2.tar.gz/reverse_words_oliinyk_andrii-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with open("README.md", "r") as f:
+    description = f.read()
+# Setting up
+setup(
+    name="reverse_words_Oliinyk_Andrii",
+    version='0.2',
+    author="oliinyka",
+    packages=find_packages(),
+    install_requires=[],
+    long_description_content_type="text/markdown",
+    long_description=description
+)
