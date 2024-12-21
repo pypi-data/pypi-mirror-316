45d1c727c8ded431f56958e3b6b5dc44776763ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 06:05:13 +0000

    Add package gimpey-com-sol-gateway/0.0.3/gimpey_com_sol_gateway-0.0.3.tar.gz

---
+++ packages/gimpey-com-sol-gateway/gimpey_com_sol_gateway-0.0.3.tar.gz/gimpey_com_sol_gateway-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gimpey_com_sol_gateway",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "grpcio>=1.50.0",
+        "protobuf>=4.0.0",
+    ],
+    description="Python package for the `gimpey.com` `sol-gateway` service.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gimpey-com/sol-gateway",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
