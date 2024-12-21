c37c997f318a0a0ef4729f3e3898a9668215a512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 06:04:55 +0000

    Add package reverse-words-Oliinyk-Andrii/0.1/reverse_words_oliinyk_andrii-0.1.tar.gz

---
+++ packages/reverse-words-Oliinyk-Andrii/reverse_words_oliinyk_andrii-0.1.tar.gz/reverse_words_oliinyk_andrii-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(
+    name="reverse_words_Oliinyk_Andrii",
+    version='0.1',
+    author="Oliinyk_Andrii",
+    packages=find_packages(),
+    install_requires=[],
+)
