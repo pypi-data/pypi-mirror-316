11584b7731ea70aaefbb24ab1c7cb6a5f3d7a7fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:46 +0000

    Add package p1-auth/0.1.7/p1_auth-0.1.7-py3-none-any.whl

---
+++ packages/p1-auth/p1_auth-0.1.7-py3-none-any.whl/p1_auth/middleware.py
@@ -0,0 +1,14 @@
+from django.contrib import auth
+from django.utils.deprecation import MiddlewareMixin
+
+
+class AuthenticateSessionMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+        if hasattr(request, 'user') and request.user.is_authenticated:
+            return
+        user = auth.authenticate(request)
+        if hasattr(user, 'backend'):
+            request._cached_user = user
+            auth.login(
+                request, user, user.backend)
