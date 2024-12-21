be7cc47a0ac3e7ee8908117ecfc07c7d7cb49159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:08:49 +0000

    Add package syncmaster-commons/0.0.2/syncmaster_commons-0.0.2.tar.gz

---
+++ packages/syncmaster-commons/syncmaster_commons-0.0.2.tar.gz/syncmaster_commons-0.0.2/syncmaster_commons/agents/request_payload.py
@@ -0,0 +1,32 @@
+from typing import Union
+from abstract.baseclass import SMBaseClass
+from gupshup.request_payload import _AgentRequestPayloadGupshup
+
+
+
+class AgentRequestPayload(SMBaseClass):
+    """
+    AgentRequestPayload is a pydantic model for the agent request payload.
+
+    """
+    agent_request_payload: Union[_AgentRequestPayloadGupshup]
+
+
+    @classmethod
+    def from_dict(cls, client, request_payload: dict) -> "AgentRequestPayload":
+        """
+        Creates a AgentRequestPayload object from a dictionary.
+        Args:
+            payload_dict (dict): The dictionary containing the payload data.
+        Returns:
+            AgentRequestPayload: The AgentRequestPayload object created from the dictionary.
+        """
+        if client == "gupshup":
+            agent_request_payload = _AgentRequestPayloadGupshup.from_dict(request_payload) 
+        else:
+            raise ValueError(f"Client {client} is not supported.")
+        return cls(
+            agent_request_payload=agent_request_payload,
+        )
+
+    
