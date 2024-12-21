ebfddf895d6a80fec48de0a1116f3f0bac2f66df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:20 +0000

    Add package elog-cli/0.1.11/elog_cli-0.1.11-py3-none-any.whl

---
+++ packages/elog-cli/elog_cli-0.1.11-py3-none-any.whl/elog_cli/main.py
@@ -0,0 +1,35 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import click
+from elog_cli.auth_manager import AuthManager
+from elog_cli.commands.login import login
+from elog_cli.commands.create_entry import create_entry
+from elog_cli.commands.list_logbooks import list_logbooks
+from elog_cli.commands.show_logbook import show_logbook
+from elog_cli.commands.show_entry import show_entry
+import importlib.metadata
+
+@click.group()
+@click.version_option(
+    importlib.metadata.version("elog_cli"),
+    prog_name="ELOG CLI",
+    message="%(prog)s Version: %(version)s"
+)
+@click.pass_context
+def cli(ctx, version):
+    """Root CLI for Elog Management."""
+    auth_manager = AuthManager()
+    ctx.ensure_object(dict)
+    ctx.obj["auth_manager"] = auth_manager
+
+
+cli.add_command(login)
+cli.add_command(show_logbook)
+cli.add_command(create_entry)
+cli.add_command(list_logbooks)
+cli.add_command(show_logbook)
+cli.add_command(show_entry)
+if __name__ == "__main__":
+    cli()
