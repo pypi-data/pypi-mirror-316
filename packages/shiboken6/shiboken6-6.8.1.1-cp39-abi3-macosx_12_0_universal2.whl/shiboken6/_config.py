226e791dee77e31984a55f4a144c64b1dd6bcece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:08:35 +0000

    Add package shiboken6/6.8.1.1/shiboken6-6.8.1.1-cp39-abi3-macosx_12_0_universal2.whl

---
+++ packages/shiboken6/shiboken6-6.8.1.1-cp39-abi3-macosx_12_0_universal2.whl/shiboken6/_config.py
@@ -0,0 +1,12 @@
+shiboken_library_soversion = str(6.8)
+
+version = "6.8.1.1"
+version_info = (6, 8, 1.1, "", "")
+
+__build_date__ = '2024-12-19T10:25:19+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.8.1.1'
+__qt_macos_min_deployment_target__ = '12'
