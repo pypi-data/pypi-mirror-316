57e6cebb5232717014fb3a47917de00ae0ee7ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:05:26 +0000

    Add package axinite/1.4.1/axinite-1.4.1.tar.gz

---
+++ packages/axinite/axinite-1.4.1.tar.gz/axinite-1.4.1/axinite/tools/show.py
@@ -0,0 +1,20 @@
+import axinite as ax
+from vpython import *
+import axinite.tools as axtools
+from itertools import cycle
+import vpython as vp
+
+colors = cycle([color.red, color.blue, color.green, color.orange, color.purple, color.yellow])
+
+def show(_args, frontend=axtools.vpython_frontend):
+    args = _args
+    if args.rate is None:
+        args.rate = 100
+    if args.radius_multiplier is None:
+        args.radius_multiplier = 1
+    if args.retain is None:
+        args.retain = 200
+
+    while args.t < args.limit:
+        frontend(args.t, bodies=args.bodies)
+        args.t += args.delta
