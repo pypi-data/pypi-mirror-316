c6a8d0a91797bbbf8f3158e7822c6f848bf1d425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:07:38 +0000

    Add package doris-alchemy/0.2.5/doris_alchemy-0.2.5.tar.gz

---
+++ packages/doris-alchemy/doris_alchemy-0.2.5.tar.gz/doris_alchemy-0.2.5/src/doris_alchemy/playground.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from sqlalchemy import Boolean, Enum, Integer
+from doris_alchemy.orm_base import DorisBase
+from sqlalchemy.orm import Mapped, mapped_column, Session
+from sqlalchemy import create_engine, Engine
+
+
+class Tst(DorisBase):
+    __tablename__ = 'test_doris_table_5'
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # bool_flag: Mapped[bool] = mapped_column(Boolean())
+    # bool_flag: Mapped[bool]
+    en: Mapped[Optional[str]] = mapped_column(Enum('READY', 'VAITING', 'CANCELED'), nullable=True)
+
+
+
+if __name__ == '__main__':
+    eng: Engine = create_engine('doris+pymysql://root:MA5fRnxhsLCVs5sm@10.0.100.115:9030/mpu_procurement_data')
+    Tst.create(eng)
+    
+    # with Session(eng) as s:
+    #     row1 = Tst(id=1, bool_flag=True)
+    #     row2 = Tst(id=2, bool_flag=False)
+    #     s.add(row1)
+    #     s.add(row2)
+    #     s.commit()
