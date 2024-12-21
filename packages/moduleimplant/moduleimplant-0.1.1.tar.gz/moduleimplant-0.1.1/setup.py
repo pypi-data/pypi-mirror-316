eb620ceba07b409129f7e3f63893e662d841fef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:08:58 +0000

    Add package moduleimplant/0.1.1/moduleimplant-0.1.1.tar.gz

---
+++ packages/moduleimplant/moduleimplant-0.1.1.tar.gz/moduleimplant-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+            
+setup(
+    name='moduleimplant',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'ultralytics',
+    ],
+    description='A custom module implant system for Ultralytics models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='acelych',
+    author_email='acelych@foxmail.com',
+    url='https://github.com/Gelinzh/moduleimplant',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    entry_points={
+        'console_scripts': [
+            'moduleimplant=moduleimplant.modify:main',
+        ],
+    },
+)
