5713d5448cdef776595633094a901b20f3db3e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:04:57 +0000

    Add package cmind/3.5.2/cmind-3.5.2-py3-none-any.whl

---
+++ packages/cmind/cmind-3.5.2-py3-none-any.whl/cmind/__init__.py
@@ -0,0 +1,12 @@
+# Collective Mind init
+#
+# Written by Grigori Fursin
+
+__version__ = "3.5.2"
+
+from cmind.core import access
+from cmind.core import x
+from cmind.core import error
+from cmind.core import errorx
+from cmind.core import halt
+from cmind.core import CM
---
+++ packages/cmind/cmind-3.5.2-py3-none-any.whl/cmind/repo/automation/core/module_misc.py
@@ -0,0 +1,19 @@
+# Generate CM UID
+#
+# Written by Grigori Fursin
+
+import cmind.utils as utils
+
+############################################################
+
+
+def uid(i):
+
+    console = i.get('out') == 'con'
+
+    r = utils.gen_uid()
+
+    if console:
+        print(r['uid'])
+
+    return r
