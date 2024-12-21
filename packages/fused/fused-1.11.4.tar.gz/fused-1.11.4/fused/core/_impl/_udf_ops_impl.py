351137286184cb8e123fb46512d3958e50b0430f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:25 +0000

    Add package fused/1.11.4/fused-1.11.4.tar.gz

---
+++ packages/fused/fused-1.11.4.tar.gz/fused-1.11.4/fused/core/_impl/_udf_ops_impl.py
@@ -0,0 +1,52 @@
+from typing import Any, Optional
+
+from fused.models.api import UdfJobStepConfig
+from fused.models.udf import RootAnyBaseUdf
+from fused.models.udf._eval_result import UdfEvaluationResult
+from fused.models.udf.output import Output
+
+
+def get_step_config_from_server(
+    email_or_handle: Optional[str],
+    slug: str,
+    cache_key: Any,
+    _is_public: bool = False,
+) -> UdfJobStepConfig:
+    from fused.api.api import FusedAPI
+
+    # cache_key is unused
+    api = FusedAPI()
+    if _is_public:
+        obj = api._get_public_udf(slug)
+    else:
+        obj = api._get_udf(email_or_handle, slug)
+    udf = RootAnyBaseUdf.model_validate_json(obj["udf_body"]).root
+
+    step_config = UdfJobStepConfig(udf=udf)
+    return step_config
+
+
+def get_github_udf_from_server(
+    url: str,
+    cache_key: Any,
+):
+    from fused.api.api import FusedAPI
+
+    # cache_key is unused
+    # TODO: Do this locally in fused-py
+    api = FusedAPI(credentials_needed=False)
+    obj = api._get_code_by_url(url)
+    udf = RootAnyBaseUdf.model_validate_json(obj["udf_body"]).root
+
+    step_config = UdfJobStepConfig(udf=udf)
+    return step_config
+
+
+def run_and_get_data(udf, *args, **kwargs):
+    # TODO: This is a silly way to do this, because we have to pass parameters in such an odd way
+    job = udf(*args, **kwargs)
+    result = job.run_local()
+    if isinstance(result, (Output, UdfEvaluationResult)):
+        return result.data
+    else:
+        return result
