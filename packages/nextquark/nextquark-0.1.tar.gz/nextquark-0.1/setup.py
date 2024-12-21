0f93c0eb9671c6ea7458439a3ad0f627a8b57024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 03:08:49 +0000

    Add package nextquark/0.1/nextquark-0.1.tar.gz

---
+++ packages/nextquark/nextquark-0.1.tar.gz/nextquark-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name='nextquark',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/nextquark',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.7',
+)
