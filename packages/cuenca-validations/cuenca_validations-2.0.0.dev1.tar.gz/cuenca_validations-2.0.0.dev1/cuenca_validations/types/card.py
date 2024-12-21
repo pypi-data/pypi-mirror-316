72adc021a6432bc4ef93d1f448657fe4e9a0571f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 05:06:38 +0000

    Add package cuenca-validations/2.0.0.dev1/cuenca_validations-2.0.0.dev1.tar.gz

---
+++ packages/cuenca-validations/cuenca_validations-2.0.0.dev1.tar.gz/cuenca_validations-2.0.0.dev1/cuenca_validations/types/card.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, field_validator
+from pydantic_core import PydanticCustomError
+from pydantic_extra_types.payment import PaymentCardBrand, PaymentCardNumber
+
+from ..card_bins import CARD_BINS
+
+
+class StrictPaymentCardNumber(BaseModel):
+
+    card_number: PaymentCardNumber
+
+    @field_validator('card_number')
+    def validate_bin(cls, card_number: PaymentCardNumber) -> PaymentCardNumber:
+        if card_number.bin not in CARD_BINS:
+            raise PydanticCustomError(
+                'payment_card_number.bin', 'Invalid BIN: Bank code not found.'
+            )
+        return card_number
+
+    @property
+    def brand(self) -> PaymentCardBrand:
+        return self.card_number.brand
+
+    @property
+    def bank_code(self) -> str:
+        return CARD_BINS[self.card_number.bin]
