14d490a1e588f5ede2ffa1cbca73cbc678babbe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-21 04:04:49 +0000

    Add package frappeapi/0.1.3/frappeapi-0.1.3.tar.gz

---
+++ packages/frappeapi/frappeapi-0.1.3.tar.gz/frappeapi-0.1.3/frappeapi/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1.3"
+
+from fastapi.datastructures import UploadFile  # noqa: F401
+from fastapi.params import (
+	Body,  # noqa: F401
+	Depends,  # noqa: F401
+	File,  # noqa: F401
+	Form,  # noqa: F401
+	Header,  # noqa: F401
+	Query,  # noqa: F401
+)
+
+from frappeapi.applications import FrappeAPI  # noqa: F401
