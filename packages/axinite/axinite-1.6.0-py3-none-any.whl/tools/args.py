e6542bf4bdd7401433dcd506462f37fc9188181a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:53 +0000

    Add package axinite/1.6.0/axinite-1.6.0-py3-none-any.whl

---
+++ packages/axinite/axinite-1.6.0-py3-none-any.whl/tools/args.py
@@ -0,0 +1,17 @@
+import axinite as ax
+
+class AxiniteArgs:
+    def __init__(self):
+        self.name = None
+        self.delta = None
+        self.limit = None
+        self.action = None
+        self.t = None
+        self.bodies: list = []
+        self.radius_multiplier = None
+        self.rate = None
+        self.retain = None
+        self.modifiers = []
+
+    def unpack(self):
+        return self.delta, self.limit, self.action, *self.bodies
