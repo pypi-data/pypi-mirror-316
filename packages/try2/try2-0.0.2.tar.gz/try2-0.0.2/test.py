d5343bc08434b8b7e2d7b2d889789051b8dae715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:05:26 +0000

    Add package try2/0.0.2/try2-0.0.2.tar.gz

---
+++ packages/try2/try2-0.0.2.tar.gz/try2-0.0.2/test.py
@@ -0,0 +1,17 @@
+from src.try2 import try2
+
+def reciprocal(n):
+    return 1 / n
+
+ls = [1, 2, 0, 3, 0, 4]
+
+for number in ls:
+    x = try2(reciprocal, number, default="bad")
+    print(number, "=>", x)
+
+# 1 => 1.0
+# 2 => 0.5
+# 0 => bad
+# 3 => 0.3333333333333333
+# 0 => bad
+# 4 => 0.25
