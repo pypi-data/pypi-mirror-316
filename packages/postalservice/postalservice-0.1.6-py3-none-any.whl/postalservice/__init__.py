92f54ec6441410b5c65fb061668ec53ee55c0335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:44 +0000

    Add package postalservice/0.1.6/postalservice-0.1.6-py3-none-any.whl

---
+++ packages/postalservice/postalservice-0.1.6-py3-none-any.whl/postalservice/__init__.py
@@ -0,0 +1,3 @@
+from .mercariservice import MercariService
+from .frilservice import FrilService
+from .yjpservice import YJPService
---
+++ packages/postalservice/postalservice-0.1.6-py3-none-any.whl/postalservice/baseservice.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+import asyncio
+import json
+import httpx
+from postalservice.utils.search_utils import SearchParams, SearchResults
+
+class BaseService(ABC):
+    @abstractmethod
+    def fetch_data(self, params: dict) -> httpx.Response:
+
+        pass
+
+    @abstractmethod
+    async def fetch_data_async(self, params: dict) -> httpx.Response:
+
+        pass
+
+    @abstractmethod
+    def parse_response(self, response: str) -> str:
+
+        pass
+
+    @abstractmethod
+    async def parse_response_async(self, response: str) -> str:
+
+        pass
+
+
+    @abstractmethod
+    def get_search_params(self, data: SearchParams) -> str:
+
+        pass
+
+    async def get_search_results_async(self, params: dict) -> SearchResults:
+        res = await self.fetch_data_async(params)
+        items = await self.parse_response_async(res)
+        searchresults = SearchResults(items)
+        return searchresults
+    
+    def get_search_results(self, params: dict) -> SearchResults:
+        res = self.fetch_data(params)
+        items = self.parse_response(res)
+        searchresults = SearchResults(items)
+        return searchresults
