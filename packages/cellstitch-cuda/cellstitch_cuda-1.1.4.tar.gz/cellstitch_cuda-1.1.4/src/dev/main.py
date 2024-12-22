4aa7ea2d21c3e7a2f66ebde3f55ee3759b54c4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:42 +0000

    Add package cellstitch-cuda/1.1.4/cellstitch_cuda-1.1.4.tar.gz

---
+++ packages/cellstitch-cuda/cellstitch_cuda-1.1.4.tar.gz/cellstitch_cuda-1.1.4/src/dev/main.py
@@ -0,0 +1,8 @@
+from cellstitch_cuda.postprocessing_cupy import fill_holes_and_remove_small_masks, filter_nuclei_cells
+import tifffile
+from cellstitch_cuda.pipeline import cellstitch_cuda
+
+
+img = r"E:\1_DATA\Rheenen\tvl_jr\3d stitching\raw4-large\raw.tif"
+
+masks = cellstitch_cuda(img, output_masks=True, verbose=True)
