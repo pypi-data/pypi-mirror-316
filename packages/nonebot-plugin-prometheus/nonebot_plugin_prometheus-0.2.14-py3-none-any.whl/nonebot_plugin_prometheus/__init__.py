391567bebfe8965ec43088da79a249696b734a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:05:55 +0000

    Add package nonebot-plugin-prometheus/0.2.14/nonebot_plugin_prometheus-0.2.14-py3-none-any.whl

---
+++ packages/nonebot-plugin-prometheus/nonebot_plugin_prometheus-0.2.14-py3-none-any.whl/nonebot_plugin_prometheus/__init__.py
@@ -0,0 +1,22 @@
+import prometheus_client as prometheus_client
+from nonebot.plugin import PluginMetadata
+from prometheus_client import (
+    Counter as Counter,
+    Gauge as Gauge,
+    Histogram as Histogram,
+    Summary as Summary,
+)
+
+from nonebot_plugin_prometheus import api as api
+from nonebot_plugin_prometheus.config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="Prometheus 集成插件",
+    description="为 NoneBot 和其他插件提供 Prometheus 集成支持",
+    type="library",
+    usage="请参考文档",
+    homepage="https://github.com/suyiiyii/nonebot-plugin-prometheus",
+    config=Config,
+)
+
+__all__ = [prometheus_client, Counter, Gauge, Histogram, Summary]
