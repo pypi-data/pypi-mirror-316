f6d5d163af680086ebf20ab3fc38368da513ea8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:05:45 +0000

    Add package sql-compiler/0.1.1/sql_compiler-0.1.1.tar.gz

---
+++ packages/sql-compiler/sql_compiler-0.1.1.tar.gz/sql_compiler-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sql_compiler",
+    version="0.1.1",
+    author="Tawfiq",
+    author_email="twfyqbhyry4@gmail.com",
+    description="A lightweight SQL compiler using PLY",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tawfiqbhyry/compiler_test",
+    packages=find_packages(),
+    install_requires=[
+        "ply",  # Dependency for lex and yacc
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
