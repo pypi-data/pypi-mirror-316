b6cdee6ecb27ce2f6e114465f8925bab0fe64b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:30 +0000

    Add package goodfire/0.3.0.dev9/goodfire-0.3.0.dev9-py3-none-any.whl

---
+++ packages/goodfire/goodfire-0.3.0.dev9-py3-none-any.whl/goodfire/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+from .api.client import AsyncClient, Client
+from .exceptions import InferenceAbortedException
+from .features.features import Feature, FeatureEdits, FeatureGroup
+from .variants.variants import NestedScope, Variant
+
+__version__ = "0.3.0.dev.9"
+
+__all__ = [
+    "Client",
+    "AsyncClient",
+    "FeatureGroup",
+    "Feature",
+    "Variant",
+    "NestedScope",
+    "FeatureEdits",
+    "InferenceAbortedException",
+]
