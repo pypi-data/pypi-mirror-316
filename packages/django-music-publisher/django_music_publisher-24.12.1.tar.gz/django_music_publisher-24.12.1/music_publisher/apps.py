4ea7cbbd866a4a76ea53c1b059b80f0abd62516b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:48 +0000

    Add package django-music-publisher/24.12.1/django_music_publisher-24.12.1.tar.gz

---
+++ packages/django-music-publisher/django_music_publisher-24.12.1.tar.gz/django_music_publisher-24.12.1/music_publisher/apps.py
@@ -0,0 +1,26 @@
+"""Django app definition for :mod:`music_publisher`."""
+
+from django.apps import AppConfig
+
+from .validators import validate_settings
+import os
+
+
+class MusicPublisherConfig(AppConfig):
+    """Configuration for Music Publisher app.
+
+    Attributes:
+        label (str): app label
+        name (str): app name
+        verbose_name (str): app verbose name
+    """
+
+    name = "music_publisher"
+    label = "music_publisher"
+    verbose_name = "Music Publisher"
+
+    def ready(self):
+        """Validate settings when ready to prevent deployments with invalid
+        settings."""
+        if os.getenv("DATABASE_URL") != "":
+            validate_settings()
