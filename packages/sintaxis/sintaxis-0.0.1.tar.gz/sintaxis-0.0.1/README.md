09fd187490b29581f86b0ca94f623b2fc42d6a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:49 +0000

    Add package sintaxis/0.0.1/sintaxis-0.0.1.tar.gz

---
+++ packages/sintaxis/sintaxis-0.0.1.tar.gz/sintaxis-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Codigo CLI
+
+Codigo CLI es un comando line interface (CLI) que permite crear y ejecutar proyectos de codigo. El objetivo es facilitar la creación de proyectos de codigo y la ejecución de tareas de verificación de código.
---
+++ packages/sintaxis/sintaxis-0.0.1.tar.gz/sintaxis-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = "0.0.1"
+DESCRIPTION = "Codigo CLI es un comando line interface (CLI) que permite crear y ejecutar proyectos de codigo. El objetivo es facilitar la creación de proyectos de codigo y la ejecución de tareas de verificación de código."
+PACKAGE_NAME = "sintaxis"
+AUTHOR = "Paolo Soncco"
+EMAIL = "paolosonccodev@gmail.com"
+GITHUB = "https://github.com/12aptor"
+
+setup(
+    name=PACKAGE_NAME,
+    packages=[PACKAGE_NAME],
+    version=VERSION,
+    license="Apache License 2.0",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=GITHUB,
+    keywords=[],
+    install_requires=[
+        "colorama",
+        "python-socketio[asyncio_client]"
+    ],
+    entry_points={
+        "console_scripts": [
+            "codigo=app.main:main",
+        ],
+    },
+)
