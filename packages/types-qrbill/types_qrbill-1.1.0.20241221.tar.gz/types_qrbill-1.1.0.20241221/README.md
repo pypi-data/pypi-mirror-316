d211017200edfc33e24dbc02b282b00f83329890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:13:22 +0000

    Add package types-qrbill/1.1.0.20241221/types_qrbill-1.1.0.20241221.tar.gz

---
+++ packages/types-qrbill/types_qrbill-1.1.0.20241221.tar.gz/types_qrbill-1.1.0.20241221/README.md
@@ -0,0 +1,26 @@
+## Typing stubs for qrbill
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`qrbill`](https://github.com/claudep/swiss-qr-bill) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `qrbill`. This version of
+`types-qrbill` aims to provide accurate annotations for
+`qrbill==1.1.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/qrbill`](https://github.com/python/typeshed/tree/main/stubs/qrbill)
+directory.
+
+This package was tested with
+mypy 1.14.0,
+pyright 1.1.389,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`097581ea47d0fd77f097c88d80d5947e0218d9c4`](https://github.com/python/typeshed/commit/097581ea47d0fd77f097c88d80d5947e0218d9c4).
---
+++ packages/types-qrbill/types_qrbill-1.1.0.20241221.tar.gz/types_qrbill-1.1.0.20241221/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-qrbill"
+description = "Typing stubs for qrbill"
+long_description = '''
+## Typing stubs for qrbill
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`qrbill`](https://github.com/claudep/swiss-qr-bill) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `qrbill`. This version of
+`types-qrbill` aims to provide accurate annotations for
+`qrbill==1.1.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/qrbill`](https://github.com/python/typeshed/tree/main/stubs/qrbill)
+directory.
+
+This package was tested with
+mypy 1.14.0,
+pyright 1.1.389,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`097581ea47d0fd77f097c88d80d5947e0218d9c4`](https://github.com/python/typeshed/commit/097581ea47d0fd77f097c88d80d5947e0218d9c4).
+'''.lstrip()
+
+setup(name=name,
+      version="1.1.0.20241221",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/qrbill.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-qrcode'],
+      packages=['qrbill-stubs'],
+      package_data={'qrbill-stubs': ['__init__.pyi', 'bill.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
