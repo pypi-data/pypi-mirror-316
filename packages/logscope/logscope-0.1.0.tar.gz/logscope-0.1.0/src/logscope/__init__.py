680c0e4e2f5db8ea4e79622d3253d6c8d267a532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:05:28 +0000

    Add package logscope/0.1.0/logscope-0.1.0.tar.gz

---
+++ packages/logscope/logscope-0.1.0.tar.gz/logscope-0.1.0/src/logscope/__init__.py
@@ -0,0 +1,10 @@
+"""
+LogScope - Queryable logging with SQLite storage
+
+LogScope provides enhanced logging capabilities with SQLite storage and pretty console output.
+"""
+
+from logscope.core import logger
+
+__version__ = "0.1.0"
+__all__ = ['logger']
