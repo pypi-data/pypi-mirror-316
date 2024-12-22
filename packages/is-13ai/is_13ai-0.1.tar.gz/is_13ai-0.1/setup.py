992e578a5e67a28ff6bef6725809bc0425494530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:14:11 +0000

    Add package is-13ai/0.1/is_13ai-0.1.tar.gz

---
+++ packages/is-13ai/is_13ai-0.1.tar.gz/is_13ai-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='is_13ai',
+    version='0.1',
+    description='A package that uses Azure AI to determine if a number is 13 or not.',
+    author='Cyrus Mante',
+    packages=find_packages(),
+)
