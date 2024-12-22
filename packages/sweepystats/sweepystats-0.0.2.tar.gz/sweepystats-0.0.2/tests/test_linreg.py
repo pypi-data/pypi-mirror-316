9ccb75af1f960f22ad7b016d1949ab517d0f9ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:51 +0000

    Add package sweepystats/0.0.2/sweepystats-0.0.2.tar.gz

---
+++ packages/sweepystats/sweepystats-0.0.2.tar.gz/sweepystats-0.0.2/tests/test_linreg.py
@@ -0,0 +1,31 @@
+import numpy as np
+import sweepystats as sw
+import pytest
+
+def test_linreg():
+    n, p = 5, 3
+    X = np.random.rand(n, p)
+    y = np.random.rand(n)
+    ols = sw.LinearRegression(X, y)
+    ols.fit()
+
+    # least squares solution by QR
+    beta, resid, _, _ = np.linalg.lstsq(X, y)
+    sigma2 = resid[0] / (n - p)
+    beta_cov = sigma2 * np.linalg.inv(X.T @ X)
+    beta_std = np.sqrt(np.diag(beta_cov))
+
+    assert np.allclose(ols.coef(), beta)         # beta hat
+    assert np.allclose(ols.resid(), resid)       # residual
+    assert np.allclose(ols.cov(), beta_cov)      # Var(beta hat)
+    assert np.allclose(ols.coef_std(), beta_std) # std of beta hat
+
+def test_high_dimensional():
+    n, p = 5, 10
+    X = np.random.rand(n, p)
+    y = np.random.rand(n)
+    ols = sw.LinearRegression(X, y)
+
+    # X'X is singular, so there must be 1 eigenvalue that is 0
+    with pytest.raises(ZeroDivisionError):
+        ols.fit()
