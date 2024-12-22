2f5400f89597d2c3cc03b17c92fbe17ba3b79c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:20 +0000

    Add package ephemerista/0.1.0a3/ephemerista-0.1.0a3.tar.gz

---
+++ packages/ephemerista/ephemerista-0.1.0a3.tar.gz/ephemerista-0.1.0a3/src/ephemerista/comms/transmitter.py
@@ -0,0 +1,14 @@
+from pydantic import Field
+
+from ephemerista import BaseModel
+from ephemerista.comms.antennas import Antenna
+from ephemerista.comms.utils import to_db
+
+
+class Transmitter(BaseModel):
+    frequency: float = Field(gt=0.0)
+    power: float = Field(gt=0.0)
+    line_loss: float = Field(ge=0.0)
+
+    def equivalent_isotropic_radiated_power(self, antenna: Antenna, angle: float) -> float:
+        return antenna.gain(self.frequency, angle) + to_db(self.power) - self.line_loss
