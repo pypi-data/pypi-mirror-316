ab24716d0a4abe66b3b82947437e4b13d798c30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:44 +0000

    Add package rk-screenshot/1.2.8/rk_screenshot-1.2.8.tar.gz

---
+++ packages/rk-screenshot/rk_screenshot-1.2.8.tar.gz/rk_screenshot-1.2.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '1.2.8'
+DESCRIPTION = 'Screenshot using Gesture hand detection'
+LONG_DESCRIPTION = open("README.md").read()
+# Setting up
+setup(
+    name="rk_screenshot",
+    version=VERSION,
+    author="Rohit Kumar Yadav",
+    author_email="<rohitkuyadav2003@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    license="MIT",  # Add the license type here
+    license_files=("LICENSE",), 
+    install_requires=['pyscreenshot', 'opencv-python', 'mediapipe','playsound'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    nclude_package_data=True,  # Include non-Python files specified in MANIFEST.in
+    package_data={
+        '': ['shutter.mp3'],  # Include the shutter.mp3 file
+    },
+)
