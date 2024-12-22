855886a72961daa8a2ee769befb961b29c27248d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:22 +0000

    Add package Pycodz/2.8/Pycodz-2.8-py3-none-any.whl

---
+++ packages/Pycodz/Pycodz-2.8-py3-none-any.whl/Pycodz/ai/main.py
@@ -0,0 +1,16 @@
+from api import API
+
+class blackboxai:
+    def __init__(
+            self
+    ):
+        self.titel =  """Generate response from www.blackboxai.com"""
+    def chat(
+            self,
+            message: str,
+            module
+        ):
+        return API.generate(
+            message=message,
+            module=module
+            )
