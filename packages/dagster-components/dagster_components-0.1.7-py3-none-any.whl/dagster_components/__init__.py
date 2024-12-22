74002503505741104a15271dd7c2c2ea175c3693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:40 +0000

    Add package dagster-components/0.1.7/dagster_components-0.1.7-py3-none-any.whl

---
+++ packages/dagster-components/dagster_components-0.1.7-py3-none-any.whl/dagster_components/__init__.py
@@ -0,0 +1,11 @@
+from dagster_components.core.component import (
+    Component as Component,
+    ComponentGenerateRequest as ComponentGenerateRequest,
+    ComponentLoadContext as ComponentLoadContext,
+    ComponentRegistry as ComponentRegistry,
+    component as component,
+)
+from dagster_components.core.component_defs_builder import (
+    build_component_defs as build_component_defs,
+)
+from dagster_components.version import __version__ as __version__
