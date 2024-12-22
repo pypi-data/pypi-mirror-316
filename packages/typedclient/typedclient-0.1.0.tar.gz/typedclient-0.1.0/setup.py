9e132bf0f537386d311d84bef49788e3fdbbbb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:13:39 +0000

    Add package typedclient/0.1.0/typedclient-0.1.0.tar.gz

---
+++ packages/typedclient/typedclient-0.1.0.tar.gz/typedclient-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="typedclient",  # Replace with your package name
+    version="0.1.0",
+    description="A short description of your package",
+    packages=find_packages(),  # Automatically finds submodules in `my_package/`
+    python_requires=">=3.3",  # Specify minimum Python version
+)
