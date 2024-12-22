35e469cb21ea8eacb5109f753f78cc3926668d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:26 +0000

    Add package plutus-lightweight-charts/2.2.0/plutus_lightweight_charts-2.2.0-py3-none-any.whl

---
+++ packages/plutus-lightweight-charts/plutus_lightweight_charts-2.2.0-py3-none-any.whl/lightweight_charts/toolbox.py
@@ -0,0 +1,73 @@
+from enum import Enum
+import json
+
+
+class Tool(Enum):
+    MEASURE = "measure"
+    TREND_LINE = "trendLine"
+    HORIZONTAL_LINE = "horizontalLine"
+    RAY_LINE = "rayLine"
+    VERTICAL_LINE = "verticalLine"
+    BOX = "box"
+
+
+class ToolBox:
+    def __init__(self, chart):
+        self.run_script = chart.run_script
+        self.id = chart.id
+        self._save_under = None
+        self.drawings = {}
+        chart.win.handlers[f"save_drawings{self.id}"] = self._save_drawings
+        self.run_script(f"{self.id}.createToolBox()")
+
+    def save_drawings_under(self, widget: "Widget"):
+        """
+        Drawings made on charts will be saved under the widget given. eg `chart.toolbox.save_drawings_under(chart.topbar['symbol'])`.
+        """
+        self._save_under = widget
+
+    def load_drawings(self, tag: str):
+        """
+        Loads and displays the drawings on the chart stored under the tag given.
+        """
+        if not self.drawings.get(tag):
+            return
+        self.run_script(
+            f"if ({self.id}.toolBox) {self.id}.toolBox.loadDrawings({json.dumps(self.drawings[tag])})"
+        )
+
+    def import_drawings(self, file_path):
+        """
+        Imports a list of drawings stored at the given file path.
+        """
+        with open(file_path, "r") as f:
+            json_data = json.load(f)
+            self.drawings = json_data
+
+    def export_drawings(self, file_path):
+        """
+        Exports the current list of drawings to the given file path.
+        """
+        with open(file_path, "w+") as f:
+            json.dump(self.drawings, f, indent=4)
+
+    def _save_drawings(self, drawings):
+        if not self._save_under:
+            return
+        self.drawings[self._save_under.value] = json.loads(drawings)
+
+    def hide_tool(self, tool: Tool):
+        """Hide the tool on the toolbox.
+
+        Args:
+            tool (Tools): Tool to hide
+        """
+        self.run_script(f"{self.id}.toolBox.hideButton('{tool.value}')")
+
+    def clear_drawings(self):
+        """Clear all drawings made on the chart."""
+        self.run_script(f"{self.id}.toolBox.clearDrawings()")
+
+    def reposition_on_time(self):
+        """Reposition all drawings made on the chart."""
+        self.run_script(f"{self.id}.toolBox.repositionOnTime()")
