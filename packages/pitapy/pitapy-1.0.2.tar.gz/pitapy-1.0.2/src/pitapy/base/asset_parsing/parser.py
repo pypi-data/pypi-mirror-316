8fc9489e3519bd0f5b99f8d886174d93cb6bac0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:22 +0000

    Add package pitapy/1.0.2/pitapy-1.0.2.tar.gz

---
+++ packages/pitapy/pitapy-1.0.2.tar.gz/pitapy-1.0.2/src/pitapy/base/asset_parsing/parser.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from dm_control import mjcf
+
+
+class Parser:
+    """Class to parse xml-file in given path to mujoco's MJCF-object."""
+
+    @staticmethod
+    def get_mjcf(xml_path: str) -> mjcf.RootElement:
+        """Parses xml-file to MJCF-object.
+
+        Parameters:
+            xml_path (str): Path to xml-file
+
+        Returns:
+            mjcf_obj (mjcf.RootElement): MJCF-object of given xml-file
+        """
+        logger = logging.getLogger()
+        if xml_path is None:
+            logger.error("No xml file provided.")
+            raise ValueError("No xml file provided.")
+        if not os.path.isfile(xml_path):
+            logger.error(f"Could not find xml path '{xml_path}'.")
+            raise ValueError(f"Could not find xml path '{xml_path}'.")
+        mjcf_obj = mjcf.from_path(xml_path)
+
+        return mjcf_obj
