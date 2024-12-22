1276ca8b06efc6ec544dc6885de1e11d66c6857f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:35 +0000

    Add package pyrust-project/0.1.14/pyrust_project-0.1.14.tar.gz

---
+++ packages/pyrust-project/pyrust_project-0.1.14.tar.gz/pyrust_project-0.1.14/src/constants.rs
@@ -0,0 +1,40 @@
+/// Number of cards in standard deck
+pub const CARD_COUNT: u8 = 52;
+
+/// Number of ranks in a sandard deck
+/// (2 -> A)
+pub const RANK_COUNT: u8 = 13;
+pub const SUIT_COUNT: u8 = 4;
+
+/// char to u8 rank table
+pub const RANK_TO_CHAR: &[char; 13] = &[
+    '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+];
+
+/// char to u8 suit table
+pub static SUIT_TO_CHAR: &[char; 4] = &['s', 'h', 'd', 'c'];
+
+pub static SUIT_MASK: u8 = 0x3;
+pub static RANK_MASK: u8 = 0x3c;
+
+/// Tables of unique primes for hashing hands
+pub const RANKS: &[u64; 13] = &[
+    8192, 32769, 69632, 237568, 593920, 1531909, 3563520, 4300819, 4685870, 4690024, 4767972,
+    4780561, 4801683,
+];
+
+/// Table of power of 2 flush ranks
+pub const FLUSH_RANKS: &[u64; 13] = &[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096];
+
+pub const HAND_CATEGORY_OFFSET: u16 = 0x1000;
+pub const HAND_CATEGORY_SHIFT: u16 = 12;
+
+pub const HIGH_CARD: u16 = HAND_CATEGORY_OFFSET;
+pub const PAIR: u16 = 2 * HAND_CATEGORY_OFFSET;
+pub const TWO_PAIR: u16 = 3 * HAND_CATEGORY_OFFSET;
+pub const THREE_OF_A_KIND: u16 = 4 * HAND_CATEGORY_OFFSET;
+pub const STRAIGHT: u16 = 5 * HAND_CATEGORY_OFFSET;
+pub const FLUSH: u16 = 6 * HAND_CATEGORY_OFFSET;
+pub const FULL_HOUSE: u16 = 7 * HAND_CATEGORY_OFFSET;
+pub const FOUR_OF_A_KIND: u16 = 8 * HAND_CATEGORY_OFFSET;
+pub const STRAIGHT_FLUSH: u16 = 9 * HAND_CATEGORY_OFFSET;
---
+++ packages/pyrust-project/pyrust_project-0.1.14.tar.gz/pyrust_project-0.1.14/src/equity_calculator/mod.rs
@@ -0,0 +1,5 @@
+mod combined_range;
+mod simulator;
+
+pub use combined_range::CombinedRange;
+pub use simulator::{approx_equity, exact_equity};
