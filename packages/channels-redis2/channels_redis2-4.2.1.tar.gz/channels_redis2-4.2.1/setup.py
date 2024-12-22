e4d1ce6de45eab3e61651647120e10b7412f78d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:13:47 +0000

    Add package channels-redis2/4.2.1/channels_redis2-4.2.1.tar.gz

---
+++ packages/channels-redis2/channels_redis2-4.2.1.tar.gz/channels_redis2-4.2.1/setup.py
@@ -0,0 +1,40 @@
+from os.path import dirname, join
+
+from setuptools import find_packages, setup
+
+from channels_redis import __version__
+
+# We use the README as the long_description
+readme = open(join(dirname(__file__), "README.rst")).read()
+
+crypto_requires = ["cryptography>=1.3.0"]
+
+test_requires = crypto_requires + [
+    "pytest",
+    "pytest-asyncio",
+    "async-timeout",
+    "pytest-timeout",
+]
+
+
+setup(
+    name="channels_redis2",
+    version=__version__,
+    url="http://github.com/django/channels_redis/",
+    author="Django Software Foundation",
+    author_email="foundation@djangoproject.com",
+    description="Redis-backed ASGI channel layer implementation",
+    long_description=readme,
+    license_file="LICENSE",
+    zip_safe=False,
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+        "redis>=4.6",
+        "msgpack~=1.0",
+        "asgiref>=3.2.10,<4",
+        "channels",
+    ],
+    extras_require={"cryptography": crypto_requires, "tests": test_requires},
+)
