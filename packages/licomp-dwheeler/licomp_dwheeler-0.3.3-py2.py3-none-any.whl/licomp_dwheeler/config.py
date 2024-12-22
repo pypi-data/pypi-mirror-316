7a0d646ab8e24eff6405e278000a6b859b5d5a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:06:24 +0000

    Add package licomp-dwheeler/0.3.3/licomp_dwheeler-0.3.3-py2.py3-none-any.whl

---
+++ packages/licomp-dwheeler/licomp_dwheeler-0.3.3-py2.py3-none-any.whl/licomp_dwheeler/config.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_dwheeler_version = '0.3.3'
+my_supported_api_version = '0.3'
+
+repo = 'https://github.com/hesa/licomp-dwheeler/'
+disclaimer = 'This information is believed to be correct, but is not legal advice; for formal legal advice, please consult an attorney.'
+cli_name = 'licomp-dwheeler'
+module_name = 'licomp_dwheeler'
+
+description = """
+
+Simple command line tool to check compatibility between two licenses, given context
+
+"""
+
+epilog = """
+
+
+
+AUTHOR
+  Henrik Sandklef
+
+
+PROJECT SITE
+
+  https://github.com/hesa/licomp-dwheeler
+
+REPORTING BUGS
+
+  File a ticket at https://github.com/hesa/licomp-dwheeler/issues
+
+COPYRIGHT
+
+  Copyright (c) 2024 Henrik Sandklef<hesa@sandklef.com>.
+
+  License GPL-3.0-or-later
+
+ATTRIBUTION
+  licomp-dwheeler is using the license compatibility matrix from David Wheeler's license compatibility graph.
+"""
