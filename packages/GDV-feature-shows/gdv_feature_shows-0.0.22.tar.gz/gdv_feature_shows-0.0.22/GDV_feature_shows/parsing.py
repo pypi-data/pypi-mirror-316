23739ffc137a5e43130ae0de56ac82858511791e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:34 +0000

    Add package GDV-feature-shows/0.0.22/gdv_feature_shows-0.0.22.tar.gz

---
+++ packages/GDV-feature-shows/gdv_feature_shows-0.0.22.tar.gz/gdv_feature_shows-0.0.22/GDV_feature_shows/parsing.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import argparse
+from GDV_feature_shows import __version__
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="GDV_feature_shows helps to visualize GDV scans. ")
+
+    parser.add_argument("--version", action="version", version=f"V{__version__}", help="Check version. ")
+
+    # subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # main
+    # main_parser = subparsers.add_parser("main", help='main')
+
+    # main_parser
+    parser.add_argument("gdv_path", type=str, help="Path to folder with pictures of GDV scans. ")
+
+    # main_parser
+    parser.add_argument("settings_path", type=str, help="Path to json file with settings. "
+                                                        "It creates new if does not exists. ")
+
+    return parser.parse_args()
