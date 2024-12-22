1b8bc2b1a8a43624f2d6f739cee82683d50f20b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:40 +0000

    Add package yemot/0.0.8/yemot-0.0.8.tar.gz

---
+++ packages/yemot/yemot-0.0.8.tar.gz/yemot-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='yemot',
+    version='0.0.8',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
+    
