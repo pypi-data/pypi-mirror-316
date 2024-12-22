0d4644b88f266f01c2243a9e842fb5d4b095ae40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:06:27 +0000

    Add package pytaa/0.5.1/pytaa-0.5.1-py3-none-any.whl

---
+++ packages/pytaa/pytaa-0.5.1-py3-none-any.whl/pytaa/backtest/positions.py
@@ -0,0 +1,312 @@
+"""Store base functions for weights and positions."""
+
+from datetime import datetime
+from typing import Callable, List
+
+import numpy as np
+import pandas as pd
+
+from pytaa.tools.risk import (
+    Covariance,
+    calculate_min_variance_portfolio,
+    calculate_risk_parity_portfolio,
+    calculate_rolling_volatility,
+    weighted_covariance_matrix,
+)
+
+
+class Positions:
+    """Base class for all portfolio weight classes."""
+
+    def __init__(self, assets: List[str], rebalance_dates: List[datetime]):
+        """Initialize base class with assets and rebalance dates.
+
+        Creates dataframe ``self.weights`` which will be inherited downstream.
+
+        Args:
+            assets (List[str]): list of tickers
+            rebalance_dates (List[str]): list of rebalance dates
+        """
+        self.assets = assets
+        self.rebalance_dates = pd.DatetimeIndex(rebalance_dates, tz="utc").tz_convert(None)
+        self.n_assets, self.n_obs = len(assets), len(self.rebalance_dates)
+
+        # set up multilevel index
+        index = pd.MultiIndex.from_product([self.rebalance_dates, assets])
+        self.weights = pd.DataFrame(index=index)
+
+
+class EqualWeights(Positions):
+    """Store equally weighted portfolio."""
+
+    def __init__(self, assets: List[str], rebalance_dates: List[datetime]):
+        """Initialize and create equally weighted portfolio."""
+        super().__init__(assets, rebalance_dates)
+        self.__name__ = "EW"
+        position = np.ones(self.n_assets) / self.n_assets
+        self.weights[self.__name__] = np.tile(position, self.n_obs)
+        self.weights.sort_index(inplace=True)
+        self.weights.index.names = ["Date", "ID"]
+
+
+class RiskParity(Positions):
+    """Store naive implementation of Risk Parity."""
+
+    def __init__(
+        self,
+        assets: List[str],
+        rebalance_dates: List[datetime],
+        returns: pd.DataFrame,
+        estimator: str = "hist",
+        lookback: int = 21,
+        **kwargs: dict
+    ):
+        """Initialize class for Risk Parity calculation.
+
+        Args:
+            assets (List[str]): list of asset tickers
+            rebalance_dates (List[datetime]): list of rebalancing dates
+            returns (pd.DataFrame): dataframe of daily asset returns
+            estimator (str, optional): volatility estimation method. Defaults to "hist".
+            lookback (float, optional): volatility estimation window. Defaults to 21.
+        """
+        super().__init__(assets, rebalance_dates)
+        self.returns = returns
+        self.__name__ = "RP"
+
+        # calculate historical vol (different estimators shall be used later)
+        self.weights = self.create_weights(estimator, lookback, **kwargs)
+        self.weights.index.names = ["Date", "ID"]
+
+    def create_weights(self, method: str, lookback: float, **kwargs: dict) -> pd.DataFrame:
+        """Create risk parity weights and store them in dataframe.
+
+        The estimator can be one of: ``ewma``, ``hist``, ``equal_risk`` or ``min_variance``. The
+        latter two involve an optimization process.
+
+        Additional estimation parameters can be passed as keyword arguments. For example you can
+        pass the halflife parameter for ``alpha=0.94`` when using ``ewma``.
+
+        Args:
+            method (str, optional): volatility estimation method.
+            lookback (float, optional): volatility estimation window.
+            **kwargs: keyword arguments for volatility estimation
+
+        Returns:
+            pd.DataFrame: weights for each asset
+        """
+        if method in ["hist", "ewma"]:
+            inverse_vols = 1 / calculate_rolling_volatility(
+                self.returns, estimator=method, lookback=lookback, **kwargs
+            )
+            inverse_vols = inverse_vols.reindex(self.rebalance_dates)
+            weights = inverse_vols.div(inverse_vols.sum(axis=1).values.reshape(-1, 1))
+
+        elif method in ["equal_risk", "min_variance"]:
+            optimizer = {
+                "min_variance": calculate_min_variance_portfolio,
+                "equal_risk": calculate_risk_parity_portfolio,
+            }
+            weights = rolling_optimization(
+                self.returns, self.rebalance_dates, optimizer[method], lookback, **kwargs
+            )
+        else:
+            raise NotImplementedError
+
+        weights = np.maximum(0, weights)
+        return weights.stack().rename(self.__name__).to_frame()
+
+
+def rolling_optimization(
+    returns: pd.DataFrame,
+    rebalance_dates: List[datetime],
+    optimizer: Callable,
+    lookback: int,
+    shrinkage: str = None,
+    shrinkage_factor: float = None,
+) -> pd.DataFrame:
+    """Perform rolling optimization over rebalance dates.
+
+    Args:
+        returns (pd.DataFrame): dataframe of daily asset returns
+        rebalance_dates (List[datetime]): list of rebalancing dates
+        optimizer (Callable): optimization routine, e.g. ``calculate_risk_parity_portfolio``
+        lookback (int): window for covariance data
+        shrinkage (str, optional): covariance shrinkage method. Defaults to None.
+        shrinkage_factor (float, optional): covariance shrinkage factor. Defaults to None.
+
+    Returns:
+        pd.DataFrame: table of optimized asset weights
+    """
+    weights = []
+
+    for date in rebalance_dates:
+        data = returns[returns.index <= date].iloc[-lookback:, :]
+        cov = Covariance(data, shrinkage, shrinkage_factor)
+
+        w_opt = optimizer(cov)
+        weights.append(w_opt)
+
+    weights = pd.DataFrame(np.vstack(weights), index=rebalance_dates, columns=returns.columns)
+    return weights
+
+
+def vigilant_allocation(
+    data: pd.Series,
+    risk_assets: List[str],
+    safe_assets: List[str],
+    top_k: int = 5,
+    step: float = 0.25,
+) -> pd.DataFrame:
+    """Allocate assets based on threshold using scores.
+
+    Used in computing the Vigilant portfolios. The allocation works as follows (using $k=5$):
+    Determine the number of assets $n$ with negative $Z$, if $n>4$ allocate 100% in safe asset with
+    highest momentum score, if $n=3$ put 75% in safest asset, remaining 25% is split equally in 5
+    risk assets with highest momentum, if $n=2$ put 50% in safest asset, 50% split evenly top 5
+    risk assets etc.
+
+    Args:
+        data (pd.Series): dataframe with signals
+        risk_assets (List[str]): list of risky assets
+        safe_assets (List[str]): list of safety assets
+        top_k (int, optional): rank threshold. Defaults to 5.
+        step (float, optional): step in allocation to risk assets given signal. Defaults to 0.25.
+
+    Returns:
+        pd.DataFrame: dataframe of weights
+    """
+    is_neg = sum(np.where(data < 0, 1, 0))
+    empty = data * np.nan
+    safety = pd.concat([data.loc[safe_assets].rank(ascending=False), empty.loc[risk_assets]])
+    safety = safety[~safety.index.duplicated()].sort_index()
+    risky = pd.concat([data.loc[risk_assets].rank(ascending=False), empty.loc[safe_assets]])
+    risky = risky[~risky.index.duplicated()].sort_index()
+
+    # allocate assets based on number of negative scores
+    safe_weights = np.where(safety == 1, min([1, step * is_neg]), 0)
+    risk_weights = np.where(risky <= top_k, (1 - min([1, step * is_neg])) / top_k, 0)
+    weights = safe_weights + risk_weights
+    return pd.DataFrame(weights, index=safety.index).T
+
+
+def kipnis_allocation(
+    returns: pd.DataFrame,
+    signals: pd.DataFrame,
+    rebalance_dates: List[pd.Timestamp],
+    canary_assets: List[str],
+    risk_assets: List[str],
+    safe_assets: List[str],
+    top_k: int = 5,
+) -> pd.DataFrame:
+    """Kipnis defensive asset allocation scheme.
+
+    The strategy allocates based on a momentum score, which is a weighted average over different
+    windows. A canary universe is used to allocate either towards risk or safe assets, e.g. if all
+    canary assets are positively trending, then allocate 100% towards the investment universe. If
+    only one canary asset (out of two) has positive trend, then put 50% into crash protection (e.g.
+    a govy ETF like ``IEF``). If no assets has positive trend, then pull the entire funds into crash
+    protection, that is a cash ETF like ``BIL``.
+
+    The final allocation is done using the minimum variance portfolio with a cleaned covariance
+    matrix. It uses a weighted average correlation (over 4 windows) and the 1 month realised
+    volatility.
+
+    The strategy is more closely explained in:
+    https://quantstrattrader.com/2019/01/24/right-now-its-kda-asset-allocation/
+
+    Args:
+        returns (pd.DataFrame): table of asset returns
+        signals (pd.DataFrame): table of asset signals
+        rebalance_dates (List[pd.Timestamp]): list of rebalance dates
+        canary_assets (List[str]): list of canary or proxy assets
+        risk_assets (List[str]): list of risky assets (e.g. equities)
+        safe_assets (List[str]): list of safe assets (e.g. govies)
+        top_k (int, optional): number of risky assets to include. Defaults to 5.
+
+    Returns:
+        pd.DataFrame: table of returns
+    """
+    all_weights = []
+    rebal_dates = [i for i in rebalance_dates if i >= signals.index[0]]
+
+    for date in rebal_dates:
+        signal_sample = signals[signals.index <= date].iloc[-1, :]
+        risk_on = set(signal_sample[signal_sample > 0].index).intersection(canary_assets)
+        ranked = signal_sample[risk_assets].rank(ascending=False)
+        buy_assets = ranked[ranked <= top_k].index
+
+        # if canary assets are positive then go risk on, else allocate to safe assets
+        if len(risk_on) == len(canary_assets):
+            canary_weight = 0
+        elif len(risk_on) == 0:
+            equal_weight = np.ones((1, len(safe_assets))) / len(safe_assets)
+            weights = pd.DataFrame(equal_weight, columns=safe_assets, index=[date])
+            all_weights.append(weights)
+            continue
+        elif signal_sample[safe_assets].le(0).any():
+            weights = pd.DataFrame({"BIL": [1]}, index=[date])
+            all_weights.append(weights)
+            continue
+        else:
+            canary_weight = 0.5
+
+        # calculate min variance portfolio for top k risk assets
+        return_sample = returns.loc[returns.index <= date, list(buy_assets)].iloc[-260:, :]
+        w_cov = weighted_covariance_matrix(return_sample)
+        risky_weights = calculate_min_variance_portfolio(w_cov).reshape(1, -1)
+
+        # stack risk and safe assets together, weighting them by the canary factor
+        safe_weights = np.ones((1, len(safe_assets))) / len(safe_assets)
+        weights = np.hstack([(1 - canary_weight) * risky_weights, canary_weight * safe_weights])
+        cols = list(buy_assets) + list(safe_assets)
+        weights = pd.DataFrame(weights, index=[date], columns=cols)
+
+        # if any assets show up twice remove them
+        weights = weights.loc[:, ~weights.columns.duplicated()]
+        all_weights.append(weights)
+    return pd.concat(all_weights, join="outer").fillna(0)
+
+
+def aqr_trend_allocation(
+    returns: pd.DataFrame,
+    signal: pd.DataFrame,
+    rebalance_dates: pd.DatetimeIndex,
+    risk_assets: List[str],
+    cash_asset: str,
+) -> pd.DataFrame:
+    """Allocate portfolio to trending assets using AQR Risk Parity methodology.
+
+    Puts share of non-trending assets into cash, depending on signal strength.ary_
+
+    Args:
+        returns (pd.DataFrame): table of asset returns
+        signal (pd.DataFrame): table of signals, i.e. SMA crossovers
+        rebalance_dates (pd.DatetimeIndex): list of rebalance dates
+        risk_assets (List[str]): list of risky assets
+        cash_asset (str): safe or cash asset
+
+    Returns:
+        pd.DataFrame: a table of portfolio weights
+    """
+    strategy_weights = []
+
+    for date in rebalance_dates[rebalance_dates >= signal.dropna().index.min()]:
+        return_sample = returns.loc[returns.index <= date].iloc[-260 * 3:, :]
+        monthly_ret = return_sample.resample("BME").apply(lambda x: np.prod(1 + x) - 1)
+        excess_ret = monthly_ret[risk_assets].sub(monthly_ret[[cash_asset]].values)
+
+        # weight assets by inverse of vol: s_i = 1 / vol_i / (1 / sum[vol_i])
+        inv_vol = 1 / excess_ret.std() * np.sqrt(12)
+        buy_assets = signal.loc[signal.index <= date, risk_assets].iloc[-1].ge(0)
+        buy_assets = buy_assets.index[buy_assets]
+
+        # portfolio weights calculation
+        risk_allocation = len(buy_assets) / len(risk_assets)
+        risk_weights = inv_vol.loc[buy_assets] / inv_vol.loc[buy_assets].sum() * risk_allocation
+        weights = risk_weights.rename(date).to_frame().T
+        weights.columns.name, weights.index.name = "ID", "Date"
+        weights[[cash_asset]] = 1 - risk_allocation
+        strategy_weights.append(weights)
+
+    return pd.concat(strategy_weights).fillna(0)
---
+++ packages/pytaa/pytaa-0.5.1-py3-none-any.whl/pytaa/tools/risk.py
@@ -0,0 +1,347 @@
+"""Utility functions such as volatility estimators and statistical measures."""
+
+import logging
+from typing import Union
+
+import numpy as np
+import pandas as pd
+import scipy
+
+from pytaa.tools.logger import setup_logger
+
+setup_logger()
+logger = logging.getLogger(__name__)
+
+
+def autocorrelation(returns: pd.DataFrame, order: int = 1) -> np.array:
+    """Calculate autocorrelation for array of returns.
+
+    Args:
+        returns (pd.DataFrame): table of returns
+        order (int, optional): time series lag. Defaults to 1.
+
+    Returns:
+        np.array: array of autocorrelation coefficients
+    """
+    index = returns.index.name
+    merged = returns.merge(returns.shift(order), on=index, suffixes=["", "_lag"])
+    n_assets = returns.shape[1]
+    corr = merged.corr().iloc[:n_assets, n_assets:]
+    return np.diag(corr)
+
+
+def calculate_rolling_volatility(
+    returns: pd.DataFrame,
+    lookback: int = 21,
+    factor: int = 252,
+    estimator: str = "hist",
+    decay: float = 0.97,
+) -> pd.DataFrame:
+    r"""Calculate historical volatility for all calendar days.
+
+    Currently supported estimators are ``hist`` (realised standard deviation over lookback) and
+    ``ewma`` (exponentially weighted with decay factor equal to :math:`1 - \lambda`).
+
+    Args:
+        returns (pd.DataFrame): table of asset returns
+        lookback (int, optional): window over which vol is calculated. Defaults to 21.
+        factor (int, optional): number of business days in a year. Defaults to 252.
+        decay (float, optional): half life, or lambda, if using EWMA. Defaults to 0.97.
+        estimator (str, optional): volatility estimator. Defaults to hist.
+
+    Returns:
+        pd.DataFrame: time series of volatilities matching rebalance dates
+
+    Raises:
+        NotImplementedError: when estimator is invalid
+    """
+    if estimator == "hist":
+        rolling_volatility = returns.rolling(lookback).std()
+    elif estimator == "ewma":
+        rolling_volatility = returns.ewm(alpha=1 - decay).std()
+    else:
+        raise NotImplementedError
+    return rolling_volatility.asfreq("D").ffill() * np.sqrt(factor)
+
+
+def ledoit_wolf_constant_correlation(
+    data: Union[pd.DataFrame, np.array], shrink: float = None
+) -> np.array:
+    """Shrink sample covariance matrix using Ledoit-Wolf (2003).
+
+    The Matlab code provided by Ledoit and Wolf can be found here:
+    https://github.com/oledoit/covShrinkage/blob/main/covCor.m
+
+    Note tha the shrinkage value ``shrink`` will be estimated from the data if it is not provided.
+
+    Args:
+        data (Union[pd.DataFrame, np.array]): a table of data
+        shrink (float, optional): shrinkage value. Defaults to None.
+
+    Returns:
+        np.array: a shrunk covariance matrix
+    """
+    X = np.asarray(data)  # noqa: N806
+    x = X - X.mean(axis=0)
+    t, n = np.shape(X)
+
+    cov_sample = np.cov(X, rowvar=False)
+
+    var = np.diag(cov_sample).reshape(-1, 1)
+    std = np.sqrt(var)
+    _var = np.tile(var, (n,))
+    _std = np.tile(std, (n,))
+    r_bar = (np.sum(cov_sample / (_std * _std.T)) - n) / (n * (n - 1))
+    prior = r_bar * (_std * _std.T)
+    prior[np.diag_indices_from(prior)] = var[:, 0]
+
+    if shrink is None:
+        # pi hat
+        y = np.square(x)
+        pi_mat = y.T @ y / t - 2 * x.T @ x * cov_sample / t + np.square(cov_sample)
+        pi_hat = np.sum(pi_mat)
+
+        # rho hat
+        term1 = np.power(x, 3).T @ x / t
+        help_mat = x.T @ x / t
+        help_diag = np.diag(help_mat)
+        term2 = np.tile(help_diag, (n, 1)).T * cov_sample
+        term3 = help_mat * _var
+        term4 = _var * cov_sample
+        theta_mat = term1 - term2 - term3 + term4
+        theta_mat[np.diag_indices_from(theta_mat)] = np.zeros(n)
+        rho_hat = sum(np.diag(pi_mat)) + r_bar * np.sum(((1 / std) @ std.T) * theta_mat)
+
+        # gamma hat
+        gamma_hat = np.square(np.linalg.norm(cov_sample - prior, "fro"))
+
+        # calculate shrinkage if not provided
+        kappa_hat = (pi_hat - rho_hat) / gamma_hat
+        delta = max(0, min(1, kappa_hat / t))
+    else:
+        delta = shrink
+
+    # calculate final shrunk covariance matrix
+    shrunk_cov = delta * prior + (1 - delta) * cov_sample
+    return shrunk_cov
+
+
+class Covariance(np.ndarray):
+    """Instantiate covariance matrix as numpy object."""
+
+    def __new__(
+        cls,
+        data: pd.DataFrame,
+        shrinkage: str = None,
+        shrinkage_factor: float = None,
+        **kwargs: dict
+    ):
+        """Initialise covariance matrix.
+
+        Currently the following shrinkage factors are supported: ``None``, ``ledoit_wolf``,
+        ``constant`` and ``weighted``. Constant shrinkage shrinks the covar matrix towards the
+        identity matrix. If using constant shrinkage, then a shrinkage factor must be provided.
+        The weighted method follows Ilya Kipnis (2019).
+
+        Args:
+            data (pd.DataFrame): data table
+            shrinkage (str, optional): shrinkage method. Defaults to None.
+            shrinkage_factor (float, optional): shrinkage factor between 0 and 1. Defaults to None.
+            **kwargs (dict): additional keyword arguments for estimation method
+        """
+        self = np.asarray(data).view(cls)
+        self.shrinkage = shrinkage
+        self.shrinkage_factor = shrinkage_factor
+        return self._estimate(**kwargs)
+
+    def _estimate(self, **kwargs) -> np.array:
+        """Estimate variance-covariance matrix.
+
+        Raises:
+            NotImplementedError: if shrinkage method is invalid
+
+        Returns:
+            np.array: NxN covariance matrix
+        """
+        if self.shrinkage is None:  # noqa: E1101
+            return np.cov(self, rowvar=False)
+        elif self.shrinkage == "ledoit_wolf":  # noqa: E1101
+            return ledoit_wolf_constant_correlation(self, self.shrinkage_factor)  # noqa: E1101
+        elif self.shrinkage == "constant":  # noqa: E1101
+            sample_cov = np.cov(self, rowvar=False)
+            target_cov = (1 - self.shrinkage_factor) * np.eye(sample_cov.shape[1])
+            return self.shrinkage_factor * sample_cov + target_cov
+        elif self.shrinkage == "weighted":  # noqa: E1101
+            return weighted_covariance_matrix(self, **kwargs)
+        else:
+            raise NotImplementedError
+
+
+def calculate_risk_parity_portfolio(
+    cov: Union[Covariance, np.array], risk_budget: np.array = None
+) -> np.array:
+    """Allocate risk equally without leverage.
+
+    The methodology can be found in  Maillard, Roncalli & Teiletche (2009):
+    http://www.thierry-roncalli.com/download/erc.pdf
+
+    Args:
+        cov (Union[Covariance, np.array]): covariance matrix
+        risk_budget(np.array): vector of risk bugets summing up to 1
+
+    Returns:
+        np.array: a 1d vector of weights
+    """
+    inv_sigma = 1 / np.sqrt(np.diag(cov))
+    initial_weights = inv_sigma / np.sum(inv_sigma)
+
+    if risk_budget is None:
+        risk_budget = np.ones((cov.shape[1], 1)) / cov.shape[1]
+
+    c_max = -cov.shape[0] * np.log(cov.shape[0])
+
+    constraints = (
+        {"type": "ineq", "fun": lambda w: w},
+        {"type": "ineq", "fun": lambda w: np.sum(risk_budget * np.log(w)) - c_max},
+    )
+
+    result = scipy.optimize.minimize(
+        lambda w, S: np.sqrt(w.T @ S @ w),
+        x0=initial_weights,
+        args=(cov),
+        method="SLSQP",
+        constraints=constraints,
+        options={"disp": False},
+        tol=1e-9,
+    )
+    optimal_weights = result.x.flatten() / np.sum(result.x.flatten())
+    if result.success:
+        return optimal_weights
+    logger.warning("Risk Parity optimization failed: returning inverse vol weights.")
+    return initial_weights
+
+
+def calculate_min_variance_portfolio(cov: Union[Covariance, np.array]) -> np.array:
+    """Calculate minimum variance portfolio with no leverage.
+
+    Args:
+        cov (Union[Covariance, np.array]): covariance matrix
+
+    Returns:
+        np.array: a 1d vector of weights
+    """
+    inv_sigma = 1 / np.sqrt(np.diag(cov))
+    initial_weights = inv_sigma / np.sum(inv_sigma)
+
+    constraints = ({"type": "eq", "fun": lambda w: np.sum(w) - 1},)
+
+    result = scipy.optimize.minimize(
+        lambda w, S: w.T @ S @ w,
+        initial_weights,
+        args=[cov],
+        method="SLSQP",
+        bounds=scipy.optimize.Bounds(0, 1),
+        constraints=constraints,
+        options={"disp": False},
+        tol=1e-9,
+    )
+
+    if result.success:
+        return result.x.flatten()
+    logger.warning("Min variance optimization failed: returning inverse vol weights.")
+    return initial_weights
+
+
+def calculate_mean_variance_portfolio(
+    mu: np.array, cov: Union[Covariance, np.array]  # noqa: C0103
+) -> np.array:
+    """Calculate mean-variance portfolio with no leverage.
+
+    Args:
+        mu (np.array): vector of expected returns
+        cov (Union[Covariance, np.array]): covariance matrix
+
+    Returns:
+        np.array: a 1d vector of weights
+    """
+    initial_weights = np.ones(cov.shape[0]) / cov.shape[0]
+
+    constraints = (
+        {"type": "ineq", "fun": lambda w: w},
+        {"type": "eq", "fun": lambda w: np.sum(w) - 1},
+    )
+
+    result = scipy.optimize.minimize(
+        lambda w, S, mu: w.T @ S @ w - w.T @ mu,
+        initial_weights,
+        args=[cov, mu],
+        method="SLSQP",
+        bounds=scipy.optimize.Bounds(0, 1),
+        constraints=constraints,
+        options={"disp": False},
+        tol=1e-9,
+    )
+
+    if result.success:
+        return result.x.flatten()
+    logger.warning("Mean variance optimization failed: returning equal weights.")
+    return initial_weights
+
+
+def risk_contribution(weights: np.array, cov: np.array) -> np.array:
+    """Calculate ex-post risk contribution of portfolio allocation.
+
+    Args:
+        weights (np.array): Mx1 vector of weight
+        cov (np.array): MxM covariance matrix
+
+    Returns:
+        np.array: Mx1 vector of portfolio risk contributions
+    """
+    port_vol = np.sqrt(weights.T @ cov @ weights)
+    marginal_risk_contrib = cov @ weights
+    port_risk_contrib = marginal_risk_contrib * weights / port_vol
+    return port_risk_contrib
+
+
+# TODO: make this a class WeightedCovariance similar to above
+def weighted_covariance_matrix(
+    data: pd.DataFrame, weights: list = (12, 4, 2, 1), vol_window: int = 21, ann_factor: int = 252
+) -> np.array:
+    """Calculate weighted covariance matrix.
+
+    This technique is used in Kipnis Asset Allocation. Each weight in the ``weights`` list refers
+    to a number of months. The weighted correlation matrix is first calculated as:
+
+    R = [(21-day correlation * 12) + (63-day correlation * 4) + (126-day correlation * 2) + \
+        (252-day correlation)] / 19
+
+    Then the covariance matrix is calculated using 21-day volatility: diag(vol) .* R .* diag(vol).
+
+    More details can be found on:
+    https://quantstrattrader.com/2019/01/24/right-now-its-kda-asset-allocation/
+
+    Args:
+        data (pd.DataFrame): table of returns
+        weights (list): list of weights in months. Defaults to [12, 4, 2, 1].
+        vol_window (int, optional): sample size of volatility. Defaults to 21.
+        ann_factor (int, optional): number of days in year. Defaults to 252.
+
+    Returns:
+        np.array: variance covariance matrix
+    """
+    weighted_corr = None
+    _data = np.asarray(data)
+
+    for window in weights:
+        lookback = int(ann_factor / window)
+        sample_corr = window * np.corrcoef(_data[-lookback:, :], rowvar=False)
+
+        if weighted_corr is None:
+            weighted_corr = sample_corr
+        else:
+            weighted_corr += sample_corr
+
+    weighted_corr = sample_corr / np.sum(weights)
+    sample_vol = np.std(_data[-vol_window:, :], axis=0)
+    return np.diag(sample_vol) @ weighted_corr @ np.diag(sample_vol)
