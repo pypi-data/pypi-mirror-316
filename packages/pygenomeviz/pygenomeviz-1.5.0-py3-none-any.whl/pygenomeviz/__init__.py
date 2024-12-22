108320501165ef4dcc3d62d421cd224bfb09044a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:13:31 +0000

    Add package pygenomeviz/1.5.0/pygenomeviz-1.5.0-py3-none-any.whl

---
+++ packages/pygenomeviz/pygenomeviz-1.5.0-py3-none-any.whl/pygenomeviz/__init__.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl
+
+from pygenomeviz.genomeviz import GenomeViz
+
+__version__ = "1.5.0"
+
+__all__ = [
+    "GenomeViz",
+]
+
+###########################################################
+# Matplotlib Runtime Configuration
+###########################################################
+
+# Setting matplotlib rc(runtime configuration) parameters
+# https://matplotlib.org/stable/tutorials/introductory/customizing.html
+mpl_rc_params = {
+    # # Savefig
+    "savefig.bbox": "tight",  # Default: None
+    "savefig.pad_inches": 0.5,  # Default: 0.1
+    # SVG
+    "svg.fonttype": "none",
+}
+mpl.rcParams.update(mpl_rc_params)
---
+++ packages/pygenomeviz/pygenomeviz-1.5.0-py3-none-any.whl/pygenomeviz/scripts/gui.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+import argparse
+import importlib.util
+import os
+import signal
+import subprocess as sp
+import sys
+import textwrap
+from pathlib import Path
+
+import pygenomeviz
+
+CLI_NAME = "pgv-gui"
+
+
+def main() -> None:
+    """Launch pyGenomeViz WebApp"""
+    # Get arguments
+    args = get_args()
+    port: int = args.port
+
+    # Check streamlit installation
+    if importlib.util.find_spec("streamlit") is None:
+        err_msg = textwrap.dedent(
+            """
+            Failed to launch pyGenomeViz WebApp. Streamlit is not installed!!
+
+            # Install PyPI package
+            $ pip install -U pygenomeviz[gui]
+
+            # Install conda-forge package
+            $ conda install -c conda-forge streamlit
+            """
+        )
+        print(err_msg)
+        sys.exit(1)
+
+    # Launch app
+    app_path = Path(__file__).parent.parent / "gui" / "app.py"
+    os.environ["STREAMLIT_THEME_BASE"] = "dark"
+    os.environ["STREAMLIT_SERVER_RUN_ON_SAVE"] = "true"
+    os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
+    os.environ["STREAMLIT_SERVER_MAX_UPLOAD_SIZE"] = "100"
+    os.environ["PGV_GUI_LOCAL"] = "true"
+    try:
+        sp.run(f"streamlit run {app_path} --server.port {port}", shell=True)
+    except KeyboardInterrupt:
+        sys.exit(-signal.SIGINT)
+
+
+def get_args(cli_args: list[str] | None = None) -> argparse.Namespace:
+    """Get arguments
+
+    Parameters
+    ----------
+    cli_args : list[str] | None, optional
+        CLI arguments (Used in unittest)
+
+    Returns
+    -------
+    args : argparse.Namespace
+        Argument parameters
+    """
+    description = textwrap.dedent(
+        """
+        pyGenomeViz CLI for launching Streamlit Web Application
+
+        Users can access the web app with http://localhost:8501 (default).
+        """
+    )[1:-1]
+    parser = argparse.ArgumentParser(
+        description=description,
+        usage=f"{CLI_NAME} [options]",
+        epilog="",
+        add_help=False,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    default_port = 8501
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help=f"Port number to open web browser (Default: {default_port})",
+        default=default_port,
+        metavar="",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        version=f"v{pygenomeviz.__version__}",
+        help="Print version information",
+        action="version",
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        help="Show this help message and exit",
+        action="help",
+    )
+
+    args = parser.parse_args(cli_args)
+
+    return args
