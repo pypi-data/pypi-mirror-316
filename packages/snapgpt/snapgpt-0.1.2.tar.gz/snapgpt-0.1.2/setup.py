11df7054b7661c30e2387caaf1dcd2b8625d7f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:52 +0000

    Add package snapgpt/0.1.2/snapgpt-0.1.2.tar.gz

---
+++ packages/snapgpt/snapgpt-0.1.2.tar.gz/snapgpt-0.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# Read the README for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="snapgpt",
+    version="0.1.2",
+    author="Daniel Price",
+    author_email="",
+    description="A tool to create readable snapshots of your codebase",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/halfprice06/snapgpt",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    entry_points={
+        'console_scripts': [
+            'snapgpt=snapgpt.cli:main',
+        ],
+    },
+    python_requires=">=3.7",
+)
