2a43f157ea0e4238bdccd5a5043142b98174cca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:41 +0000

    Add package model-checker/0.5.9/model_checker-0.5.9.tar.gz

---
+++ packages/model-checker/model_checker-0.5.9.tar.gz/model_checker-0.5.9/src/model_checker/test/utils.py
@@ -0,0 +1,26 @@
+"""run 'pytest' from the '.../model_checker' directory"""
+from model_checker.model_structure import make_model_for
+
+max_time = 2
+
+def failure_string(desired, premises, conclusions, time):
+    if desired is False:
+        return f'Erroneously found a model:\n\nPremises:\n{premises}\n\nConclusions:\n{conclusions}\n\nRun time: {time} seconds\n\nMax time: {max_time}'
+    return f'Erroneously did not find a model:\n\nPremises:\n{premises}\n\nConclusions:\n{conclusions}\n\nRun time: {time} seconds\n\nMax time: {max_time}'
+
+def check_model_status(N, premises, conclusions, desired, contingent_bool, disjoint_bool):
+    mod_setup = make_model_for(N, premises, conclusions, max_time, contingent_bool, disjoint_bool)
+    mod_status = mod_setup.model_status
+    mod_time = mod_setup.model_runtime
+    mod_timeout = mod_setup.timeout
+    assert (mod_timeout is False), f'Model timed out. Consider increasing max_time to be > {max_time} seconds.'
+    assert (mod_status == desired), failure_string(desired, premises, conclusions, mod_time)
+
+# def find_model_status(premises, conclusions, desired, N):
+#     mod_setup = make_model_for(N, premises, conclusions, max_time, contingent)
+#     mod_status = mod_setup.model_status
+#     mod_time = mod_setup.model_runtime
+#     if mod_status != desired and N < 10:
+#         N += 1
+#         find_model_status(premises, conclusions, desired, N)
+#     print(f"Found model in {mod_time} for N = {N}.")
