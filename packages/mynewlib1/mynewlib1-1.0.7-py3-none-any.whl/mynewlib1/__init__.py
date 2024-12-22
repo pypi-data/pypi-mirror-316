9d6c543b18c2afcfab4d1ce700e06bb797bb2ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:33 +0000

    Add package mynewlib1/1.0.7/mynewlib1-1.0.7-py3-none-any.whl

---
+++ packages/mynewlib1/mynewlib1-1.0.7-py3-none-any.whl/mynewlib1/__init__.py
@@ -0,0 +1,3 @@
+from  .operations import *
+from  .operations_str import *
+from  .test_math_operations import *
---
+++ packages/mynewlib1/mynewlib1-1.0.7-py3-none-any.whl/mynewlib1/operations_str.py
@@ -0,0 +1,28 @@
+class Task:
+    def __init__(self, formula):
+        self.formula = formula
+
+    def compute(self):
+        operators = ['+', '-', '*', '/']
+
+        # Пробуем получить наиболее приоритетный оператор
+        for op in operators:
+            if op in self.formula:
+                index = self.formula.index(op)
+
+                # Разделяем на левую и правую часть
+                left = Task(self.formula[:index])
+                right = Task(self.formula[index + 1:])
+
+                match op:
+                    case '+':
+                        return left.compute() + right.compute()
+                    case '-':
+                        return left.compute() - right.compute()
+                    case '*':
+                        return left.compute() * right.compute()
+                    case '/':
+                        return left.compute() / right.compute()
+
+        # Если нет операторов, просто возвращаем число
+        return float(self.formula)
