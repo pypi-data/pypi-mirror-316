8a519647019196de042958996cb6e6bcbbf6c36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:11 +0000

    Add package my-pac/1.0.0/my_pac-1.0.0.tar.gz

---
+++ packages/my-pac/my_pac-1.0.0.tar.gz/my_pac-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='my_pac',
+   version='1.0.0',
+   author='yousef',
+   author_email='yousefouddane3@gmail.com',
+   packages=[''],
+   url='http://pypi.python.org/pypi/my_pac/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['my_pac']
+)
