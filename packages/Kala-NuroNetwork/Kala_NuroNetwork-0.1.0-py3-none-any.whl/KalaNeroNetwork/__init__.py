498d409ed4360b92af11ca87cf7b4cb53b1e63f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:06:19 +0000

    Add package Kala-NuroNetwork/0.1.0/Kala_NuroNetwork-0.1.0-py3-none-any.whl

---
+++ packages/Kala-NuroNetwork/Kala_NuroNetwork-0.1.0-py3-none-any.whl/KalaNeroNetwork/__init__.py
@@ -0,0 +1,8 @@
+from .KalaNeroNetwork import QuantumLayer,KalaNuroNetwork,KalaNuroTrainer,generate_large_data
+
+__all__ = ["QuantumLayer","KalaNuroNetwork","KalaNuroTrainer","generate_large_data"]
+
+__version__ = "0.1.0"
+__author__ = "N V R K SAI KAMESH YADAVALLI"
+__email__ = "saikamesh.y@gmail.com"
+__license__ = "MIT"
