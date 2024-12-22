28e3b44fbb8228be57969a1ab9e462a4a8420198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:37 +0000

    Add package axinite/1.5.0/axinite-1.5.0.tar.gz

---
+++ packages/axinite/axinite-1.5.0.tar.gz/axinite-1.5.0/README.md
@@ -0,0 +1,38 @@
+# Axinite
+A powerful open-source engine for advanced celestial mechanics.
+
+![axinite-1](https://github.com/user-attachments/assets/bcd7bc7e-627e-44e5-bfc6-d2ddd787a208)
+
+## Overview
+**Axinite** is an engine for calculating celestial motion. 
+You can use it to design your own solar systems, predict trajectories of rockets, simulate gravitational interactions and more.
+
+Check out the docs [here](https://jewels86.gitbook.io/axinite/axinite/getting-started).
+### What is `axtools`?
+`axtools` is a library to help abstract and simplify Axinite. With `axtools`, Developers can deploy Axinite powered applications with ease. 
+You can find the docs [here](https://jewels86.gitbook.io/axinite/axtools/quickstart).
+
+### Executable
+Axinite will soon be coming to executable format! 
+
+Development is still in progress.
+
+## Supporting Axinite
+### Contributing
+Feel free to open up a [pull request](https://github.com/jewels86/Axinite/pulls) or an [issue](https://github.com/jewels86/Axinite/issues) if there are any changes you'd like introduced to Axinite.
+Any help will be appricated!
+### Other ways to support
+- Giving this repository a star! This really helps me to get Axinite out into the world
+- Reaching out! I'd love to hear about your thoughts, ideas, and issues with Axinite.
+
+## Gallery
+![axinite-2](https://github.com/user-attachments/assets/2e952d41-5585-484d-bc3b-05c92aeefe2d)
+![axinite-3](https://github.com/user-attachments/assets/ba434ce4-79a3-4a04-a7c4-45232d9fa11a)
+![axinite-4](https://github.com/user-attachments/assets/af13ee05-f6ef-4d24-8446-39e6544df2ca)
+![axinite-5](https://github.com/user-attachments/assets/c16db758-2ad2-47d8-9f1d-190727f9e881)
+
+
+## Todos
+- Create `ax-cli` executable
+- ~~Add live mode~~
+- ~~Move actools into axinite.tools and add axinite[tools]~~
---
+++ packages/axinite/axinite-1.5.0.tar.gz/axinite-1.5.0/axinite/tools/frontends/vpython.py
@@ -0,0 +1,75 @@
+from axinite.tools import AxiniteArgs, string_to_color, Body
+from vpython import *
+from itertools import cycle
+from astropy.coordinates import CartesianRepresentation
+from vpython.no_notebook import stop_server
+  
+def to_vec(cartesian_representation: CartesianRepresentation):
+    return vector(cartesian_representation.x.value, cartesian_representation.y.value, cartesian_representation.z.value)
+    
+def vpython_frontend(args: AxiniteArgs, mode: str, **kwargs):
+    if mode == "live" or mode == "run":
+        return vpython_rt(args, **kwargs)
+    elif mode == "show":
+        return vpython_static(args, **kwargs)
+    
+def vpython_rt(args: AxiniteArgs):
+    if args.rate is None:
+        args.rate = 100
+    if args.radius_multiplier is None:
+        args.radius_multiplier = 1
+    if args.retain is None:
+       args.retain = 200
+    
+    scene = canvas(title=args.name)
+    scene.select()
+
+    colors = cycle([color.red, color.blue, color.green, color.orange, color.purple, color.yellow])
+
+    global pause
+    pause = False
+    def pause_handler():
+        global pause
+        pause = not pause
+    button(text='Pause', bind=pause_handler, pos=scene.caption_anchor)
+
+    spheres = {}
+    labels = {}
+    lights = {}
+
+    for body in args.bodies:
+        body_color = string_to_color(body.color, "vpython") if body.color != "" else next(colors)
+        body_retain = body.retain if body.retain != None else args.retain
+        spheres[body.name] = sphere(pos=to_vec(body.r[0]), radius=body.radius.value * args.radius_multiplier, color=body_color, make_trail=True, retain=body_retain, interval=10)
+        labels[body.name] = label(pos=spheres[body.name].pos, text=body.name, xoffset=15, yoffset=15, space=30, height=10, border=4, font='sans')
+        if body.light == True: 
+            lights[body.name] = local_light(pos=to_vec(body.r[0]), color=body_color)
+            attach_light(spheres[body.name], lights[body.name])
+
+    def fn(t, **kwargs):
+        rate(args.rate)
+        for body in kwargs["bodies"]:
+            spheres[body.name].pos = to_vec(body.r[t.value])
+            labels[body.name].pos = spheres[body.name].pos
+            try: lights[body.name].pos = spheres[body.name].pos
+            except: pass
+        print(f"t = {t}", end='\r')
+        if pause: 
+            while pause: rate(10)
+
+    return fn, stop_server
+
+def vpython_static(args: AxiniteArgs):
+    scene = canvas(title=args.name)
+    scene.select()
+
+    colors = cycle([color.red, color.blue, color.green, color.orange, color.purple, color.yellow])
+
+    def fn(body: Body):
+        body_color = string_to_color(body.color, "vpython") if body.color != "" else next(colors)
+        
+        label(pos=to_vec(body.r[0]), text=body.name, xoffset=15, yoffset=15, space=30, height=10, border=4, font='sans', color=body_color)
+        curve(pos=[to_vec(r) for r in body.r.values()], color=body_color)
+        sphere(pos=to_vec(body.r[0]), radius=body.radius.value * args.radius_multiplier, color=body_color, opacity=0.2, make_trail=False)
+
+    return fn, None, stop_server
