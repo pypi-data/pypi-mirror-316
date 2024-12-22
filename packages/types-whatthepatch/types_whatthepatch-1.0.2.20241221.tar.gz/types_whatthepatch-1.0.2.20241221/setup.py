80647af4a421f93b85ef76a65869d5e659b0eaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:13:20 +0000

    Add package types-whatthepatch/1.0.2.20241221/types_whatthepatch-1.0.2.20241221.tar.gz

---
+++ packages/types-whatthepatch/types_whatthepatch-1.0.2.20241221.tar.gz/types_whatthepatch-1.0.2.20241221/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-whatthepatch"
+description = "Typing stubs for whatthepatch"
+long_description = '''
+## Typing stubs for whatthepatch
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`whatthepatch`](https://github.com/cscorley/whatthepatch) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `whatthepatch`. This version of
+`types-whatthepatch` aims to provide accurate annotations for
+`whatthepatch==1.0.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/whatthepatch`](https://github.com/python/typeshed/tree/main/stubs/whatthepatch)
+directory.
+
+This package was tested with
+mypy 1.14.0,
+pyright 1.1.389,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`097581ea47d0fd77f097c88d80d5947e0218d9c4`](https://github.com/python/typeshed/commit/097581ea47d0fd77f097c88d80d5947e0218d9c4).
+'''.lstrip()
+
+setup(name=name,
+      version="1.0.2.20241221",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/whatthepatch.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['whatthepatch-stubs'],
+      package_data={'whatthepatch-stubs': ['__init__.pyi', 'apply.pyi', 'exceptions.pyi', 'patch.pyi', 'snippets.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
