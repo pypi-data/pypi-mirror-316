186c55fbc99e73cf027c8eaccf30c96a111481f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:14:24 +0000

    Add package falsifier/2.0/falsifier-2.0-py3-none-any.whl

---
+++ packages/falsifier/falsifier-2.0-py3-none-any.whl/falsifier/__init__.py
@@ -0,0 +1,34 @@
+# vim: set filetype=python fileencoding=utf-8:
+# -*- coding: utf-8 -*-
+
+#============================================================================#
+#                                                                            #
+#  Licensed under the Apache License, Version 2.0 (the "License");           #
+#  you may not use this file except in compliance with the License.          #
+#  You may obtain a copy of the License at                                   #
+#                                                                            #
+#      http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                            #
+#  Unless required by applicable law or agreed to in writing, software       #
+#  distributed under the License is distributed on an "AS IS" BASIS,         #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#  See the License for the specific language governing permissions and       #
+#  limitations under the License.                                            #
+#                                                                            #
+#============================================================================#
+
+
+''' Production of falsey objects. '''
+
+
+from . import __
+from . import exceptions
+from . import objects
+
+from .objects import *
+
+
+__version__ = '2.0'
+
+
+__.reclassify_modules( __name__, recursive = True )
