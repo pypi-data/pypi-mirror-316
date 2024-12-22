dba1c30eb45faf232dfb33c0b897aacb4b2e35c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:06:29 +0000

    Add package hotpot-zzy/0.5.0.2/hotpot_zzy-0.5.0.2.tar.gz

---
+++ packages/hotpot-zzy/hotpot_zzy-0.5.0.2.tar.gz/hotpot_zzy-0.5.0.2/hotpot/plugins/ccdc_api/miner.py
@@ -0,0 +1,17 @@
+"""
+python v3.9.0
+@Project: hotpot0.5.0
+@File   : miner
+@Auther : Zhiyuan Zhang
+@Data   : 2024/6/4
+@Time   : 21:00
+Note: This module requires CCDC Python API, see https://www.ccdc.cam.ac.uk/
+"""
+import ccdc
+from ccdc import io
+from ccdc.molecule import Molecule
+
+
+def remove_solvents(mol: Molecule):
+    """"""
+
