bffd43eaf65fdbda57b57e89bd37473882bd1bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:40 +0000

    Add package pyesoify/0.3.2/pyesoify-0.3.2-py3-none-any.whl

---
+++ packages/pyesoify/pyesoify-0.3.2-py3-none-any.whl/pyesoify/esoterify.py
@@ -0,0 +1,13 @@
+import ast
+
+from pyesoify.obfuscate import obfuscate
+
+
+def esoterify(code: str, output: str | None) -> None:
+    write_buffer = open(output, 'w') if output else None
+
+    print('__builtins__:', end='(', file=write_buffer)
+    for elem in ast.parse(code).body:
+        print(obfuscate(elem), end=',', file=write_buffer)
+
+    print(')=__import__("builtins").__dict__', file=write_buffer)
