cf00a7192d0138196918024ad834f8f039fa0ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:14:35 +0000

    Add package pyesoify/0.2.2/pyesoify-0.2.2-py3-none-any.whl

---
+++ packages/pyesoify/pyesoify-0.2.2-py3-none-any.whl/pyesoify/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+import ast
+
+from pyesoify.esoterify import esoterify
+
+
+def get_code(fpath) -> str:
+    with open(fpath, "r") as f:
+        code = f.read()
+
+    return code
+
+def main():
+    parser = argparse.ArgumentParser(prog="pyesoify", description="Esoterify Python code")
+    parser.add_argument("fpath", type=str, help="Path to the Python file to esoterify")
+    parser.add_argument("--output", type=str, help="Path to the output file", default=None)
+
+    args = parser.parse_args()
+
+    code = get_code(args.fpath)
+    esoterify(code, output=args.output)
+
+if __name__ == "__main__":
+    main()
