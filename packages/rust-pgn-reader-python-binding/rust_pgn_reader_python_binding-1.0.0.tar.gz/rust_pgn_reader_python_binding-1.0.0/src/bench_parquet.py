45389b63f5f2c8643937323fb9cbb7c473173b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:20 +0000

    Add package rust-pgn-reader-python-binding/1.0.0/rust_pgn_reader_python_binding-1.0.0.tar.gz

---
+++ packages/rust-pgn-reader-python-binding/rust_pgn_reader_python_binding-1.0.0.tar.gz/rust_pgn_reader_python_binding-1.0.0/src/bench_parquet.py
@@ -0,0 +1,21 @@
+import rust_pgn_reader_python_binding
+from fastparquet import ParquetFile
+
+from datetime import datetime
+
+
+file_path = "2013-07-train-00000-of-00001.parquet"
+
+
+a = datetime.now()
+
+pf = ParquetFile(file_path)
+
+for df in pf.iter_row_groups():
+    for row in df.movetext:
+        extractor = rust_pgn_reader_python_binding.parse_moves(row)
+        moves = extractor.moves
+        comments = extractor.comments
+
+b = datetime.now()
+print(b - a)
