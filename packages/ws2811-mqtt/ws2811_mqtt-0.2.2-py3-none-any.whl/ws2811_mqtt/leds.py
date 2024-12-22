a3851cc3406f854b0255569b1a56e3dace826418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:38 +0000

    Add package ws2811-mqtt/0.2.2/ws2811_mqtt-0.2.2-py3-none-any.whl

---
+++ packages/ws2811-mqtt/ws2811_mqtt-0.2.2-py3-none-any.whl/ws2811_mqtt/leds.py
@@ -0,0 +1,20 @@
+import board
+import neopixel
+import os
+from .logger import log_client
+
+
+NUM_LEDS = int(os.getenv("NUM_LEDS") or 50) # Number
+
+# Initialize the NeoPixel strip
+pixels = neopixel.NeoPixel(board.D18, NUM_LEDS, brightness=1, auto_write=False)
+# pixels = [(0, 0, 0) for _ in range(NUM_LEDS)]
+
+
+# Function to set a LED's color to white (255, 255, 255)
+def set_led_white(led_index):
+    try:
+        pixels[led_index] = (255, 255, 255)
+        log_client.info(f"[LED] LED {led_index} color set to white.")
+    except Exception as e:
+        log_client.error(f"[LED] Error setting LED color: {e}")
---
+++ packages/ws2811-mqtt/ws2811_mqtt-0.2.2-py3-none-any.whl/ws2811_mqtt/logger.py
@@ -0,0 +1,16 @@
+import logging
+from .args import args_client
+
+def init_logger():
+    global log_client
+    log_client = logging.getLogger(__name__)
+    verbosity_levels = {
+        0: logging.ERROR,
+        1: logging.WARNING,
+        2: logging.INFO,
+        3: logging.DEBUG
+    }
+    logging.basicConfig()
+    log_client.setLevel(verbosity_levels.get(args_client.verbosity, logging.INFO))
+
+log_client = init_logger()
