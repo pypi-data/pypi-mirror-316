6074024e7bf1fac721cda157e7dbec04b0628969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:11 +0000

    Add package ufaas-fastapi-business/0.4.4/ufaas_fastapi_business-0.4.4-py3-none-any.whl

---
+++ packages/ufaas-fastapi-business/ufaas_fastapi_business-0.4.4-py3-none-any.whl/ufaas_fastapi_business/core/config.py
@@ -0,0 +1,16 @@
+import os
+
+from fastapi_mongo_base.core.config import Settings as BaseSettings
+
+
+class Settings(BaseSettings):
+    business_domains_url: str = (
+        os.getenv(
+            "UFAAS_BUSINESS_DOMAINS_URL",
+            "https://business.ufaas.io/api/v1/apps/business",
+        )
+        + "/businesses/"
+    )
+    usso_app_auth_url: str = os.getenv(
+        "USSO_APP_AUTH_URL", "https://sso.ufaas.io/app-auth/access"
+    )
