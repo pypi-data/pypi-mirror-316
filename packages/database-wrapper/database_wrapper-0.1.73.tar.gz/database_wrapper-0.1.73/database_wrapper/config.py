3dbe5a808ead737878555d1e3d77ade028136155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:30 +0000

    Add package database-wrapper/0.1.73/database_wrapper-0.1.73.tar.gz

---
+++ packages/database-wrapper/database_wrapper-0.1.73.tar.gz/database_wrapper-0.1.73/database_wrapper/config.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+CONFIG: dict[str, Any] = {
+    # These are supposed to be set automatically by a git pre-compile script
+    # They are one git commit hash behind, if used automatically
+    "git_commit_hash": "443d5e2328c4e0bc041cab252394454c3dfe09bd",
+    "git_commit_date": "26.11.2024 22:13",
+    "app_version": "0.1.73",
+}
