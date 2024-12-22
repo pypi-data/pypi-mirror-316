5262eb2264a2d7461c694a922b46f55de9db97da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:19 +0000

    Add package unihan-etl/0.37.0/unihan_etl-0.37.0.tar.gz

---
+++ packages/unihan-etl/unihan_etl-0.37.0.tar.gz/unihan_etl-0.37.0/src/unihan_etl/__main__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""For accessing cihai as a package."""
+
+import pathlib
+import sys
+import typing as t
+
+if t.TYPE_CHECKING:
+    from typing_extensions import TypeAlias
+
+    # see https://github.com/python/typeshed/issues/8513#issue-1333671093 for the
+    # rationale behind this alias
+    _ExitCode: TypeAlias = t.Optional[t.Union[str, int]]
+
+
+def run() -> "_ExitCode":
+    """Execute unihan-etl via CLI entrypoint."""
+    base = pathlib.Path(__file__).parent.parent
+    sys.path.insert(0, str(base))
+    from .core import Packager
+
+    p = Packager.from_cli(sys.argv[1:])
+    p.download()
+    p.export()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(run())
