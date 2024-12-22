e019866eb4953d12e5388e79f04c87c46d377e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 14:05:25 +0000

    Add package polars-ds/0.7.0/polars_ds-0.7.0-cp39-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/polars-ds/polars_ds-0.7.0-cp39-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/polars_ds/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import polars as pl
+from ._utils import str_to_expr
+
+from polars_ds.num import *  # noqa: F403
+from polars_ds.metrics import *  # noqa: F403
+from polars_ds.stats import *  # noqa: F403
+from polars_ds.string import *  # noqa: F403
+from polars_ds.ts_features import *  # noqa: F403
+from polars_ds.expr_knn import *  # noqa: F403
+from polars_ds.expr_linear import *  # noqa: F403
+
+__version__ = "0.7.0"
+
+def frame(size: int = 2_000, index_name: str = "row_num") -> pl.DataFrame:
+    """
+    Generates a frame with only an index (row number) column.
+    This is a convenience function to be chained with pds.random(...) when running simulations and tests.
+
+    Parameters
+    ----------
+    size
+        The total number of rows in this dataframe
+    index_name
+        The name of the index column
+    """
+    return pl.DataFrame({index_name: range(size)})
