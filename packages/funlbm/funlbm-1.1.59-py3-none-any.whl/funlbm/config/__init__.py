1ba9a391c33519b2a9e62187d04df772aa69bd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 15:05:42 +0000

    Add package funlbm/1.1.59/funlbm-1.1.59-py3-none-any.whl

---
+++ packages/funlbm/funlbm-1.1.59-py3-none-any.whl/funlbm/config/__init__.py
@@ -0,0 +1,17 @@
+from .base import (
+    Boundary,
+    BoundaryCondition,
+    Config,
+    CoordConfig,
+    FlowConfig,
+    ParticleConfig,
+)
+
+__all__ = [
+    "Config",
+    "Boundary",
+    "BoundaryCondition",
+    "FlowConfig",
+    "ParticleConfig",
+    "CoordConfig",
+]
