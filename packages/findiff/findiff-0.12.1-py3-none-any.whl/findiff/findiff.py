6f1e3b44d369746184cc0d900ba8c2639b2e937c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:44 +0000

    Add package findiff/0.12.1/findiff-0.12.1-py3-none-any.whl

---
+++ packages/findiff/findiff-0.12.1-py3-none-any.whl/findiff/findiff.py
@@ -0,0 +1,266 @@
+import numpy as np
+from scipy import sparse
+
+from findiff.coefs import coefficients_non_uni, coefficients
+from findiff.grids import GridAxis, EquidistantAxis, NonEquidistantAxis
+from findiff.utils import (
+    get_long_indices_for_all_grid_points_as_ndarray,
+    to_long_index,
+    get_list_of_multiindex_tuples,
+    get_long_indices_for_all_grid_points_as_1d_array,
+)
+
+
+def build_differentiator(order: int, axis: GridAxis, acc):
+    if isinstance(axis, EquidistantAxis):
+        if not axis.periodic:
+            return _FinDiffUniform(axis.dim, order, axis.spacing, acc)
+        else:
+            return _FinDiffUniformPeriodic(axis.dim, order, axis.spacing, acc)
+    elif isinstance(axis, NonEquidistantAxis):
+        if not axis.periodic:
+            return _FinDiffNonUniform(axis.dim, order, axis.coords, acc)
+        else:
+            raise NotImplementedError("Periodic nonuniform axes not yet implemented")
+    else:
+        raise TypeError("Unknown axis type.")
+
+
+class _FinDiffBase:
+
+    def __init__(self, axis, order):
+        self.axis = axis
+        self.order = order
+
+    def validate_f(self, f):
+        try:
+            f.shape[self.axis]
+        except AttributeError as err:
+            raise ValueError(
+                "Diff objects can only be applied to arrays or evaluated(!) functions returning arrays"
+            ) from err
+
+    def apply_to_array(self, yd, y, weights, off_slices, ref_slice, dim):
+        """Applies the finite differences only to slices along a given axis"""
+
+        ndims = len(y.shape)
+
+        all = slice(None, None, 1)
+
+        ref_multi_slice = [all] * ndims
+        ref_multi_slice[dim] = ref_slice
+
+        for w, s in zip(weights, off_slices):
+            off_multi_slice = [all] * ndims
+            off_multi_slice[dim] = s
+            if abs(1 - w) < 1.0e-14:
+                yd[tuple(ref_multi_slice)] += y[tuple(off_multi_slice)]
+            else:
+                yd[tuple(ref_multi_slice)] += w * y[tuple(off_multi_slice)]
+
+    def shift_slice(self, sl, off, max_index):
+
+        if sl.start + off < 0 or sl.stop + off > max_index:
+            raise IndexError("Shift slice out of bounds")
+
+        return slice(sl.start + off, sl.stop + off, sl.step)
+
+    def matrix(self, shape):
+        siz = np.prod(shape)
+        mat = sparse.lil_matrix((siz, siz))
+        self.write_matrix_entries(mat, shape)
+        return sparse.csr_matrix(mat)
+
+    def write_matrix_entries(self, mat, shape):
+        raise NotImplementedError
+
+
+class _FinDiffUniform(_FinDiffBase):
+
+    def __init__(self, axis, order, spacing, acc):
+        super().__init__(axis, order)
+        self.spacing = spacing
+        self.acc = acc
+        coef_schemes = coefficients(self.order, acc)
+        self.forward = coef_schemes["forward"]
+        self.backward = coef_schemes["backward"]
+        self.center = coef_schemes["center"]
+
+    def __call__(self, f):
+        self.validate_f(f)
+        if np.issubdtype(f.dtype, np.integer):
+            f = f.astype(np.float64)
+
+        npts = f.shape[self.axis]
+        fd = np.zeros_like(f)
+        num_bndry_points = len(self.center["coefficients"]) // 2
+
+        self._apply_central_coefs(f, fd, npts, num_bndry_points)
+
+        self._apply_forward_coefs(f, fd, npts, num_bndry_points)
+
+        self._apply_backward_coefs(f, fd, npts, num_bndry_points)
+
+        h_inv = 1.0 / self.spacing**self.order
+        return fd * h_inv
+
+    def _apply_backward_coefs(self, f, fd, npts, num_bndry_points):
+        weights = self.backward["coefficients"]
+        offsets = self.backward["offsets"]
+        ref_slice = slice(npts - num_bndry_points, npts, 1)
+        off_slices = [
+            self.shift_slice(ref_slice, offsets[k], npts) for k in range(len(offsets))
+        ]
+        self.apply_to_array(fd, f, weights, off_slices, ref_slice, self.axis)
+
+    def _apply_forward_coefs(self, f, fd, npts, num_bndry_points):
+        weights = self.forward["coefficients"]
+        offsets = self.forward["offsets"]
+        ref_slice = slice(0, num_bndry_points, 1)
+        off_slices = [
+            self.shift_slice(ref_slice, offsets[k], npts) for k in range(len(offsets))
+        ]
+        self.apply_to_array(fd, f, weights, off_slices, ref_slice, self.axis)
+
+    def _apply_central_coefs(self, f, fd, npts, num_bndry_points):
+        weights = self.center["coefficients"]
+        offsets = self.center["offsets"]
+        ref_slice = slice(num_bndry_points, npts - num_bndry_points, 1)
+        off_slices = [
+            self.shift_slice(ref_slice, offsets[k], npts) for k in range(len(offsets))
+        ]
+        self.apply_to_array(fd, f, weights, off_slices, ref_slice, self.axis)
+
+    def write_matrix_entries(self, mat, shape):
+        long_indices_nd = get_long_indices_for_all_grid_points_as_ndarray(shape)
+        for scheme in ["center", "forward", "backward"]:
+
+            offsets_long = self._convert_1D_offsets_to_long_indices(
+                self.axis, getattr(self, scheme)["offsets"], shape
+            )
+
+            multi_slice = self._get_multislice_for_scheme(self.axis, scheme, shape)
+            Is = long_indices_nd[tuple(multi_slice)].reshape(-1)
+
+            coefs = getattr(self, scheme)["coefficients"]
+            for o, c in zip(offsets_long, coefs):
+                v = c / self.spacing**self.order
+                mat[Is, Is + o] = v
+
+    def _get_multislice_for_scheme(self, axis, scheme, shape):
+        ndims = len(shape)
+        multi_slice = [slice(None, None)] * ndims
+        nside = len(self.center["coefficients"]) // 2
+        if scheme == "center":
+            multi_slice[axis] = slice(nside, -nside)
+        elif scheme == "forward":
+            multi_slice[axis] = slice(0, nside)
+        else:
+            multi_slice[axis] = slice(-nside, None)
+        return multi_slice
+
+    def _convert_1D_offsets_to_long_indices(self, axis, offsets_1d, shape):
+        ndims = len(shape)
+        offsets_long = []
+        for o_1d in offsets_1d:
+            o_nd = np.zeros(ndims, dtype=int)
+            o_nd[axis] = o_1d
+            o_long = to_long_index(o_nd, shape)
+            offsets_long.append(o_long)
+        return offsets_long
+
+
+class _FinDiffUniformPeriodic(_FinDiffBase):
+
+    def __init__(self, axis, order, spacing, acc):
+        super().__init__(axis, order)
+        self.spacing = spacing
+        self.acc = acc
+        self.coefs = coefficients(self.order, acc)["center"]
+
+    def __call__(self, f):
+        self.validate_f(f)
+        if np.issubdtype(f.dtype, np.integer):
+            f = f.astype(np.float64)
+
+        if np.issubdtype(f.dtype, np.integer):
+            f = f.astype(np.float64)
+
+        fd = np.zeros_like(f)
+        for off, coef in zip(self.coefs["offsets"], self.coefs["coefficients"]):
+            fd += coef * np.roll(f, -off, axis=self.axis)
+        h_inv = 1.0 / self.spacing**self.order
+        return fd * h_inv
+
+    def write_matrix_entries(self, mat, shape):
+        Is = get_long_indices_for_all_grid_points_as_1d_array(shape)
+        h_inv = 1 / self.spacing**self.order
+        for o, c in zip(self.coefs["offsets"], self.coefs["coefficients"]):
+            Is_off = self._get_offset_indices_long(o, shape)
+            mat[Is, Is_off] = c * h_inv
+
+    def _get_offset_indices_long(self, o, shape):
+        ndims = len(shape)
+        idxs_short = [np.arange(n) for n in shape]
+        idxs_short[self.axis] = np.roll(np.arange(shape[self.axis]), -o)
+        grid = np.meshgrid(*idxs_short, indexing="ij")
+        index_tuples = np.stack(grid, axis=-1).reshape(-1, ndims)
+        Is_off = np.ravel_multi_index(index_tuples.T, shape)
+        return Is_off
+
+
+class _FinDiffNonUniform(_FinDiffBase):
+    def __init__(self, axis, order, coords, acc):
+        super().__init__(axis, order)
+        self.coords = coords
+        self.acc = acc
+        self.coef_list = []
+        for i in range(len(self.coords)):
+            self.coef_list.append(coefficients_non_uni(order, self.acc, self.coords, i))
+
+    def __call__(self, y):
+        """The core function to take a partial derivative on a non-uniform grid"""
+
+        if np.issubdtype(y.dtype, np.integer):
+            y = y.astype(np.float64)
+
+        order, dim = self.order, self.axis
+
+        yd = np.zeros_like(y)
+
+        ndims = len(y.shape)
+        multi_slice = [slice(None, None)] * ndims
+        ref_multi_slice = [slice(None, None)] * ndims
+
+        for i, x in enumerate(self.coords):
+
+            coefs = self.coef_list[i]
+            ref_multi_slice[dim] = i
+
+            for off, w in zip(coefs["offsets"], coefs["coefficients"]):
+                multi_slice[dim] = i + off
+                yd[tuple(ref_multi_slice)] += w * y[tuple(multi_slice)]
+
+        return yd
+
+    def write_matrix_entries(self, mat, shape):
+
+        coords = self.coords
+
+        short_inds = get_list_of_multiindex_tuples(shape)
+
+        coef_dicts = []
+        for i in range(len(coords)):
+            coef_dicts.append(coefficients_non_uni(self.order, self.acc, coords, i))
+
+        long_inds = get_long_indices_for_all_grid_points_as_ndarray(shape)
+        for base_ind_long, base_ind_short in enumerate(short_inds):
+            cd = coef_dicts[base_ind_short[self.axis]]
+            cs, os = cd["coefficients"], cd["offsets"]
+            for c, o in zip(cs, os):
+                off_short = np.zeros(len(shape), dtype=int)
+                off_short[self.axis] = int(o)
+                off_ind_short = np.array(base_ind_short, dtype=int) + off_short
+                off_long = long_inds[tuple(off_ind_short)]
+
+                mat[base_ind_long, off_long] += c
---
+++ packages/findiff/findiff-0.12.1-py3-none-any.whl/findiff/operators.py
@@ -0,0 +1,264 @@
+import numbers
+from abc import ABC, abstractmethod
+
+import numpy
+import numpy as np
+from scipy import sparse
+
+from findiff.findiff import build_differentiator
+from findiff.grids import Grid, GridAxis, make_grid
+from findiff.stencils import StencilSet
+
+
+class Expression(ABC):
+    """Represents a differential operator expression."""
+
+    __array_priority__ = 100  # Makes sure custom multiplication is called over numpy's
+
+    def __init__(self, *args, **kwargs):
+        self.children = []
+
+    @abstractmethod
+    def __call__(self, f, *args, **kwargs):
+        """Applies the differential operator expression L to array f.
+
+        Parameters
+        ----------
+        f : numpy.ndarray
+            The arrow to differentiate.
+
+        Returns
+        -------
+        numpy.ndarray
+            Array of the same shape as f containing the derivative values.
+        """
+        pass
+
+    @abstractmethod
+    def matrix(self, shape):
+        """Returns a matrix representation of the differential operator for a given grid shape."""
+        pass
+
+    def stencil(self, shape):
+        """Returns a stencil representation of the differential operator for a given grid shape."""
+        return StencilSet(self, shape)
+
+    def __add__(self, other):
+        """Allows to add differential operator expressions."""
+        return Add(self, other)
+
+    def __radd__(self, other):
+        """Allows to add differential operator expressions."""
+        return Add(self, other)
+
+    def __sub__(self, other):
+        """Allows to subtract differential operator expressions."""
+        return Add(ScalarOperator(-1) * other, self)
+
+    def __rsub__(self, other):
+        """Allows to subtract differential operator expressions."""
+        return Add(ScalarOperator(-1) * other, self)
+
+    def __mul__(self, other):
+        """Allows to multiply differential operator expressions."""
+        return Mul(self, other)
+
+    def __rmul__(self, other):
+        """Allows to multiply differential operator expressions."""
+        return Mul(other, self)
+
+    @property
+    def grid(self):
+        """Returns the grid used."""
+        return getattr(self, "_grid", None)
+
+    def set_grid(self, grid):
+        """Sets the grid for the given differential operator expression.
+
+        Parameters
+        ----------
+        grid: dict | Grid
+            Specifies the grid to use. If a dict is given, an equidistant grid
+            is assumed and the dict specifies the spacings along the required axes.
+        """
+        self._grid = make_grid(grid)
+        for child in self.children:
+            child.set_grid(self._grid)
+
+    def set_accuracy(self, acc):
+        """Sets the requested accuracy for the given differential operator expression.
+
+        Parameters
+        ----------
+        acc: int
+            The accuracy order. Must be a positive, even number.
+        """
+        self.acc = acc
+        for child in self.children:
+            child.set_accuracy(acc)
+
+
+class FieldOperator(Expression):
+    """An operator that multiplies an array pointwise."""
+
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+
+    def __call__(self, f, *args, **kwargs):
+        if isinstance(f, (numbers.Number, np.ndarray)):
+            return self.value * f
+        return self.value * super().__call__(f, *args, **kwargs)
+
+    def matrix(self, shape):
+        if isinstance(self.value, np.ndarray):
+            diag_values = self.value.reshape(-1)
+            return sparse.diags(diag_values)
+        elif isinstance(self.value, numbers.Number):
+            siz = np.prod(shape)
+            return sparse.diags(self.value * np.ones(siz))
+
+
+class ScalarOperator(FieldOperator):
+    """A multiple of the identity operator."""
+
+    def __init__(self, value):
+        if not isinstance(value, numbers.Number):
+            raise ValueError("Expected number, got " + str(type(value)))
+        super().__init__(value)
+
+    def matrix(self, shape):
+        siz = np.prod(shape)
+        mat = sparse.lil_matrix((siz, siz))
+        diag = list(range(siz))
+        mat[diag, diag] = self.value
+        return sparse.csr_matrix(mat)
+
+
+class Identity(ScalarOperator):
+    """The identity operator."""
+
+    def __init__(self):
+        super().__init__(1)
+
+
+class BinaryOperation(Expression):
+
+    @property
+    def left(self):
+        return self.children[0]
+
+    @property
+    def right(self):
+        return self.children[1]
+
+
+class Add(BinaryOperation):
+    def __init__(self, left, right):
+        if isinstance(left, (numbers.Number, np.ndarray)):
+            left = FieldOperator(left)
+        if isinstance(right, (numbers.Number, np.ndarray)):
+            right = FieldOperator(right)
+        super().__init__()
+        self.children = [left, right]
+
+    def __call__(self, f, *args, **kwargs):
+        return self.left(f, *args, **kwargs) + self.right(f, *args, **kwargs)
+
+    def matrix(self, shape):
+        return self.left.matrix(shape) + self.right.matrix(shape)
+
+
+class Mul(BinaryOperation):
+    def __init__(self, left, right):
+        if isinstance(left, (numbers.Number, np.ndarray)):
+            left = FieldOperator(left)
+        if isinstance(right, (numbers.Number, np.ndarray)):
+            right = FieldOperator(right)
+        super().__init__()
+        self.children = [left, right]
+
+    def __call__(self, f, *args, **kwargs):
+        return self.left(self.right(f, *args, **kwargs), *args, **kwargs)
+
+    def matrix(self, shape):
+        return self.left.matrix(shape) * self.right.matrix(shape)
+
+
+class Diff(Expression):
+
+    DEFAULT_ACC = 2
+
+    def __init__(self, dim, axis: GridAxis = None, acc=DEFAULT_ACC):
+        """Initializes a Diff instance.
+
+        Parameters
+        ----------
+        dim: int
+            The 0-based index of the axis along which to take the derivative.
+        axis: GridAxis
+            The grid axis.
+        acc: (optional) int
+            The accuracy order to use. Must be a positive even number.
+        """
+        super().__init__()
+
+        self.set_axis(axis)
+        self.dim = dim
+        self.acc = acc
+        self._order = 1
+        self._differentiator = None
+
+    def set_grid(self, grid):
+        super().set_grid(grid)
+        self.set_axis(self.grid.get_axis(self.dim))
+
+    def set_axis(self, axis: GridAxis):
+        self._axis = axis
+        self._differentiator = None
+
+    @property
+    def axis(self):
+        return self._axis
+
+    @property
+    def order(self):
+        """Returns the order of the derivative."""
+        return self._order
+
+    def __call__(self, f, *args, **kwargs):
+        """Applies the differential operator."""
+
+        if "acc" in kwargs:
+            # allow to pass down new accuracy deep in expression tree
+            new_acc = kwargs["acc"]
+            if new_acc != self.acc:
+                self._differentiator = None
+                self.set_accuracy(new_acc)
+
+        if isinstance(f, Expression):
+            f = f(*args, **kwargs)
+
+        return self.differentiator(f)
+
+    @property
+    def differentiator(self):
+        if self._differentiator is None:
+            self._differentiator = build_differentiator(self.order, self.axis, self.acc)
+        return self._differentiator
+
+    def matrix(self, shape):
+        return self.differentiator.matrix(shape)
+
+    def __pow__(self, power):
+        """Returns a Diff instance for a higher order derivative."""
+        new_diff = Diff(self.dim, self.axis, acc=self.acc)
+        new_diff._order *= power
+        return new_diff
+
+    def __mul__(self, other):
+        if isinstance(other, Diff) and self.dim == other.dim:
+            new_diff = Diff(self.dim, self.axis, acc=self.acc)
+            new_diff._order += other.order
+            return new_diff
+        return super().__mul__(other)
