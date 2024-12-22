f11291f76a2c915bbc6288d838a9f5e25df085c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 15:05:40 +0000

    Add package uncurl-pk/0.0.1/uncurl_pk-0.0.1.tar.gz

---
+++ packages/uncurl-pk/uncurl_pk-0.0.1.tar.gz/uncurl_pk-0.0.1/tests/test_bin.py
@@ -0,0 +1,23 @@
+from mock import patch
+import six
+from uncurl.bin import main
+
+print_module = "__builtin__.print" if six.PY2 else "uncurl.bin.print"
+
+
+@patch("uncurl.bin.sys")
+@patch(print_module)
+def test_main(printer, fake_sys):
+    fake_sys.argv = ['uncurl', "curl 'https://pypi.python.org/pypi/uncurl' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Accept-Language: en-US,en;q=0.8'"]
+    main()
+
+    printer.assert_called_once_with(
+        """
+requests.get("https://pypi.python.org/pypi/uncurl",
+    headers={
+        "Accept-Encoding": "gzip,deflate,sdch",
+        "Accept-Language": "en-US,en;q=0.8"
+    },
+    cookies={},
+    auth=(),
+)""")
