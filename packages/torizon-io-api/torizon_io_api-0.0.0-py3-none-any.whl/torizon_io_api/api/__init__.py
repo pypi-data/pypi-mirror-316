3b1f918abc79a6ade48e41d492a90b8b50ac7f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:12:28 +0000

    Add package torizon-io-api/0.0.0/torizon_io_api-0.0.0-py3-none-any.whl

---
+++ packages/torizon-io-api/torizon_io_api-0.0.0-py3-none-any.whl/torizon_io_api/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from torizon_io_api.api.device_metrics_api import DeviceMetricsApi
+from torizon_io_api.api.devices_api import DevicesApi
+from torizon_io_api.api.fleets_api import FleetsApi
+from torizon_io_api.api.packages_api import PackagesApi
+from torizon_io_api.api.updates_api import UpdatesApi
+
