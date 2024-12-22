21ccdecade801dbd7ac15b1f6e7c66894bc64543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:23 +0000

    Add package gimpey-com-sol-gateway/0.1.0/gimpey_com_sol_gateway-0.1.0-py3-none-any.whl

---
+++ packages/gimpey-com-sol-gateway/gimpey_com_sol_gateway-0.1.0-py3-none-any.whl/gimpey_com_sol_gateway/requests/__init__.py
@@ -0,0 +1,13 @@
+from ..generated import pumpfun_pb2
+
+BuyRequest = pumpfun_pb2.BuyRequest
+SellRequest = pumpfun_pb2.SellRequest
+CreateTokenRequest = pumpfun_pb2.CreateTokenRequest
+SendRequest = pumpfun_pb2.SendRequest
+
+__all__ = [
+    "BuyRequest",
+    "SellRequest",
+    "CreateTokenRequest",
+    "SendRequest",
+]
