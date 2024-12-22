797f7f112b95ab0a07ebe0af6a561561ce6f75a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 04:06:16 +0000

    Add package refnx/0.1.51/refnx-0.1.51-cp313-cp313-win_amd64.whl

---
+++ packages/refnx/refnx-0.1.51-cp313-cp313-win_amd64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.51'
+version = '0.1.51'
+full_version = '0.1.51'
+git_revision = '326c703d36b67adcd38a430fc8f08024e37f4cc4'
+release = True
+if not release:
+    version = full_version
