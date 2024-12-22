793addaef188b087de6e6f731e60f1382232df9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:26 +0000

    Add package zenoml-next/0.6.7/zenoml_next-0.6.7-py3-none-any.whl

---
+++ packages/zenoml-next/zenoml_next-0.6.7-py3-none-any.whl/zeno/classes/base.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from typing import Optional
+
+from pandas import Series
+from pydantic import BaseModel, ConfigDict
+
+
+def to_camel(string: str) -> str:
+    components = string.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+class CamelModel(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True  # replaces allow_population_by_field_name=True
+    )
+
+
+class ZenoColumnType(str, Enum):
+    METADATA = "METADATA"
+    PREDISTILL = "PREDISTILL"
+    OUTPUT = "OUTPUT"
+    EMBEDDING = "EMBEDDING"
+    POSTDISTILL = "POSTDISTILL"
+
+
+class MetadataType(str, Enum):
+    NOMINAL = "NOMINAL"
+    CONTINUOUS = "CONTINUOUS"
+    BOOLEAN = "BOOLEAN"
+    DATETIME = "DATETIME"
+    OTHER = "OTHER"
+
+
+class ZenoColumn(CamelModel):
+    column_type: ZenoColumnType
+    name: str
+    metadata_type: Optional[MetadataType] = MetadataType.OTHER
+    model: Optional[str] = ""
+
+    # Append the column type to the name unless it is a vanilla metadata column.
+    # We don't save the metadata_type since we dynamically change it
+    # and it's not identifying.
+    def __str__(self):
+        m = ""
+        if self.model is not None:
+            m = self.model
+        t = ""
+        if self.column_type is not ZenoColumnType.METADATA:
+            t = self.column_type
+        return t + self.name + m
+
+    def __hash__(self):
+        return hash(str(self))
+
+
+class DataProcessingReturn(BaseModel):
+    """Return type for data processing functions."""
+
+    column: ZenoColumn
+    output: Series
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
---
+++ packages/zenoml-next/zenoml_next-0.6.7-py3-none-any.whl/zeno/classes/classes.py
@@ -0,0 +1,73 @@
+"""Internal classes for Zeno."""
+
+from typing import List, Optional, Tuple, Union
+
+from pydantic import BaseModel
+from zeno.classes.base import CamelModel, ZenoColumn
+from zeno.classes.slice import FilterIds, FilterPredicateGroup, Slice
+
+
+class ZenoSettings(CamelModel):
+    view: str
+    id_column: ZenoColumn
+    label_column: ZenoColumn
+    data_column: ZenoColumn
+    samples: int
+    calculate_histogram_metrics: bool
+    total_size: int
+
+
+class ZenoVariables(CamelModel):
+    metrics: List[str]
+    models: List[str]
+    folders: List[str]
+
+
+class StatusResponse(CamelModel):
+    status: str
+    done_processing: bool
+    complete_columns: List[ZenoColumn]
+
+
+class MetricKey(CamelModel):
+    sli: Slice
+    model: str
+    metric: str
+
+
+class MetricRequest(CamelModel):
+    metric_keys: List[MetricKey]
+    tag_ids: Optional[FilterIds] = None
+    filter_ids: Optional[FilterIds] = None
+    tag_list: Optional[List[str]] = None
+
+
+class TableRequest(CamelModel):
+    columns: List[ZenoColumn]
+    diff_column_1: Optional[ZenoColumn] = None
+    diff_column_2: Optional[ZenoColumn] = None
+    slice_range: List[int]
+    filter_predicates: FilterPredicateGroup
+    sort: Tuple[Union[ZenoColumn, str, None], bool]
+    tag_ids: FilterIds
+    filter_ids: Optional[FilterIds] = None
+    tag_list: List[str]
+
+
+class PlotRequest(CamelModel):
+    filter_predicates: FilterPredicateGroup
+    tag_ids: FilterIds
+
+
+class EmbedProject2DRequest(CamelModel):
+    model: str
+    column: ZenoColumn
+
+
+class ColorsProjectRequest(CamelModel):
+    column: ZenoColumn
+
+
+class EntryRequest(BaseModel):
+    id: Union[int, str]
+    columns: List[ZenoColumn] = []
