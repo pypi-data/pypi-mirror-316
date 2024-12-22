c5d300a081519a18684f4ec42424f5eeaf371804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 15:05:43 +0000

    Add package Buke-autoTrade-Pypi/7.0.0/Buke_autoTrade_Pypi-7.0.0.tar.gz

---
+++ packages/Buke-autoTrade-Pypi/Buke_autoTrade_Pypi-7.0.0.tar.gz/Buke_autoTrade_Pypi-7.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='Buke_autoTrade_Pypi', # 패키지 명
+
+version='7.0.0',
+
+description='Test Package',
+
+author='zaza011',
+
+author_email='yesben@naver.com',
+
+url='https://eu4ng.tistory.com',
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+
+py_modules=['Buke_autoTrade_Pypi'], # 패키지에 포함되는 모듈
+
+python_requires='>=3',
+
+install_requires=[], # 패키지 사용을 위해 필요한 추가 설치 패키지
+
+packages=['Buke_autoTrade_Pypi'] # 패키지가 들어있는 폴더들
+
+)
