bb186207eea7666bc64b29eae1115adce08a795a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 03:14:51 +0000

    Add package omlish/0.0.0.dev183/omlish-0.0.0.dev183-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev183-py3-none-any.whl/omlish/logs/color.py
@@ -0,0 +1,24 @@
+# ruff: noqa: ANN201 N802
+import logging
+import typing as ta
+
+from .. import term
+from .standard import StandardLogFormatter
+
+
+class ColorLogFormatter(StandardLogFormatter):
+    LEVEL_COLORS: ta.Mapping[int, term.SGRs.FG] = {
+        logging.WARNING: term.SGRs.FG.BRIGHT_YELLOW,
+        logging.ERROR: term.SGRs.FG.BRIGHT_RED,
+        logging.CRITICAL: term.SGRs.FG.BRIGHT_RED,
+    }
+
+    def formatMessage(self, record):
+        buf = super().formatMessage(record)
+        try:
+            c = self.LEVEL_COLORS[record.levelno]
+        except KeyError:
+            pass
+        else:
+            buf = term.SGR(c) + buf + term.SGR(term.SGRs.RESET)
+        return buf
