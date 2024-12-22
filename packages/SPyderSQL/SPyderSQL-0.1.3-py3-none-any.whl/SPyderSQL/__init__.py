f8e80765869eb1e770af1eac957572840a9861cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 06:05:26 +0000

    Add package SPyderSQL/0.1.3/SPyderSQL-0.1.3-py3-none-any.whl

---
+++ packages/SPyderSQL/SPyderSQL-0.1.3-py3-none-any.whl/SPyderSQL/__init__.py
@@ -0,0 +1,19 @@
+from .__meta__ import __version__, __author__, __email__, __description__, __url__
+
+from .client import SQLite
+from .client.SQLite import TypesSQLite
+from .logger import Logger
+
+
+logger = Logger(__name__).get_logger()
+#
+__all__ = (
+    '__version__',
+    '__author__',
+    '__email__',
+    '__description__',
+    '__url__',
+    'SQLite',
+    'TypesSQLite',
+    'logger'
+)
