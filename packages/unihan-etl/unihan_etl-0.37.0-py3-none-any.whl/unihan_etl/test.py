2c3d691fd45bcca9fc6e83b2735bac87a81d6f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:19 +0000

    Add package unihan-etl/0.37.0/unihan_etl-0.37.0-py3-none-any.whl

---
+++ packages/unihan-etl/unihan_etl-0.37.0-py3-none-any.whl/unihan_etl/test.py
@@ -0,0 +1,32 @@
+"""Test helpers functions for downloading and processing Unihan data."""
+
+import typing as t
+
+
+def assert_dict_contains_subset(
+    subset: dict[str, t.Any],
+    dictionary: t.Mapping[str, t.Any],
+    msg: t.Optional[str] = None,
+) -> None:
+    """
+    Ported assertion for dict subsets in py.test.
+
+    Parameters
+    ----------
+    subset : dict
+        needle
+    dictionary : dict
+        haystack
+    msg : str, optional
+        message display if assertion fails
+
+    >>> assert_dict_contains_subset({ 'test': 3 }, { 'more_data': '_', 'test': 3 })
+
+    >>> not assert_dict_contains_subset({ 'test': 3 }, { 'more_data': '_', 'test': 4 })
+    Traceback (most recent call last):
+        ...
+    AssertionError: None
+    """
+    for key, value in subset.items():
+        assert key in dictionary, msg
+        assert dictionary[key] == value, msg
---
+++ packages/unihan-etl/unihan_etl-0.37.0-py3-none-any.whl/unihan_etl/types.py
@@ -0,0 +1,88 @@
+"""Typings for unihan-etl."""
+
+import dataclasses
+import pathlib
+import sys
+import typing as t
+
+if t.TYPE_CHECKING:
+    from http.client import HTTPMessage
+    from os import PathLike
+    from urllib.request import _DataType
+
+    from typing_extensions import TypeAlias
+
+StrPath: "TypeAlias" = t.Union[str, "PathLike[str]"]
+""":class:`os.PathLike` or :class:`str`
+
+:class:`StrPath` is based on `typeshed`__'s.
+
+.. __: https://github.com/python/typeshed/blob/5df8de7/stdlib/_typeshed/__init__.pyi#L115-L118
+"""  # E501
+
+# Log levels
+LogLevel = t.Literal["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+# Column data
+ColumnData = t.Sequence[str]
+ColumnDataTuple = tuple[str, ...]
+
+# In situ
+UntypedUnihanData = t.Mapping[str, t.Any]
+
+UntypedNormalizedData = t.Sequence[UntypedUnihanData]
+
+# Export w/ listify()
+ListifiedExport = list[list[str]]
+
+# Export w/ listify() -> expand_delimiters()
+ExpandedExport = t.Sequence[t.Mapping[str, t.Any]]
+
+# Valid output formats
+UnihanFormats = t.Literal["json", "csv", "yaml", "python"]
+
+
+@dataclasses.dataclass()
+class Options:
+    """unihan-etl options."""
+
+    source: str
+    destination: pathlib.Path
+    zip_path: pathlib.Path
+    work_dir: pathlib.Path
+    fields: tuple[str, ...]
+    format: UnihanFormats
+    input_files: list[str]
+    download: bool
+    expand: bool
+    prune_empty: bool
+    cache: bool
+    log_level: LogLevel
+
+
+class ReportHookFn(t.Protocol):
+    """Progress bar callback for download()."""
+
+    def __call__(
+        self,
+        count: int,
+        block_size: int,
+        total_size: int,
+        out: t.IO[str] = sys.stdout,
+    ) -> object:
+        """Print progress bar during download."""
+        ...
+
+
+class UrlRetrieveFn(t.Protocol):
+    """Type annotation for :func:`urllib.request.urlretrieve`."""
+
+    def __call__(
+        self,
+        url: str,
+        filename: t.Optional["StrPath"] = None,
+        reporthook: t.Optional["ReportHookFn"] = None,
+        data: "t.Optional[_DataType]" = None,
+    ) -> tuple[str, "HTTPMessage"]:
+        """Download logic for :func:`urllib.request.urlretrieve`."""
+        ...
