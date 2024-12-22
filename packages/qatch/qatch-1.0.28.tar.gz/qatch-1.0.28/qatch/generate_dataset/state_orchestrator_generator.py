a4eab4ae58af7e57fef18d6fcd2e631940daf8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:06:23 +0000

    Add package qatch/1.0.28/qatch-1.0.28.tar.gz

---
+++ packages/qatch/qatch-1.0.28.tar.gz/qatch-1.0.28/qatch/generate_dataset/state_orchestrator_generator.py
@@ -0,0 +1,12 @@
+import operator
+from typing import Annotated, TypedDict
+
+from ..connectors import ConnectorTable, BaseConnector
+
+
+class StateOrchestratorGenerator(TypedDict):
+    connector: BaseConnector
+    database: dict[str, ConnectorTable]
+    generated_templates: Annotated[list, operator.add]
+    column_to_include: str
+    tbl_names: list[str]
