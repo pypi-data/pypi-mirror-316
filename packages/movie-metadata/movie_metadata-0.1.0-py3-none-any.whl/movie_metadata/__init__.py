4af4814f3d14b8dea551ffea8da69025f9240260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-22 05:05:40 +0000

    Add package movie-metadata/0.1.0/movie_metadata-0.1.0-py3-none-any.whl

---
+++ packages/movie-metadata/movie_metadata-0.1.0-py3-none-any.whl/movie_metadata/__init__.py
@@ -0,0 +1,5 @@
+from .client import TMDB
+from .exceptions import TMDBException
+
+__version__ = "0.1.0"
+__all__ = ["TMDB", "TMDBException"]
---
+++ packages/movie-metadata/movie_metadata-0.1.0-py3-none-any.whl/movie_metadata/constants.py
@@ -0,0 +1,13 @@
+BASE_URL = "https://api.themoviedb.org/3"
+DEFAULT_LANGUAGE = "en-US"
+
+SORT_OPTIONS = {
+    "popularity.asc": "Popularity Ascending",
+    "popularity.desc": "Popularity Descending",
+    "revenue.asc": "Revenue Ascending",
+    "revenue.desc": "Revenue Descending",
+    "vote_average.asc": "Rating Ascending",
+    "vote_average.desc": "Rating Descending",
+    "release_date.asc": "Release Date Ascending",
+    "release_date.desc": "Release Date Descending",
+}
