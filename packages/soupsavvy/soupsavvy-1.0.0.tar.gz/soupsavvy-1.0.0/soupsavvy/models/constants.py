043e1c555e4dfb150af2b2fab4b9a65ed4dcd376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:58 +0000

    Add package soupsavvy/1.0.0/soupsavvy-1.0.0.tar.gz

---
+++ packages/soupsavvy/soupsavvy-1.0.0.tar.gz/soupsavvy-1.0.0/soupsavvy/models/constants.py
@@ -0,0 +1,21 @@
+"""Module with constants and defaults for model classes."""
+
+# default recursive value for finding fields of model within the scope
+DEFAULT_RECURSIVE = True
+# default strict value for finding fields of model within the scope
+DEFAULT_STRICT = False
+
+# attribute to set on method that post-processes field value of model
+POST_ATTR = "__post_process_method__"
+# name of the attribute that keep tracks of initialization state
+INITIALIZED = "_initialized"
+
+SCOPE = "__scope__"
+INHERIT_FIELDS = "__inherit_fields__"
+POST_PROCESSORS = "__post_processors__"
+FROZEN = "__frozen__"
+
+SPECIAL_FIELDS = {SCOPE, INHERIT_FIELDS, POST_PROCESSORS, FROZEN}
+
+# based model classes that skip initialization checks
+BASE_MODELS = {"BaseModel"}
