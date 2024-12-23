65fa1dfa1d7f6a73d97cfe2ebeff27d46493d0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:38 +0000

    Add package behavemae/0.0rc1/behavemae-0.0rc1-py3-none-any.whl

---
+++ packages/behavemae/behavemae-0.0rc1-py3-none-any.whl/behavemae/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) A. Mathis Group and contributors. All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from behavemae.version import __version__
