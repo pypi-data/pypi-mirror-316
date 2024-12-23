24e60d9f7cff59f4db49ecaca506451ead3f03e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 14:05:16 +0000

    Add package dev-cmd/0.2.1/dev_cmd-0.2.1.tar.gz

---
+++ packages/dev-cmd/dev_cmd-0.2.1.tar.gz/dev_cmd-0.2.1/dev_cmd/model.py
@@ -0,0 +1,51 @@
+# Copyright 2024 John Sirois.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import PurePath
+from typing import Any, Iterable, Mapping
+
+from dev_cmd.errors import InvalidModelError
+
+
+@dataclass(frozen=True)
+class Command:
+    name: str
+    env: Mapping[str, str]
+    args: tuple[str, ...]
+    cwd: PurePath
+    accepts_extra_args: bool
+
+
+@dataclass(frozen=True)
+class Dev:
+    commands: Mapping[str, Command]
+    aliases: Mapping[str, tuple[Command, ...]]
+    default: tuple[str, tuple[Command, ...]] | None = None
+    source: Any = "<code>"
+
+
+@dataclass(frozen=True)
+class Invocation:
+    @classmethod
+    def create(cls, *tasks: tuple[str, Iterable[Command]]) -> Invocation:
+        _tasks: dict[str, tuple[Command, ...]] = {}
+        accepts_extra_args: Command | None = None
+        for task, commands in tasks:
+            _tasks[task] = tuple(commands)
+            for command in commands:
+                if command.accepts_extra_args:
+                    if accepts_extra_args is not None:
+                        raise InvalidModelError(
+                            f"The command {command.name!r} accepts extra args, but only one "
+                            f"command can accept extra args per invocation and command "
+                            f"{accepts_extra_args.name!r} already does."
+                        )
+                    accepts_extra_args = command
+
+        return cls(tasks=_tasks, accepts_extra_args=accepts_extra_args is not None)
+
+    tasks: Mapping[str, tuple[Command, ...]]
+    accepts_extra_args: bool
