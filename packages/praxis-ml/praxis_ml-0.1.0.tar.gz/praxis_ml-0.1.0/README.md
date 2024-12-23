aa9a7f257b81c215259b6c7f2182b89c44889ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:57 +0000

    Add package praxis-ml/0.1.0/praxis_ml-0.1.0.tar.gz

---
+++ packages/praxis-ml/praxis_ml-0.1.0.tar.gz/praxis_ml-0.1.0/README.md
@@ -0,0 +1,2 @@
+# praxis
+A PySpark-based library for transparent and interpretable machine learning, providing tools to explain model predictions at scale.
---
+++ packages/praxis-ml/praxis_ml-0.1.0.tar.gz/praxis_ml-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="praxis-ml",
+    version="0.1.1",
+    description="A PySpark-based library for transparent and interpretable machine learning.",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=["pyspark"],
+)
