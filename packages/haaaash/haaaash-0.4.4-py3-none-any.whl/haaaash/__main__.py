3ae952ef54f084894bddc734230e7668a7713d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:45 +0000

    Add package haaaash/0.4.4/haaaash-0.4.4-py3-none-any.whl

---
+++ packages/haaaash/haaaash-0.4.4-py3-none-any.whl/haaaash/__main__.py
@@ -0,0 +1,20 @@
+import argparse
+from . import outs
+from .hashs import hash
+def main():
+    parser = argparse.ArgumentParser(
+                        prog='haaaash',
+                        description='详细解析：',
+                        epilog='By Gudupao (MIT License)')
+    parser.add_argument('file',help='文件(夹)路径')
+    parser.add_argument('-m','--method',help='哈希方法（默认为 sha256）',default='sha256')
+    parser.add_argument('-l','--length',help='哈希长度（算法为 shake_128 shake_256 时）',type=int,default=20)
+    parser.add_argument('-o','--outmod',help='输出模式',type=str,default="default")
+
+    args = parser.parse_args()
+    hash_list = hash(args.file,args.method,args.length)
+    out = outs.chmod(hash_list,args.outmod)
+    print(out)
+
+if __name__ == '__main__':
+    main()
