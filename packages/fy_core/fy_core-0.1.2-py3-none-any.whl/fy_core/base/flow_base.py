3f3076c8fc6b60ebfa08e4412232a9729e5f6332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:36 +0000

    Add package fy_core/0.1.2/fy_core-0.1.2-py3-none-any.whl

---
+++ packages/fy_core/fy_core-0.1.2-py3-none-any.whl/fy_core/base/flow_base.py
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import abc
+from typing import Generic, TypeVar
+
+ResultT = TypeVar("ResultT")
+
+
+class FlowBase(Generic[ResultT], abc.ABC):
+    @abc.abstractmethod
+    def __call__(self) -> ResultT:
+        raise NotImplementedError()
