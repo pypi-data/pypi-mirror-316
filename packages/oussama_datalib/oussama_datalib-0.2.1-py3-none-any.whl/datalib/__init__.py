55736399c72c12c043867aaaba24df7385ab9f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:06:00 +0000

    Add package oussama_datalib/0.2.1/oussama_datalib-0.2.1-py3-none-any.whl

---
+++ packages/oussama_datalib/oussama_datalib-0.2.1-py3-none-any.whl/datalib/__init__.py
@@ -0,0 +1,18 @@
+"""
+Initialize the datalib package.
+
+This package includes various modules for data visualization, analysis, processing,
+statistical operations, supervised learning, unsupervised learning, and reinforcement learning.
+
+Modules:
+- visualization: Functions for plotting and visualizations.
+- analysis: Functions for regression and analysis.
+- data_processing: Functions for loading, cleaning, and preprocessing data.
+- statistics: Functions for statistical analysis.
+- supervised: Functions for supervised machine learning models.
+- unsupervised: Functions for unsupervised learning methods.
+- reinforcement: Functions for reinforcement learning algorithms.
+"""
+
+__version__ = "0.1.0"
+__all__ = ["visualization", "analysis", "data_processing", "statistics", "supervised", "unsupervised", "reinforcement"]
