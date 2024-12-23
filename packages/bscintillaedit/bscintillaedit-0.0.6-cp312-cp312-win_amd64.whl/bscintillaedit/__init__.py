e0cc9fecab47002fd4667be2369616b59533d256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:45 +0000

    Add package bscintillaedit/0.0.6/bscintillaedit-0.0.6-cp312-cp312-win_amd64.whl

---
+++ packages/bscintillaedit/bscintillaedit-0.0.6-cp312-cp312-win_amd64.whl/bscintillaedit/__init__.py
@@ -0,0 +1,20 @@
+"""PySide6 bindings for the `BScintillaEdit` widget derived from
+[QScrollArea](https://doc.qt.io/qt-6/qscrollarea.html) that wraps a
+`ScintillaEditBase` widget from [Scintilla](https://www.scintilla.org/).
+"""
+
+# the binding will not load if we don't explicitly importing PySide6.QtWidgets
+import PySide6.QtWidgets
+
+# import the C++ classes wrapped by the binding
+from .bscintillaedit import (
+    BScintillaEdit,
+)
+
+# make the C++ classes wrapped by binding available directly from the python
+# package
+__all__ = [
+    "BScintillaEdit",
+]
+
+__version__ = "0.0.6"
