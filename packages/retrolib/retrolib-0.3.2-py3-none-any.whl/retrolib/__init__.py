67877b25a0a8c2f1b5c3ce44f989a043935241ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:59 +0000

    Add package retrolib/0.3.2/retrolib-0.3.2-py3-none-any.whl

---
+++ packages/retrolib/retrolib-0.3.2-py3-none-any.whl/retrolib/__init__.py
@@ -0,0 +1,6 @@
+# RetroLib, une bibliothèque que j'espère qui va devenir beaucoup utilisée.
+# RetroLib, a library that I hope will become widely used.
+
+from . import PixelArt
+
+__version__ = "0.3.2"
