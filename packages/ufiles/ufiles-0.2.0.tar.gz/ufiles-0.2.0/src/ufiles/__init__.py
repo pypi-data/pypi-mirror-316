e72febc1cf726ef059774c8e15602439ac6d3392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 14:05:39 +0000

    Add package ufiles/0.2.0/ufiles-0.2.0.tar.gz

---
+++ packages/ufiles/ufiles-0.2.0.tar.gz/ufiles-0.2.0/src/ufiles/__init__.py
@@ -0,0 +1,5 @@
+from .async_ufiles import AsyncUFiles
+from .schemas import UFileItem, PermissionEnum, PermissionSchema
+from .ufiles import UFiles
+
+__all__ = ["UFiles", "AsyncUFiles", "UFileItem", "PermissionEnum", "PermissionSchema"]
