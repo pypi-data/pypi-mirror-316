250746f55cf77b094153545ebd6d3f1d35f4bc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:37 +0000

    Add package artix-engine/0.0.2/artix_engine-0.0.2.tar.gz

---
+++ packages/artix-engine/artix_engine-0.0.2.tar.gz/artix_engine-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="artix_engine",
+    version="0.0.2",
+    description="Хочете спробувати себе у створенні бота у діскорд? Давай з Artix",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="harttman",
+    author_email="dakiv.work.vitaliy@example.com",
+    url="https://github.com/harttman/artix_engine",
+    packages=find_packages(),
+    install_requires=[
+        "websocket-client>=1.5.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
