1d1ed79f5b76b677c5f935d090e4ba737444f76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:05:32 +0000

    Add package uncurl-pk/0.0.2/uncurl_pk-0.0.2.tar.gz

---
+++ packages/uncurl-pk/uncurl_pk-0.0.2.tar.gz/uncurl_pk-0.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='uncurl-pk',
+    version='0.0.2',
+    description='A library to convert curl requests to python-requests. Forked version of spulec/uncurl ',
+    author='Pankaj Kumar',
+    author_email='pnkjpvt@gmail.com',
+    url='https://github.com/pnkjkpvt/uncurl/tree/master',
+    entry_points={
+        'console_scripts': [
+            'uncurl = uncurl.bin:main',
+        ],
+    },
+    install_requires=['pyperclip', 'six'],
+    packages=find_packages(exclude=("tests", "tests.*")),
+)
