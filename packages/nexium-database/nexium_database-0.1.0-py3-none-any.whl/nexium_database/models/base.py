2606c055239bf9f4db735ec7c6ce76d31aa599b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:45 +0000

    Add package nexium-database/0.1.0/nexium_database-0.1.0-py3-none-any.whl

---
+++ packages/nexium-database/nexium_database-0.1.0-py3-none-any.whl/nexium_database/models/base.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class BaseModel(SQLModel):
+    id: int = Field(primary_key=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(), nullable=False)
+    updated_at: Optional[datetime] = Field(default=None)
+    deleted_at: Optional[datetime] = Field(default=None)
+    is_deleted: bool = Field(default=False, nullable=False)
