714ec95fb995e2d9dbecd298eb6f38db7028f059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:49 +0000

    Add package rosreestr2coord/5.0.0/rosreestr2coord-5.0.0.tar.gz

---
+++ packages/rosreestr2coord/rosreestr2coord-5.0.0.tar.gz/rosreestr2coord-5.0.0/rosreestr2coord/utils.py
@@ -0,0 +1,39 @@
+import math
+import re
+from typing import List
+
+
+def y2lat(y: float) -> float:
+    return (2 * math.atan(math.exp(y / 6378137)) - math.pi / 2) / (math.pi / 180)
+
+
+def x2lon(x: float) -> float:
+    return x / (math.pi / 180.0) / 6378137.0
+
+
+def xy2lonlat(x: float, y: float) -> List[float]:
+    return [x2lon(x), y2lat(y)]
+
+
+def code_to_filename(code: str) -> str:
+    return code.replace(":", "_").replace("/", "-")
+
+
+def clear_code(code: str) -> str:
+    """
+    Remove first nulls from code xxxx:00xx >> xxxx:xx
+    but if the cadastral number, for example "02:02-6.667",
+    then the all parts will remain zeros
+    """
+    is_delimited_code = re.match(r"^\d+(\:\d+)", code)
+    leave_zeros = "." in code
+    if is_delimited_code and not leave_zeros:
+        parts = []
+        for x in code.split(":"):
+            strip_zeros = x.lstrip("0")
+            if strip_zeros:
+                parts.append(strip_zeros)
+            else:
+                parts.append("0")
+        return ":".join(parts)
+    return code
