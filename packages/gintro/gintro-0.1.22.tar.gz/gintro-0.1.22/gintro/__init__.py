66205b9ed70a27a7458698d72b2d88d6a40e4871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:11:17 +0000

    Add package gintro/0.1.22/gintro-0.1.22.tar.gz

---
+++ packages/gintro/gintro-0.1.22.tar.gz/gintro-0.1.22/gintro/__init__.py
@@ -0,0 +1,6 @@
+from .stream import Stream
+from .decorators import timeit, time_it
+from .mail import send_mail
+
+
+from basic import logit, sigmoid, get_auc
---
+++ packages/gintro/gintro-0.1.22.tar.gz/gintro-0.1.22/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gintro',
+    version='0.1.22',
+    author='jfshen',
+    packages=find_packages(),
+    install_requires=[
+        # 依赖列表
+    ],
+    # 其他元数据
+)
+
