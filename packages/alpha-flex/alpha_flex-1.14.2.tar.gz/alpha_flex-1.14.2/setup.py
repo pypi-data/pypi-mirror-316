914f1beaf9e248f89b4565a0620af70f6531b98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:06:07 +0000

    Add package alpha-flex/1.14.2/alpha_flex-1.14.2.tar.gz

---
+++ packages/alpha-flex/alpha_flex-1.14.2.tar.gz/alpha_flex-1.14.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README.md
+def read_readme():
+    with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+setup(
+    name="alpha_flex",
+    version="1.14.2",
+    description="A Python library for building growth-focused ETF portfolios.",
+    long_description=read_readme(),  # Use the README.md contents for PyPI
+    long_description_content_type="text/markdown",  # Ensure it's recognized as markdown
+    author="Eniola Sobimpe",
+    author_email="sobimpeeniola@gmail.com",
+    url="https://github.com/esobimpe/alpha_flex",
+    packages=find_packages(),
+    install_requires=[
+        "yfinance",
+        "numpy",
+        "pandas",
+        "finvizfinance"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",  # Correct license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
