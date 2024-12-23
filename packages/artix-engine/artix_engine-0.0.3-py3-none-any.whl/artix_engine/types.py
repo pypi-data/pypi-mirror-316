5a9c71a56b2d77546f47022eec175c27b1e923f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:57 +0000

    Add package artix-engine/0.0.3/artix_engine-0.0.3-py3-none-any.whl

---
+++ packages/artix-engine/artix_engine-0.0.3-py3-none-any.whl/artix_engine/types.py
@@ -0,0 +1,17 @@
+from main import Client
+
+class Message:
+    def __init__(self, client: Client, data: dict):
+        self.client = client
+        self.data = data
+        self.channel_id = data.get("channel_id")
+        self.content = data.get("content")
+        self.author = data.get("author", {}).get("username")
+
+    async def send_message(self, content: str):
+        """Відправка повідомлення в поточний канал."""
+        url = f"{self.client.gateway_url}/channels/{self.channel_id}/messages"
+        payload = {"content": content}
+        headers = self.client.headers
+        async with self.client.session.post(url, json=payload, headers=headers) as response:
+            return await response.json()
