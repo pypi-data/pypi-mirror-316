e4e0bba7bd9f191510ad76eb0a7b603ddc4eb202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:52 +0000

    Add package blue-prints/0.0.9/blue_prints-0.0.9.tar.gz

---
+++ packages/blue-prints/blue_prints-0.0.9.tar.gz/blue_prints-0.0.9/blueprints/type_alias.py
@@ -0,0 +1,82 @@
+"""Module to keep track of type aliases used in Blueprints."""
+
+# <editor-fold desc="DISTANCES">
+MM = float
+CM = float
+M = float
+KM = float
+# </editor-fold>
+
+# <editor-fold desc="ROTATIONS">
+DEG = float
+RAD = float
+# </editor-fold>
+
+# <editor-fold desc="WEIGHTS">
+KG = float
+G = float
+KG_M3 = float
+KG_M = float
+# </editor-fold>
+
+# <editor-fold desc="FORCES">
+N = float
+KN = float
+KN_M = float
+# </editor-fold>
+
+# <editor-fold desc="MOMENTS">
+NM = float
+KNM = float
+# </editor-fold>
+
+# <editor-fold desc="STRESSES">
+MPA = float
+KPA = float
+GPA = float
+# </editor-fold>
+
+# <editor-fold desc="AREAS">
+MM2 = float
+MM2_M = float
+CM2 = float
+DM2 = float
+M2 = float
+KM2 = float
+# </editor-fold>
+
+# <editor-fold desc="VOLUMES">
+MM3 = float
+CM3 = float
+M3 = float
+M3_M = float
+# </editor-fold>
+
+# <editor-fold desc="BI-QUADRATIC">
+MM4 = float
+CM4 = float
+M4 = float
+# </editor-fold>
+
+# <editor-fold desc="COMPOUNDS">
+N_M2 = float
+KN_M2 = float
+# </editor-fold>
+
+# <editor-fold desc="TIME">
+HOURS = float
+MINUTES = float
+SECONDS = float
+DAYS = float
+WEEKS = float
+MONTHS = float
+YEARS = float
+# </editor-fold>
+
+# <editor-fold desc="Ratio's">
+RATIO = float
+PERCENTAGE = float
+DIMENSIONLESS = float
+PER_MILLE = float
+PER_DEGREE = float
+# </editor-fold>
