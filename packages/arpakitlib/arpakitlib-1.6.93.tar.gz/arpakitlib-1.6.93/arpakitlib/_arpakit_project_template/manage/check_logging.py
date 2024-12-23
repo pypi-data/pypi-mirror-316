0a75da7815ff851722a2f8eccc8e3e5a4a2a4c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:05:20 +0000

    Add package arpakitlib/1.6.93/arpakitlib-1.6.93.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.6.93.tar.gz/arpakitlib-1.6.93/arpakitlib/_arpakit_project_template/manage/check_logging.py
@@ -0,0 +1,13 @@
+import logging
+
+from src.core.util import setup_logging
+
+_logger = logging.getLogger(__name__)
+
+def command():
+    setup_logging()
+    _logger.info("checking logging")
+
+
+if __name__ == '__main__':
+    command()
