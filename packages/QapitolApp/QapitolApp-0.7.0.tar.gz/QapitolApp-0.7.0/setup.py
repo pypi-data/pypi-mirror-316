08554799484f9ff99b9375e3492a6340f5771258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:05:38 +0000

    Add package QapitolApp/0.7.0/QapitolApp-0.7.0.tar.gz

---
+++ packages/QapitolApp/QapitolApp-0.7.0.tar.gz/QapitolApp-0.7.0/setup.py
@@ -0,0 +1,24 @@
+# filepath: /c:/Users/Qapitol QA/Desktop/QapitolApp/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="QapitolApp",
+    version="0.7.0",
+    author="Abinash Sahoo",
+    author_email="abinash.sahoo@qapitol.com",
+    description="For internal use only",
+    packages=find_packages(),  # Automatically finds your packages
+    include_package_data=True,
+    install_requires=open('utilityapp/requirements.txt').read().splitlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "flask_app=utilityapp.__init__:create_app",
+        ],
+    },
+)
---
+++ packages/QapitolApp/QapitolApp-0.7.0.tar.gz/QapitolApp-0.7.0/utilityapp/run.bat
@@ -0,0 +1,16 @@
+@echo off
+REM This is a batch file to run the Flask application
+
+REM Navigate to the directory where your Flask app is located
+cd /d "C:\Users\Qapitol QA\Desktop"  REM Replace with the actual path to your app
+
+REM Install dependencies from requirements.txt
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+
+REM Run the Flask application
+echo Running Flask app...
+python app.py
+
+REM Pause the command window to keep it open after execution
+pause
