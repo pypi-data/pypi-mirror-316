d76d393dbdd7e4c2a380f4bc1a26e5a2d2f95c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:05:21 +0000

    Add package xlwinxxx/0.3/xlwinxxx-0.3.tar.gz

---
+++ packages/xlwinxxx/xlwinxxx-0.3.tar.gz/xlwinxxx-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='xlwinxxx',
+      version='0.3',
+      description='xlwinxxx',
+      packages=['xlwinxxx'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
