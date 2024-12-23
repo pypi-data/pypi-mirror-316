0e820ef81ccf0d93422f0dc35d54c2664d38419f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:54 +0000

    Add package datalib-sghaier-project/0.1.1/datalib_sghaier_project-0.1.1-py3-none-any.whl

---
+++ packages/datalib-sghaier-project/datalib_sghaier_project-0.1.1-py3-none-any.whl/datalib/__init__.py
@@ -0,0 +1,15 @@
+# src/datalib/__init__.py
+
+"""
+DataLib
+=======
+
+A Python library for data manipulation and analysis.
+
+Modules:
+    - data_processing: Load and manipulate data.
+    - statistics: Perform statistical operations.
+    - visualization: Generate visual representations of data.
+"""
+
+__version__ = "0.1.0"
---
+++ packages/datalib-sghaier-project/datalib_sghaier_project-0.1.1-py3-none-any.whl/datalib/statistics.py
@@ -0,0 +1,13 @@
+"""
+Module for statistical calculations.
+"""
+import numpy as np
+
+def calculate_mean(data):
+    """Calculates the mean of a list of numbers."""
+    return np.mean(data)
+
+
+def calculate_median(data):
+    """Calculates the median of a list of numbers."""
+    return np.median(data)
