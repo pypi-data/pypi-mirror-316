c7b89f8088fec79a643d765e2dc76ec41dbc3133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:11:09 +0000

    Add package completescraper/0.1.2/completescraper-0.1.2-py3-none-any.whl

---
+++ packages/completescraper/completescraper-0.1.2-py3-none-any.whl/completescraper/__init__.py
@@ -0,0 +1,4 @@
+from .http_session.http_session import HTTPSession
+from .parser.html_containers import HtmlContainer, HtmlForm, HtmlSelect, HtmlUl
+
+__all__ = ["HTTPSession", "HtmlContainer", "HtmlForm", "HtmlSelect", "HtmlUl"]
