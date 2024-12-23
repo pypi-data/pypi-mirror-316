b53268b8f29dba5f86275a57f499487f93ca2c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 16:06:13 +0000

    Add package JaxVidFlow/0.0.7/jaxvidflow-0.0.7.tar.gz

---
+++ packages/JaxVidFlow/jaxvidflow-0.0.7.tar.gz/jaxvidflow-0.0.7/tests/video_reader_test.py
@@ -0,0 +1,40 @@
+import fractions
+import os
+import platform
+import sys
+import time
+
+import jax
+from jax import numpy as jnp
+import numpy as np
+import pytest
+
+sys.path.append('.')
+
+from JaxVidFlow import video_reader
+
+def test_video_reader():
+  reader = video_reader.VideoReader('test_files/lionfish.mp4')
+  assert reader.width() == 5120
+  assert reader.height() == 3840
+  assert reader.frame_rate() == fractions.Fraction(25, 1)
+  assert reader.num_frames() == 203
+  assert reader.duration() == pytest.approx(8.12, 0.1)
+  reader.seek(4.3)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(4.3, 0.1)
+  reader.seek(6.1)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(6.1, 0.1)
+  reader.seek(0.5)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(0.5, 0.1)
+  reader.seek(1.5)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(1.5, 0.1)
+  reader.seek(6.7)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(6.7, 0.1)
+  reader.seek(20.0)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(8.08, 0.1)
