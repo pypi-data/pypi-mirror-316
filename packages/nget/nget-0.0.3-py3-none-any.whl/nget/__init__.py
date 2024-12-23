367061862d58d2a3e85bc912cbd4b071526b4726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 14:05:43 +0000

    Add package nget/0.0.3/nget-0.0.3-py3-none-any.whl

---
+++ packages/nget/nget-0.0.3-py3-none-any.whl/nget/__init__.py
@@ -0,0 +1,8 @@
+"""
+NGET (Nested Get) - Simple Python package for safely retrieving nested values from data structures using sequences of keys or indexes.
+"""
+
+from .nget import nget
+
+__version__ = "0.0.3"
+__all__ = ["nget"]
