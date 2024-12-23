913529e22db6b5b1bd3a4385029ea0e675d60f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 14:05:08 +0000

    Add package litemodel-cli/0.1.3/litemodel_cli-0.1.3.tar.gz

---
+++ packages/litemodel-cli/litemodel_cli-0.1.3.tar.gz/litemodel_cli-0.1.3/src/litemodel_cli/artifacts/admin.py
@@ -0,0 +1,12 @@
+from litemodel_starlette.admin import register_admin
+from litemodel_starlette.views import BaseAdminView
+
+
+# register your admin views here
+# Ex.
+# from app.models import Question
+
+# @register_admin
+# class QuestionAdmin(BaseAdminView):
+#     model = Question
+
---
+++ packages/litemodel-cli/litemodel_cli-0.1.3.tar.gz/litemodel_cli-0.1.3/src/litemodel_cli/artifacts/jinja_filters.py
@@ -0,0 +1,7 @@
+
+# define custom jinja filters here..
+
+# Ex.
+def my_filter(value: str) -> str:
+    # do something useful in here with value
+    return value
