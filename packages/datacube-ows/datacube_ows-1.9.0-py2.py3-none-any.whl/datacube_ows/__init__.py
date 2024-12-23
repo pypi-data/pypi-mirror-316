ae2ff505fdf7fb6f8b9f4b28c8bfe6195a1720f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 14:05:34 +0000

    Add package datacube-ows/1.9.0/datacube_ows-1.9.0-py2.py3-none-any.whl

---
+++ packages/datacube-ows/datacube_ows-1.9.0-py2.py3-none-any.whl/datacube_ows/__init__.py
@@ -0,0 +1,10 @@
+# This file is part of datacube-ows, part of the Open Data Cube project.
+# See https://opendatacube.org for more information.
+#
+# Copyright (c) 2017-2024 OWS Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "1.9.0?"
