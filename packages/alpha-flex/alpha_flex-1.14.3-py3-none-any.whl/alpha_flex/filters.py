d1224c31ea3e8210fca97bcde535efb3ae4087cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:05:19 +0000

    Add package alpha-flex/1.14.3/alpha_flex-1.14.3-py3-none-any.whl

---
+++ packages/alpha-flex/alpha_flex-1.14.3-py3-none-any.whl/alpha_flex/filters.py
@@ -0,0 +1,52 @@
+FILTERS = {
+    "High Growth": {
+        'Market Cap.': '+Mid (over $2bln)',
+        'EPS growthqtr over qtr': 'Over 25%',
+        'EPS growthnext year': 'Over 25%',
+        'EPS growththis year': 'Over 25%',
+        'Sales growthqtr over qtr': 'Over 25%',
+        'Average Volume': 'Over 400K',
+        'RSI (14)': 'Not Oversold (>50)',
+        '200-Day Simple Moving Average': 'Price above SMA200'
+    },
+    "High Sales Growth": {
+        'Debt/Equity': 'Under 0.5',
+        'Gross Margin': 'Over 40%',
+        'Return on Equity': 'Over +15%',
+        'Sales growthpast 5 years': 'Over 20%',
+        'Sales growthqtr over qtr': 'Over 20%',
+        'Average Volume': 'Over 200K',
+        'InstitutionalOwnership': 'Over 60%',
+        'Price': 'Over $5',
+        'Float Short': 'Under 5%'
+    },
+    "Buy and Hold": {
+        'Market Cap.': '+Micro (over $50mln)',
+        'Current Ratio': 'Over 1.5',
+        'EPS growthnext 5 years': 'Over 10%',
+        'PEG': 'Over 1',
+        'Return on Equity': 'Over +15%',
+        'Beta': 'Over 1.5',
+        '20-Day Simple Moving Average': 'Price above SMA20'
+    },
+    "Bullish": {
+        'EPS growthpast 5 years': 'Positive (>0%)',
+        'EPS growthqtr over qtr': 'Over 20%',
+        'EPS growththis year': 'Over 25%',
+        'EPS growthnext year': 'Over 15%',
+        'EPS growthnext 5 years': 'Positive (>0%)',
+        'Return on Equity': 'Over +15%',
+        'InstitutionalOwnership': 'Over 10%',
+        'Price': 'Over $15',
+        '52-Week High/Low': '90% or more above Low',
+        'RSI (14)': 'Not Oversold (>50)'
+    },
+    "CANSLIM": {
+        'EPS growthpast 5 years': 'Over 20%',
+        'EPS growthqtr over qtr': 'Over 20%',
+        'EPS growththis year': 'Over 20%',
+        'Sales growthpast 5 years': 'Over 20%',
+        'Sales growthqtr over qtr': 'Over 20%',
+        'Current Volume': 'Over 1M'
+    }
+}
