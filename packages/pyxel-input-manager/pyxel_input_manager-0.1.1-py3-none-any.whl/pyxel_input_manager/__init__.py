fee9d7d077dbd7ea5ed3798852a15c861e2d6528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:10:30 +0000

    Add package pyxel-input-manager/0.1.1/pyxel_input_manager-0.1.1-py3-none-any.whl

---
+++ packages/pyxel-input-manager/pyxel_input_manager-0.1.1-py3-none-any.whl/pyxel_input_manager/__init__.py
@@ -0,0 +1,6 @@
+from .input_types import InputType
+from .input_bindings import AnalogBinding, Stick2DBinding
+from .input_map import InputMap
+from .input_event import InputEvent
+
+__all__ = ['InputType', 'AnalogBinding', 'Stick2DBinding', 'InputMap', 'InputEvent']
