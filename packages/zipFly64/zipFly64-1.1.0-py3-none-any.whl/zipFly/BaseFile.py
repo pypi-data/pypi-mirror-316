c2da03061ea3f0de9ba494b41ec7b1bdbe6f0c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 14:05:30 +0000

    Add package zipFly64/1.1.0/zipFly64-1.1.0-py3-none-any.whl

---
+++ packages/zipFly64/zipFly64-1.1.0-py3-none-any.whl/zipFly/BaseFile.py
@@ -0,0 +1,85 @@
+from abc import ABC, abstractmethod
+from typing import Generator, AsyncGenerator
+
+from . import consts
+from .Compressor import Compressor
+
+
+class BaseFile(ABC):
+    def __init__(self, compression_method: int):
+        self.original_size = 0
+        self.compressed_size = 0
+        self.offset = 0  # Offset to local file header
+        self.crc = 0
+        self.flags = 0b00001000  # flag about using data descriptor is always on
+        self.compression_method = compression_method or consts.NO_COMPRESSION
+
+    def __str__(self):
+        return f"FILE[{self.name}]"
+
+    def generate_processed_file_data(self) -> Generator[bytes, None, None]:
+        compressor = Compressor(self)
+
+        """
+        Generates compressed file data
+        """
+        for chunk in self._generate_file_data():
+            chunk = compressor.process(chunk)
+            if len(chunk) > 0:
+                yield chunk
+            chunk = compressor.tail()
+            if len(chunk) > 0:
+                yield chunk
+
+    async def async_generate_processed_file_data(self) -> AsyncGenerator[bytes, None]:
+        compressor = Compressor(self)
+
+        """
+        Generates compressed file data
+        """
+        async for chunk in self._async_generate_file_data():
+            chunk = compressor.process(chunk)
+            if len(chunk) > 0:
+                yield chunk
+            chunk = compressor.tail()
+            if len(chunk) > 0:
+                yield chunk
+
+    def get_mod_time(self) -> int:
+        return int(self.modification_time) & 0xFFFF
+
+    def get_mod_date(self) -> int:
+        return int(self.modification_time / 86400 + 365 * 20) & 0xFFFF
+
+    @property
+    def file_path_bytes(self) -> bytes:
+        try:
+            return self.name.encode("ascii")
+        except UnicodeError:
+            self.flags |= consts.UTF8_FLAG
+            return self.name.encode("utf-8")
+
+    @abstractmethod
+    def _generate_file_data(self) -> Generator[bytes, None, None]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def _async_generate_file_data(self) -> AsyncGenerator[bytes, None]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_file_name(self, new_name: str) -> None:
+        raise NotImplementedError
+
+    @property
+    def size(self) -> int:
+        raise NotImplementedError
+
+    @property
+    def modification_time(self) -> float:
+        raise NotImplementedError
+
+    @property
+    def name(self) -> str:
+        raise NotImplementedError
+
---
+++ packages/zipFly64/zipFly64-1.1.0-py3-none-any.whl/zipFly/LocalFile.py
@@ -0,0 +1,59 @@
+import os
+import time
+from typing import Generator, AsyncGenerator
+from .BaseFile import BaseFile
+
+import aiofiles
+
+class LocalFile(BaseFile):
+
+    def __init__(self, file_path: str, name: str = None, compression_method: int = None):
+        if not os.path.isfile(file_path):
+            raise ValueError(f"{file_path} is not a correct file path.")
+        self._file_path = file_path
+        self.chunk_size = 1048
+        self._name = name if name else file_path
+        super().__init__(compression_method)
+
+    async def _async_generate_file_data(self) -> AsyncGenerator[bytes, None]:
+
+        async with aiofiles.open(self._file_path, "rb") as fh:
+            while True:
+                part = await fh.read(self.chunk_size)
+                if not part:
+                    break
+                yield part
+
+    def _generate_file_data(self) -> Generator[bytes, None, None]:
+        with open(self._file_path, 'rb') as file:
+            while True:
+                chunk = file.read(self.chunk_size)
+                if not chunk:
+                    break
+                yield chunk
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def size(self) -> int:
+        return os.path.getsize(self._file_path)
+
+    @property
+    def modification_time(self) -> float:
+        return os.path.getmtime(self._file_path)
+
+    def get_mod_time(self) -> int:
+        # Extract hours, minutes, and seconds from the modification time
+        t = time.localtime(self.modification_time)
+        return ((t.tm_hour << 11) | (t.tm_min << 5) | (t.tm_sec // 2)) & 0xFFFF
+
+    def get_mod_date(self) -> int:
+        # Extract year, month, and day from the modification time
+        t = time.localtime(self.modification_time)
+        year = t.tm_year - 1980  # ZIP format years start from 1980
+        return ((year << 9) | (t.tm_mon << 5) | t.tm_mday) & 0xFFFF
+
+    def set_file_name(self, new_name: str) -> None:
+        self._name = new_name
