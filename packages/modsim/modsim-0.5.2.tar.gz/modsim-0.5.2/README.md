98a729e1dc791e288c0a025c44119da6e86a54d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:11:02 +0000

    Add package modsim/0.5.2/modsim-0.5.2.tar.gz

---
+++ packages/modsim/modsim-0.5.2.tar.gz/modsim-0.5.2/README.md
@@ -0,0 +1,76 @@
+# ModSim - A Simple Modbus TCP Device Simulator
+
+*modsim* is a simple Modbus TCP device simulator deployed at `modsim.topmaker.net:502`.
+It works as a remote Modbus-TCP (virtual) device, which has already been populated with all 4 types of registers, i.e.
+- Coil Type
+- Discrete input Type
+- Input register Type
+- Holding register Type
+
+It is recommended to use [modpoll](https://github.com/gavinying/modpoll) tool to communicate with it, the example configuration is as following,
+
+```
+device,modsim01,1,,
+poll,coil,0,16,BE_BE
+ref,coil01-08,0,bool8,rw
+ref,coil09-16,1,bool8,rw
+poll,discrete_input,10000,16,BE_BE
+ref,di01-08,10000,bool8,rw
+ref,di09-16,10001,bool8,rw
+poll,input_register,30000,20,BE_BE
+ref,input_reg01,30000,uint16,rw
+ref,input_reg02,30001,uint16,rw
+ref,input_reg03,30002,uint16,rw
+ref,input_reg04,30003,uint16,rw
+ref,input_reg05,30004,int16,rw
+ref,input_reg06,30005,int16,rw
+ref,input_reg07,30006,int16,rw
+ref,input_reg08,30007,int16,rw
+ref,input_reg09,30008,uint32,rw
+ref,input_reg10,30010,uint32,rw
+ref,input_reg11,30012,int32,rw
+ref,input_reg12,30014,int32,rw
+ref,input_reg13,30016,float32,rw
+ref,input_reg14,30018,float32,rw
+poll,holding_register,40000,44,BE_BE
+ref,holding_reg01,40000,uint16,rw
+ref,holding_reg02,40001,uint16,rw
+ref,holding_reg03,40002,uint16,rw
+ref,holding_reg04,40003,uint16,rw
+ref,holding_reg05,40004,int16,rw
+ref,holding_reg06,40005,int16,rw
+ref,holding_reg07,40006,int16,rw
+ref,holding_reg08,40007,int16,rw
+ref,holding_reg09,40008,uint32,rw
+ref,holding_reg10,40010,uint32,rw
+ref,holding_reg11,40012,int32,rw
+ref,holding_reg12,40014,int32,rw
+ref,holding_reg13,40016,float32,rw
+ref,holding_reg14,40018,float32,rw
+ref,holding_reg15,40020,uint64,rw
+ref,holding_reg16,40024,int64,rw
+ref,holding_reg17,40028,float64,rw
+ref,holding_reg18,40032,float64,rw
+ref,holding_reg19,40036,string16,rw
+```
+
+## Quick Start
+
+A docker image has been provided for user to directly run the program,
+
+```bash
+docker run -p 5020:5020 topmaker/modsim:latest
+```
+
+The above one-line command will create a virtual Modbus TCP device running at `localhost:5020`.
+
+To verify it, you can test by [modpoll](https://github.com/gavinying/modpoll) tool with the following command,
+
+```bash
+modpoll \
+  --tcp localhost \
+  --tcp-port 5020 \
+  --config https://raw.githubusercontent.com/gavinying/modpoll/master/examples/modsim.csv
+```
+
+> **_Note_**: add `sudo` before `docker run` command if you want to use the Modbus TCP standard port `502`.
---
+++ packages/modsim/modsim-0.5.2.tar.gz/modsim-0.5.2/modsim/utils.py
@@ -0,0 +1,116 @@
+"""A collection of utility tools"""
+
+import argparse
+import logging.config
+from datetime import datetime, timezone
+from typing import Any, Optional, Dict
+from pymodbus.transaction import (
+    ModbusAsciiFramer,
+    ModbusBinaryFramer,
+    ModbusRtuFramer,
+    ModbusSocketFramer,
+    ModbusTlsFramer,
+)
+
+from . import __version__
+
+# global
+args: Optional[argparse.Namespace] = None
+config: Dict[str, Any] = {}
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=f"Modsim v{__version__} - A Simple Modbus TCP Device Simulator used for modpoll tool"
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"{__package__} v{__version__}"
+    )
+    parser.add_argument(
+        "--comm",
+        choices=["tcp", "udp", "serial", "tls"],
+        help="set communication, default is tcp",
+        default="tcp",
+        type=str,
+    )
+    parser.add_argument(
+        "--framer",
+        choices=["ascii", "binary", "rtu", "socket", "tls"],
+        help="set framer, default depends on --comm",
+        type=str,
+    )
+    parser.add_argument(
+        "--log",
+        choices=["critical", "error", "warning", "info", "debug"],
+        help="set log level, default is info",
+        default="info",
+        type=str,
+    )
+    parser.add_argument(
+        "--port",
+        help="set port",
+        type=str,
+    )
+    parser.add_argument(
+        "--store",
+        choices=["sequential", "sparse", "factory", "none"],
+        help="set type of datastore",
+        default="sequential",
+        type=str,
+    )
+    parser.add_argument(
+        "--slaves",
+        help="set number of slaves, default is 0 (any)",
+        default=0,
+        type=int,
+        nargs="+",
+    )
+    parser.add_argument(
+        "--context",
+        help="ADVANCED USAGE: set datastore context object",
+        default=None,
+    )
+    return parser
+
+
+def get_commandline() -> argparse.Namespace:
+    args = get_parser().parse_args()
+
+    # set defaults
+    comm_defaults: Dict[str, list] = {
+        "tcp": ["socket", 5020],
+        "udp": ["socket", 5020],
+        "serial": ["rtu", "/dev/ptyp0"],
+        "tls": ["tls", 5020],
+    }
+    framers: Dict[str, Any] = {
+        "ascii": ModbusAsciiFramer,
+        "binary": ModbusBinaryFramer,
+        "rtu": ModbusRtuFramer,
+        "socket": ModbusSocketFramer,
+        "tls": ModbusTlsFramer,
+    }
+    args.framer = framers[args.framer or comm_defaults[args.comm][0]]
+    args.port = args.port or comm_defaults[args.comm][1]
+    if args.comm != "serial" and args.port:
+        args.port = int(args.port)
+    return args
+
+
+def get_logger() -> logging.Logger:
+    if config and config.get("logging"):
+        logging.config.dictConfig(config["logging"])
+    else:
+        print("no config file found, load default settings.")
+        logging.basicConfig(
+            level=get_commandline().log.upper(),
+            format="%(asctime)s | %(levelname).1s | %(processName)s | %(name)s | %(message)s",
+        )
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+def get_utc_time() -> datetime:
+    dt = datetime.now(timezone.utc)
+    utc_time = dt.replace(tzinfo=timezone.utc)
+    return utc_time
