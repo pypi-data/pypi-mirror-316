6a0686a5a7ef9dbc6b44626c1f3fb41107646b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:53 +0000

    Add package molsystem/2024.12.22/molsystem-2024.12.22.tar.gz

---
+++ packages/molsystem/molsystem-2024.12.22.tar.gz/molsystem-2024.12.22/tests/test_rdkit.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the RDKit mixin of the class."""
+
+import pprint  # noqa: F401
+import rdkit
+import rdkit.Chem
+import rdkit.Chem.AllChem
+import pytest  # noqa: F401
+
+from molsystem import rdkit_version
+
+
+def test_version():
+    """Test the version of RDKit."""
+    rdkit_version()
+
+
+def test_to_RDKMol(Acetate):
+    """Test creating a RDKMol object from a structure."""
+    correct = {
+        "SEAMM|float property|float|kcal/mol": 3.14,
+        "SEAMM|int property|int|": 2,
+        "SEAMM|net charge|int|": -1,
+        "SEAMM|spin multiplicity|int|": 1,
+        "SEAMM|str property|str|": "Hi!",
+    }
+
+    mol = Acetate.to_RDKMol(properties="*")
+
+    bond_types = {
+        rdkit.Chem.rdchem.BondType.SINGLE: 1,
+        rdkit.Chem.rdchem.BondType.DOUBLE: 2,
+        rdkit.Chem.rdchem.BondType.TRIPLE: 3,
+    }
+    bond_list = [bond_types[bt.GetBondType()] for bt in mol.GetBonds()]
+
+    assert Acetate.n_atoms == mol.GetNumAtoms()
+    assert Acetate.bonds.bondorders == bond_list
+
+    data = mol.GetPropsAsDict()
+    if data != correct:
+        pprint.pprint(data)
+    assert data == correct
+
+
+def test_from_RDKMol(configuration):
+    """Test creating a structure from an RDKMol object."""
+    mol = rdkit.Chem.MolFromSmiles("CC=O")
+    mol = rdkit.Chem.rdmolops.AddHs(mol, addCoords=True)
+
+    rdkit.Chem.AllChem.EmbedMolecule(mol)
+
+    configuration.from_RDKMol(mol)
+
+    assert configuration.n_atoms == 7
+    assert configuration.bonds.bondorders == [1, 2, 1, 1, 1, 1]
