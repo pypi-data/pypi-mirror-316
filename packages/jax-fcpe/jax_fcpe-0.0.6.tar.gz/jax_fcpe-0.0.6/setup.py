c37035f784ac2362e727e85131d33461c0251477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 15:05:39 +0000

    Add package jax-fcpe/0.0.6/jax_fcpe-0.0.6.tar.gz

---
+++ packages/jax-fcpe/jax_fcpe-0.0.6.tar.gz/jax_fcpe-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open('README.md', encoding='utf8') as file:
+    long_description = file.read()
+
+
+setup(
+    name='jax-fcpe',
+    description='The JAX implementation of Fast Context-based Pitch Estimation (FCPE)',
+    version='0.0.6',
+    author='flyingblackshark',
+    author_email='aliu2000@outlook.com',
+    url='https://github.com/flyingblackshark/jax-fcpe',
+    install_requires=['jax', 'flax', 'torch', 'numpy','librosa'],
+    packages=['jax_fcpe'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['License :: OSI Approved :: MIT License'])
