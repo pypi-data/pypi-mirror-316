b80a107284f600775970fa9c4a688219f58e3f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:58 +0000

    Add package portableDB/1.5/portabledb-1.5.tar.gz

---
+++ packages/portableDB/portabledb-1.5.tar.gz/portabledb-1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='portableDB',
+    version='1.5',
+    author='DoubleSquad',
+    description="This library is an easyest way to use local database!",
+    long_description="""
+    Here is an example:
+
+    from portableDB import DATABASE
+
+    DTB = DATABASE()
+    DTB.LogState('COLORFUL') #'BASE' - just normal cmd, 'NONE' - no comments when working
+    DTB.Create_Database('Database') #'Database' - name
+
+    DTB.Write_Database('Database',['String value1', 32], 1) #'Database' - name, [] - here should be your values, 1 - index (can be only positive number)
+
+    print(DTB.Read_Database('Database', 1, 'ALL')) #'Database' - name, 1 - index, 'ALL' - index of array ('ALL' or any index of value in array)
+
+    DTB.Rename_Database('Database','DB') #'Database' - current name, 'DB' - new name
+
+    DTB.Delete_Database('Database') #No comments
+    """,
+    packages=find_packages(),
+    install_requires=[
+        'colorama>=0.4.6'
+    ],
+)
