cd89ee55991cdd8c9cc926924638f62cba55c5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 16:06:31 +0000

    Add package selfhelperscripts/0.1.0/selfhelperscripts-0.1.0-py3-none-any.whl

---
+++ packages/selfhelperscripts/selfhelperscripts-0.1.0-py3-none-any.whl/selfhelperscripts/src/constants/__init__.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+ROOT_DIRECTORY = Path(__file__).parents[2].as_posix()
+
+CONFLUENCE_URL = ''
+JIRA_URL = ''
+"""file example ./config/.creds
+export VAR=DATA
+
+"""
+CREDS_PATH = ROOT_DIRECTORY + '/config' + '/.creds'
+
+
