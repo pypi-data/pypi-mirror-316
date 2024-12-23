953316e78616f41aca8cbaea87f90fbe42903222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 03:11:16 +0000

    Add package jing/0.1.3/jing-0.1.3.tar.gz

---
+++ packages/jing/jing-0.1.3.tar.gz/jing-0.1.3/jing/__init__.py
@@ -0,0 +1,13 @@
+from .module1 import my_function
+from .module2 import MyClass
+
+from .y import Y
+from .x import X
+
+from .rule import Rule
+from .rule import RuleSimple
+from .rule import RuleMa50Ma200
+from .rule import RulePriceBreakout
+from .rule import RuleVolumeBreakout
+
+__version__ = "0.1.0"
