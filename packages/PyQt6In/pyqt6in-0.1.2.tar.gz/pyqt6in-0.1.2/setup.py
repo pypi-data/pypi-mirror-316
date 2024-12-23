7a4f6c4abafe3a04990f1dc999a9accffbb9590c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:53 +0000

    Add package PyQt6In/0.1.2/pyqt6in-0.1.2.tar.gz

---
+++ packages/PyQt6In/pyqt6in-0.1.2.tar.gz/pyqt6in-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="PyQt6In",
+    version="0.1.2",
+    description="libary",
+    author="Berg679",
+    author_email="redmigardo@gmail.com",
+    packages=find_packages(),
+    install_requires=[],  # Сюда можно добавлять зависимости
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
