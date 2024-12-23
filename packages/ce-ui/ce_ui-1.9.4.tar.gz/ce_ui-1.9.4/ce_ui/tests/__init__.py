ea8f58e49197756a6f6622d1820e262747b9a4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 04:05:47 +0000

    Add package ce-ui/1.9.4/ce_ui-1.9.4.tar.gz

---
+++ packages/ce-ui/ce_ui-1.9.4.tar.gz/ce_ui-1.9.4/ce_ui/tests/__init__.py
@@ -0,0 +1,10 @@
+from topobank.analysis.registry import register_implementation
+from topobank.testing.functions import (
+    SecondTestImplementation, TestImplementation, TestImplementationWithError,
+    TestImplementationWithErrorInDependency, TopographyOnlyTestImplementation)
+
+register_implementation(SecondTestImplementation)
+register_implementation(TestImplementation)
+register_implementation(TestImplementationWithError)
+register_implementation(TestImplementationWithErrorInDependency)
+register_implementation(TopographyOnlyTestImplementation)
