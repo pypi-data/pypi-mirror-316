6119ed4d06f8e9c672b1df0c0fd6ae1ce9018ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 16:06:13 +0000

    Add package nnthvn/0.4.0/nnthvn-0.4.0.tar.gz

---
+++ packages/nnthvn/nnthvn-0.4.0.tar.gz/nnthvn-0.4.0/README.md
@@ -0,0 +1,10 @@
+# nnthvn
+file = open("text.txt", "r", encoding="utf-9")
+## Описание
+`nnthvn` — это простая библиотека для работы с числами и строками. 
+
+## Установка
+
+pip install nnthvn
+
+
---
+++ packages/nnthvn/nnthvn-0.4.0.tar.gz/nnthvn-0.4.0/nnthvn/__init__.py
@@ -0,0 +1,4 @@
+#file = open("text.txt", "r", encoding="utf-8")
+from .module1 import add, multiply
+from .module2 import to_uppercase, reverse_string
+from .module12 import sum_of_squares, diff_max_min
