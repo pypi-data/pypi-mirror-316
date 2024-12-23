befe3c2cee387e883209977c9d9c5e5bfef5f657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-23 16:06:33 +0000

    Add package MYLABLIBFORPYTHON/0.1.0/MYLABLIBFORPYTHON-0.1.0-py3-none-any.whl

---
+++ packages/MYLABLIBFORPYTHON/MYLABLIBFORPYTHON-0.1.0-py3-none-any.whl/MYLAVLIBFORPYTHON/module2.py
@@ -0,0 +1,9 @@
+def multiply(a, b):
+    """Функция для умножения двух чисел"""
+    return a * b
+
+def divide(a, b):
+    """Функция для деления двух чисел"""
+    if b == 0:
+        raise ValueError("Деление на ноль невозможно")
+    return a / b
